3f8f7789b3fd4dc0ae6078786c555373c78bcbd5
JoelKatz <DavidJoelSchwartz@GMail.com>
2011-11-11 16:38:49 -0800

    Tiny mistake in previous commit.

--- ScopedLock.h
+++ ScopedLock.h
@@ -6,47 +6,46 @@
 // This is a returnable lock holder.
 // I don't know why Boost doesn't provide a good way to do this.
 
-class ScopedLock
-{
-private:
-    boost::mutex *mMutex;	// parent object has greater scope, so guaranteed valid
-    mutable bool mValid;
-
-    ScopedLock();		// no implementation
-    
-public:
-    ScopedLock(boost::mutex &mutex) : mMutex(&mutex), mValid(true)
+    class ScopedLock
     {
-        mMutex->lock();
-    }
+    private:
+        boost::mutex *mMutex;	// parent object has greater scope, so guaranteed valid
+        mutable bool mValid;
 
-    ~ScopedLock()
-    {
-        if(mValid) mMutex->unlock();
-    }
-
-    ScopedLock(const ScopedLock &sl)
-    {
-    	mMutex=sl.mMutex;
-    	if(sl.mValid)
-    	{
-    		mValid=true;
-    		sl.mValid=false;
+        ScopedLock();		// no implementation
+        
+    public:
+        ScopedLock(boost::mutex &mutex) : mMutex(&mutex), mValid(true)
+        {
+            mMutex->lock();
         }
-	else mValid=false;
-    }
 
-    ScopedLock &operator=(const ScopedLock &sl)
-    { // we inherit any lock the other class member had
-        if(mValid) mMutex->unlock();
-        mMutex=sl.mMutex;
-        if(sl.mValid)
+        ~ScopedLock()
         {
             if(mValid) mMutex->unlock();
-            mValid=true;
-            sl.mValid=false;
         }
-    }
-};
+
+        ScopedLock(const ScopedLock &sl)
+        {
+            mMutex=sl.mMutex;
+            if(sl.mValid)
+            {
+                    mValid=true;
+                    sl.mValid=false;
+            }
+            else mValid=false;
+        }
+
+        ScopedLock &operator=(const ScopedLock &sl)
+        { // we inherit any lock the other class member had
+            if(mValid) mMutex->unlock();
+            mMutex=sl.mMutex;
+            if(sl.mValid)
+            {
+                mValid=true;
+                sl.mValid=false;
+            }
+        }
+    };
 
 #endif
