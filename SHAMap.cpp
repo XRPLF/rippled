ccea015c6a22714191581918b1947bb8b59951b3
JoelKatz <DavidJoelSchwartz@GMail.com>
2011-11-14 15:21:14 -0800

    Start coding the guts of the SHAMap structure that will hold, synchronize, sign, and validate our account balances and transactions.

--- SHAMap.cpp
+++ SHAMap.cpp
@@ -39,3 +39,49 @@ bool SHAMapNodeID::operator!=(const SHAMapNodeID &s) const
  return (s.mDepth!=mDepth) || (s.mNodeID!=mNodeID);
 }
 
+void SHAMapNodeID::ClassInit()
+{
+    int i;
+    char HexBuf[65];
+
+    for(i=0; i<64; i++) HexBuf[i]='0';
+        HexBuf[64]=0;
+    for(i=0; i<leafDepth; i++)
+    {
+        smMasks[i].SetHex(HexBuf);
+        HexBuf[2*i]='1';
+        HexBuf[2*i+1]='F';
+    }
+}
+
+SHAMapNodeID::SHAMapNodeID(int depth, const uint256 &hash)
+{
+    assert(depth>=0 && depth<leafDepth);
+    mDepth=depth;
+    mNodeID=getNodeID(depth, hash);
+}
+
+SHAMapNodeID SHAMapNodeID::getChildNodeID(int m)
+{
+    assert(!isLeaf());
+
+    uint256 branch=m;
+    branch>>=(mDepth*8);
+
+    return SHAMapNodeID(mDepth+1, mNodeID | branch);
+}
+
+int SHAMapNodeID::selectBranch(const uint256 &hash)
+{
+    if(isLeaf())	// no nodes under this node
+        return -1;
+    if((hash&smMasks[mDepth])!=mNodeID)
+        return -1;	// does not go under this node
+
+    uint256 selector=hash&smMasks[mDepth+1];
+    int branch=*(selector.begin()+mDepth);
+
+    assert(branch>=0 && branch<32);
+    return branch;
+}
+
--- SHAMap.h
+++ SHAMap.h
@@ -8,11 +8,40 @@
 #include "uint256.h"
 #include "ScopedLock.h"
 
+class SHAMap;
+
+// A tree-like map of SHA256 hashes
+// 10 levels, consisting of a root, 9 interior node levels, and leaves
+// The trees are designed for rapid synchronization and compression of differences
+
+
 class SHAMapNodeID
-{
-public:
+{ // Identified a node in a SHA256 hash
+private:
+	static uint256	smMasks[11]; // AND with hash to get node id
+
 	int	mDepth;
 	uint256	mNodeID;
+
+public:
+
+	// 0 is root, 10 is leaf
+	static const int rootDepth=0;
+	static const int leafDepth=10;
+
+	SHAMapNodeID(int depth, const uint256 &hash);
+	int getDepth() { return mDepth; }
+	const uint256 &getNodeID();
+
+	bool isRoot() const { return mDepth==0; }
+	bool isLeaf() const { return mDepth==leafDepth; }
+	bool isInner() const { return !isRoot() && !isLeaf(); }
+	SHAMapNodeID getParentNodeID() { return SHAMapNodeID(mDepth-1, mNodeID); }
+	SHAMapNodeID getChildNodeID(int m);
+
+	int selectBranch(const uint256 &hash);
+
+	static uint256 getNodeID(int depth, const uint256 &hash);
 	
         bool operator<(const SHAMapNodeID &) const;
         bool operator>(const SHAMapNodeID &) const;
@@ -20,60 +49,63 @@ public:
         bool operator!=(const SHAMapNodeID &) const;
         bool operator<=(const SHAMapNodeID &) const;
         bool operator>=(const SHAMapNodeID &) const;
+
+        static void ClassInit(void);
 };
+
 
 class SHAMapLeafNode
 {
+	friend class SHAMap;
+
 public:
 	typedef boost::shared_ptr<SHAMapLeafNode> pointer;
 
 private:
-	uint256			mNodeID;
-	std::list<uint256>	mHashes;
-	uint256			mNodeHash;
+	SHAMapNodeID mNodeID;
+	uint256	mHash;
+	std::list<uint256> mHashes;
+
+	void updateHash(void);
+
+protected:
+	bool hasHash(const uint256 &hash) const;
+	bool addHash(const uint256 &hash);
+	bool delHash(const uint256 &hash);
 
 public:
 	SHAMapLeafNode(const uint256& NodeID);
 
-	const uint256& GetNodeID(void) const { return mNodeID; }
-	bool HasHash(const uint256& Hash) const;
+	const SHAMapNodeID& GetNodeID(void) const { return mNodeID; }
+	const uint256& GetNodeHash() const { return mHash; }
 
-	uint256& GetNodeHash();
-
-	bool AddHash(const uint256& Hash);
-	bool DelHash(const uint256& Hash);
 };
 
 class SHAMapInnerNode
 {
+	friend class SHAMap;
+
 public:
 	typedef boost::shared_ptr<SHAMapInnerNode> pointer;
 
 private:
-	int		mDepth;
-	uint256		mNodeID;
+	SHAMapNodeID	mNodeID;
 	uint256		mHash;
 	uint256		mHashes[32];
 
+	void updateHash(void);
+
+protected:
+	void SetChildHash(int m, const uint256 &hash);
+
 public:
 	SHAMapInnerNode(int Depth, const uint256 &NodeID);
-
-	int GetDepth() const { return mDepth; }
-	const uint256& GetNodeID() const { return mNodeID; }
+	const SHAMapNodeID& GetNodeID() const { return mNodeID; }
+	const uint256& GetNodeHash() const  { return mHash; }
 	const uint256& GetChildHash(int m) const;
-	const uint256& GetNodeHash() const;
-
-	bool IsRootNode() const { return mDepth==0; }
-	bool IsLastInnerNode() const { return mDepth==9; }
-
-	uint256 GetChildNodeID(int m) const;
-	void SetChildHash(int m, const uint256& Hash);
-	void SetChildHash(const SHAMapInnerNode &mChild);
-	void SetChildHash(const SHAMapLeafNode &mLeaf);
 };
 
-// Not all nodes need to be resident in memory, the class can hold subsets
-// This class does not handle any inter-node logic because that requires reads/writes
+
 
 class SHAMap
 {
@@ -90,13 +122,32 @@ public:
 
 	ScopedLock Lock() const { return ScopedLock(mLock); }
 
-	bool HasInnerNode(int m, const uint160 &nodeID);
+	// inner node access functions
+	bool HasInnerNode(const SHAMapNodeID &id);
 	bool GiveInnerNode(SHAMapInnerNode::pointer);
-	SHAMapInnerNode::pointer GetInnerNode(int m, const uint160 &nodeID);
+	SHAMapInnerNode::pointer GetInnerNode(const SHAMapNodeID &);
 
-	bool HasLeafNode(int m, const uint160 &nodeID);
+	// leaf node access functions
+	bool HasLeafNode(const SHAMapNodeID &id);
 	bool GiveLeafNode(SHAMapLeafNode::pointer);
-	SHAMapLeafNode::pointer GetLeafNode(int m, const uint160 &nodeID);
+	SHAMapLeafNode::pointer GetLeafNode(const SHAMapNodeID &);
+
+	// generic node functions
+	std::vector<unsigned char> getRawNode(const SHAMapNodeID &id);
+	bool addRawNode(const SHAMapNodeID& nodeID, std::vector<unsigned char> rawNode);
+
+	// normal hash access functions
+	bool hasHash(const uint256 &hash);
+	bool addHash(const uint256 &hash);
+	bool delHash(const uint256 &hash);
+	bool firstHash(uint256 &hash);
+	bool lastHash(uint256 &hash);
+	bool nextHash(uint256 &hash);
+	bool prevHash(uint256 &hash);
+
+	// special overloads for backed maps
+	virtual bool fetchNode(const uint256 &hash, std::vector<unsigned char>& rawNode);
+	virtual bool writeNode(const uint256 &hash, const std::vector<unsigned char>& rawNode);
 };
 
 #endif
