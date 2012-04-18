5d7a2ed8e3f48f985b473915723c7edc77bbfabe
Arthur Britto <ahbritto@gmail.com>
2012-04-17 19:43:28 -0700

    Add strJoin to utils.h

--- src/utils.h
+++ src/utils.h
@@ -2,6 +2,7 @@
 #define __UTILS__
 
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/format.hpp>
 
 #define nothing()   do {} while (0)
 
@@ -9,4 +10,18 @@ boost::posix_time::ptime ptEpoch();
 int iToSeconds(boost::posix_time::ptime ptWhen);
 boost::posix_time::ptime ptFromSeconds(int iSeconds);
 
+template<class Iterator>
+std::string strJoin(Iterator first, Iterator last, std::string strSeperator)
+{
+	std::ostringstream  ossValues;
+
+	for (Iterator start = first; first != last; first++)
+	{
+		ossValues << str(boost::format("%s%s") % (start == first ? "" : strSeperator) % *first);
+	}
+
+	return ossValues.str();
+}
 #endif
+
+// vim:ts=4
