29b1cede64b79abee893ad7d8d54e55a9fbda3bc
JoelKatz <DavidJoelSchwartz@GMail.com>
2012-05-05 15:03:08 -0700

    Optimizations suggested by Arthur.

--- src/Serializer.cpp
+++ src/Serializer.cpp
@@ -81,28 +81,31 @@ int Serializer::addRaw(const void *ptr, int len)
 bool Serializer::get16(uint16& o, int offset) const
 {
 	if ((offset + 2) > mData.size()) return false;
-	o = mData.at(offset++);
-	o <<= 8; o |= mData.at(offset);
+	unsigned char *ptr = &mData[offset];
+	o = *ptr++; o <<= 8; o |= *ptr;
 	return true;
 }
 
 bool Serializer::get32(uint32& o, int offset) const
 {
 	if ((offset + 4) > mData.size()) return false;
-	o=mData.at(offset++);
-	o<<=8; o |= mData.at(offset++); o <<= 8; o |= mData.at(offset++);
-	o<<=8; o |= mData.at(offset);
+	unsigned char *ptr = &mData[offset];
+	o = *ptr++;
+	o <<= 8; o |= *ptr++;
+	o <<= 8; o |= *ptr++;
+	o <<= 8; o |= *ptr;
 	return true;
 }
 
 bool Serializer::get64(uint64& o, int offset) const
 {
 	if ((offset + 8) > mData.size()) return false;
-	o=mData.at(offset++);
-	o<<=8; o|= mData.at(offset++); o <<= 8; o |= mData.at(offset++);
-	o<<=8; o|= mData.at(offset++); o <<= 8; o |= mData.at(offset++);
-	o<<=8; o|= mData.at(offset++); o <<= 8; o |= mData.at(offset++);
-	o<<=8; o|= mData.at(offset);
+	unsigned char *ptr = &mData[offset];
+	o = *ptr++;
+	o <<= 8; o |= *ptr++; o <<= 8; o |= *ptr++;
+	o <<= 8; o |= *ptr++; o <<= 8; o |= *ptr++;
+	o <<= 8; o |= *ptr++; o <<= 8; o |= *ptr++;
+	o <<= 8; o |= *ptr;
 	return true;
 }
 
