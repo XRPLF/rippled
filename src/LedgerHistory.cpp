d2db80dc32d09d8d2f1295e0781ba17af57a613a
JoelKatz <DavidJoelSchwartz@GMail.com>
2012-05-10 16:07:15 -0700

    Cosmetic changes.

--- src/LedgerHistory.cpp
+++ src/LedgerHistory.cpp
@@ -39,12 +39,12 @@ Ledger::pointer LedgerHistory::getLedgerBySeq(uint32 index)
 {
 	boost::recursive_mutex::scoped_lock sl(mLedgersByHash.peekMutex());
 	std::map<uint32, Ledger::pointer>::iterator it(mLedgersByIndex.find(index));
-	if(it!=mLedgersByIndex.end()) return it->second;
+	if (it != mLedgersByIndex.end()) return it->second;
 	sl.unlock();
 
 	Ledger::pointer ret(Ledger::loadByIndex(index));
-	if(!ret) return ret;
-	assert(ret->getLedgerSeq()==index);
+	if (!ret) return ret;
+	assert(ret->getLedgerSeq() == index);
 
 	sl.lock();
 	mLedgersByHash.canonicalize(ret->getHash(), ret);
@@ -54,16 +54,16 @@ Ledger::pointer LedgerHistory::getLedgerBySeq(uint32 index)
 
 Ledger::pointer LedgerHistory::getLedgerByHash(const uint256& hash)
 {
-	Ledger::pointer ret=mLedgersByHash.fetch(hash);
-	if(ret) return ret;
+	Ledger::pointer ret = mLedgersByHash.fetch(hash);
+	if (ret) return ret;
 
-	ret=Ledger::loadByHash(hash);
-	if(!ret) return ret;
-	assert(ret->getHash()==hash);
+	ret = Ledger::loadByHash(hash);
+	if (!ret) return ret;
+	assert(ret->getHash() == hash);
 
 	boost::recursive_mutex::scoped_lock sl(mLedgersByHash.peekMutex());
 	mLedgersByHash.canonicalize(hash, ret);
-	if(ret->isAccepted()) mLedgersByIndex[ret->getLedgerSeq()]=ret;
+	if (ret->isAccepted()) mLedgersByIndex[ret->getLedgerSeq()] = ret;
 	return ret;
 }
 
@@ -71,17 +71,17 @@ Ledger::pointer LedgerHistory::canonicalizeLedger(Ledger::pointer ledger, bool s
 {
 	uint256 h(ledger->getHash());
 
-	if(!save)
+	if (!save)
 	{ // return input ledger if not in map, otherwise, return corresponding map ledger
-		Ledger::pointer ret=mLedgersByHash.fetch(h);
-		if(ret) return ret;
+		Ledger::pointer ret = mLedgersByHash.fetch(h);
+		if (ret) return ret;
 		return ledger;
 	}
 
 	// save input ledger in map if not in map, otherwise return corresponding map ledger
 	boost::recursive_mutex::scoped_lock sl(mLedgersByHash.peekMutex());
 	mLedgersByHash.canonicalize(h, ledger);
-	if(ledger->isAccepted()) mLedgersByIndex[ledger->getLedgerSeq()]=ledger;
+	if (ledger->isAccepted()) mLedgersByIndex[ledger->getLedgerSeq()]=ledger;
 	return ledger;
 }
 // vim:ts=4
