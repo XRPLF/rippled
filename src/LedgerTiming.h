96bdc953bfa474a8ae696542d54c1b1ed15bdbbd
JoelKatz <DavidJoelSchwartz@GMail.com>
2012-05-15 04:12:32 -0700

    Cleanup.

--- src/LedgerTiming.h
+++ src/LedgerTiming.h
@@ -7,18 +7,23 @@
 #ifdef LEDGER_CLOSE_FAST
 
 // Time between one ledger close and the next ledger close
-#	define LEDGER_INTERVAL		60
+#	define LEDGER_INTERVAL			60
 
 // Time we expect avalanche to finish
-#	define LEDGER_CONVERGE		20
+#	define LEDGER_CONVERGE			20
+
+// Time we forcibly abort avalanche
+#	define LEDGER_FORCE_CONVERGE	30
 
 #endif
 
 #ifdef LEDGER_CLOSE_SLOW
 
-#	define LEDGER_INTERVAL		1800
+#	define LEDGER_INTERVAL			1800
 
-#	define LEDGER_CONVERGE		180
+#	define LEDGER_CONVERGE			180
+
+#	define LEDGER_FORCE_CONVERGE	240
 
 // Time a transaction must be unconflicted before we consider it protected
 #	define LEDGER_PROTECT		90
