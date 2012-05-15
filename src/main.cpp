f208c570bf69e620b10046a354460d38df752cc8
Arthur Britto <ahbritto@gmail.com>
2012-05-15 16:33:06 -0700

    Fix unit test calling code.

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 
+#include <boost/foreach.hpp>
 #include <boost/program_options.hpp>
 #include <boost/test/included/unit_test.hpp>
 
@@ -108,18 +109,20 @@ int main(int argc, char* argv[])
 	}
 	else if (vm.count("test"))
 	{
-		std::vector<std::string>	vCmd;
-		int							iCmd	= vm.count("parameters");
+		int					iCmd	= vm.count("parameters");
+		std::vector<char*>	pvCmd;
+
+		pvCmd.push_back(argv[0]);
 
 		if (iCmd)
+		{
+			std::vector<std::string>	vCmd;
+
 			vCmd	= vm["parameters"].as<std::vector<std::string> >();
 
-		std::vector<char*>			pvCmd;
-
-		pvCmd.resize(iCmd);
-
-		for (int i=0; i != iCmd; ++i)
-			pvCmd[i]	= (char*) (vCmd[i].c_str());
+			BOOST_FOREACH(std::string& param, vCmd)
+				pvCmd.push_back(const_cast<char*>(param.c_str()));
+		}
 
 		iResult	= unit_test_main(init_unit_test, iCmd, &pvCmd[0]);
 	}
