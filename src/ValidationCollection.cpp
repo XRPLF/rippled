c7fd34bf9eb1420ce7cc6a4b3a84d0a2e4728c7c
JoelKatz <DavidJoelSchwartz@GMail.com>
2012-06-20 04:18:36 -0700

    bugfix.

--- src/ValidationCollection.cpp
+++ src/ValidationCollection.cpp
@@ -12,12 +12,15 @@ bool ValidationCollection::addValidation(SerializedValidation::pointer val)
 	uint256 hash = val->getLedgerHash();
 	uint160 node = val->getSignerPublic().getNodeID();
 
-	boost::mutex::scoped_lock sl(mValidationLock);
-	bool ret = mValidations[hash].insert(std::make_pair(node, val)).second;
-	if (ret)
-		Log(lsINFO) << "Val for " << hash.GetHex() << " from " << node.GetHex() << " added " <<
-			(val->isTrusted() ? "trusted" : "UNtrusted");
-	return ret;
+	{
+		boost::mutex::scoped_lock sl(mValidationLock);
+		if (!mValidations[hash].insert(std::make_pair(node, val)).second)
+			return false;
+	}
+
+	Log(lsINFO) << "Val for " << hash.GetHex() << " from " << node.GetHex() << " added " <<
+		(val->isTrusted() ? "trusted" : "UNtrusted");
+	return true;
 }
 
 ValidationSet ValidationCollection::getValidations(const uint256& ledger)
@@ -38,10 +41,9 @@ void ValidationCollection::getValidationCount(const uint256& ledger, int& truste
 	boost::unordered_map<uint256, ValidationSet>::iterator it = mValidations.find(ledger);
 	if (it != mValidations.end())
 	{
-		for (ValidationSet::iterator vit = it->second.begin(), end = it->second.end();
-			vit != end; ++vit)
+		for (ValidationSet::iterator vit = it->second.begin(), end = it->second.end(); vit != end; ++vit)
 		{
-			if(vit->second->isTrusted())
+			if (vit->second->isTrusted())
 				++trusted;
 			else
 				++untrusted;
