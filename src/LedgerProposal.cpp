c839f93d3c7a734aee4405a2912ee905cc12fdb4
JoelKatz <DavidJoelSchwartz@GMail.com>
2012-07-12 01:43:56 -0700

    32-bit network times. Close time cleanup.

--- src/LedgerProposal.cpp
+++ src/LedgerProposal.cpp
@@ -7,7 +7,7 @@
 #include "Application.h"
 #include "HashPrefixes.h"
 
-LedgerProposal::LedgerProposal(const uint256& pLgr, uint32 seq, const uint256& tx, uint64 closeTime,
+LedgerProposal::LedgerProposal(const uint256& pLgr, uint32 seq, const uint256& tx, uint32 closeTime,
 		const NewcoinAddress& naPeerPublic) :
 	mPreviousLedger(pLgr), mCurrentHash(tx), mCloseTime(closeTime), mProposeSeq(seq)
 {
@@ -21,7 +21,7 @@ LedgerProposal::LedgerProposal(const uint256& pLgr, uint32 seq, const uint256& t
 
 
 LedgerProposal::LedgerProposal(const NewcoinAddress& naSeed, const uint256& prevLgr,
-		const uint256& position, uint64 closeTime) :
+		const uint256& position, uint32 closeTime) :
 	mPreviousLedger(prevLgr), mCurrentHash(position), mCloseTime(closeTime), mProposeSeq(0)
 {
 	mPublicKey	= NewcoinAddress::createNodePublic(naSeed);
@@ -29,7 +29,7 @@ LedgerProposal::LedgerProposal(const NewcoinAddress& naSeed, const uint256& prev
 	mPeerID		= mPublicKey.getNodeID();
 }
 
-LedgerProposal::LedgerProposal(const uint256& prevLgr, const uint256& position, uint64 closeTime) :
+LedgerProposal::LedgerProposal(const uint256& prevLgr, const uint256& position, uint32 closeTime) :
 	mPreviousLedger(prevLgr), mCurrentHash(position), mCloseTime(closeTime), mProposeSeq(0)
 {
 	;
@@ -37,13 +37,13 @@ LedgerProposal::LedgerProposal(const uint256& prevLgr, const uint256& position, 
 
 uint256 LedgerProposal::getSigningHash() const
 {
-	Serializer s((32 + 32 + 256 + 256 + 64) / 8);
+	Serializer s((32 + 32 + 32 + 256 + 256) / 8);
 
 	s.add32(sHP_Proposal);
 	s.add32(mProposeSeq);
+	s.add32(mCloseTime);
 	s.add256(mPreviousLedger);
 	s.add256(mCurrentHash);
-	s.add64(mCloseTime);
 
 	return s.getSHA512Half();
 }
@@ -53,7 +53,7 @@ bool LedgerProposal::checkSign(const std::string& signature, const uint256& sign
 	return mPublicKey.verifyNodePublic(signingHash, signature);
 }
 
-void LedgerProposal::changePosition(const uint256& newPosition, uint64 closeTime)
+void LedgerProposal::changePosition(const uint256& newPosition, uint32 closeTime)
 {
 	mCurrentHash = newPosition;
 	mCloseTime = closeTime;
