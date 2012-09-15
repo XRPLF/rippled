86700c30bb9fb51665ba564c36a495690897db1a
Arthur Britto <ahbritto@gmail.com>
2012-09-15 15:45:33 -0700

    Build testing newcoind.cfg as needed.

---
+++ test/config.js
@@ -0,0 +1,15 @@
+//
+// Configuration for unit tests
+//
+
+exports.servers = {
+	alpha : {
+		"peer_ip" : "0.0.0.0",
+		"peer_port" : 51235,
+		"websocket_ip" : "127.0.0.1",
+		"websocket_port" : 5005,
+		"validation_seed" : "shhDFVsmS2GSu5vUyZSPXYfj1r79h",
+		"validators" : "n9L8LZZCwsdXzKUN9zoVxs4YznYXZ9hEhsQZY7aVpxtFaSceiyDZ beta"
+	}
+};
+// vim:ts=4
