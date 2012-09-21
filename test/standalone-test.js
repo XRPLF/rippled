169f0f487a47627ae1928bd12863a27bdee1c2ef
Arthur Britto <ahbritto@gmail.com>
2012-09-21 14:37:16 -0700

    UT: Rework server module to provide an object.

--- test/standalone-test.js
+++ test/standalone-test.js
@@ -6,11 +6,11 @@ var buster = require("buster");
 var server = require("./server.js");
 
 buster.testCase("Check standalone server startup", {
-    "server start and stop": function (done) {
+    "server start and stop": function(done) {
 			server.start("alpha",
-				function (e) {
+				function(e) {
 					buster.refute(e);
-					server.stop("alpha", function (e) {
+					server.stop("alpha", function(e) {
 						buster.refute(e);
 						done();
 					});
@@ -18,5 +18,4 @@ buster.testCase("Check standalone server startup", {
 		}
 });
 
-// console.log("standalone-test.js<");
 // vim:ts=4
