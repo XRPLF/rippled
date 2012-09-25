04f0b12a13529a7b2251a876cb3e0c371e41e86f
Arthur Britto <ahbritto@gmail.com>
2012-09-25 15:14:27 -0700

    UT: Initial tests of creating and connecting to a standalone store.

--- test/standalone-test.js
+++ test/standalone-test.js
@@ -4,6 +4,7 @@ var fs = require("fs");
 var buster = require("buster");
 
 var server = require("./server.js");
+var remote = require("../js/remote.js");
 
 buster.testCase("Check standalone server startup", {
     "server start and stop": function(done) {
@@ -15,6 +16,46 @@ buster.testCase("Check standalone server startup", {
 						done();
 					});
 				});
+		}
+});
+
+buster.testCase("Check websocket connection", {
+	'setUp' :
+		function(done) {
+			server.start("alpha",
+				function(e) {
+					buster.refute(e);
+					done();
+			});
+		},
+
+	'tearDown' :
+		function(done) {
+			server.stop("alpha", function(e) {
+				buster.refute(e);
+				done();
+			});
+		},
+
+    "websocket connect and disconnect" :
+		function(done) {
+			var alpha	= remote.remoteConfig("alpha");
+
+			alpha.connect(function(stat) {
+				buster.assert(1 == stat);			// OPEN
+
+				alpha.disconnect(function(stat) {
+						buster.assert(3 == stat);	// CLOSED
+						done();
+					});
+				});
+		},
+});
+
+buster.testCase("Check assert", {
+	"assert" :
+		function() {
+			buster.assert(true);
 		}
 });
 
