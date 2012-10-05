4bab95440c5815dc5586c15e44e920ca7055bd12
JoelKatz <DavidJoelSchwartz@GMail.com>
2012-10-04 23:44:36 -0700

    Log how many peers we sent a validation to.

--- src/LedgerConsensus.cpp
+++ src/LedgerConsensus.cpp
@@ -1114,12 +1114,13 @@ void LedgerConsensus::accept(SHAMap::ref set)
 		SerializedValidation::pointer v = boost::make_shared<SerializedValidation>
 			(newLCLHash, theApp->getOPs().getValidationTimeNC(), mValSeed, mProposing);
 		v->setTrusted();
-		Log(lsINFO) << "CNF Val " << newLCLHash;
 		theApp->getValidations().addValidation(v);
 		std::vector<unsigned char> validation = v->getSigned();
 		newcoin::TMValidation val;
 		val.set_validation(&validation[0], validation.size());
-		theApp->getConnectionPool().relayMessage(NULL, boost::make_shared<PackedMessage>(val, newcoin::mtVALIDATION));
+		int j = theApp->getConnectionPool().relayMessage(NULL,
+			boost::make_shared<PackedMessage>(val, newcoin::mtVALIDATION));
+		Log(lsINFO) << "CNF Val " << newLCLHash << " to " << j << " peers";
 	}
 	else
 		Log(lsINFO) << "CNF newLCL " << newLCLHash;
