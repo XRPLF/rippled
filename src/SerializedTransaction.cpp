ffd1f03fe25d65a78287bbb069695630a727e7f7
JoelKatz <DavidJoelSchwartz@GMail.com>
2012-10-05 02:36:04 -0700

    Missed one.

--- src/SerializedTransaction.cpp
+++ src/SerializedTransaction.cpp
@@ -211,7 +211,6 @@ BOOST_AUTO_TEST_CASE( STrans_test )
 	}
 	std::auto_ptr<STObject> new_obj = STObject::parseJson(j.getJson(0), sfGeneric);
 	if (new_obj.get() == NULL) BOOST_FAIL("Unable to build object from json");
-	Log(lsINFO) << new_obj->getJson(0);
 }
 
 BOOST_AUTO_TEST_SUITE_END();
