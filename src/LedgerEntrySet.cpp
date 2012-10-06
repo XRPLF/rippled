2ae5a92fa1cc8e65952c2aca0101e16b43948dc2
Arthur Britto <ahbritto@gmail.com>
2012-10-05 18:01:39 -0700

    Cosmetic.

--- src/LedgerEntrySet.cpp
+++ src/LedgerEntrySet.cpp
@@ -888,8 +888,6 @@ uint32 LedgerEntrySet::rippleTransferRate(const uint160& uIssuerID)
 
 uint32 LedgerEntrySet::rippleTransferRate(const uint160& uSenderID, const uint160& uReceiverID, const uint160& uIssuerID)
 {
-	uint32			uQuality;
-
 	return uSenderID == uIssuerID || uReceiverID == uIssuerID
 			? QUALITY_ONE
 			: rippleTransferRate(uIssuerID);
