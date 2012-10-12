237ff73916446d169ac17d8ef51dea489ae626e0
Arthur Britto <ahbritto@gmail.com>
2012-10-12 15:12:56 -0700

    Bump protocol version.

--- src/Version.h
+++ src/Version.h
@@ -5,7 +5,7 @@
 //
 
 #define SERVER_VERSION_MAJOR		0
-#define SERVER_VERSION_MINOR		6
+#define SERVER_VERSION_MINOR		7
 #define SERVER_VERSION_SUB			"-a"
 #define SERVER_NAME					"NewCoin"
 
@@ -16,11 +16,11 @@
 
 // Version we prefer to speak:
 #define PROTO_VERSION_MAJOR			1
-#define PROTO_VERSION_MINOR			1
+#define PROTO_VERSION_MINOR			2
 
 // Version we will speak to:
 #define MIN_PROTO_MAJOR				1
-#define MIN_PROTO_MINOR				1
+#define MIN_PROTO_MINOR				2
 
 #define MAKE_VERSION_INT(maj,min)	((maj << 16) | min)
 #define GET_VERSION_MAJOR(ver)		(ver >> 16)
