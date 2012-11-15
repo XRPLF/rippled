c9cb46577614273cd8e0d7f64e61ede47b38d93e
jed <jed@ripple.com>
2012-11-14 16:28:33 -0800

    .

--- test/config.js
+++ test/config.js
@@ -14,12 +14,13 @@ exports.servers = {
   // A local test server.
   "alpha" : {
     'trusted' : true,
+    'no_server' : true,
     // "peer_ip" : "0.0.0.0",
     // "peer_port" : 51235,
     'rpc_ip' : "0.0.0.0",
     'rpc_port' : 5005,
     'websocket_ip' : "127.0.0.1",
-    'websocket_port' : 6005,
+    'websocket_port' : 5006,
     // 'validation_seed' : "shhDFVsmS2GSu5vUyZSPXYfj1r79h",
     // 'validators' : "n9L8LZZCwsdXzKUN9zoVxs4YznYXZ9hEhsQZY7aVpxtFaSceiyDZ beta"
   }
