d27b395c1ccae6d6c55f4ca7f0683afefe7d448f
JoelKatz <DavidJoelSchwartz@GMail.com>
2013-01-01 18:18:27 -0800

    Helper function.

--- src/cpp/ripple/LedgerHistory.h
+++ src/cpp/ripple/LedgerHistory.h
@@ -15,6 +15,7 @@ public:
 	void addLedger(Ledger::pointer ledger);
 	void addAcceptedLedger(Ledger::pointer ledger, bool fromConsensus);
 
+	uint256 getLedgerHash(uint32 index);
 	Ledger::pointer getLedgerBySeq(uint32 index);
 	Ledger::pointer getLedgerByHash(const uint256& hash);
 	Ledger::pointer canonicalizeLedger(Ledger::pointer, bool cache);
