271bf901eca717dae3a19be82db93f9b7e391043
JoelKatz <DavidJoelSchwartz@GMail.com>
2013-01-15 23:47:42 -0800

    Support for compiling on 32-bit platforms. The main issue was BN_ULONG size and not using 'long' where 'long long' or 'uint64' should be used.

---
+++ src/cpp/ripple/BigNum64.h
@@ -0,0 +1,22 @@
+
+// Support 64-bit word operations on 32-bit platforms
+
+static int BN_add_word64(BIGNUM *a, uint64 w)
+{
+	CBigNum bn(w);
+	return BN_add(a, &bn, a);
+}
+
+static int BN_mul_word64(BIGNUM *a, uint64 w)
+{
+	CBigNum bn(w);
+	CAutoBN_CTX ctx;
+	return BN_mul(a, &bn, a, ctx);
+}
+
+static uint64 BN_div_word64(BIGNUM *a, uint64 w)
+{
+	CBigNum bn(w);
+	CAutoBN_CTX ctx;
+	return (BN_div(a, NULL, a, &bn, ctx) == 1) ? 0 : ((uint64)-1);
+}
