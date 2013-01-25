c04b76e1caff4a61d7ebacd6c498966b2cfc9462
JoelKatz <DavidJoelSchwartz@GMail.com>
2013-01-24 19:23:12 -0800

    Fix typos.

--- src/cpp/websocketpp/src/roles/server.hpp
+++ src/cpp/websocketpp/src/roles/server.hpp
@@ -65,9 +65,9 @@ static std::pair<bufIterator, bool> match_header(bufIterator begin, bufIterator 
 	// Do we have a complete HTTP request
 	bufIterator it = std::search(begin, end, header_match.begin(), header_match.end());
 	if (it != end)
-		return std::make_pair(it + header_match.size());
+		return std::make_pair(it + header_match.size(), true);
 	it = std::search(begin, end, alt_header_match.begin(), alt_header_match.end());
-		return std::make_pair(it + alt_header_match_size(), true);
+		return std::make_pair(it + alt_header_match.size(), true);
 
 	// If we don't have a flash policy request, we're done
 	it = std::search(begin, end, flash_match.begin(), flash_match.end());
