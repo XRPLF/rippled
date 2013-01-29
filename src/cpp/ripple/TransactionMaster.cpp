6b4141f583b2c717d8d5684ca922f855fbcc5371
JoelKatz <DavidJoelSchwartz@GMail.com>
2013-01-29 14:28:05 -0800

    Cleanups.

--- src/cpp/ripple/TransactionMaster.cpp
+++ src/cpp/ripple/TransactionMaster.cpp
@@ -66,18 +66,23 @@ SerializedTransaction::pointer TransactionMaster::fetch(SHAMapItem::ref item, SH
 	return txn;
 }
 
-static void saveTransactionHelper(Transaction::pointer txn, LoadEvent::pointer)
+static void saveHelper(Transaction::pointer txn, LoadEvent::pointer l)
 {
-	Transaction::saveTransaction(txn);
+	txn->save();
+	l.reset();
 }
 
 bool TransactionMaster::canonicalize(Transaction::pointer& txn, bool may_be_new)
 {
 	uint256 tid = txn->getID();
-	if (!tid) return false;
-	if (mCache.canonicalize(tid, txn)) return true;
+	if (!tid)
+		return false;
+
+	if (mCache.canonicalize(tid, txn))
+		return true;
+
 	if (may_be_new)
-		theApp->getAuxService().post(boost::bind(&saveTransactionHelper, txn,
+		theApp->getAuxService().post(boost::bind(saveHelper, txn,
 			theApp->getJobQueue().getLoadEvent(jtDISK)));
 	return false;
 }
