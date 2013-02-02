1012833991ae711b19a6794845e68803ddffa280
JoelKatz <DavidJoelSchwartz@GMail.com>
2013-02-01 18:09:21 -0800

    Additional override.

--- src/cpp/ripple/AutoSocket.h
+++ src/cpp/ripple/AutoSocket.h
@@ -141,6 +141,16 @@ public:
 	}
 
 
+	template <typename Allocator, typename Handler>
+		void async_write(basic_streambuf<Allocator>& buffers, Handler handler)
+	{
+		if (isSecure())
+			boost::asio::async_write(*mSocket, buffers, handler);
+		else
+			boost::asio::async_write(PlainSocket(), buffers, handler);
+	}
+
+
 	template <typename Buf, typename Condition, typename Handler>
 		void async_read(const Buf& buffers, Condition cond, Handler handler)
 	{
