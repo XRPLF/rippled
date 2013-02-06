c4cbc509d3ea03153881e8ddf62cc6e1c5283a2b
jed <jed@ripple.com>
2013-02-06 11:39:47 -0800

    didn't mean to check that in

--- test/send-test.js
+++ test/send-test.js
@@ -39,8 +39,8 @@ buster.testCase("Fee Changes", {
   */
 
 buster.testCase("Sending", {
-  //'setUp'     : testutils.build_setup(),
-  'setUp'     : testutils.build_setup({verbose: true , no_server: true}),
+  'setUp'     : testutils.build_setup(),
+  //'setUp'     : testutils.build_setup({verbose: true , no_server: true}),
   'tearDown'  : testutils.build_teardown(),
 
   "send XRP to non-existent account with insufficent fee" :
@@ -974,7 +974,7 @@ buster.testCase("Indirect ripple", {
         });
     },
 
-  "=> indirect ripple with path" :
+  "indirect ripple with path" :
     function (done) {
       var self = this;
 
