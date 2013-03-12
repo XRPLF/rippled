566b36715dbaabf87c547d840bd1bcbe02c8c80f
JoelKatz <DavidJoelSchwartz@GMail.com>
2013-03-11 17:57:12 -0700

    Simplify the ledger acquire logic. This fixes a few bugs related to it including the issue if you remove the ledger DB.

--- src/cpp/ripple/Ledger.cpp
+++ src/cpp/ripple/Ledger.cpp
@@ -516,10 +516,7 @@ void Ledger::saveAcceptedLedger(Job&, bool fromConsensus)
 	}
 
 	if (!fromConsensus)
-	{
 		dropCache();
-		return;
-	}
 
 	if (theApp->getJobQueue().getJobCount(jtPUBOLDLEDGER) == 0)
 		theApp->getLedgerMaster().resumeAcquiring();
