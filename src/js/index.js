1bca0154da4d3f3ec364cb0fc3b36efb0493e9c0
Arthur Britto <ahbritto@gmail.com>
2013-03-14 20:38:50 -0700

    JS: Export Transaction.

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,11 @@
-exports.Remote = require('./remote').Remote;
-exports.Amount = require('./amount').Amount;
-exports.Currency = require('./currency').Currency;
-exports.UInt160 = require('./amount').UInt160;
-exports.Seed = require('./amount').Seed;
+exports.Remote      = require('./remote').Remote;
+exports.Amount      = require('./amount').Amount;
+exports.Currency    = require('./currency').Currency;
+exports.UInt160     = require('./amount').UInt160;
+exports.Seed        = require('./amount').Seed;
+exports.Transaction = require('./amount').Transaction;
 
-exports.utils = require('./utils');
+exports.utils       = require('./utils');
 
 // Important: We do not guarantee any specific version of SJCL or for any
 // specific features to be included. The version and configuration may change at
@@ -13,6 +14,8 @@ exports.utils = require('./utils');
 // However, for programs that are tied to a specific version of ripple.js like
 // the official client, it makes sense to expose the SJCL instance so we don't
 // have to include it twice.
-exports.sjcl = require('../../build/sjcl');
+exports.sjcl      = require('../../build/sjcl');
 
-exports.config = require('./config');
+exports.config    = require('./config');
+
+// vim:sw=2:sts=2:ts=8:et
