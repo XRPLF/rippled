39c63550ee042ea27be62053c2c7114e3ae0869a
Stefan Thomas <justmoon@members.fsf.org>
2013-03-17 20:08:42 +0100

    JS: Expose Meta class.

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ exports.Currency    = require('./currency').Currency;
 exports.UInt160     = require('./amount').UInt160;
 exports.Seed        = require('./amount').Seed;
 exports.Transaction = require('./transaction').Transaction;
+exports.Meta        = require('./meta').Meta;
 
 exports.utils       = require('./utils');
 
