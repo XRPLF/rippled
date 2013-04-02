e7a41fab8f4d0a38ce00e6dd2c94dcbca8f7019b
JoelKatz <DavidJoelSchwartz@GMail.com>
2013-04-02 16:46:28 -0700

    Restore the original behavior. We do not want to call RAND_screen because it calls RAND_poll. Let me know if this breaks anything.

--- src/cpp/ripple/PlatRand.cpp
+++ src/cpp/ripple/PlatRand.cpp
@@ -8,12 +8,9 @@
 
 bool AddSystemEntropy()
 { // Get entropy from the Windows crypto provider
-	RAND_screen();  // this isn't really that safe since it only works for end users not servers
-
-/* TODO: you need the cryptoAPI installed I think for the below to work. I suppose we should require people to install this to build the windows version
 	char name[512], rand[128];
 	DWORD count = 500;
-	HCRYPTOPROV cryptoHandle;
+	HCRYPTPROV cryptoHandle;
 
 	if (!CryptGetDefaultProvider(PROV_RSA_FULL, NULL, CRYPT_MACHINE_DEFAULT, name, &count))
 	{
@@ -43,7 +40,6 @@ bool AddSystemEntropy()
 	CryptReleaseContext(cryptoHandle, 0);
 	RAND_seed(rand, 128);
 
-*/
 	return true;
 }
 
