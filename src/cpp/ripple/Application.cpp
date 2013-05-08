bc257406b68bd33a39dc4333a6670fdfc1c544de
JoelKatz <DavidJoelSchwartz@GMail.com>
2013-05-08 11:38:30 -0700

    LevelDB import speedups.

--- src/cpp/ripple/Application.cpp
+++ src/cpp/ripple/Application.cpp
@@ -178,6 +178,8 @@ void Application::setup()
 		options.block_cache = leveldb::NewLRUCache(theConfig.getSize(siHashNodeDBCache) * 1024 * 1024);
 		if (theConfig.NODE_SIZE >= 2)
 			options.filter_policy = leveldb::NewBloomFilterPolicy(10);
+		if (theConfig.LDB_IMPORT)
+			options.write_buffer_size = 32 << 20;
 		leveldb::Status status = leveldb::DB::Open(options, (theConfig.DATA_DIR / "hashnode").string(), &mHashNodeLDB);
 		if (!status.ok() || !mHashNodeLDB)
 		{
