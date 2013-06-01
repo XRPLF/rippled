f344d7a2c37d7ad485df635abe09223f03ed836e
Vinnie Falco <vinnie.falco@gmail.com>
2013-06-01 09:52:48 -0700

    Refactor ValidationCollection into IValidations

--- modules/ripple_ledger/ripple_ledger.h
+++ modules/ripple_ledger/ripple_ledger.h
@@ -34,4 +34,98 @@
 
 #include "modules/ripple_basics/ripple_basics.h"
 
+#include "../ripple_data/ripple_data.h"
+
+//#define WIN32_LEAN_AND_MEAN 
+
+#include <algorithm>
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <openssl/ec.h>
+#include <openssl/ripemd.h>
+#include <openssl/sha.h>
+#include <string>
+#include <vector>
+
+#include <boost/algorithm/string.hpp>
+#include <boost/bind.hpp>
+#include <boost/foreach.hpp>
+#include <boost/format.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/pointer_cast.hpp>
+#include <boost/ref.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/smart_ptr/shared_ptr.hpp>
+#include <boost/test/unit_test.hpp>
+#include <boost/thread.hpp>
+#include <boost/tuple/tuple_comparison.hpp>
+#include <boost/unordered_set.hpp>
+
+#include "src/cpp/database/SqliteDatabase.h"
+
+#include "src/cpp/ripple/AcceptedLedger.h"
+#include "src/cpp/ripple/AccountItems.h"
+#include "src/cpp/ripple/AccountSetTransactor.h"
+#include "src/cpp/ripple/AccountState.h"
+#include "src/cpp/ripple/Application.h"
+#include "src/cpp/ripple/CanonicalTXSet.h"
+#include "src/cpp/ripple/ChangeTransactor.h"
+#include "src/cpp/ripple/Config.h"
+#include "src/cpp/ripple/HashPrefixes.h"
+#include "src/cpp/ripple/Ledger.h"
+#include "src/cpp/ripple/LedgerAcquire.h"
+#include "src/cpp/ripple/LedgerConsensus.h"
+#include "src/cpp/ripple/LedgerEntrySet.h"
+#include "src/cpp/ripple/LedgerFormats.h"
+#include "src/cpp/ripple/LedgerHistory.h"
+#include "src/cpp/ripple/LedgerMaster.h"
+#include "src/cpp/ripple/LedgerProposal.h"
+#include "src/cpp/ripple/LedgerTiming.h"
+#include "src/cpp/ripple/NetworkOPs.h"
+#include "src/cpp/ripple/NicknameState.h"
+#include "src/cpp/ripple/Offer.h"
+#include "src/cpp/ripple/OfferCancelTransactor.h"
+#include "src/cpp/ripple/OfferCreateTransactor.h"
+#include "src/cpp/ripple/OrderBook.h"
+#include "src/cpp/ripple/OrderBookDB.h"
+#include "src/cpp/ripple/PackedMessage.h"
+#include "src/cpp/ripple/PaymentTransactor.h"
+#include "src/cpp/ripple/PFRequest.h"
+#include "src/cpp/ripple/RegularKeySetTransactor.h"
+#include "src/cpp/ripple/RippleCalc.h"
+#include "src/cpp/ripple/RippleState.h"
+#include "src/cpp/ripple/SerializedLedger.h"
+#include "src/cpp/ripple/SerializedObject.h"
+#include "src/cpp/ripple/SerializedTransaction.h"
+#include "src/cpp/ripple/SerializedTypes.h"
+#include "src/cpp/ripple/SerializedValidation.h"
+#include "src/cpp/ripple/SHAMapSync.h"
+#include "src/cpp/ripple/Transaction.h"
+#include "src/cpp/ripple/TransactionEngine.h"
+#include "src/cpp/ripple/TransactionErr.h"
+#include "src/cpp/ripple/TransactionFormats.h"
+#include "src/cpp/ripple/TransactionMaster.h"
+#include "src/cpp/ripple/TransactionMeta.h"
+#include "src/cpp/ripple/TransactionQueue.h"
+#include "src/cpp/ripple/Transactor.h"
+#include "src/cpp/ripple/TrustSetTransactor.h"
+#include "src/cpp/ripple/Wallet.h"
+#include "src/cpp/ripple/WalletAddTransactor.h"
+
+// contract stuff, order matters
+#include "src/cpp/ripple/ScriptData.h"
+#include "src/cpp/ripple/Contract.h"
+#include "src/cpp/ripple/Interpreter.h"
+#include "src/cpp/ripple/Operation.h"
+
+
+// New abstract interfaces
+#include "src/cpp/ripple/ripple_IFeatures.h"
+#include "src/cpp/ripple/ripple_IFeeVote.h"
+#include "src/cpp/ripple/ripple_ILoadFeeTrack.h"
+#include "src/cpp/ripple/ripple_IValidations.h"
+#include "src/cpp/ripple/FeatureTable.h"
+
 #endif
---
+++ src/cpp/ripple/ripple_IValidations.h
@@ -0,0 +1,47 @@
+#ifndef RIPPLE_IVALIDATIONS_H
+#define RIPPLE_IVALIDATIONS_H
+
+//#include <vector>
+//#include <boost/unordered_map.hpp>
+//#include <boost/thread/mutex.hpp>
+
+//#include "SerializedValidation.h"
+//#include "JobQueue.h"
+
+// VFALCO: TODO rename and move these typedefs into the IValidations interface
+typedef boost::unordered_map<uint160, SerializedValidation::pointer> ValidationSet;
+typedef std::pair<int, uint160> currentValidationCount; // nodes validating and highest node ID validating
+
+class IValidations
+{
+public:
+    static IValidations* New ();
+
+    virtual ~IValidations () { }
+
+	virtual bool addValidation (SerializedValidation::ref, const std::string& source) = 0;
+	
+    virtual ValidationSet getValidations (const uint256& ledger) = 0;
+
+	virtual void getValidationCount (const uint256& ledger, bool currentOnly, int& trusted, int& untrusted) = 0;
+	virtual void getValidationTypes (const uint256& ledger, int& full, int& partial) = 0;
+
+	virtual int getTrustedValidationCount (const uint256& ledger) = 0;
+
+	virtual int getNodesAfter (const uint256& ledger) = 0;
+	virtual int getLoadRatio (bool overLoaded) = 0;
+
+    // VFALCO: TODO, make a typedef for this ugly return value!
+	virtual boost::unordered_map<uint256, currentValidationCount> getCurrentValidations (
+		uint256 currentLedger, uint256 previousLedger) = 0;
+
+	virtual std::list <SerializedValidation::pointer> getCurrentTrustedValidations () = 0;
+
+	virtual void tune (int size, int age) = 0;
+	
+    virtual void flush () = 0;
+
+    virtual void sweep() = 0;
+};
+
+#endif
