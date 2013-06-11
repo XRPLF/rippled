e18122df775c6888b68bbba0593d94f11ee25eb2
JoelKatz <DavidJoelSchwartz@GMail.com>
2013-06-10 22:00:42 -0700

    Get it right.

--- src/cpp/ripple/SHAMap.h
+++ src/cpp/ripple/SHAMap.h
@@ -480,8 +480,8 @@ public:
 	typedef std::pair< uint256, std::vector<unsigned char> > fetchPackEntry_t;
 	std::list<fetchPackEntry_t> getFetchPack(SHAMap* have, bool includeLeaves, int max);
 
-	static int getFullBelowCacheSize()	{ return fullBelowCache.getSize(); }
-	static void sweep()					{ fullBelowCache.sweep(); }
+	static int getFullBelowSize()	{ return fullBelowCache.getSize(); }
+	static void sweep()				{ fullBelowCache.sweep(); }
 };
 
 #endif
