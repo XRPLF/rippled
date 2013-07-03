10a0db9160912cfe24db78002c5f760ecfbb6baa
Alex Dupre <ale@FreeBSD.org>
2013-07-03 10:00:40 +0200

    Linux implementation of isRunningUnderDebugger using ptrace() doesn't work on FreeBSD. (I doubt it'll work on Linux, too, but I'm not sure)

--- modules/beast_core/native/beast_bsd_Threads.cpp
+++ modules/beast_core/native/beast_bsd_Threads.cpp
@@ -54,20 +54,8 @@ void Process::terminate()
 
 BEAST_API bool BEAST_CALLTYPE beast_isRunningUnderDebugger()
 {
-    static char testResult = 0;
-
-    if (testResult == 0)
-    {
-        testResult = (char) ptrace (PT_TRACE_ME, 0, 0, 0);
-
-        if (testResult >= 0)
-        {
-            ptrace (PT_DETACH, 0, (caddr_t) 1, 0);
-            testResult = 1;
-        }
-    }
-
-    return testResult < 0;
+    bassertfalse; // XXX not implemented for FreeBSD!
+    return false;
 }
 
 BEAST_API bool BEAST_CALLTYPE Process::isRunningUnderDebugger()
