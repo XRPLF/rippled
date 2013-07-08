d798330f442e4d484d97fd846e72b79bfc502ae4
Vinnie Falco <vinnie.falco@gmail.com>
2013-07-08 11:08:33 -0700

    Update documentation for member

--- modules/beast_core/files/beast_File.h
+++ modules/beast_core/files/beast_File.h
@@ -246,20 +246,21 @@ public:
     int64 hashCode64() const;
 
     //==============================================================================
-    /** Returns a file based on a relative path.
+    /** Returns a file that represents a relative (or absolute) sub-path of the current one.
 
         This will find a child file or directory of the current object.
 
         e.g.
             File ("/moose/fish").getChildFile ("foo.txt") will produce "/moose/fish/foo.txt".
+            File ("/moose/fish").getChildFile ("haddock/foo.txt") will produce "/moose/fish/haddock/foo.txt".
             File ("/moose/fish").getChildFile ("../foo.txt") will produce "/moose/foo.txt".
 
         If the string is actually an absolute path, it will be treated as such, e.g.
-            File ("/moose/fish").getChildFile ("/foo.txt") will produce "/foo.txt"
+        File ("/moose/fish").getChildFile ("/foo.txt") will produce "/foo.txt"
 
         @see getSiblingFile, getParentDirectory, getRelativePathFrom, isAChildOf
     */
-    File getChildFile (String relativePath) const;
+    File getChildFile (String relativeOrAbsolutePath) const;
 
     /** Returns a file which is in the same directory as this one.
 
