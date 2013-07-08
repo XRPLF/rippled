6723b3580556098aaf9e8fffda413d92c6675206
JoelKatz <DavidJoelSchwartz@GMail.com>
2013-07-08 13:46:59 -0700

    Don't sign a transaction for non-admin when under load.

--- src/cpp/ripple/RPCHandler.cpp
+++ src/cpp/ripple/RPCHandler.cpp
@@ -60,6 +60,9 @@ RPCHandler::RPCHandler (NetworkOPs* netOps, InfoSub::pointer infoSub) : mNetOps 
 
 Json::Value RPCHandler::transactionSign (Json::Value params, bool bSubmit, bool bFailHard, ScopedLock& mlh)
 {
+    if (theApp()->getFeeTrack().isLoaded() && (mRole != ADMIN))
+        return rpcError(rpcTOO_BUSY);
+
     Json::Value     jvResult;
     RippleAddress   naSeed;
     RippleAddress   raSrcAddressID;
