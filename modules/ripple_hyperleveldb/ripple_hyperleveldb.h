81657e5e66f9c36d9c97e21554df528a87defd8d
Vinnie Falco <vinnie.falco@gmail.com>
2013-07-15 06:46:24 -0700

    Add module ripple_hyperleveldb

---
+++ modules/ripple_hyperleveldb/ripple_hyperleveldb.h
@@ -0,0 +1,15 @@
+//------------------------------------------------------------------------------
+/*
+    Copyright (c) 2011-2013, OpenCoin, Inc.
+*/
+//==============================================================================
+
+#ifndef RIPPLE_LEVELDB_RIPPLEHEADER
+#define RIPPLE_LEVELDB_RIPPLEHEADER
+
+//#include "hyperleveldb/hyperleveldb/cache.h"
+//#include "hyperleveldb/hyperleveldb/filter_policy.h"
+#include "hyperleveldb/hyperleveldb/db.h"
+//#include "hyperleveldb/hyperleveldb/write_batch.h"
+
+#endif
