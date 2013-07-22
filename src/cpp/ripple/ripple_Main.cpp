20c20fd05617c14463762ce2d9b237f0fedf2778
Vinnie Falco <vinnie.falco@gmail.com>
2013-07-22 14:06:45 -0700

    Fix path passed to DoSustain

--- src/cpp/ripple/ripple_Main.cpp
+++ src/cpp/ripple/ripple_Main.cpp
@@ -299,7 +299,7 @@ int rippleMain (int argc, char** argv)
     if (HaveSustain () &&
             !iResult && !vm.count ("parameters") && !vm.count ("fg") && !vm.count ("standalone") && !vm.count ("unittest"))
     {
-        std::string logMe = DoSustain (theConfig.DEBUG_LOGFILE.c_str());
+        std::string logMe = DoSustain (theConfig.DEBUG_LOGFILE.string());
 
         if (!logMe.empty ())
             Log (lsWARNING) << logMe;
