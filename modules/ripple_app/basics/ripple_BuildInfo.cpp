e187e7e282a70732a97f03d89478630630ddf060
JoelKatz <DavidJoelSchwartz@GMail.com>
2013-08-15 12:07:51 -0700

    0.12.0-rc1

--- modules/ripple_app/basics/ripple_BuildInfo.cpp
+++ modules/ripple_app/basics/ripple_BuildInfo.cpp
@@ -12,7 +12,7 @@ char const* BuildInfo::getRawVersionString ()
     //
     //  The build version number (edit this for each release)
     //
-        "0.11.0-rc2"
+        "0.12.0-rc1"
     //
     //  Must follow the format described here:
     //
