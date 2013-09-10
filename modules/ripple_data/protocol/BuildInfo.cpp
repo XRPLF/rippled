6234f0040d40c0eefaf7d5a1871ea46620f896db
JoelKatz <DavidJoelSchwartz@GMail.com>
2013-09-09 18:36:00 -0700

    0.12.1-alpha.1

--- modules/ripple_data/protocol/BuildInfo.cpp
+++ modules/ripple_data/protocol/BuildInfo.cpp
@@ -12,7 +12,7 @@ char const* BuildInfo::getRawVersionString ()
     //
     //  The build version number (edit this for each release)
     //
-        "0.12.0"
+        "0.12.1-alpha.1"
     //
     //  Must follow the format described here:
     //
