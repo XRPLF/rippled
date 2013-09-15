9b7663835173be3c1992eec642ae20064ab1b7bb
Vinnie Falco <vinnie.falco@gmail.com>
2013-09-15 09:44:14 -0700

    Tidy up SharedSingleton doc comments

--- src/beast/modules/beast_core/memory/SharedSingleton.h
+++ src/beast/modules/beast_core/memory/SharedSingleton.h
@@ -26,7 +26,10 @@
     object that depends on it, the order of destruction of objects is assured
     to be correct.
 
-    class Object must provide the function `Object* Object::createInstance()`
+    Object Requirements:
+        DefaultConstructible
+        TriviallyDestructible (when lifetime == neverDestroyed)
+        Destructible
 
     @class SharedSingleton
     @ingroup beast_core
