a602eb3b5d6215479009f327998c0bcd0a8d6f3e
Vinnie Falco <vinnie.falco@gmail.com>
2013-09-17 11:35:17 -0700

    Add ripple_sophia module

---
+++ src/ripple/sophia/ripple_sophia.cpp
@@ -0,0 +1,26 @@
+//------------------------------------------------------------------------------
+/*
+    Copyright (c) 2011-2013, OpenCoin, Inc.
+*/
+//==============================================================================
+
+#include "BeastConfig.h"
+
+#include "ripple_sophia.h"
+
+#if RIPPLE_SOPHIA_AVAILBLE
+
+#include "../sophia/db/cat.c"
+#include "../sophia/db/crc.c"
+#include "../sophia/db/cursor.c"
+#include "../sophia/db/e.c"
+#include "../sophia/db/file.c"
+#include "../sophia/db/gc.c"
+#include "../sophia/db/i.c"
+#include "../sophia/db/merge.c"
+#include "../sophia/db/recover.c"
+#include "../sophia/db/rep.c"
+#include "../sophia/db/sp.c"
+#include "../sophia/db/util.c"
+
+#endif
---
+++ src/ripple/sophia/ripple_sophia.h
@@ -0,0 +1,22 @@
+//------------------------------------------------------------------------------
+/*
+    Copyright (c) 2011-2013, OpenCoin, Inc.
+*/
+//==============================================================================
+
+#ifndef RIPPLE_SOPHIA_H_INCLUDED
+#define RIPPLE_SOPHIA_H_INCLUDED
+
+#include "beast/modules/beast_core/system/TargetPlatform.h"
+
+#if ! BEAST_WIN32
+
+#define RIPPLE_SOPHIA_AVAILABLE 1
+
+#else
+
+#define RIPPLE_SOPHIA_AVAILABLE 0
+
+#endif
+
+#endif
