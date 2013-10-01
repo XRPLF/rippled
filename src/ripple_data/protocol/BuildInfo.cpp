73700a483d2b7fb9cc3bb1c7f20e70beac192267
Vinnie Falco <vinnie.falco@gmail.com>
2013-10-01 15:05:30 -0700

    Set version to 0.14.0-rc2

--- src/ripple_data/protocol/BuildInfo.cpp
+++ src/ripple_data/protocol/BuildInfo.cpp
@@ -26,7 +26,7 @@ char const* BuildInfo::getRawVersionString ()
     //
     //  The build version number (edit this for each release)
     //
-        "0.14.0-rc1"
+        "0.14.0-rc2"
     //
     //  Must follow the format described here:
     //
