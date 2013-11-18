e683c380e5b08a5d1b8e978a600736241d023233
JoelKatz <DavidJoelSchwartz@GMail.com>
2013-11-18 00:25:59 -0800

    Fix a defect in RangeSet.

--- src/ripple_basics/containers/RangeSet.cpp
+++ src/ripple_basics/containers/RangeSet.cpp
@@ -217,7 +217,7 @@ void RangeSet::simplify ()
         if (it->second >= (nit->first - 1))
         {
             // ranges overlap
-            it->second = nit->second;
+            it->second = std::max(it->second, nit->second);
             mRanges.erase (nit);
         }
         else
