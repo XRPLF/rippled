05673d6c8153e7b45ce132cdb53dcc255e819456
NATTSiM <tlewkow@gmail.com>
2013-12-24 11:03:34 -0800

    [FOLD] Bug fix 4

--- src/ripple_app/main/Application.cpp
+++ src/ripple_app/main/Application.cpp
@@ -914,7 +914,7 @@ private:
     NodeStoreScheduler m_nodeStoreScheduler;
     ScopedPointer <NodeStore::Database> m_nodeStore;
     ScopedPointer <SNTPClient> m_sntpClient;
-    unique_ptr <InboundLedgers> m_inboundLedgers;
+    beast::unique_ptr <InboundLedgers> m_inboundLedgers;
     ScopedPointer <TxQueue> m_txQueue;
     ScopedPointer <Validators::Manager> m_validators;
     ScopedPointer <IFeatures> mFeatures;
