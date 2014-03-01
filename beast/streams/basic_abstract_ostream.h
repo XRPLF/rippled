d32b91e0de841dae5414f85c0b4613212a2e8919
Vinnie Falco <vinnie.falco@gmail.com>
2014-03-01 09:56:32 -0800

    Add abstract streams module: * New basic_abstract_ostream template for generic output * New abstract_ostream, common type alias * basic_scoped_ostream, RAII output to abstract streams

---
+++ beast/streams/basic_abstract_ostream.h
@@ -0,0 +1,118 @@
+//------------------------------------------------------------------------------
+/*
+    This file is part of Beast: https://github.com/vinniefalco/Beast
+    Copyright 2013, Vinnie Falco <vinnie.falco@gmail.com>
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose  with  or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE  SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH  REGARD  TO  THIS  SOFTWARE  INCLUDING  ALL  IMPLIED  WARRANTIES  OF
+    MERCHANTABILITY  AND  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY  SPECIAL ,  DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER  RESULTING  FROM  LOSS  OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION  OF  CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+//==============================================================================
+
+#ifndef BEAST_STREAMS_BASIC_ABSTRACT_OSTREAM_H_INCLUDED
+#define BEAST_STREAMS_BASIC_ABSTRACT_OSTREAM_H_INCLUDED
+
+#include "basic_scoped_ostream.h"
+
+#include <functional>
+#include <memory>
+#include <sstream>
+
+namespace beast {
+
+/** Abstraction for an output stream similar to std::basic_ostream. */
+template <
+    class CharT,
+    class Traits = std::char_traits <CharT>
+>
+class basic_abstract_ostream
+{
+public:
+    typedef std::basic_string <CharT, Traits> string_type;
+    typedef basic_scoped_ostream <CharT, Traits> scoped_stream_type;
+
+    basic_abstract_ostream() = default;
+
+    virtual
+    ~basic_abstract_ostream() = default;
+
+    basic_abstract_ostream (basic_abstract_ostream const&) = default;
+    basic_abstract_ostream& operator= (
+        basic_abstract_ostream const&) = default;
+
+    /** Returns `true` if the stream is active.
+        Inactive streams do not produce output.
+    */
+    /** @{ */
+    virtual
+    bool
+    active() const
+    {
+        return true;
+    }
+
+    explicit
+    operator bool() const
+    {
+        return active();
+    }
+    /** @} */
+
+    /** Returns a basic_scoped_ostream without appending anything.
+        This can be used if the caller wants to put the scoped stream
+        on the stack to build up output in a thread-safe fashion.
+    */
+#if 0
+    scoped_stream_type
+    scoped_stream ()
+    {
+#if 0
+        return scoped_stream_type (
+            [this](string_type const& s)
+            {
+                this->write (s);
+            });
+#else
+        return scoped_stream_type (*this);
+#endif
+    }
+#endif
+
+    /** Called to output each string. */
+    virtual
+    void
+    write (string_type const& s) = 0;
+
+    scoped_stream_type
+    operator<< (std::ostream& manip (std::ostream&))
+    {
+        return scoped_stream_type (manip,
+            [this](string_type const& s)
+            {
+                this->write (s);
+            });
+    }
+
+    template <class T>
+    scoped_stream_type
+    operator<< (T const& t)
+    {
+        return scoped_stream_type (t,
+            [this](string_type const& s)
+            {
+                this->write (s);
+            });
+    }
+};
+
+}
+
+#endif
---
+++ beast/streams/basic_scoped_ostream.h
@@ -0,0 +1,149 @@
+//------------------------------------------------------------------------------
+/*
+    This file is part of Beast: https://github.com/vinniefalco/Beast
+    Copyright 2013, Vinnie Falco <vinnie.falco@gmail.com>
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose  with  or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE  SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH  REGARD  TO  THIS  SOFTWARE  INCLUDING  ALL  IMPLIED  WARRANTIES  OF
+    MERCHANTABILITY  AND  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY  SPECIAL ,  DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER  RESULTING  FROM  LOSS  OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION  OF  CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+//==============================================================================
+
+#ifndef BEAST_STREAMS_BASIC_SCOPED_OSTREAM_H_INCLUDED
+#define BEAST_STREAMS_BASIC_SCOPED_OSTREAM_H_INCLUDED
+
+#include "../cxx14/memory.h" // <memory>
+#include <functional>
+#include <memory>
+#include <sstream>
+
+// gcc libstd++ doesn't have move constructors for basic_ostringstream
+// http://gcc.gnu.org/bugzilla/show_bug.cgi?id=54316
+//
+#ifndef BEAST_NO_STDLIB_STREAM_MOVE
+# ifdef __GLIBCXX__
+#  define BEAST_NO_STDLIB_STREAM_MOVE 1
+# else
+#  define BEAST_NO_STDLIB_STREAM_MOVE 0
+# endif
+#endif
+
+namespace beast {
+
+template <
+    class CharT,
+    class Traits
+>
+class basic_abstract_ostream;
+
+/** Scoped output stream that forwards to a functor upon destruction. */
+template <
+    class CharT,
+    class Traits = std::char_traits <CharT>,
+    class Allocator = std::allocator <CharT>
+>
+class basic_scoped_ostream
+{
+private:
+    typedef std::function <void (
+        std::basic_string <CharT, Traits, Allocator> const&)> handler_t;
+
+    typedef std::basic_ostringstream <
+        CharT, Traits, Allocator> stream_type;
+
+    handler_t m_handler;
+
+#if BEAST_NO_STDLIB_STREAM_MOVE
+    std::unique_ptr <stream_type> m_ss;
+
+    stream_type& stream()
+    {
+        return *m_ss;
+    }
+
+#else
+    stream_type m_ss;
+
+    stream_type& stream()
+    {
+        return m_ss;
+    }
+
+#endif
+
+public:
+    typedef std::basic_string <CharT, Traits> string_type;
+
+    // Disallow copy since that would duplicate the output
+    basic_scoped_ostream (basic_scoped_ostream const&) = delete;
+    basic_scoped_ostream& operator= (basic_scoped_ostream const) = delete;
+
+    template <class Handler>
+    explicit basic_scoped_ostream (Handler&& handler)
+        : m_handler (std::forward <Handler> (handler))
+    #if BEAST_NO_STDLIB_STREAM_MOVE
+        , m_ss (std::make_unique <stream_type>())
+    #endif
+    {
+    }
+
+    template <class T, class Handler>
+    basic_scoped_ostream (T const& t, Handler&& handler)
+        : m_handler (std::forward <Handler> (handler))
+    #if BEAST_NO_STDLIB_STREAM_MOVE
+        , m_ss (std::make_unique <stream_type>())
+    #endif
+    {
+        stream() << t;
+    }
+
+    basic_scoped_ostream (basic_abstract_ostream <
+        CharT, Traits>& ostream)
+        : m_handler (
+            [&](string_type const& s)
+            {
+                ostream.write (s);
+            })
+    {
+    }
+
+    basic_scoped_ostream (basic_scoped_ostream&& other)
+        : m_handler (std::move (other.m_handler))
+        , m_ss (std::move (other.m_ss))
+    {
+    }
+
+    ~basic_scoped_ostream()
+    {
+        auto const& s (stream().str());
+        if (! s.empty())
+            m_handler (s);
+    }
+
+    basic_scoped_ostream&
+    operator<< (std::ostream& manip (std::ostream&))
+    {
+        stream() << manip;
+        return *this;
+    }
+
+    template <class T>
+    basic_scoped_ostream&
+    operator<< (T const& t)
+    {
+        stream() << t;
+        return *this;
+    }
+};
+
+}
+
+#endif
