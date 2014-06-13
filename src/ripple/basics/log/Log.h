313b65d112ef1096036c7109635d232246588d05
Vinnie Falco <vinnie.falco@gmail.com>
2014-06-12 20:53:11 -0700

    [FOLD] Move LogSeverity to Log.h

--- src/ripple/basics/log/Log.h
+++ src/ripple/basics/log/Log.h
@@ -32,6 +32,18 @@
 
 namespace ripple {
 
+// DEPRECATED use beast::Journal::Severity instead
+enum LogSeverity
+{
+    lsINVALID   = -1,   // used to indicate an invalid severity
+    lsTRACE     = 0,    // Very low-level progress information, details inside an operation
+    lsDEBUG     = 1,    // Function-level progress information, operations
+    lsINFO      = 2,    // Server-level progress information, major operations
+    lsWARNING   = 3,    // Conditions that warrant human attention, may indicate a problem
+    lsERROR     = 4,    // A condition that indicates a problem
+    lsFATAL     = 5     // A severe condition that indicates a server problem
+};
+
 /** Manages partitions for logging. */
 class Logs
 {
