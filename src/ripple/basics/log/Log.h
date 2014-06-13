62b24ad55fd04b25624d72e7dd1d1e00b9958bc9
Vinnie Falco <vinnie.falco@gmail.com>
2014-06-12 17:07:07 -0700

    [WIP] Add Logs interface and deprecatedLogs global

--- src/ripple/basics/log/Log.h
+++ src/ripple/basics/log/Log.h
@@ -20,11 +20,116 @@
 #ifndef RIPPLE_BASICS_LOG_H_INCLUDED
 #define RIPPLE_BASICS_LOG_H_INCLUDED
 
+#include <beast/utility/Journal.h>
 #include <ripple/basics/log/LogSeverity.h>
 #include <ripple/basics/log/LogSink.h>
 #include <ripple/basics/log/LogPartition.h>
+#include <mutex>
+#include <unordered_map>
+#include <utility>
 
 namespace ripple {
+
+/** Manages partitions for logging. */
+class Logs
+{
+private:
+    class Sink : public beast::Journal::Sink
+    {
+    private:
+        Logs& logs_;
+        std::string partition_;
+
+    public:
+        Sink (std::string const& partition, Logs& logs)
+            : logs_(logs)
+            , partition_(partition)
+        {
+        }
+
+        Sink (Sink const&) = delete;
+        Sink& operator= (Sink const&) = delete;
+
+        void
+        write (beast::Journal::Severity level, std::string const& text)
+        {
+            logs_.write (level, partition_, text);
+        }
+    };
+
+    std::mutex mutex_;
+    std::unordered_map <std::string, Sink> sinks_;
+
+public:
+    Logs()
+    {
+    }
+
+    Logs (Logs const&) = delete;
+    Logs& operator= (Logs const&) = delete;
+
+    Sink&
+    operator[] (std::string const& name)
+    {
+        std::lock_guard <std::mutex> lock (mutex_);
+        auto const result (sinks_.emplace(std::piecewise_construct,
+            std::forward_as_tuple(name), std::forward_as_tuple(name, *this)));
+        return result.first->second;
+    }
+
+    void
+    write (beast::Journal::Severity level, std::string const& partition,
+        std::string const& text)
+    {
+        std::lock_guard <std::mutex> lock (mutex_);
+        //
+    }
+
+    static
+    LogSeverity
+    fromSeverity (beast::Journal::Severity level)
+    {
+        using beast::Journal;
+        switch (level)
+        {
+        case Journal::kTrace:   return lsTRACE;
+        case Journal::kDebug:   return lsDEBUG;
+        case Journal::kInfo:    return lsINFO;
+        case Journal::kWarning: return lsWARNING;
+        case Journal::kError:   return lsERROR;
+
+        default:
+            bassertfalse;
+        case Journal::kFatal:
+            break;
+        }
+
+        return lsFATAL;
+    }
+
+    static
+    beast::Journal::Severity
+    toSeverity (LogSeverity level)
+    {
+        using beast::Journal;
+        switch (level)
+        {
+        case lsTRACE:   return Journal::kTrace;
+        case lsDEBUG:   return Journal::kDebug;
+        case lsINFO:    return Journal::kInfo;
+        case lsWARNING: return Journal::kWarning;
+        case lsERROR:   return Journal::kError;
+        default:
+            bassertfalse;
+        case lsFATAL:
+            break;
+        }
+
+        return Journal::kFatal;
+    }
+};
+
+//------------------------------------------------------------------------------
 
 /** RAII helper for writing to the LogSink. */
 class Log : public beast::Uncopyable
@@ -70,17 +175,27 @@ private:
     LogPartition* m_partition;
 };
 
-// Manually test for whether we should log
-//
-#define ShouldLog(s, k) (LogPartition::get <k> ().doLog (s))
+//------------------------------------------------------------------------------
+/*  DEPRECATED
+    Inject beast::Journal instead
+*/
 
-// Write to the log at the given severity level
-//
-#define WriteLog(s, k) if (!ShouldLog (s, k)) do {} while (0); else Log (s, LogPartition::get <k> ())
+// This is here temporarily until an interface is available to all clients
+inline
+Logs&
+deprecatedLogs()
+{
+    static Logs logs;
+    return logs;
+}
 
-// Write to the log conditionally
-//
-#define CondLog(c, s, k) if (!ShouldLog (s, k) || !(c)) do {} while(0); else Log(s, LogPartition::get <k> ())
+#define ShouldLog(s, k) deprecatedLogs()[#k].active(Logs::toSeverity(s))
+
+#define WriteLog(s, k) if (!ShouldLog (s, k)) do {} while (0); else \
+    beast::Journal::Stream(deprecatedLogs()[#k], Logs::toSeverity(s))
+
+#define CondLog(c, s, k) if (!ShouldLog (s, k) || !(c)) do {} while(0); else \
+    beast::Journal::Stream(deprecatedLogs()[#k], Logs::toSeverity(s))
 
 } // ripple
 
