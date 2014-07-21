ace53fa405a77dd451b6637ed351f8c1cbbe699e
Vinnie Falco <vinnie.falco@gmail.com>
2014-07-21 10:39:48 -0700

    Set version to 0.26.0-rc2

--- src/ripple/module/data/protocol/BuildInfo.cpp
+++ src/ripple/module/data/protocol/BuildInfo.cpp
@@ -31,7 +31,7 @@ char const* BuildInfo::getRawVersionString ()
     //
     //  The build version number (edit this for each release)
     //
-        "0.26.0-rc1"
+        "0.26.0-rc2"
     //
     //  Must follow the format described here:
     //
