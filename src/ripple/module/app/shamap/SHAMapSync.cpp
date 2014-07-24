96e1ec6d310a2b3a2a83bac00ec92541b8e39b68
Tom Ritchford <tom@swirly.com>
2014-07-24 12:17:18 -0400

    Fix build warnings and .gitignore.
    * Comment out unused local function for both clang and g++.
    * Get rid of numerous Boost warnings for clang.
    * Remove some unused local variables.
    * Put TAGS into the .gitignore.

--- src/ripple/module/app/shamap/SHAMapSync.cpp
+++ src/ripple/module/app/shamap/SHAMapSync.cpp
@@ -651,10 +651,14 @@ bool SHAMap::hasLeafNode (uint256 const& tag, uint256 const& targetNodeHash)
     return false; // If this was a matching leaf, we would have caught it already
 }
 
-static void addFPtoList (std::list<SHAMap::fetchPackEntry_t>& list, const uint256& hash, const Blob& blob)
+#if 0
+static
+void addFPtoList (std::list<SHAMap::fetchPackEntry_t>& list,
+                  const uint256& hash, const Blob& blob)
 {
     list.push_back (SHAMap::fetchPackEntry_t (hash, blob));
 }
+#endif
 
 /**
 @param have A pointer to the map that the recipient already has (if any).
