df95e0ed74076e317429dbad159595612351e30a
Miguel Portilla <miguelportilla@pobros.com>
2014-09-02 18:38:04 -0400

    Add date to tx command (RIPD-542)

--- src/ripple/module/rpc/handlers/Tx.cpp
+++ src/ripple/module/rpc/handlers/Tx.cpp
@@ -46,9 +46,9 @@ Json::Value doTx (RPC::Context& context)
 #ifdef READY_FOR_NEW_TX_FORMAT
         // TODO(tom): what new format is this?
         Json::Value ret;
-        ret[jss::transaction] = txn->getJson (0, binary);
+        ret[jss::transaction] = txn->getJson (1, binary);
 #else
-        Json::Value ret = txn->getJson (0, binary);
+        Json::Value ret = txn->getJson (1, binary);
 #endif
 
         if (txn->getLedger () != 0)
