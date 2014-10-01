b47611e1fe4d0b618caec9383632d58cfcacb0c8
Tom Ritchford <tom@swirly.com>
2014-09-30 20:11:23 -0400

    New class RPC::Status.

---
+++ src/ripple/rpc/Status.h
@@ -0,0 +1,112 @@
+//------------------------------------------------------------------------------
+/*
+    This file is part of rippled: https://github.com/ripple/rippled
+    Copyright (c) 2012, 2013 Ripple Labs Inc.
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose  with  or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE  SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH  REGARD  TO  THIS  SOFTWARE  INCLUDING  ALL  IMPLIED  WARRANTIES  OF
+    MERCHANTABILITY  AND  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY  SPECIAL ,  DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER  RESULTING  FROM  LOSS  OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION  OF  CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+//==============================================================================
+
+#ifndef RIPPLE_STATUS_H
+#define RIPPLE_STATUS_H
+
+#include <ripple/data/protocol/TER.h>
+#include <ripple/rpc/ErrorCodes.h>
+
+namespace ripple {
+namespace RPC {
+namespace New {
+
+/** Status represents the results of an operation that might fail.
+
+    It wraps the legacy codes TER and error_code_i, providing both a uniform
+    interface and a way to attach additional information to existing status
+    returns.
+
+    A Status can also be used to fill a Json::Value with a JSON-RPC 2.0
+    error response:  see http://www.jsonrpc.org/specification#error_object
+ */
+struct Status
+{
+public:
+    enum class Type {none, TER, error_code_i};
+    using Code = int;
+    using Strings = std::vector <std::string>;
+
+    static const Code OK = 0;
+
+    Status () = default;
+
+    Status (Code code, Strings d = {})
+            : type_ (Type::none), code_ (code), messages_ (std::move (d))
+    {
+    }
+
+    Status (TER ter, Strings d = {})
+            : type_ (Type::TER), code_ (ter), messages_ (std::move (d))
+    {
+    }
+
+    Status (error_code_i e, Strings d = {})
+            : type_ (Type::error_code_i), code_ (e), messages_ (std::move (d))
+    {
+    }
+
+    /* Returns a representation of the integer status Code as a string.
+       If the Status is OK, the result is an empty string.
+    */
+    std::string codeString () const;
+
+    /** Fill a Json::Value. If the Status is OK, fillJson has no effect. */
+    void fillJson(Json::Value&);
+
+    /** Returns true if the Status is *not* OK. */
+    operator bool() const
+    {
+        return code_ != OK;
+    }
+
+    /** Returns true if the Status is OK. */
+    bool operator !() const
+    {
+        return ! bool (*this);
+    }
+
+    operator TER() const
+    {
+        assert (type_ == Type::TER);
+        return TER (code_);
+    }
+
+    operator error_code_i() const
+    {
+        assert (type_ == Type::error_code_i);
+        return error_code_i (code_);
+    }
+
+    Strings const& messages() const
+    {
+        return messages_;
+    }
+
+private:
+    Type type_ = Type::none;
+    Code code_ = OK;
+    Strings messages_;
+};
+
+} // namespace New
+} // namespace RPC
+} // ripple
+
+#endif
---
+++ src/ripple/rpc/impl/Status.cpp
@@ -0,0 +1,77 @@
+//------------------------------------------------------------------------------
+/*
+    This file is part of rippled: https://github.com/ripple/rippled
+    Copyright (c) 2012, 2013 Ripple Labs Inc.
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose  with  or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE  SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH  REGARD  TO  THIS  SOFTWARE  INCLUDING  ALL  IMPLIED  WARRANTIES  OF
+    MERCHANTABILITY  AND  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY  SPECIAL ,  DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER  RESULTING  FROM  LOSS  OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION  OF  CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+//==============================================================================
+
+#include <ripple/rpc/Status.h>
+
+namespace ripple {
+namespace RPC {
+namespace New {
+
+std::string Status::codeString () const
+{
+    if (!*this)
+        return {};
+
+    if (type_ == Type::none)
+        return {std::to_string (code_)};
+
+    if (type_ == Status::Type::TER)
+    {
+        std::string s1, s2;
+
+        auto success = transResultInfo (TER (code_), s1, s2);
+        assert (success);
+        (void) success;
+
+        return s1 + ": " + s2;
+    }
+
+    if (type_ == Status::Type::error_code_i)
+    {
+        auto info = get_error_info (error_code_i (code_));
+        return info.token +  ": " + info.message;
+    }
+
+    assert (false);
+    return "";
+}
+
+void Status::fillJson (Json::Value& value)
+{
+    static const std::string separator = ": ";
+
+    if (!*this)
+        return;
+
+    auto& error = value[jss::error];
+    error[jss::code] = code_;
+    error[jss::message] = codeString ();
+
+    // Are there any more messages?
+    if (!messages_.empty ())
+    {
+        auto& messages = error[jss::data];
+        for (auto& i: messages_)
+            messages.append (i);
+    }
+}
+
+} // namespace New
+} // namespace RPC
+} // ripple
