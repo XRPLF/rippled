f2bb1c69c3f756f7fbae6567ae213b3f737b34d0
Vinnie Falco <vinnie.falco@gmail.com>
2014-10-28 07:55:07 -0700

    [FOLD] rename members

--- src/beast/beast/unit_test/amount.h
+++ src/beast/beast/unit_test/amount.h
@@ -31,15 +31,15 @@ namespace unit_test {
 class amount
 {
 private:
-    std::size_t n;
-    std::string const& what;
+    std::size_t n_;
+    std::string const& what_;
 
 public:
     amount (amount const&) = default;
     amount& operator= (amount const&) = delete;
 
     template <class = void>
-    amount (std::size_t n_, std::string const& what_);
+    amount (std::size_t n, std::string const& what);
 
     friend
     std::ostream&
@@ -47,9 +47,9 @@ public:
 };
 
 template <class>
-amount::amount (std::size_t n_, std::string const& what_)
-    : n (n_)
-    , what (what_)
+amount::amount (std::size_t n, std::string const& what)
+    : n_ (n)
+    , what_ (what)
 {
 }
 
@@ -57,7 +57,7 @@ inline
 std::ostream&
 operator<< (std::ostream& s, amount const& t)
 {
-    s << t.n << " " << t.what << ((t.n != 1) ? "s" : "");
+    s << t.n_ << " " << t.what_ << ((t.n_ != 1) ? "s" : "");
     return s;
 }
 
