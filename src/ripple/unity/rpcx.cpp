1ea62a899cf0b5e1e91c3bd23891fc5b7ef27881
Tom Ritchford <tom@swirly.com>
2014-11-10 12:47:08 -0500

    Wrap Output in a coroutine that periodically yields.

--- src/ripple/unity/rpcx.cpp
+++ src/ripple/unity/rpcx.cpp
@@ -30,13 +30,13 @@
 #include <ripple/overlay/Overlay.h>
 #include <tuple>
 
-#include <ripple/rpc/RPCHandler.h>
 #include <ripple/rpc/impl/ErrorCodes.cpp>
 #include <ripple/rpc/impl/JsonObject.cpp>
 #include <ripple/rpc/impl/JsonWriter.cpp>
 #include <ripple/rpc/impl/Manager.cpp>
 #include <ripple/rpc/impl/RPCHandler.cpp>
 #include <ripple/rpc/impl/Status.cpp>
+#include <ripple/rpc/impl/Yield.cpp>
 #include <ripple/rpc/impl/Status_test.cpp>
 
 #include <ripple/rpc/handlers/Handlers.h>
@@ -110,5 +110,9 @@
 #include <ripple/rpc/impl/ParseAccountIds.cpp>
 #include <ripple/rpc/impl/TransactionSign.cpp>
 
-#include <ripple/rpc/impl/JsonObject_test.cpp>
-#include <ripple/rpc/impl/JsonWriter_test.cpp>
+#include <ripple/rpc/impl/Coroutine.test.cpp>
+#include <ripple/rpc/impl/JsonObject.test.cpp>
+#include <ripple/rpc/impl/JsonWriter.test.cpp>
+#include <ripple/rpc/impl/Status.test.cpp>
+#include <ripple/rpc/impl/WriteJson.test.cpp>
+#include <ripple/rpc/impl/Yield.test.cpp>
