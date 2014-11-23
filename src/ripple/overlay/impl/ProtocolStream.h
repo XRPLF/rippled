4dcd35d19cdfd060a74f2368c79e0a8db068234b
Vinnie Falco <vinnie.falco@gmail.com>
2014-11-23 06:51:53 -0800

    [FOLD] de-inline and tidy

--- src/ripple/overlay/impl/ProtocolStream.h
+++ src/ripple/overlay/impl/ProtocolStream.h
@@ -42,6 +42,26 @@ private:
     std::vector <std::uint8_t> header_; // VFALCO TODO Use std::array
     std::vector <std::uint8_t> body_;
 
+public:
+    ProtocolStream()
+    {
+        header_.resize (Message::kHeaderBytes);
+    }
+
+    /** Write data to the protocol stream.
+        This may call the handler for up to one complete message.
+    */
+    /** @{ */
+    template <class ProtocolHandler>
+    boost::system::error_code
+    write (boost::asio::const_buffer buffer, ProtocolHandler& handler);
+
+    template <class ConstBufferSequence, class ProtocolHandler>
+    boost::system::error_code
+    write (ConstBufferSequence const& buffers, ProtocolHandler& handler);
+    /** @} */
+
+private:
     static
     boost::system::error_code
     parse_error()
@@ -52,119 +72,108 @@ private:
 
     template <class Message, class ProtocolHandler>
     boost::system::error_code
-    invoke (ProtocolHandler& handler)
+    invoke (ProtocolHandler& handler);
+};
+
+template <class ProtocolHandler>
+boost::system::error_code
+ProtocolStream::write (boost::asio::const_buffer buffer,
+    ProtocolHandler& handler)
+{
+    using namespace boost::asio;
+    boost::system::error_code ec;
+    std::size_t remain (buffer_size(buffer));
+    while (remain)
     {
-        boost::system::error_code ec;
-        std::shared_ptr <Message> const m (
-            std::make_shared <Message>());
-        bool const parsed (m->ParseFromArray (body_.data(), length_));
-        if (! parsed)
-            return parse_error();
-        ec = handler.on_message_begin (type_, m);
-        if (! ec)
+        if (header_bytes_ < header_.size())
         {
-            ec = handler.on_message (m);
-            handler.on_message_end (type_, m);
-        }
-        return ec;
-    }
-
-public:
-    ProtocolStream ()
-    {
-        header_.resize (Message::kHeaderBytes);
-    }
-
-    /** Push a single buffer through.
-        The handler is called for each complete protocol message contained
-        in the buffer.
-    */
-    template <class ProtocolHandler>
-    boost::system::error_code
-    write_one (boost::asio::const_buffer buffer,
-        ProtocolHandler& handler)
-    {
-        using namespace boost::asio;
-        boost::system::error_code ec;
-        std::size_t remain (buffer_size(buffer));
-        while (remain)
-        {
-            if (header_bytes_ < header_.size())
-            {
-                std::size_t const n (buffer_copy (
-                    mutable_buffer (header_.data() + header_bytes_,
-                        header_.size() - header_bytes_), buffer));
-                header_bytes_ += n;
-                buffer = buffer + n;
-                remain = remain - n;
-                if (header_bytes_ >= header_.size())
-                {
-                    assert (header_bytes_ == header_.size());
-                    length_ = Message::getLength (header_);
-                    type_ = Message::getType (header_);
-                    body_.resize (length_);
-                }
-            }
-
+            std::size_t const n (buffer_copy (
+                mutable_buffer (header_.data() + header_bytes_,
+                    header_.size() - header_bytes_), buffer));
+            header_bytes_ += n;
+            buffer = buffer + n;
+            remain = remain - n;
             if (header_bytes_ >= header_.size())
             {
-                std::size_t const n (buffer_copy (mutable_buffer (body_.data() +
-                    body_bytes_, body_.size() - body_bytes_), buffer));
-                body_bytes_ += n;
-                buffer = buffer + n;
-                remain = remain - n;
-                if (body_bytes_ >= length_)
-                {
-                    assert (body_bytes_ == length_);
-                    switch (type_)
-                    {
-                    case protocol::mtHELLO:           ec = invoke <protocol::TMHello> (handler); break;
-                    case protocol::mtPING:            ec = invoke <protocol::TMPing> (handler); break;
-                    case protocol::mtPROOFOFWORK:     ec = invoke <protocol::TMProofWork> (handler); break;
-                    case protocol::mtCLUSTER:         ec = invoke <protocol::TMCluster> (handler); break;
-                    case protocol::mtGET_PEERS:       ec = invoke <protocol::TMGetPeers> (handler); break;
-                    case protocol::mtPEERS:           ec = invoke <protocol::TMPeers> (handler); break;
-                    case protocol::mtENDPOINTS:       ec = invoke <protocol::TMEndpoints> (handler); break;
-                    case protocol::mtTRANSACTION:     ec = invoke <protocol::TMTransaction> (handler); break;
-                    case protocol::mtGET_LEDGER:      ec = invoke <protocol::TMGetLedger> (handler); break;
-                    case protocol::mtLEDGER_DATA:     ec = invoke <protocol::TMLedgerData> (handler); break;
-                    case protocol::mtPROPOSE_LEDGER:  ec = invoke <protocol::TMProposeSet> (handler); break;
-                    case protocol::mtSTATUS_CHANGE:   ec = invoke <protocol::TMStatusChange> (handler); break;
-                    case protocol::mtHAVE_SET:        ec = invoke <protocol::TMHaveTransactionSet> (handler); break;
-                    case protocol::mtVALIDATION:      ec = invoke <protocol::TMValidation> (handler); break;
-                    case protocol::mtGET_OBJECTS:     ec = invoke <protocol::TMGetObjectByHash> (handler); break;
-                    default:
-                        ec = handler.on_message_unknown(type_);
-                        break;
-                    }
-                    header_bytes_ = 0;
-                    body_bytes_ = 0;
-                }
+                assert (header_bytes_ == header_.size());
+                length_ = Message::getLength (header_);
+                type_ = Message::getType (header_);
+                body_.resize (length_);
             }
         }
-        return ec;
-    }
 
-    /** Push a set of buffers through.
-        The handler is called for each complete protocol message contained
-        in the buffers.
-    */
-    template <class ConstBufferSequence,
-        class ProtocolHandler>
-    boost::system::error_code
-    write (ConstBufferSequence const& buffers,
-        ProtocolHandler& handler)
-    {
-        boost::system::error_code ec;
-        for (auto const& buffer : buffers)
+        if (header_bytes_ >= header_.size())
         {
-            ec = write_one(buffer, handler);
-            if (ec)
-                break;
+            std::size_t const n (buffer_copy (mutable_buffer (body_.data() +
+                body_bytes_, body_.size() - body_bytes_), buffer));
+            body_bytes_ += n;
+            buffer = buffer + n;
+            remain = remain - n;
+            if (body_bytes_ >= length_)
+            {
+                assert (body_bytes_ == length_);
+                switch (type_)
+                {
+                case protocol::mtHELLO:           ec = invoke <protocol::TMHello> (handler); break;
+                case protocol::mtPING:            ec = invoke <protocol::TMPing> (handler); break;
+                case protocol::mtPROOFOFWORK:     ec = invoke <protocol::TMProofWork> (handler); break;
+                case protocol::mtCLUSTER:         ec = invoke <protocol::TMCluster> (handler); break;
+                case protocol::mtGET_PEERS:       ec = invoke <protocol::TMGetPeers> (handler); break;
+                case protocol::mtPEERS:           ec = invoke <protocol::TMPeers> (handler); break;
+                case protocol::mtENDPOINTS:       ec = invoke <protocol::TMEndpoints> (handler); break;
+                case protocol::mtTRANSACTION:     ec = invoke <protocol::TMTransaction> (handler); break;
+                case protocol::mtGET_LEDGER:      ec = invoke <protocol::TMGetLedger> (handler); break;
+                case protocol::mtLEDGER_DATA:     ec = invoke <protocol::TMLedgerData> (handler); break;
+                case protocol::mtPROPOSE_LEDGER:  ec = invoke <protocol::TMProposeSet> (handler); break;
+                case protocol::mtSTATUS_CHANGE:   ec = invoke <protocol::TMStatusChange> (handler); break;
+                case protocol::mtHAVE_SET:        ec = invoke <protocol::TMHaveTransactionSet> (handler); break;
+                case protocol::mtVALIDATION:      ec = invoke <protocol::TMValidation> (handler); break;
+                case protocol::mtGET_OBJECTS:     ec = invoke <protocol::TMGetObjectByHash> (handler); break;
+                default:
+                    ec = handler.on_message_unknown(type_);
+                    break;
+                }
+                header_bytes_ = 0;
+                body_bytes_ = 0;
+            }
         }
-        return ec;
     }
-};
+    return ec;
+}
+
+template <class ConstBufferSequence, class ProtocolHandler>
+boost::system::error_code
+ProtocolStream::write (ConstBufferSequence const& buffers,
+    ProtocolHandler& handler)
+{
+    boost::system::error_code ec;
+    for (auto const& buffer : buffers)
+    {
+        ec = write(buffer, handler);
+        if (ec)
+            break;
+    }
+    return ec;
+}
+
+template <class Message, class ProtocolHandler>
+boost::system::error_code
+ProtocolStream::invoke (ProtocolHandler& handler)
+{
+    boost::system::error_code ec;
+    std::shared_ptr <Message> const m (
+        std::make_shared <Message>());
+    bool const parsed (m->ParseFromArray (body_.data(), length_));
+    if (! parsed)
+        return parse_error();
+    ec = handler.on_message_begin (type_, m);
+    if (! ec)
+    {
+        ec = handler.on_message (m);
+        handler.on_message_end (type_, m);
+    }
+    return ec;
+}
 
 } // ripple
 
