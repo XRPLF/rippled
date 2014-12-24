0102e073259138516cc4259f25562c9bccfaf9d6
Nik Bougalis <nikb@bougalis.net>
2014-12-23 19:04:15 -0800

    [FOLD] Remove ServiceQueue

--- src/beast/beast/Threads.h
+++ src/beast/beast/Threads.h
@@ -25,7 +25,6 @@
 #include <beast/threads/SharedLockGuard.h>
 #include <beast/threads/SharedMutexAdapter.h>
 #include <beast/threads/SharedData.h>
-#include <beast/threads/ServiceQueue.h>
 #include <beast/threads/SpinLock.h>
 #include <beast/threads/Stoppable.h>
 #include <beast/threads/Thread.h>
