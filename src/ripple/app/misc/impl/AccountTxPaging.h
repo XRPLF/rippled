fcd37dd49c5b4b55b4a1c36c52d9d2ef92c213dc
Nicholas Dudfield <ndudfield@gmail.com>
2015-01-09 00:52:31 +0700

    Improve transaction enumeration in account_tx (RIPD-734):
    * Don't return the same txn more than once
    * Improve resumption
    * Add unit tests
    * Cleanup

--- src/ripple/app/misc/impl/AccountTxPaging.h
+++ src/ripple/app/misc/impl/AccountTxPaging.h
@@ -17,13 +17,47 @@
 */
 //==============================================================================
 
-#include <BeastConfig.h>
+#ifndef RIPPLE_APP_MISC_IMPL_ACCOUNTTXPAGING_H_INCLUDED
+#define RIPPLE_APP_MISC_IMPL_ACCOUNTTXPAGING_H_INCLUDED
 
-#include <ripple/app/ledger/InboundLedgers.cpp>
-#include <ripple/app/ledger/LedgerHistory.cpp>
-#include <ripple/app/tx/TransactionAcquire.cpp>
-#include <ripple/app/tx/LocalTxs.cpp>
-#include <ripple/app/tx/InboundTransactions.cpp>
-#include <ripple/app/misc/NetworkOPs.cpp>
-#include <ripple/app/misc/impl/AccountTxPaging.cpp>
-#include <ripple/app/misc/tests/AccountTxPaging.test.cpp>
+#include <ripple/app/misc/NetworkOPs.h>
+#include <cstdint>
+#include <string>
+#include <utility>
+
+
+//------------------------------------------------------------------------------
+
+namespace ripple {
+
+void
+convertBlobsToTxResult (
+    NetworkOPs::AccountTxs& to,
+    std::uint32_t ledger_index,
+    std::string const& status,
+    Blob const& rawTxn,
+    Blob const& rawMeta);
+
+void
+saveLedgerAsync (std::uint32_t seq);
+
+void
+accountTxPage (
+    DatabaseCon& database,
+    std::function<void (std::uint32_t)> const& onUnsavedLedger,
+    std::function<void (std::uint32_t,
+                        std::string const&,
+                        Blob const&,
+                        Blob const&)> const&,
+    RippleAddress const& account,
+    std::int32_t minLedger,
+    std::int32_t maxLedger,
+    bool forward,
+    Json::Value& token,
+    int limit,
+    bool bAdmin,
+    std::uint32_t pageLength);
+
+}
+
+#endif
