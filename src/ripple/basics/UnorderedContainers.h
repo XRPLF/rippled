92ac701a15be8b2211484ce7e186506f28fdc83d
Vinnie Falco <vinnie.falco@gmail.com>
2015-01-16 11:19:45 -0800

    Make xxhasher the default hardened_hash hash function

--- src/ripple/basics/UnorderedContainers.h
+++ src/ripple/basics/UnorderedContainers.h
@@ -22,6 +22,7 @@
 
 #include <beast/container/hardened_hash.h>
 #include <beast/container/hash_append.h>
+#include <beast/container/xxhasher.h>
 #include <unordered_map>
 #include <unordered_set>
 
@@ -63,22 +64,24 @@ using hash_multiset = std::unordered_multiset <Value, Hash, Pred, Allocator>;
 
 // hardened_hash containers
 
-template <class Key, class Value, class Hash = beast::hardened_hash<>,
+using strong_hash = beast::xxhasher;
+
+template <class Key, class Value, class Hash = beast::hardened_hash<strong_hash>,
           class Pred = std::equal_to<Key>,
           class Allocator = std::allocator<std::pair<Key const, Value>>>
 using hardened_hash_map = std::unordered_map <Key, Value, Hash, Pred, Allocator>;
 
-template <class Key, class Value, class Hash = beast::hardened_hash<>,
+template <class Key, class Value, class Hash = beast::hardened_hash<strong_hash>,
           class Pred = std::equal_to<Key>,
           class Allocator = std::allocator<std::pair<Key const, Value>>>
 using hardened_hash_multimap = std::unordered_multimap <Key, Value, Hash, Pred, Allocator>;
 
-template <class Value, class Hash = beast::hardened_hash<>,
+template <class Value, class Hash = beast::hardened_hash<strong_hash>,
           class Pred = std::equal_to<Value>,
           class Allocator = std::allocator<Value>>
 using hardened_hash_set = std::unordered_set <Value, Hash, Pred, Allocator>;
 
-template <class Value, class Hash = beast::hardened_hash<>,
+template <class Value, class Hash = beast::hardened_hash<strong_hash>,
           class Pred = std::equal_to<Value>,
           class Allocator = std::allocator<Value>>
 using hardened_hash_multiset = std::unordered_multiset <Value, Hash, Pred, Allocator>;
