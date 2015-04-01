091ff0cce01f68789bbe99b33bbe4df5878774b7
Tom Ritchford <tom@swirly.com>
2015-03-31 21:50:45 -0400

    Set version to 0.28.0-b18

--- src/ripple/protocol/impl/BuildInfo.cpp
+++ src/ripple/protocol/impl/BuildInfo.cpp
@@ -35,7 +35,7 @@ char const* getRawVersionString ()
     //
     //  The build version number (edit this for each release)
     //
-        "0.28.0-b17"
+        "0.28.0-b18"
     //
     //  Must follow the format described here:
     //
