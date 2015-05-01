25ad97554306cb837b675cc942e13e3e16ed665a
crazyquark <sam.bitcoin@gmail.com>
2015-05-01 07:17:51 +0300

    Minor cleanup

--- Builds/EclipseCDT/install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cd ../..
-for file in $(ls -a Builds/EclipseCDT|egrep "^\.\w+"); do rm -rf $file; done;
-for file in $(ls -a Builds/EclipseCDT|egrep "^\.\w+"); do ln -s Builds/EclipseCDT/$file $file; done;
