0187c6a5a1c81d0b168956d319dc99f40f04db28
Nik Bougalis <nikb@bougalis.net>
2015-05-13 09:08:05 -0700

    Set version to 0.28.0-hf2

--- src/ripple/protocol/impl/BuildInfo.cpp
+++ src/ripple/protocol/impl/BuildInfo.cpp
@@ -35,7 +35,7 @@ char const* getRawVersionString ()
     //
     //  The build version number (edit this for each release)
     //
-        "0.28.0"
+        "0.28.0-hf2"
     //
     //  Must follow the format described here:
     //
