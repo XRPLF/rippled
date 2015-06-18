7d329570f4f1599cf6586273b6bf8025d44ab9a9
Vinnie Falco <vinnie.falco@gmail.com>
2015-06-18 13:56:54 -0700

    Set version to 0.28.2-b10

--- src/ripple/protocol/impl/BuildInfo.cpp
+++ src/ripple/protocol/impl/BuildInfo.cpp
@@ -35,7 +35,7 @@ char const* getRawVersionString ()
     //
     //  The build version number (edit this for each release)
     //
-        "0.28.2-b9"
+        "0.28.2-b10"
     //
     //  Must follow the format described here:
     //
