b8f2fdb6ac079b85f5817b7975656df0499b8223
Nik Bougalis <nikb@bougalis.net>
2015-07-31 19:19:14 -0700

    Remove obsolete DHUtil

--- src/ripple/unity/crypto.cpp
+++ src/ripple/unity/crypto.cpp
@@ -22,7 +22,6 @@
 #include <ripple/crypto/impl/Base58.cpp>
 #include <ripple/crypto/impl/Base58Data.cpp>
 #include <ripple/crypto/impl/CBigNum.cpp>
-#include <ripple/crypto/impl/DHUtil.cpp>
 #include <ripple/crypto/impl/ec_key.cpp>
 #include <ripple/crypto/impl/ECDSA.cpp>
 #include <ripple/crypto/impl/ECDSACanonical.cpp>
