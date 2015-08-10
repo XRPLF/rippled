02e6bc0b425bf0c833710d84308352fc8f655c9d
Tom Ritchford <tom@swirly.com>
2015-08-10 16:26:43 -0400

    Bring some constants into RPC::Tuning.h.

--- src/ripple/rpc/impl/Tuning.h
+++ src/ripple/rpc/impl/Tuning.h
@@ -30,54 +30,66 @@ namespace Tuning {
 /** Default account objects returned per request from the
     account_objects command when no limit param is specified
 */
-static unsigned int const defaultObjectsPerRequest (200);
+static unsigned int const defaultObjectsPerRequest = 200;
 
 /** Minimum account objects returned per request from the
     account_objects command. Specified in the limit param.
 */
-static unsigned int const minObjectsPerRequest (10);
+static unsigned int const minObjectsPerRequest = 10;
 
 /** Maximum account objects returned per request from the
     account_objects command. Specified in the limit param.
 */
-static unsigned int const maxObjectsPerRequest (400);
+static unsigned int const maxObjectsPerRequest = 400;
 
 /** Default account lines returned per request from the
     account_lines command when no limit param is specified
 */
-static unsigned int const defaultLinesPerRequest (200);
+static unsigned int const defaultLinesPerRequest = 200;
 
 /** Minimum account lines returned per request from the
     account_lines command. Specified in the limit param.
 */
-static unsigned int const minLinesPerRequest (10);
+static unsigned int const minLinesPerRequest = 10;
 
 /** Maximum account lines returned per request from the
     account_lines command. Specified in the limit param.
 */
-static unsigned int const maxLinesPerRequest (400);
+static unsigned int const maxLinesPerRequest = 400;
 
 /** Default offers returned per request from the
     account_offers command when no limit param is specified.
 */
-static unsigned int const defaultOffersPerRequest (200);
+static unsigned int const defaultOffersPerRequest = 200;
 
 /** Minimum offers returned per request from the
     account_offers command. Specified in the limit param.
 */
-static unsigned int const minOffersPerRequest (10);
+static unsigned int const minOffersPerRequest = 10;
 
 /** Maximum offers returned per request from the
     account_lines command. Specified in the limit param.
 */
-static unsigned int const maxOffersPerRequest (400);
+static unsigned int const maxOffersPerRequest = 400;
 
-static int const defaultAutoFillFeeMultiplier (10);
-static int const maxPathfindsInProgress (2);
-static int const maxPathfindJobCount (50);
-static int const maxJobQueueClients (500);
-static int const maxValidatedLedgerAge (120);
-static int const maxRequestSize (1000000);
+static int const defaultAutoFillFeeMultiplier = 10;
+static int const maxPathfindsInProgress = 2;
+static int const maxPathfindJobCount = 50;
+static int const maxJobQueueClients = 500;
+static int const maxValidatedLedgerAge = 120;
+static int const maxRequestSize = 1000000;
+
+/** Maximum number of pages in one response from a binary LedgerData request. */
+static int const binaryPageLength = 2048;
+
+/** Maximum number of pages in one response from a Json LedgerData request. */
+static int const jsonPageLength = 256;
+
+/** Maximum number of pages in a LedgerData response. */
+inline int pageLength(bool isBinary)
+{
+    return isBinary ? binaryPageLength : jsonPageLength;
+}
 
 } // Tuning
 /** @} */
