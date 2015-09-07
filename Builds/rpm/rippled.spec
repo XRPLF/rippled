0f05ebd83481056e7b3928ee74fbe25baacc62cc
Vinnie Falco <vinnie.falco@gmail.com>
2015-09-07 14:05:27 -0700

    Set version to 0.29.1-b8

--- Builds/rpm/rippled.spec
+++ Builds/rpm/rippled.spec
@@ -1,5 +1,5 @@
 Name:           rippled
-Version:        0.29.1-b7
+Version:        0.29.1-b8
 Release:        1%{?dist}
 Summary:        Ripple peer-to-peer network daemon
 
--- src/ripple/protocol/impl/BuildInfo.cpp
+++ src/ripple/protocol/impl/BuildInfo.cpp
@@ -35,7 +35,7 @@ char const* getRawVersionString ()
     //
     //  The build version number (edit this for each release)
     //
-        "0.29.1-b7"
+        "0.29.1-b8"
     //
     //  Must follow the format described here:
     //
