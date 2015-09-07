6a55f99edec9fa23aed532df33026fae54f3f9c5
Nik Bougalis <nikb@bougalis.net>
2015-09-07 09:17:06 -0700

    Set version to 0.29.0-hf1

--- Builds/rpm/rippled.spec
+++ Builds/rpm/rippled.spec
@@ -1,5 +1,5 @@
 Name:           rippled
-Version:        0.29.0
+Version:        0.29.0-hf1
 Release:        1%{?dist}
 Summary:        Ripple peer-to-peer network daemon
 
--- src/ripple/protocol/impl/BuildInfo.cpp
+++ src/ripple/protocol/impl/BuildInfo.cpp
@@ -35,7 +35,7 @@ char const* getRawVersionString ()
     //
     //  The build version number (edit this for each release)
     //
-        "0.29.0"
+        "0.29.0-hf1"
     //
     //  Must follow the format described here:
     //
