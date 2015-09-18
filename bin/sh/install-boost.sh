6a8d24372ea33c35facfd4a329e3946b43309c01
seelabs <scott.determan@yahoo.com>
2015-09-18 15:49:37 -0400

    Fix Travis compile

--- bin/sh/install-boost.sh
+++ bin/sh/install-boost.sh
@@ -4,16 +4,18 @@
 # and contain valid values.
 # 2) The last namepart of BOOST_ROOT matches the
 # folder name internal to boost's .tar.gz
+# When testing you can force a boost build by clearing travis caches:
+# https://travis-ci.org/ripple/rippled/caches
 set -e
 if [ ! -d "$BOOST_ROOT/lib" ]
 then
   wget $BOOST_URL -O /tmp/boost.tar.gz
   cd `dirname $BOOST_ROOT`
+  rm -fr ${BOOST_ROOT}
   tar xzf /tmp/boost.tar.gz
   cd $BOOST_ROOT && \
     ./bootstrap.sh --prefix=$BOOST_ROOT && \
-    ./b2 -d1 define=-D_GLIBCXX_USE_CXX11_ABI=0 && ./b2 -d0 define=-D_GLIBCXX_USE_CXX11_ABI=0 install
-  define=-D_GLIBCXX_USE_CXX11_ABI=0
+    ./b2 -d1 define=_GLIBCXX_USE_CXX11_ABI=0 && ./b2 -d0 define=_GLIBCXX_USE_CXX11_ABI=0 install
 else
   echo "Using cached boost at $BOOST_ROOT"
 fi
