206cd3b529d56a32369fc66fbdb027f69fe3a62d
JoelKatz <DavidJoelSchwartz@GMail.com>
2015-09-23 01:26:23 -0700

    IPAddressV4 fixes: * Loopback addresses are not publicly routable * The Internet is not classful

--- beast/net/impl/IPAddressV4.cpp
+++ beast/net/impl/IPAddressV4.cpp
@@ -147,8 +147,7 @@ bool is_public (AddressV4 const& addr)
 {
     return
         ! is_private (addr) &&
-        ! is_multicast (addr) &&
-        (addr != AddressV4::broadcast (addr));
+        ! is_multicast (addr);
 }
 
 //------------------------------------------------------------------------------
