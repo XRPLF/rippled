f6f4452231b60a67995ecd5893978539161c9eb7
Nik Bougalis <nikb@bougalis.net>
2015-12-01 05:49:35 -0800

    Set version to 0.30.1-b9

--- src/ripple/protocol/impl/BuildInfo.cpp
+++ src/ripple/protocol/impl/BuildInfo.cpp
@@ -35,7 +35,7 @@ char const* getRawVersionString ()
     //
     //  The build version number (edit this for each release)
     //
-        "0.30.1-b8"
+        "0.30.1-b9"
     //
     //  Must follow the format described here:
     //
