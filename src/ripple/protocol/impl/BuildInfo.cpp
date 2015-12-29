39b95903e36d32853c6e9ce5b6ee3d099c35ca11
Vinnie Falco <vinnie.falco@gmail.com>
2015-12-29 12:43:36 -0500

    Set version to 0.30.1-b13

--- src/ripple/protocol/impl/BuildInfo.cpp
+++ src/ripple/protocol/impl/BuildInfo.cpp
@@ -35,7 +35,7 @@ char const* getRawVersionString ()
     //
     //  The build version number (edit this for each release)
     //
-        "0.30.1-b12"
+        "0.30.1-b13"
     //
     //  Must follow the format described here:
     //
