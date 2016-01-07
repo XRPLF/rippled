457ad333b20b9cc1430727672ec478027891e0e3
Miguel Portilla <miguelportilla@pobros.com>
2016-01-07 13:17:54 -0500

    Websocket dispatch fix

--- src/ripple/websocket/Handler.h
+++ src/ripple/websocket/Handler.h
@@ -301,11 +301,14 @@ public:
             [ptr] (Job&) { ConnectionImpl <WebSocket>::destroy(ptr); });
     }
 
-    void message_job(std::string const& name,
-                     connection_ptr const& cpClient)
+    void message_job(std::string const& name, connection_ptr const& cpClient)
     {
-        auto msgs = [this, cpClient] (Job& j) { do_messages(j, cpClient); };
-        app_.getJobQueue ().addJob (jtCLIENT, "WSClient::" + name, msgs);
+        app_.getJobQueue().postCoro(jtCLIENT, "WSClient",
+            [this, cpClient]
+            (auto const& jc)
+            {
+                this->do_messages(jc, cpClient);
+            });
     }
 
     void on_message (connection_ptr cpClient, message_ptr mpMessage) override
@@ -342,7 +345,8 @@ public:
             message_job("command", cpClient);
     }
 
-    void do_messages (Job& job, connection_ptr const& cpClient)
+    void do_messages (std::shared_ptr<JobCoro> const& jc,
+        connection_ptr const& cpClient)
     {
         wsc_ptr ptr;
         {
@@ -362,23 +366,20 @@ public:
         for (int i = 0; i < 3; ++i)
         {
             message_ptr msg = ptr->getMessage ();
-
-            if (!msg)
+            if (! msg)
                 return;
 
-            if (!do_message (job, cpClient, ptr, msg))
-            {
-                ptr->returnMessage(msg);
-                return;
-            }
+            do_message(jc, cpClient, ptr, msg);
         }
 
         if (ptr->checkMessage ())
             message_job("more", cpClient);
     }
 
-    bool do_message (Job& job, const connection_ptr cpClient,
-                     wsc_ptr conn, const message_ptr& mpMessage)
+    void
+    do_message (std::shared_ptr<JobCoro> const& jc,
+        const connection_ptr cpClient, wsc_ptr conn,
+            const message_ptr& mpMessage)
     {
         Json::Value jvRequest;
         Json::Reader jrReader;
@@ -417,33 +418,19 @@ public:
         }
         else
         {
-            if (jvRequest.isMember (jss::command))
-            {
-                Json::Value& jCmd = jvRequest[jss::command];
-                if (jCmd.isString())
-                    job.rename ("WSClient::" + jCmd.asString());
-            }
-
-            app_.getJobQueue().postCoro(jtCLIENT, "WSClient",
-                [this, conn, cpClient, jvRequest = std::move(jvRequest)]
-                (std::shared_ptr<JobCoro> jc)
-                {
-                    using namespace std::chrono;
-                    auto const start = high_resolution_clock::now();
-                    auto buffer = to_string(conn->invokeCommand(jvRequest, jc));
-                    rpc_time_.notify (
-                        static_cast <beast::insight::Event::value_type> (
-                            duration_cast <milliseconds> (
-                                high_resolution_clock::now () - start)));
-                    ++rpc_requests_;
-                    rpc_size_.notify (
-                        static_cast <beast::insight::Event::value_type>
-                            (buffer.size()));
-                    send (cpClient, buffer, false);
-                });
+            using namespace std::chrono;
+            auto const start = high_resolution_clock::now();
+            auto buffer = to_string(conn->invokeCommand(jvRequest, jc));
+            rpc_time_.notify (
+                static_cast <beast::insight::Event::value_type> (
+                    duration_cast <milliseconds> (
+                        high_resolution_clock::now () - start)));
+            ++rpc_requests_;
+            rpc_size_.notify (
+                static_cast <beast::insight::Event::value_type>
+                    (buffer.size()));
+            send (cpClient, buffer, false);
         }
-
-        return true;
     }
 
     boost::asio::ssl::context&
