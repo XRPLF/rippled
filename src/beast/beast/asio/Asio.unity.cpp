477118a8df6b7cc79e07e1706154add461cc5529
Vinnie Falco <vinnie.falco@gmail.com>
2016-03-12 10:31:40 -0500

    Add append_buffers

--- src/beast/beast/asio/Asio.unity.cpp
+++ src/beast/beast/asio/Asio.unity.cpp
@@ -23,6 +23,7 @@
 
 #include <beast/asio/impl/IPAddressConversion.cpp>
 #include <beast/asio/impl/error.cpp>
+#include <beast/asio/tests/append_buffers_test.cpp>
 #include <beast/asio/tests/bind_handler.test.cpp>
 #include <beast/asio/tests/streambuf.test.cpp>
 #include <beast/asio/tests/error_test.cpp>
