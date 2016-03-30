73ddda4651218abcf18ed31d9d11750739084469
Vinnie Falco <vinnie.falco@gmail.com>
2016-03-30 09:16:06 -0400

    C++17 compatibility:
    * std::void_t
    * std::bool_constant

--- src/ripple/beast/container/detail/aged_ordered_container.h
+++ src/ripple/beast/container/detail/aged_ordered_container.h
@@ -25,8 +25,6 @@
 #include <ripple/beast/container/aged_container.h>
 #include <beast/clock/abstract_clock.h>
 #include <beast/empty_base_optimization.h>
-#include <beast/empty_base_optimization.h>
-#include <beast/cxx14/type_traits.h>
 #include <boost/intrusive/list.hpp>
 #include <boost/intrusive/set.hpp>
 #include <boost/version.hpp>
@@ -35,7 +33,7 @@
 #include <initializer_list>
 #include <iterator>
 #include <memory>
-#include <type_traits>
+#include <beast/cxx17/type_traits.h> // <type_traits>
 #include <utility>
 
 namespace beast {
