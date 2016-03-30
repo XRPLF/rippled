ff907cb4175adce20259a25614c2ab1df1e77fab
Vinnie Falco <vinnie.falco@gmail.com>
2016-03-30 10:52:02 -0400

    Add WebSocket implementation:
    Beast.WSProto provides developers with a robust WebSocket
    implementation built on Boost.Asio with a consistent asynchronous
    model using a modern C++ approach.
    
    See <beast/wsproto/README.md> for details

---
+++ src/beast/beast/wsproto/detail/frame.h
@@ -0,0 +1,378 @@
+//------------------------------------------------------------------------------
+/*
+    This file is part of Beast: https://github.com/vinniefalco/Beast
+    Copyright 2013, Vinnie Falco <vinnie.falco@gmail.com>
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose  with  or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE  SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH  REGARD  TO  THIS  SOFTWARE  INCLUDING  ALL  IMPLIED  WARRANTIES  OF
+    MERCHANTABILITY  AND  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY  SPECIAL ,  DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER  RESULTING  FROM  LOSS  OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION  OF  CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+//==============================================================================
+
+#ifndef BEAST_WSPROTO_FRAME_H_INCLUDED
+#define BEAST_WSPROTO_FRAME_H_INCLUDED
+
+#include <beast/wsproto/rfc6455.h>
+#include <beast/wsproto/static_string.h>
+#include <beast/wsproto/detail/utf8_checker.h>
+#include <beast/asio/consuming_buffers.h>
+#include <beast/asio/static_streambuf.h>
+#include <boost/asio/buffer.hpp>
+#include <boost/endian/buffers.hpp>
+#include <cassert>
+#include <cstdint>
+
+namespace beast {
+namespace wsproto {
+namespace detail {
+
+// Contents of a WebSocket frame header
+struct frame_header
+{
+    opcode::value op;
+    bool fin;
+    bool mask;
+    bool rsv1;
+    bool rsv2;
+    bool rsv3;
+    std::uint64_t len;
+    std::uint32_t key;
+};
+
+// holds the largest possible frame header
+using fh_streambuf =
+    static_streambuf_n<14>;
+
+// holds the largest possible control frame
+using frame_streambuf =
+    static_streambuf_n<139>; // 14 + 125
+
+inline
+bool constexpr
+is_reserved(opcode::value op)
+{
+    return
+        (op >= opcode::rsv3  && op <= opcode::rsv7) ||
+        (op >= opcode::crsvb && op <= opcode::crsvf);
+}
+
+inline
+bool constexpr
+is_valid(opcode::value op)
+{
+    return op >= 0 && op <= opcode::crsvf;
+}
+
+inline
+bool constexpr
+is_control(opcode::value op)
+{
+    return op >= opcode::close;
+}
+
+// Returns `true` if a close code is valid
+// Note this accepts 0 as a valid close code.
+inline
+bool
+is_valid(close::value code)
+{
+    auto const v = static_cast<
+        std::uint16_t>(code);
+    switch(v)
+    {
+    case 1000:
+    case 1001:
+    case 1002:
+    case 1003:
+    case 1007:
+    case 1008:
+    case 1009:
+    case 1010:
+    case 1011:
+    case 1012:
+    case 1013:
+        return true;
+
+    // explicitly reserved
+    case 1004:
+    case 1005:
+    case 1006:
+    case 1014:
+    case 1015:
+        return false;
+    }
+    // reserved
+    if(v >= 1016 && v <= 2999)
+        return false;
+    // not used
+    if(v >= 0 && v <= 999)
+        return false;
+    return true;
+}
+
+//------------------------------------------------------------------------------
+
+// Write frame header to streambuf
+//
+template<class Streambuf>
+void
+write(Streambuf& sb, frame_header const& fh)
+{
+    using boost::asio::buffer;
+    using boost::asio::buffer_copy;
+    using namespace boost::endian;
+    std::size_t n;
+    std::uint8_t b[14];
+    b[0] = (fh.fin ? 0x80 : 0x00) | fh.op;
+    b[1] = fh.mask ? 0x80 : 0x00;
+    if (fh.len <= 125)
+    {
+        b[1] |= fh.len;
+        n = 2;
+    }
+    else if (fh.len <= 65535)
+    {
+        b[1] |= 126;
+        ::new(&b[2]) big_uint16_buf_t{
+            (std::uint16_t)fh.len};
+        n = 4;
+    }
+    else
+    {
+        b[1] |= 127;
+        ::new(&b[2]) big_uint64_buf_t{fh.len};
+        n = 10;
+    }
+    if(fh.mask)
+    {
+        little_uint32_buf_t key(fh.key);
+        std::copy(key.data(),
+            key.data() + 4, &b[n]);
+        n += 4;
+    }
+    sb.commit(buffer_copy(
+        sb.prepare(n), buffer(b)));
+}
+
+// Read fixed frame header
+// Requires at least 2 bytes
+//
+template<class Streambuf>
+std::size_t
+read_fh1(frame_header& fh, Streambuf& sb,
+    role_type role, close::value& code)
+{
+    using boost::asio::buffer;
+    using boost::asio::buffer_copy;
+    using boost::asio::buffer_size;
+    std::uint8_t b[2];
+    assert(buffer_size(sb.data()) >= sizeof(b));
+    sb.consume(buffer_copy(buffer(b), sb.data()));
+    std::size_t need;
+    fh.len = b[1] & 0x7f;
+    switch(fh.len)
+    {
+        case 126: need = 2; break;
+        case 127: need = 8; break;
+        default:
+            need = 0;
+    }
+    if((fh.mask = (b[1] & 0x80)))
+        need += 4;
+    fh.op   = static_cast<opcode::value>(b[0] & 0x0f);
+    fh.fin  = b[0] & 0x80;
+    fh.rsv1 = b[0] & 0x40;
+    fh.rsv2 = b[0] & 0x20;
+    fh.rsv3 = b[0] & 0x10;
+    // invalid length for control message
+    if(is_control(fh.op) && fh.len > 125)
+    {
+        code = close::protocol_error;
+        return 0;
+    }
+    // reserved bits not cleared
+    if(fh.rsv1 || fh.rsv2 || fh.rsv3)
+    {
+        code = close::protocol_error;
+        return 0;
+    }
+    // reserved opcode
+    if(is_reserved(fh.op))
+    {
+        code = close::protocol_error;
+        return 0;
+    }
+    // invalid opcode
+    // (only in locally generated headers)
+    if(! is_valid(fh.op))
+    {
+        code = close::protocol_error;
+        return 0;
+    }
+    // fragmented control message
+    if(is_control(fh.op) && ! fh.fin)
+    {
+        code = close::protocol_error;
+        return 0;
+    }
+    // unmasked frame from client
+    if(role == role_type::server && ! fh.mask)
+    {
+        code = close::protocol_error;
+        return 0;
+    }
+    // masked frame from server
+    if(role == role_type::client && fh.mask)
+    {
+        code = close::protocol_error;
+        return 0;
+    }
+    code = close::none;
+    return need;
+}
+
+// Decode variable frame header from stream
+//
+template<class Streambuf>
+void
+read_fh2(frame_header& fh, Streambuf& sb,
+    role_type role, close::value& code)
+{
+    using boost::asio::buffer;
+    using boost::asio::buffer_copy;
+    using boost::asio::buffer_size;
+    using namespace boost::endian;
+    switch(fh.len)
+    {
+    case 126:
+    {
+        std::uint8_t b[2];
+        assert(buffer_size(sb.data()) >= sizeof(b));
+        sb.consume(buffer_copy(buffer(b), sb.data()));
+        fh.len = reinterpret_cast<
+            big_uint16_buf_t const*>(&b[0])->value();
+        // length not canonical
+        if(fh.len < 126)
+        {
+            code = close::protocol_error;
+            return;
+        }
+        break;
+    }
+    case 127:
+    {
+        std::uint8_t b[8];
+        assert(buffer_size(sb.data()) >= sizeof(b));
+        sb.consume(buffer_copy(buffer(b), sb.data()));
+        fh.len = reinterpret_cast<
+            big_uint64_buf_t const*>(&b[0])->value();
+        // length not canonical
+        if(fh.len < 65536)
+        {
+            code = close::protocol_error;
+            return;
+        }
+        break;
+    }
+    }
+    if(fh.mask)
+    {
+        std::uint8_t b[4];
+        assert(buffer_size(sb.data()) >= sizeof(b));
+        sb.consume(buffer_copy(buffer(b), sb.data()));
+        fh.key = reinterpret_cast<
+            little_uint32_buf_t const*>(&b[0])->value();
+    }
+    code = close::none;
+}
+
+// Read data from buffers
+// This is for ping and pong payloads
+//
+template<class Buffers>
+void
+read(ping_payload_type& data,
+    Buffers const& bs, close::value& code)
+{
+    using boost::asio::buffer_copy;
+    using boost::asio::buffer_size;
+    using boost::asio::mutable_buffers_1;
+    assert(buffer_size(bs) <= data.max_size());
+    data.resize(buffer_size(bs));
+    buffer_copy(mutable_buffers_1{
+        data.data(), data.size()}, bs);
+}
+
+// Read close_reason, return true on success
+// This is for the close payload
+//
+template<class Buffers>
+void
+read(close_reason& cr,
+    Buffers const& bs, close::value& code)
+{
+    using boost::asio::buffer;
+    using boost::asio::buffer_copy;
+    using boost::asio::buffer_size;
+    using namespace boost::endian;
+    auto n = buffer_size(bs);
+    assert(n <= 125);
+    if(n == 0)
+    {
+        cr = close_reason{};
+        code = close::none;
+        return;
+    }
+    if(n == 1)
+    {
+        code = close::protocol_error;
+        return;
+    }
+    consuming_buffers<Buffers> cb(bs);
+    {
+        std::uint8_t b[2];
+        buffer_copy(buffer(b), cb);
+        cr.code = static_cast<close::value>(
+            reinterpret_cast<
+                big_uint16_buf_t const*>(&b[0])->value());
+        cb.consume(2);
+        n -= 2;
+        // Check cr.code against 0 because
+        // is_valid considers 0 to be valid.
+        if(cr.code == 0 || ! is_valid(cr.code))
+        {
+            code = close::protocol_error;
+            return;
+        }
+    }
+    if(n > 0)
+    {
+        cr.reason.resize(n);
+        buffer_copy(buffer(&cr.reason[0], n), cb);
+        if(! detail::check_utf8(
+            cr.reason.data(), cr.reason.size()))
+        {
+            code = close::protocol_error;
+            return;
+        }
+    }
+    else
+    {
+        cr.reason = "";
+    }
+    code = close::none;
+}
+
+} // detail
+} // wsproto
+} // beast
+
+#endif
---
+++ src/beast/beast/wsproto/impl/accept_op.ipp
@@ -0,0 +1,206 @@
+//------------------------------------------------------------------------------
+/*
+    This file is part of Beast: https://github.com/vinniefalco/Beast
+    Copyright 2013, Vinnie Falco <vinnie.falco@gmail.com>
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose  with  or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE  SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH  REGARD  TO  THIS  SOFTWARE  INCLUDING  ALL  IMPLIED  WARRANTIES  OF
+    MERCHANTABILITY  AND  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY  SPECIAL ,  DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER  RESULTING  FROM  LOSS  OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION  OF  CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+//==============================================================================
+
+#ifndef BEAST_WSPROTO_ACCEPT_OP_H_INCLUDED
+#define BEAST_WSPROTO_ACCEPT_OP_H_INCLUDED
+
+#include <beast/asio/handler_alloc.h>
+#include <beast/asio/read_until.h>
+#include <beast/http/parser.h>
+#include <boost/optional.hpp>
+#include <cassert>
+#include <memory>
+
+namespace beast {
+namespace wsproto {
+
+// read and respond to an upgrade request
+//
+template<class Stream>
+template<class Handler>
+class socket<Stream>::accept_op
+{
+    using alloc_type =
+        handler_alloc<char, Handler>;
+
+    using streambuf_type =
+        basic_streambuf<alloc_type>;
+
+    struct data
+    {
+        socket<Stream>& ws;
+        http::body body;
+        http::message m;
+        Handler h;
+        error_code ec;
+        streambuf_type wb;
+        boost::optional<http::parser> p;
+        int state;
+
+        template<class DeducedHandler, class Buffers>
+        data(DeducedHandler&& h_,
+            socket<Stream>& ws_, Buffers const& buffers)
+            : ws(ws_)
+            , h(std::forward<DeducedHandler>(h_))
+            , wb(1024, alloc_type{h})
+            , state(0)
+        {
+            ws.stream_.buffer().commit(
+                boost::asio::buffer_copy(
+                    ws.stream_.buffer().prepare(
+                        boost::asio::buffer_size(buffers)),
+                            buffers));
+            p.emplace(m, body, true);
+        }
+
+        template<class DeducedHandler, class DeducedMessage>
+        data(DeducedHandler&& h_, socket<Stream>& ws_,
+                DeducedMessage&& m_, void const*)
+            : ws(ws_)
+            , m(std::forward<DeducedMessage>(m_))
+            , h(std::forward<DeducedHandler>(h_))
+            , wb(1024, alloc_type{h})
+            , state(1)
+        {
+        }
+    };
+
+    std::shared_ptr<data> d_;
+
+public:
+    accept_op(accept_op&&) = default;
+    accept_op(accept_op const&) = default;
+
+    template<class DeducedHandler, class... Args>
+    explicit
+    accept_op(DeducedHandler&& h, Args&&... args)
+        : d_(std::allocate_shared<data>(alloc_type{h},
+            std::forward<DeducedHandler>(h),
+                std::forward<Args>(args)...))
+    {
+        (*this)(error_code{}, 0);
+    }
+
+    void operator()(error_code ec,
+        std::size_t bytes_transferred);
+
+    friend
+    auto asio_handler_allocate(
+        std::size_t size, accept_op* op)
+    {
+        return boost_asio_handler_alloc_helpers::
+            allocate(size, op->d_->h);
+    }
+
+    friend
+    auto asio_handler_deallocate(
+        void* p, std::size_t size, accept_op* op)
+    {
+        return boost_asio_handler_alloc_helpers::
+            deallocate(p, size, op->d_->h);
+    }
+
+    friend
+    auto asio_handler_is_continuation(accept_op* op)
+    {
+        return op->d_->state >= 20 ||
+            boost_asio_handler_cont_helpers::
+                is_continuation(op->d_->h);
+    }
+
+    template <class Function>
+    friend
+    auto asio_handler_invoke(Function&& f, accept_op* op)
+    {
+        return boost_asio_handler_invoke_helpers::
+            invoke(f, op->d_->h);
+    }
+};
+
+template<class Stream>
+template<class Handler>
+void 
+socket<Stream>::accept_op<
+    Handler>::operator()(error_code ec,
+        std::size_t bytes_transferred)
+{
+    auto& d = *d_;
+    while(! ec && d.state != 999)
+    {
+        switch(d.state)
+        {
+        case 0:
+            // read data
+            d.state = 20;
+            boost::asio::async_read_until(
+                d.ws.next_layer_, d.ws.stream_.buffer(),
+                    "\r\n\r\n", std::move(*this));
+            return;
+
+        // got message
+        case 10:
+        case 30:
+            ec = d.ws.do_accept(d.m);
+            if(ec)
+            {
+                // send error response
+                d.ec = ec;
+                d.state = 40;
+                d.ws.write_error(d.wb, ec);
+                boost::asio::async_write(d.ws.stream_,
+                    d.wb.data(), std::move(*this));
+                return;
+            }
+            d.ws.write_response(d.wb, d.m);
+            // send response
+            d.state = 999;
+            d.ws.role_ = role_type::server;
+            boost::asio::async_write(d.ws.stream_,
+                d.wb.data(), std::move(*this));
+            return;
+
+        // got data
+        case 20:
+        {
+            auto const used = d.p->write(
+                prepare_buffers(bytes_transferred,
+                    d.ws.stream_.buffer().data()), ec);
+            if(ec)
+                break;
+            assert(d.p->complete());
+            d.ws.stream_.buffer().consume(used);
+            d.state = 30;
+            break;
+        }
+
+        // sent error response
+        case 40:
+            // call handler
+            ec = d.ec;
+            d.state = 999;
+            break;
+        }
+    }
+    d.h(ec);
+}
+
+} // wsproto
+} // beast
+
+#endif
