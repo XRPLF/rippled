b5f8a89c23ef280dc3ae32d1388521fb60ad9f6a
Vinnie Falco <vinnie.falco@gmail.com>
2016-04-01 11:46:32 -0400

    Beast.Asio:
    New classes:
    
      class async_completion:
        Helper class for implementing asynchronous initiation functions.
        See n3964:
            Library Foundations for Asynchronous Operations, Revision 1
            http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2014/n3964.pdf
    
      class buffered_readstream:
        Buffers a ReadStream with a ConstBufferSequence.
    
      class buffers_adapter:
        Adapts a MutableBufferSequence into the Streambuf concept.
    
      class consuming_buffers:
        Adapts a BufferSequence which wraps the underlying buffer
        sequence and presents fewer bytes, with the retained bytes
        occurring at the end of the sequence.
    
      class handler_alloc:
        A C++ Allocator the uses asio handler allocation hooks.
    
      class static_streambuf:
        An implementation of the Streambuf concept that uses a
        fixed size buffer with size determined at compile-time.
    
      class streambuf_readstream:
        Buffers a ReadStream with a Streambuf.
    
    New functions:
    
      append_buffers()
        Returns a new BufferSequence which efficiently concatenates
        two or more buffer sequences together.
    
      prepare_buffers()
        Shortens a buffer sequence. The bytes excluded are at the
        end of the underlying buffer sequence.
    
      boost::asio::read_until()
        A copy of boost::asio::read_until overloads, modified to work
        with a beast::asio::basic_streambuf.
    
    Debugging:
    
      buffers_to_string()
    
        Convert a ConstBufferSequence to a human readable string
        suitable for diagnostics.
    
    type_check.h:
    
      Metafunctions for checking asio concepts:
        AsyncReadStream, AsyncWriteStream
        SyncReadStream, SyncWriteStream
        ConstBufferSequence, MutableBufferSequence
        Streambuf
        Handler
    
    Fixes:
    
    * Fix streambuf iterator category
    
    Changes:
    
    * All symbols moved up a namespace level

--- src/beast/beast/asio/buffers_debug.h
+++ src/beast/beast/asio/buffers_debug.h
@@ -17,13 +17,36 @@
 */
 //==============================================================================
 
-#if BEAST_INCLUDE_BEASTCONFIG
-#include <BeastConfig.h>
+#ifndef BEAST_ASIO_BUFFERS_DEBUG_H_INLUDED
+#define BEAST_ASIO_BUFFERS_DEBUG_H_INLUDED
+
+#include <boost/asio/buffer.hpp>
+#include <string>
+
+namespace beast {
+namespace debug {
+
+template<class Buffers>
+static
+std::string
+buffers_to_string(Buffers const& bs)
+{
+    using boost::asio::buffer_cast;
+    using boost::asio::buffer_size;
+    std::string s;
+    s.reserve(buffer_size(bs));
+    for(auto const& b : bs)
+        s.append(buffer_cast<char const*>(b),
+            buffer_size(b));
+    for(auto i = s.size(); i-- > 0;)
+        if(s[i] == '\r')
+            s.replace(i, 1, "\\r");
+        else if(s[i] == '\n')
+            s.replace(i, 1, "\\n\n");
+    return s;
+}
+
+} // debug
+} // beast
+
 #endif
-
-#include <beast/asio/impl/IPAddressConversion.cpp>
-#include <beast/asio/impl/error.cpp>
-#include <beast/asio/tests/buffers_test.cpp>
-#include <beast/asio/tests/bind_handler.test.cpp>
-#include <beast/asio/tests/error_test.cpp>
-
