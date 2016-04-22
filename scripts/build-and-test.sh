0061f03cefa5b61bd7b7846754adf452dce0b84e
sublimator <ndudfield@gmail.com>
2016-04-22 11:09:36 -0400

    Add Travis support

---
+++ scripts/build-and-test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -u
+# We use set -e and bash with -u to bail on first non zero exit code of any
+# processes launched or upon any unbound variable
+set -e
+__dirname=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+echo "using toolset: $CC"
+echo "using variant: $VARIANT"
+
+$BOOST_ROOT/bjam toolset=$CC variant=$VARIANT
+`find . -name "core_tests"`
+`find . -name "http_tests"`
