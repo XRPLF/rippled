e29163e9226dafd2ef1564d7678898cdf1a24e7f
seelabs <scott.determan@yahoo.com>
2016-07-01 17:14:37 -0400

    Set version to 0.32.1-b2

--- src/ripple/protocol/impl/BuildInfo.cpp
+++ src/ripple/protocol/impl/BuildInfo.cpp
@@ -35,7 +35,7 @@ char const* getRawVersionString ()
     //
     //  The build version number (edit this for each release)
     //
-        "0.32.1-b1"
+        "0.32.1-b2"
 
     //
     //  Must follow the format described here:
