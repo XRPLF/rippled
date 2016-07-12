d8d0cb17ba23b486a91c2c3782849395a69db47b
Nik Bougalis <nikb@bougalis.net>
2016-07-11 22:01:41 -0700

    Cryptoconditions Framework (RIPD-1139):
    Cryptoconditions provide a mechanism to describe a signed message such
    that multiple actors in a distributed system can all verify the same
    signed message and agree on whether it matches the description. This
    provides a useful primitive for event-based systems that are distributed
    on the Internet since we can describe events in a standard deterministic
    manner (represented by signed messages) and therefore define generic
    authenticated event handlers.
    
    The cryptoconditions specification implemented is available at:
        https://tools.ietf.org/html/draft-thomas-crypto-conditions-01

---
+++ src/ripple/conditions/Condition.h
@@ -0,0 +1,175 @@
+//------------------------------------------------------------------------------
+/*
+    This file is part of rippled: https://github.com/ripple/rippled
+    Copyright (c) 2016 Ripple Labs Inc.
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose  with  or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE  SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH  REGARD  TO  THIS  SOFTWARE  INCLUDING  ALL  IMPLIED  WARRANTIES  OF
+    MERCHANTABILITY  AND  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY  SPECIAL ,  DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER  RESULTING  FROM  LOSS  OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION  OF  CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+//==============================================================================
+
+#ifndef RIPPLE_CONDITIONS_CONDITION_H
+#define RIPPLE_CONDITIONS_CONDITION_H
+
+#include <ripple/conditions/impl/base64.h> // use Beast implementation
+#include <ripple/conditions/impl/utils.h>
+#include <boost/optional.hpp>
+#include <array>
+#include <cstdint>
+#include <string>
+
+namespace ripple {
+namespace cryptoconditions {
+
+// NIKB-TODO: These should move to a separate file:
+std::uint16_t constexpr condition_hashlock         = 0;
+std::uint16_t constexpr condition_prefix_sha256    = 1;
+std::uint16_t constexpr condition_threshold_sha256 = 2;
+std::uint16_t constexpr condition_rsa_sha256       = 3;
+std::uint16_t constexpr condition_ed25519          = 4;
+
+// NIKB-TODO: These should be `enum class : std::uint32_t`
+std::uint32_t constexpr feature_sha256             = 1;
+std::uint32_t constexpr feature_preimage           = 2;
+std::uint32_t constexpr feature_prefix             = 4;
+std::uint32_t constexpr feature_threshold          = 8;
+std::uint32_t constexpr feature_rsa_pss            = 16;
+std::uint32_t constexpr feature_ed25519            = 32;
+
+/** The list of all feature suited defined in the RFC */
+std::uint32_t constexpr definedFeatures =
+    feature_sha256 |
+    feature_preimage |
+    feature_prefix |
+    feature_threshold |
+    feature_rsa_pss |
+    feature_ed25519;
+
+/** The largest fulfillment supported by this implementation.
+
+    Fulfillments larger than this value cannot be processed
+    and will not be generated.
+*/
+constexpr std::size_t maxSupportedFulfillmentLength = 65535;
+
+struct Condition
+{
+    std::uint16_t type;
+
+    /** The maximum length of a fulfillment for this condition.
+
+        While it is possible for a fulfillment to be smaller
+        it can never be bigger than this.
+    */
+    std::uint16_t maxFulfillmentLength;
+
+    /** The features suites required to process a fulfillment. */
+    std::uint32_t featureBitmask;
+
+    /** An identifier for this condition.
+
+        This fingerprint is meant to be unique only with
+        respect to other conditions of the same type.
+    */
+    std::array<std::uint8_t, 32> fingerprint;
+
+    // Can this be deleted?
+    Condition () = default;
+
+    Condition (Condition const&) = default;
+    Condition (Condition&&) = default;
+};
+
+inline
+bool
+operator== (Condition const& lhs, Condition const& rhs)
+{
+    return
+        lhs.type == rhs.type &&
+        lhs.featureBitmask == rhs.featureBitmask &&
+        lhs.maxFulfillmentLength == rhs.maxFulfillmentLength &&
+        lhs.fingerprint == rhs.fingerprint;
+}
+
+inline
+bool
+operator!= (Condition const& lhs, Condition const& rhs)
+{
+    return !(lhs == rhs);
+}
+
+/** Determine if a given condition is valid.
+
+    @note this function checks whether it understands the
+          type of the condition, and if so, whether it meets
+          the requirements mandated by the RFC.
+*/
+inline
+bool
+validate (Condition const& c)
+{
+    // This check can never trigger because of the range of
+    // the maxFulfillmentLength type. It's here in case the
+    // type is changed in the future.
+
+    if (c.maxFulfillmentLength > maxSupportedFulfillmentLength)
+        return false;
+
+    return false;
+}
+
+/** True if condition type is specified in the RFC.
+
+    @note: this function may return true even if the type
+           of condition isn't presently supported by this
+           implementation.
+*/
+inline
+bool
+isCondition (std::uint16_t type)
+{
+    switch(type)
+    {
+    case condition_hashlock:
+    case condition_prefix_sha256:
+    case condition_threshold_sha256:
+    case condition_rsa_sha256:
+    case condition_ed25519:
+        return true;
+
+    default:
+        return false;
+    }
+}
+
+/** Load a serialized condition either from its string or binary form */
+/** @{ */
+boost::optional<Condition>
+loadCondition(std::string const& s);
+
+boost::optional<Condition>
+loadCondition(Slice s);
+/** @} */
+
+// Convert a condition to its string form
+std::string
+to_string (Condition const& c);
+
+// Convert a condition to its binary form
+std::vector<std::uint8_t>
+to_blob (Condition const& c);
+
+}
+
+}
+
+#endif
---
+++ src/ripple/conditions/impl/Fulfillment.cpp
@@ -0,0 +1,209 @@
+//------------------------------------------------------------------------------
+/*
+    This file is part of rippled: https://github.com/ripple/rippled
+    Copyright (c) 2016 Ripple Labs Inc.
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose  with  or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE  SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH  REGARD  TO  THIS  SOFTWARE  INCLUDING  ALL  IMPLIED  WARRANTIES  OF
+    MERCHANTABILITY  AND  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY  SPECIAL ,  DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER  RESULTING  FROM  LOSS  OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION  OF  CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+//==============================================================================
+
+#include <ripple/conditions/Condition.h>
+#include <ripple/conditions/Fulfillment.h>
+#include <ripple/conditions/impl/utils.h>
+#include <boost/regex.hpp>
+#include <boost/optional.hpp>
+#include <type_traits>
+#include <vector>
+
+namespace ripple {
+namespace cryptoconditions {
+
+bool
+fulfills (
+    Fulfillment const& f,
+    Condition const& c)
+{
+    // Fast check: the fulfillment's type must match the
+    // conditions's type:
+    if (f.type() != c.type)
+        return false;
+
+    // Ensure that the condition is well-formed
+    if (!validate (c))
+        return false;
+
+    // The fulfillment payload can be no larger than the
+    // what the condition allows.
+    if (f.payloadSize() > c.maxFulfillmentLength)
+        return false;
+
+    return f.condition() == c;
+}
+
+bool
+validate (
+    Fulfillment const& f,
+    Condition const& c,
+    Slice m)
+{
+    return fulfills (f, c) && f.validate (m);
+}
+
+bool
+validateTrigger (
+    Fulfillment const& f,
+    Condition const& c)
+{
+    return validate (f, c, {});
+}
+
+std::unique_ptr<Fulfillment>
+loadFulfillment (std::uint16_t type, Slice payload)
+{
+    std::unique_ptr<Fulfillment> p;
+
+    switch (type)
+    {
+    default:
+        throw std::domain_error (
+            "Unknown cryptocondition type " +
+                std::to_string (type));
+    }
+
+    // If the payload can't be parsed, the load should
+    // fail.
+    if (p && !p->parsePayload(payload))
+        p.reset();
+
+    return p;
+}
+
+// Parse a condition from its string form
+std::unique_ptr<Fulfillment>
+loadFulfillment (std::string const& s)
+{
+    // CHECKME: custom parser maybe? probably faster but
+    // more work and probability of error.
+
+    // TODO: use two regex: one that accepts anything the
+    // standard supports and one which accepts only what
+    // we support. Parse with both for improved errors?
+    static boost::regex const re_current (
+            "^"                            // start of line
+            "cf:"                          // 'cf' for fulfillment
+            "([1-9a-f][0-9a-f]{0,3}|0):"   // type
+            "([a-zA-Z0-9_-]*)"             // fulfillment payload (base64url)
+            "$"                            // end of line
+        , boost::regex_constants::optimize
+    );
+
+    try
+    {
+        boost::smatch match;
+
+        if (!boost::regex_match (s, match, re_current))
+            return nullptr;
+
+        std::uint16_t const type =
+            parse_hexadecimal<std::uint16_t>(match[1]);
+
+        auto payload = base64url_decode (match[2]);
+
+        if (payload.size() > maxSupportedFulfillmentLength)
+            return nullptr;
+
+        return loadFulfillment (type, makeSlice (payload));
+    }
+    catch (std::exception const&)
+    {
+        return nullptr;
+    }
+}
+
+std::unique_ptr<Fulfillment>
+loadFulfillment (Slice s)
+{
+    if (s.empty())
+        return nullptr;
+
+    try
+    {
+        auto start = s.data();
+        auto finish = s.data() + s.size();
+
+        std::uint16_t type;
+        std::size_t len;
+
+        std::tie (start, type) =
+            oer::decode_integer<std::uint16_t> (
+                start, finish);
+
+        if (!isCondition (type))
+            return nullptr;
+
+        if (start == finish)
+            return nullptr;
+
+        std::tie (start, len) =
+            oer::decode_length(
+                start, finish);
+
+        if (len)
+        {
+            if (len > maxSupportedFulfillmentLength)
+                return nullptr;
+
+            if (std::distance (start, finish) < len)
+                return nullptr;
+        }
+
+        return loadFulfillment (type, Slice{ start, len });
+    }
+    catch (std::exception const&)
+    {
+        return nullptr;
+    }
+}
+
+std::string
+to_string (Fulfillment const& f)
+{
+    return std::string("cf:") + to_hex(f.type())
+        + ":" + base64url_encode (f.payload());
+}
+
+std::vector<std::uint8_t>
+to_blob (Fulfillment const& f)
+{
+    // NIKB TODO optimize this
+    std::vector<std::uint8_t> v;
+
+    auto const p = f.payload();
+
+    oer::encode_integer (
+        f.type(),
+        std::back_inserter(v));
+
+    oer::encode_length (
+        p.size(), std::back_inserter(v));
+
+    oer::encode_octetstring (
+        p.data(),
+        p.data() + p.size(),
+        std::back_inserter(v));
+
+    return v;
+}
+
+}
+}
