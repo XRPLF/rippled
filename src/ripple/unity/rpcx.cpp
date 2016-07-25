c4e581179c63dfcc0a86fd8663bbbb17aeae6f05
Mike Ellery <mellery451@gmail.com>
2016-07-25 12:37:03 -0700

    Migrate AccountSet unit tests (RIPD-1249): Move existing functionality of account_set-tests.js into a native unit test. Improve coverage by testing more cases.

--- src/ripple/unity/rpcx.cpp
+++ src/ripple/unity/rpcx.cpp
@@ -97,9 +97,10 @@
 #include <ripple/rpc/impl/TransactionSign.cpp>
 
 #include <ripple/rpc/tests/AccountInfo_test.cpp>
-#include <ripple/rpc/tests/AccountObjects_test.cpp>
 #include <ripple/rpc/tests/AccountLinesRPC.test.cpp>
+#include <ripple/rpc/tests/AccountObjects_test.cpp>
 #include <ripple/rpc/tests/AccountOffers.test.cpp>
+#include <ripple/rpc/tests/AccountSet.test.cpp>
 #include <ripple/rpc/tests/Book.test.cpp>
 #include <ripple/rpc/tests/JSONRPC.test.cpp>
 #include <ripple/rpc/tests/GatewayBalances.test.cpp>
