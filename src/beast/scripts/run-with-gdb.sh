7c90b9ef883f9705f8b1b7c85080112c9e6d9d3e
Vinnie Falco <vinnie.falco@gmail.com>
2016-09-15 15:07:34 -0400

    Remove Beast subtree in preparation for git-subtree add

--- src/beast/scripts/run-with-gdb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -u
-set -e
-gdb --silent \
-    --batch \
-    --return-child-result \
-    -ex="set print thread-events off" \
-    -ex=run \
-    -ex="thread apply all bt full" \
-    --args $@
