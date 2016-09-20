7731a0a3d3370375cf425e70261f00f1106eee19
johannakate <johanna@ripple.com>
2016-09-20 16:07:26 -0700

    Updated getInfo script based on feedback

--- bin/getInfoRippled.sh
+++ bin/getInfoRippled.sh
@@ -1,41 +1,72 @@
 #!/usr/bin/env bash
 
-TMP_LOC=$(mktemp -d /tmp/ripple_info.XXXX)
-echo ${TMP_LOC}
+rippled_exe=/opt/ripple/bin/rippled
+conf_file=/etc/opt/ripple/rippled.cfg
 
-cd ${TMP_LOC}
+while getopts ":e:c:" opt; do
+    case $opt in
+        e)
+            rippled_exe=${OPTARG}
+            ;;
+        c)
+            conf_file=${OPTARG}
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG"
+    esac
+done
+
+tmp_loc=$(mktemp -d /tmp/ripple_info.XXXX)
+cd /tmp
+chmod 751 ripple_info.*
+cd ~
+echo ${tmp_loc}
+
+cleaned_conf=${tmp_loc}/cleaned_rippled_cfg.txt
+
+db=$(sed -r -e 's/\<s[a-zA-Z0-9]{28}\>/secretsecretsecretsecretmaybe/g' ${conf_file} |\
+            awk -v OUT_FILE=${cleaned_conf} '
+BEGIN {skip=0; db_path="";print > OUT_FILE}
+/^\[validation_seed\]/ {skip=1; next}
+/^\[node_seed\]/ {skip=1; next}
+/^\[.*\]/ {skip=0}
+skip==1 {next}
+save==1 {save=0;db_path=$0}
+/^\[database_path\]/ {save=1}
+{print >> OUT_FILE}
+END {print db_path}
+')
+
+echo "database_path: ${db}"
+df ${db} > ${tmp_loc}/db_path_df.txt
+echo
 
 # Send output from this script to a log file
 ## this captures any messages
 ## or errors from the script itself
 
-LOG_FILE=${TMP_LOC}/get_info.log
-exec 3>&1 1>>${LOG_FILE} 2>&1
+log_file=${tmp_loc}/get_info.log
+exec 3>&1 1>>${log_file} 2>&1
 
 ## Send all stdout files to /tmp
 
-/opt/ripple/bin/rippled server_info > ${TMP_LOC}/server_info.txt
-df -h                               > ${TMP_LOC}/free_disk_space.txt
-cat /proc/meminfo                   > ${TMP_LOC}/amount_mem.txt
-cat /proc/swaps                     > ${TMP_LOC}/swap_space.txt
-ulimit -a                           > ${TMP_LOC}/reported_current_limits.txt
+${rippled_exe} server_info          > ${tmp_loc}/server_info.txt
+df -h                               > ${tmp_loc}/free_disk_space.txt
+cat /proc/meminfo                   > ${tmp_loc}/amount_mem.txt
+cat /proc/swaps                     > ${tmp_loc}/swap_space.txt
+ulimit -a                           > ${tmp_loc}/reported_current_limits.txt
 
-TMP_CONF=${TMP_LOC}/rippled.cfg
+for dev_path in $(df | awk '$1 ~ /^\/dev\// {print $1}'); do
+    # strip numbers from end and remove '/dev/'
+    dev=$(basename ${dev_path%%[0-9]})
+    if [[ "$(cat /sys/block/${dev}/queue/rotational)" = 0 ]]
+    then
+        echo "${dev} : SSD" >> ${tmp_loc}/is_ssd.txt
+    else
+        echo "${dev} : NO SSD" >> ${tmp_loc}/is_ssd.txt
+    fi
+done
 
-cp /etc/opt/ripple/rippled.cfg ${TMP_CONF}
-
-SEED_LINE_NUM=$(grep -n "\[validation_seed\]" ${TMP_CONF} | tail -n 1 | awk -F ':' '{print $1}')
-SEED_LINE_NUM=$((SEED_LINE_NUM + 1))
-
-awk -v SEED_LINE_NUM=${SEED_LINE_NUM} 'NR != SEED_LINE_NUM' "${TMP_CONF}" > ${TMP_LOC}/cleaned_rippled_cfg.txt
-
-if [[ "$(cat /sys/block/xvda/queue/rotational)" = 0 ]]
-then
-        echo "SSD exists"  > ${TMP_LOC}/is_ssd.txt
-else
-        echo "No SSD"      > ${TMP_LOC}/is_ssd.txt
-fi
-
+pushd ${tmp_loc}
 tar -czvf info-package.tar.gz *.txt *.log
-
-echo "Use the following command on your local machine to download from your rippled instance: scp <remote_rippled_username>@<remote_host>:${TMP_LOC}/info-package.tar.gz <path/to/local_machine/directory>"| tee /dev/fd/3
+popd
