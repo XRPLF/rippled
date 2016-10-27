446391468bbfe1bf13342a8476754fa74f31d48a
Mike Ellery <mellery451@gmail.com>
2016-10-27 10:36:07 -0700

    [FOLD] Reorder Tests, Improve Some Test Assertions

--- src/test/rpc/LedgerRPC_test.cpp
+++ src/test/rpc/LedgerRPC_test.cpp
@@ -38,48 +38,25 @@ class LedgerRPC_test : public beast::unit_test::suite
         return p;
     }
 
-public:
-    void testBadInput()
+    void
+    checkErrorValue(
+        Json::Value const& jv,
+        std::string const& err,
+        std::string const& msg)
     {
-        testcase("Bad Input");
-        using namespace test::jtx;
-        Env env { *this };
-        Account const gw { "gateway" };
-        auto const USD = gw["USD"];
-        Account const bob { "bob" };
-
-        env.fund(XRP(10000), gw, bob);
-        env.trust(USD(1000), bob);
-        env.close();
-
+        if (BEAST_EXPECT(jv.isMember(jss::status)))
+            BEAST_EXPECT(jv[jss::status] == "error");
+        if (BEAST_EXPECT(jv.isMember(jss::error)))
+            BEAST_EXPECT(jv[jss::error] == err);
+        if (msg.empty())
         {
-            Json::Value jvParams;
-            jvParams[jss::ledger_index] = "0"; // NOT an integer
-            auto const jrr = env.rpc ( "json", "ledger", to_string(jvParams) ) [jss::result];
-            BEAST_EXPECT(jrr[jss::error] == "invalidParams");
-            BEAST_EXPECT(jrr[jss::status] == "error");
-            BEAST_EXPECT(jrr[jss::error_message] == "ledgerIndexMalformed");
+            BEAST_EXPECT(
+                jv[jss::error_message] == Json::nullValue ||
+                jv[jss::error_message] == "");
         }
-
-        {
-            // ask for a bad ledger index
-            Json::Value jvParams;
-            jvParams[jss::ledger_index] = 10u;
-            auto const jrr = env.rpc ( "json", "ledger", to_string(jvParams) ) [jss::result];
-            BEAST_EXPECT(jrr[jss::error] == "lgrNotFound");
-            BEAST_EXPECT(jrr[jss::status] == "error");
-            BEAST_EXPECT(jrr[jss::error_message] == "ledgerNotFound");
-        }
-
-        {
-            // arbitrary text is converted to 0.
-            auto const jrr = env.rpc("ledger", "arbitrary_text") [jss::result];
-            BEAST_EXPECT(jrr[jss::error] == "lgrNotFound");
-            BEAST_EXPECT(jrr[jss::status] == "error");
-            BEAST_EXPECT(jrr[jss::error_message] == "ledgerNotFound");
-        }
-
-    }
+        else if (BEAST_EXPECT(jv.isMember(jss::error_message)))
+            BEAST_EXPECT(jv[jss::error_message] == msg);
+    };
 
     void testLedgerRequest()
     {
@@ -92,8 +69,10 @@ public:
         BEAST_EXPECT(env.current()->info().seq == 4);
 
         {
-            // arbitrary text is converted to 0.
+            // in this case, numeric string converted to number
             auto const jrr = env.rpc("ledger", "1") [jss::result];
+            BEAST_EXPECT(jrr[jss::ledger][jss::closed] == true);
+            BEAST_EXPECT(jrr[jss::ledger][jss::ledger_index] == "1");
             BEAST_EXPECT(jrr[jss::ledger][jss::accepted] == true);
             BEAST_EXPECT(jrr[jss::ledger][jss::totalCoins] == env.balance(env.master).value().getText());
         }
@@ -107,6 +86,43 @@ public:
         }
     }
 
+    void testBadInput()
+    {
+        testcase("Bad Input");
+        using namespace test::jtx;
+        Env env { *this };
+        Account const gw { "gateway" };
+        auto const USD = gw["USD"];
+        Account const bob { "bob" };
+
+        env.fund(XRP(10000), gw, bob);
+        env.close();
+        env.trust(USD(1000), bob);
+        env.close();
+
+        {
+            Json::Value jvParams;
+            jvParams[jss::ledger_index] = "0"; // NOT an integer
+            auto const jrr = env.rpc ( "json", "ledger", to_string(jvParams) ) [jss::result];
+            checkErrorValue(jrr, "invalidParams", "ledgerIndexMalformed");
+        }
+
+        {
+            // ask for a bad ledger index
+            Json::Value jvParams;
+            jvParams[jss::ledger_index] = 10u;
+            auto const jrr = env.rpc ( "json", "ledger", to_string(jvParams) ) [jss::result];
+            checkErrorValue(jrr, "lgrNotFound", "ledgerNotFound");
+        }
+
+        {
+            // unrecognized string arg -- error
+            auto const jrr = env.rpc("ledger", "arbitrary_text") [jss::result];
+            checkErrorValue(jrr, "lgrNotFound", "ledgerNotFound");
+        }
+
+    }
+
     void testLedgerCurrent()
     {
         testcase("ledger_current Request");
@@ -118,64 +134,9 @@ public:
         BEAST_EXPECT(env.current()->info().seq == 4);
 
         {
-            // arbitrary text is converted to 0.
             auto const jrr = env.rpc("ledger_current") [jss::result];
             BEAST_EXPECT(jrr[jss::ledger_current_index] == env.current()->info().seq);
         }
-    }
-
-    void testLedgerFull()
-    {
-        testcase("Ledger Request, Full Option");
-        using namespace test::jtx;
-
-        Env env {*this};
-
-        env.close();
-
-        Json::Value jvParams;
-        jvParams[jss::ledger_index] = 3u;
-        jvParams[jss::full] = true;
-        auto const jrr = env.rpc ( "json", "ledger", to_string(jvParams) ) [jss::result];
-        BEAST_EXPECT(jrr[jss::ledger].isMember(jss::accountState));
-        BEAST_EXPECT(jrr[jss::ledger][jss::accountState].isArray());
-        BEAST_EXPECT(jrr[jss::ledger][jss::accountState].size() > 0);
-    }
-
-    void testLedgerFullNonAdmin()
-    {
-        testcase("Ledger Request, Full Option Without Admin");
-        using namespace test::jtx;
-
-        Env env { *this, makeNonAdminConfig() };
-
-        env.close();
-
-        Json::Value jvParams;
-        jvParams[jss::ledger_index] = 3u;
-        jvParams[jss::full] = true;
-        auto const jrr = env.rpc ( "json", "ledger", to_string(jvParams) ) [jss::result];
-        BEAST_EXPECT(jrr[jss::error] == "noPermission");
-        BEAST_EXPECT(jrr[jss::status] == "error");
-        BEAST_EXPECT(jrr[jss::error_message] == "You don't have permission for this command.");
-    }
-
-    void testLedgerAccounts()
-    {
-        testcase("Ledger Request, Accounts Option");
-        using namespace test::jtx;
-
-        Env env {*this};
-
-        env.close();
-
-        Json::Value jvParams;
-        jvParams[jss::ledger_index] = 3u;
-        jvParams[jss::accounts] = true;
-        auto const jrr = env.rpc ( "json", "ledger", to_string(jvParams) ) [jss::result];
-        BEAST_EXPECT(jrr[jss::ledger].isMember(jss::accountState));
-        BEAST_EXPECT(jrr[jss::ledger][jss::accountState].isArray());
-        BEAST_EXPECT(jrr[jss::ledger][jss::accountState].size() > 0);
     }
 
     void testAccountRoot()
@@ -200,6 +161,57 @@ public:
         BEAST_EXPECT(jrr[jss::node][sfBalance.fieldName] == XRP(10000).value().getText());
     }
 
+    void testLedgerFull()
+    {
+        testcase("Ledger Request, Full Option");
+        using namespace test::jtx;
+
+        Env env {*this};
+
+        env.close();
+
+        Json::Value jvParams;
+        jvParams[jss::ledger_index] = 3u;
+        jvParams[jss::full] = true;
+        auto const jrr = env.rpc ( "json", "ledger", to_string(jvParams) ) [jss::result];
+        BEAST_EXPECT(jrr[jss::ledger].isMember(jss::accountState));
+        BEAST_EXPECT(jrr[jss::ledger][jss::accountState].isArray());
+        BEAST_EXPECT(jrr[jss::ledger][jss::accountState].size() == 2u);
+    }
+
+    void testLedgerFullNonAdmin()
+    {
+        testcase("Ledger Request, Full Option Without Admin");
+        using namespace test::jtx;
+
+        Env env { *this, makeNonAdminConfig() };
+
+        env.close();
+
+        Json::Value jvParams;
+        jvParams[jss::ledger_index] = 3u;
+        jvParams[jss::full] = true;
+        auto const jrr = env.rpc ( "json", "ledger", to_string(jvParams) ) [jss::result];
+        checkErrorValue(jrr, "noPermission", "You don't have permission for this command."); }
+
+    void testLedgerAccounts()
+    {
+        testcase("Ledger Request, Accounts Option");
+        using namespace test::jtx;
+
+        Env env {*this};
+
+        env.close();
+
+        Json::Value jvParams;
+        jvParams[jss::ledger_index] = 3u;
+        jvParams[jss::accounts] = true;
+        auto const jrr = env.rpc ( "json", "ledger", to_string(jvParams) ) [jss::result];
+        BEAST_EXPECT(jrr[jss::ledger].isMember(jss::accountState));
+        BEAST_EXPECT(jrr[jss::ledger][jss::accountState].isArray());
+        BEAST_EXPECT(jrr[jss::ledger][jss::accountState].size() == 2u);
+    }
+
     void testMalformedAccountRoot()
     {
         testcase("Malformed Ledger Entry Request");
@@ -215,9 +227,7 @@ public:
         jvParams[jss::account_root] = std::string(alice.human()).replace(0, 2, 2, 'x');
         jvParams[jss::ledger_hash] = jrr[jss::ledger_hash];
         jrr = env.rpc ( "json", "ledger_entry", to_string(jvParams) ) [jss::result];
-        BEAST_EXPECT(jrr[jss::error] == "malformedAddress");
-        BEAST_EXPECT(jrr[jss::status] == "error");
-        BEAST_EXPECT(jrr[jss::error_message] == Json::nullValue);
+        checkErrorValue(jrr, "malformedAddress", "");
     }
 
     void testNotFoundAccountRoot()
@@ -235,9 +245,7 @@ public:
         jvParams[jss::account_root] = Account("bob").human();
         jvParams[jss::ledger_hash] = jrr[jss::ledger_hash];
         jrr = env.rpc ( "json", "ledger_entry", to_string(jvParams) ) [jss::result];
-        BEAST_EXPECT(jrr[jss::error] == "entryNotFound");
-        BEAST_EXPECT(jrr[jss::status] == "error");
-        BEAST_EXPECT(jrr[jss::error_message] == Json::nullValue);
+        checkErrorValue(jrr, "entryNotFound", "");
     }
 
     void testAccountRootFromIndex()
@@ -253,27 +261,37 @@ public:
         BEAST_EXPECT(jrr[jss::ledger_hash] == to_string(env.closed()->info().hash));
         BEAST_EXPECT(jrr[jss::ledger_index] == 3);
 
-        Json::Value jvParams;
-        jvParams[jss::account_root] = alice.human();
-        jvParams[jss::ledger_hash] = jrr[jss::ledger_hash];
-        jrr = env.rpc ( "json", "ledger_entry", to_string(jvParams) ) [jss::result];
-        jvParams[jss::index] = jrr[jss::index];
-        jrr = env.rpc ( "json", "ledger_entry", to_string(jvParams) ) [jss::result];
-        BEAST_EXPECT(jrr.isMember(jss::node_binary));
+        {
+            Json::Value jvParams;
+            jvParams[jss::account_root] = alice.human();
+            jvParams[jss::ledger_hash] = jrr[jss::ledger_hash];
+            jrr = env.rpc ( "json", "ledger_entry", to_string(jvParams) ) [jss::result];
+        }
+        {
+            Json::Value jvParams;
+            jvParams[jss::index] = jrr[jss::index];
+            jrr = env.rpc ( "json", "ledger_entry", to_string(jvParams) ) [jss::result];
+            BEAST_EXPECT(jrr.isMember(jss::node_binary));
+            BEAST_EXPECT(jrr[jss::node_binary] ==
+                "1100612200800000240000000225000000032D00000000554294BEBE5B569"
+                "A18C0A2702387C9B1E7146DC3A5850C1E87204951C6FDAA4C426240000002"
+                "540BE4008114AE123A8556F3CF91154711376AFB0F894F832B3D");
+        }
     }
 
+public:
     void run ()
     {
         testLedgerRequest();
         testBadInput();
         testLedgerCurrent();
         testAccountRoot();
-        testMalformedAccountRoot();
-        testNotFoundAccountRoot();
-        testAccountRootFromIndex();
         testLedgerFull();
         testLedgerFullNonAdmin();
         testLedgerAccounts();
+        testMalformedAccountRoot();
+        testNotFoundAccountRoot();
+        testAccountRootFromIndex();
     }
 };
 
