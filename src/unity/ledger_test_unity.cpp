dc3571184aa1627660ae940c64fa691e9eaa3ac7
JoelKatz <DavidJoelSchwartz@GMail.com>
2016-11-28 15:56:08 -0800

    Add a flag to the ledger for SHAMapV2
    This will allow code that looks at the ledger header to know what version the
    SHAMap uses. This is helpful for code that rebuilds ledger binary structures
    from the leaves.

--- src/unity/ledger_test_unity.cpp
+++ src/unity/ledger_test_unity.cpp
@@ -22,5 +22,6 @@
 #include <test/ledger/Directory_test.cpp>
 #include <test/ledger/PaymentSandbox_test.cpp>
 #include <test/ledger/PendingSaves_test.cpp>
+#include <test/ledger/SHAMapV2_test.cpp>
 #include <test/ledger/SkipList_test.cpp>
 #include <test/ledger/View_test.cpp>
