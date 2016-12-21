e3ff30657cab64b1c16cfb9989bcac7c93572bd9
Mike Ellery <mellery451@gmail.com>
2016-12-21 15:02:52 -0800

    Eliminate npm tests (RIPD-1369)
    Remove mention of npm tests in developer docs. Eliminate `npm test` from
    automation and ci scripts.

--- bin/ci/ubuntu/build-and-test.sh
+++ bin/ci/ubuntu/build-and-test.sh
@@ -89,8 +89,4 @@ if [[ $TARGET == "coverage" ]]; then
   codecov -X gcov # don't even try and look for .gcov files ;)
 fi
 
-if [[ ${APP} == "rippled" ]]; then
-  # Run NPM tests
-  npm install --progress=false
-  npm test --rippled=$APP_PATH
-fi
+
