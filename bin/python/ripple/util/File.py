885aaab8c80d003d7355570a01c3b36489c8a0e7
wilsonianb <bwilson@ripple.com>
2017-02-14 15:27:18 -0800

    Remove ledger and manifest Python tools

--- bin/python/ripple/util/File.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-import os
-
-def normalize(f):
-    f = os.path.join(*f.split('/'))  # For Windows users.
-    return os.path.abspath(os.path.expanduser(f))
