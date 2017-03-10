6cea5d0838825b4fdaa8c4c080096570d030f48c
seelabs <scott.determan@yahoo.com>
2017-03-10 16:16:53 -0500

    Set version to 0.60.0-rc3

--- src/ripple/protocol/impl/BuildInfo.cpp
+++ src/ripple/protocol/impl/BuildInfo.cpp
@@ -33,7 +33,7 @@ char const* const versionString =
     //  The build version number. You must edit this for each release
     //  and follow the format described at http://semver.org/
     //
-        "0.60.0-rc2"
+        "0.60.0-rc3"
 
 #if defined(DEBUG) || defined(SANITIZER)
        "+"
