dbe74dffcb1c3edaaf0bc8a6f6f834fc13358ff8
Brad Chase <bchase@ripple.com>
2017-03-21 13:21:51 -0400

    Set version to 0.70.0-b2

--- src/ripple/protocol/impl/BuildInfo.cpp
+++ src/ripple/protocol/impl/BuildInfo.cpp
@@ -33,7 +33,7 @@ char const* const versionString =
     //  The build version number. You must edit this for each release
     //  and follow the format described at http://semver.org/
     //
-        "0.70.0-b1"
+        "0.70.0-b2"
 
 #if defined(DEBUG) || defined(SANITIZER)
        "+"
