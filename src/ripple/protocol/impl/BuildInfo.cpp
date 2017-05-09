418fb2698fed0dc414bec7d06caa091405525187
Nik Bougalis <nikb@bougalis.net>
2017-05-09 13:37:49 -0700

    Set version to 0.60.3

--- src/ripple/protocol/impl/BuildInfo.cpp
+++ src/ripple/protocol/impl/BuildInfo.cpp
@@ -33,7 +33,7 @@ char const* const versionString =
     //  The build version number. You must edit this for each release
     //  and follow the format described at http://semver.org/
     //
-        "0.60.2"
+        "0.60.3"
 
 #if defined(DEBUG) || defined(SANITIZER)
        "+"
