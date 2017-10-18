0668247e2c615e00191282ea8a537f5d19fa6d9a
Brad Chase <bchase@ripple.com>
2017-10-18 14:41:36 -0400

    Default account_tx RPC to validated ledger range (RIPD-1544)

--- src/test/unity/rpc_test_unity.cpp
+++ src/test/unity/rpc_test_unity.cpp
@@ -24,6 +24,7 @@
 #include <test/rpc/AccountObjects_test.cpp>
 #include <test/rpc/AccountOffers_test.cpp>
 #include <test/rpc/AccountSet_test.cpp>
+#include <test/rpc/AccountTx_test.cpp>
 #include <test/rpc/AmendmentBlocked_test.cpp>
 #include <test/rpc/Book_test.cpp>
 #include <test/rpc/Feature_test.cpp>
