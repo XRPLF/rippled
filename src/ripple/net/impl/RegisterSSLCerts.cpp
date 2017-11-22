af0d6090a58f9dc70c3af68e31dfb00638d399b8
Brad Chase <bchase@ripple.com>
2017-11-22 09:28:52 -0500

    [FOLD] Improve error handling

--- src/ripple/net/impl/RegisterSSLCerts.cpp
+++ src/ripple/net/impl/RegisterSSLCerts.cpp
@@ -18,7 +18,14 @@
 //==============================================================================
 #include <BeastConfig.h>
 #include <ripple/net/RegisterSSLCerts.h>
-#if BEAST_WINDOWS
+#include <boost/predef.h>
+#if BOOST_OS_WINDOWS
+#include <boost/asio/ssl/error.hpp>
+#include <boost/system/error_code.hpp>
+#include <memory>
+#include <openssl/err.h>
+#include <openssl/ssl.h>
+#include <openssl/x509.h>
 #include <wincrypt.h>
 #endif
 
@@ -27,32 +34,66 @@ namespace ripple {
 void
 registerSSLCerts(boost::asio::ssl::context& ctx, boost::system::error_code& ec)
 {
-#if BEAST_WINDOWS
-    HCERTSTORE hStore = CertOpenSystemStore(0, "ROOT");
-    if (hStore == NULL)
+#if BOOST_OS_WINDOWS
+    auto certStoreDelete = [](void* h) {
+        if (h != nullptr)
+            CertCloseStore(h, 0);
+    };
+    std::unique_ptr<void, decltype(certStoreDelete)> hStore{
+        CertOpenSystemStore(0, "ROOT"), certStoreDelete};
+
+    if (!hStore)
     {
+        ec = boost::system::error_code(
+            GetLastError(), boost::system::system_category());
         return;
     }
 
-    X509_STORE* store = X509_STORE_new();
-    PCCERT_CONTEXT pContext = NULL;
-    while ((pContext = CertEnumCertificatesInStore(hStore, pContext)) != NULL)
+    ERR_clear_error();
+
+    std::unique_ptr<X509_STORE, decltype(X509_STORE_free)*> store{
+        X509_STORE_new(), X509_STORE_free};
+
+    if (!store)
     {
-        X509* x509 = d2i_X509(
-            NULL,
-            (const unsigned char**)&pContext->pbCertEncoded,
-            pContext->cbCertEncoded);
-        if (x509 != NULL)
+        ec = boost::system::error_code(
+            static_cast<int>(::ERR_get_error()),
+            boost::asio::error::get_ssl_category());
+        return;
+    }
+
+    PCCERT_CONTEXT pContext = NULL;
+    while ((pContext = CertEnumCertificatesInStore(hStore.get(), pContext)) !=
+           NULL)
+    {
+        const unsigned char* pbCertEncoded = pContext->pbCertEncoded;
+        std::unique_ptr<X509, decltype(X509_free)*> x509{
+            d2i_X509(NULL, &pbCertEncoded, pContext->cbCertEncoded), X509_free};
+        if (!x509)
         {
-            X509_STORE_add_cert(store, x509);
-            X509_free(x509);
+            ec = boost::system::error_code(
+                static_cast<int>(::ERR_get_error()),
+                boost::asio::error::get_ssl_category());
+            return;
+        }
+
+        if (X509_STORE_add_cert(store.get(), x509.get()) != 1)
+        {
+            ec = boost::system::error_code(
+                static_cast<int>(::ERR_get_error()),
+                boost::asio::error::get_ssl_category());
+            return;
+        }
+        else
+        {
+            // Successfully adding to the store took ownership
+            x509.release();
         }
     }
 
-    CertFreeCertificateContext(pContext);
-    CertCloseStore(hStore, 0);
+    // This takes ownership of the store
+    SSL_CTX_set_cert_store(ctx.native_handle(), store.release());
 
-    SSL_CTX_set_cert_store(ctx.native_handle(), store);
 #else
 
     ctx.set_default_verify_paths(ec);
