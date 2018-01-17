87e37b9e1c43a79a7edbb56e62f2967cb84dc76a
Mike Ellery <mellery451@gmail.com>
2018-01-17 11:40:39 -0800

    Force boost static linking for macos builds

--- Builds/CMake/CMakeFuncs.cmake
+++ Builds/CMake/CMakeFuncs.cmake
@@ -337,8 +337,11 @@ macro(use_boost)
       set(BOOST_ROOT $ENV{CLANG_BOOST_ROOT})
     endif()
 
-
-    if (static)
+    # boost dynamic libraries don't trivially support @rpath
+    # linking right now (cmake's default), so just force
+    # static linking for macos, or if requested on linux
+    # by flag
+    if (static OR APPLE)
       set(Boost_USE_STATIC_LIBS on)
     endif()
     set(Boost_USE_MULTITHREADED on)
