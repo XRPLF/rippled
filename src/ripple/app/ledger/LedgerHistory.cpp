b07da94c31bba3965597aae94f7ea916cc5d1492
Scott Schurr <scott@ripple.com>
2018-02-02 13:47:26 -0800

    Remove two inaccurate asserts:
    The assert in LedgerHistory.cpp was incorrect since
    getLedgerByHash() can return a null object.  The assert has
    been seen to fire inappropriately during online_delete.
    
    The assert in LedgerMaster.cpp was simply misguided.  If a
    null hash were returned by getLedgerHashForHistory(), then we
    should not forward that hash to peers.  The assert only fires
    in a debug build, so a release build would forward the request
    for a null hash to peers.

--- src/ripple/app/ledger/LedgerHistory.cpp
+++ src/ripple/app/ledger/LedgerHistory.cpp
@@ -500,7 +500,6 @@ void LedgerHistory::clearLedgerCachePrior (LedgerIndex seq)
     for (LedgerHash it: m_ledgers_by_hash.getKeys())
     {
         auto const ledger = getLedgerByHash (it);
-        assert(ledger);
         if (!ledger || ledger->info().seq < seq)
             m_ledgers_by_hash.del (it, false);
     }
