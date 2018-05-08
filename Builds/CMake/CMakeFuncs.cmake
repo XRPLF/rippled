3f9dd7785be8aa9ee9ab93ad50a7241e70dab6c3
seelabs <scott.determan@yahoo.com>
2018-05-08 10:58:47 -0400

    [fold] Only turn on `-Wnon-virtual-dtor` for C++ code

--- Builds/CMake/CMakeFuncs.cmake
+++ Builds/CMake/CMakeFuncs.cmake
@@ -641,9 +641,9 @@ macro(setup_build_boilerplate)
 
   if (NOT WIN32)
     add_definitions(-D_FILE_OFFSET_BITS=64)
-    append_flags(CMAKE_CXX_FLAGS -frtti -std=c++14 -Wno-invalid-offsetof -Wdeprecated
+    append_flags(CMAKE_CXX_FLAGS -frtti -std=c++14 -Wno-invalid-offsetof -Wdeprecated -Wnon-virtual-dtor
       -DBOOST_COROUTINE_NO_DEPRECATION_WARNING -DBOOST_COROUTINES_NO_DEPRECATION_WARNING)
-    add_compile_options(-Wall -Wnon-virtual-dtor -Wno-sign-compare -Wno-char-subscripts -Wno-format
+    add_compile_options(-Wall -Wno-sign-compare -Wno-char-subscripts -Wno-format
       -Wno-unused-local-typedefs -g)
     # There seems to be an issue using generator experssions with multiple values,
     # split the expression
