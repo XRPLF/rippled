885d4c1e5a6315780f5b3aa4892f2314ce246e9d
Joe Loser <joeloser93@gmail.com>
2018-05-28 21:18:07 -0500

    [FOLD] Fix alphabetizing of unity test includes from clang-format

--- src/test/unity/basics_test_unity.cpp
+++ src/test/unity/basics_test_unity.cpp
@@ -18,18 +18,18 @@
 */
 //==============================================================================
 
+#include <test/basics/base_uint_test.cpp>
 #include <test/basics/Buffer_test.cpp>
 #include <test/basics/CheckLibraryVersions_test.cpp>
+#include <test/basics/contract_test.cpp>
 #include <test/basics/DetectCrash_test.cpp>
+#include <test/basics/hardened_hash_test.cpp>
 #include <test/basics/KeyCache_test.cpp>
+#include <test/basics/mulDiv_test.cpp>
 #include <test/basics/PerfLog_test.cpp>
+#include <test/basics/qalloc_test.cpp>
 #include <test/basics/RangeSet_test.cpp>
 #include <test/basics/Slice_test.cpp>
 #include <test/basics/StringUtilities_test.cpp>
 #include <test/basics/TaggedCache_test.cpp>
-#include <test/basics/base_uint_test.cpp>
-#include <test/basics/contract_test.cpp>
-#include <test/basics/hardened_hash_test.cpp>
-#include <test/basics/mulDiv_test.cpp>
-#include <test/basics/qalloc_test.cpp>
 #include <test/basics/tagged_integer_test.cpp>
--- src/test/unity/protocol_test_unity.cpp
+++ src/test/unity/protocol_test_unity.cpp
@@ -19,11 +19,14 @@
 //==============================================================================
 
 #include <test/protocol/BuildInfo_test.cpp>
-#include <test/protocol/IOUAmount_test.cpp>
+#include <test/protocol/digest_test.cpp>
 #include <test/protocol/InnerObjectFormats_test.cpp>
+#include <test/protocol/IOUAmount_test.cpp>
 #include <test/protocol/Issue_test.cpp>
 #include <test/protocol/PublicKey_test.cpp>
 #include <test/protocol/Quality_test.cpp>
+#include <test/protocol/SecretKey_test.cpp>
+#include <test/protocol/Seed_test.cpp>
 #include <test/protocol/SOTemplate_test.cpp>
 #include <test/protocol/STAccount_test.cpp>
 #include <test/protocol/STAmount_test.cpp>
@@ -32,9 +35,6 @@
 #include <test/protocol/STTx_test.cpp>
 #include <test/protocol/STValidation_test.cpp>
 #include <test/protocol/STVar_test.cpp>
-#include <test/protocol/SecretKey_test.cpp>
-#include <test/protocol/Seed_test.cpp>
 #include <test/protocol/TER_test.cpp>
-#include <test/protocol/XRPAmount_test.cpp>
-#include <test/protocol/digest_test.cpp>
 #include <test/protocol/types_test.cpp>
+#include <test/protocol/XRPAmount_test.cpp>
