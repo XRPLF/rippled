19c858eb8788e15f1570bc85b34f3c6b8924b861
Joe Loser <joeloser93@gmail.com>
2018-07-18 18:11:42 -0400

    [FOLD] Remove accidental bitcoin copyright

--- src/ripple/basics/literals.h
+++ src/ripple/basics/literals.h
@@ -17,11 +17,6 @@
 */
 //==============================================================================
 
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2011 The Bitcoin developers
-// Distributed under the MIT/X11 software license, see the accompanying
-// file license.txt or http://www.opensource.org/licenses/mit-license.php.
-
 #ifndef RIPPLE_BASICS_LITERALS_H_INCLUDED
 #define RIPPLE_BASICS_LITERALS_H_INCLUDED
 
