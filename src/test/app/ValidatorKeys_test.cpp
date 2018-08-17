c23a215c77d4be4602d6896fac8f18b44dca1bc9
wilsonianb <bwilson@ripple.com>
2018-08-16 22:49:50 -0500

    [FOLD] Improve tests

--- src/test/app/ValidatorKeys_test.cpp
+++ src/test/app/ValidatorKeys_test.cpp
@@ -30,78 +30,81 @@ namespace test {
 
 class ValidatorKeys_test : public beast::unit_test::suite
 {
-    // Used with [validation_seed]
-    const std::string seed = "shUwVw52ofnCUX5m7kPTKzJdr4HEH";
-
-    // Used with [validation_token]
-    const std::string tokenSecretStr =
-        "paQmjZ37pKKPMrgadBLsuf9ab7Y7EUNzh27LQrZqoexpAs31nJi";
-
-    const std::vector<std::string> tokenBlob = {
-        "    "
-        "eyJ2YWxpZGF0aW9uX3NlY3JldF9rZXkiOiI5ZWQ0NWY4NjYyNDFjYzE4YTI3NDdiNT\n",
-        " \tQzODdjMDYyNTkwNzk3MmY0ZTcxOTAyMzFmYWE5Mzc0NTdmYTlkYWY2IiwibWFuaWZl "
-        "    \n",
-        "\tc3QiOiJKQUFBQUFGeEllMUZ0d21pbXZHdEgyaUNjTUpxQzlnVkZLaWxHZncxL3ZDeE"
-        "\n",
-        "\t "
-        "hYWExwbGMyR25NaEFrRTFhZ3FYeEJ3RHdEYklENk9NU1l1TTBGREFscEFnTms4U0tG\t  "
-        "\t\n",
-        "bjdNTzJmZGtjd1JRSWhBT25ndTlzQUtxWFlvdUorbDJWMFcrc0FPa1ZCK1pSUzZQU2\n",
-        "hsSkFmVXNYZkFpQnNWSkdlc2FhZE9KYy9hQVpva1MxdnltR21WcmxIUEtXWDNZeXd1\n",
-        "NmluOEhBU1FLUHVnQkQ2N2tNYVJGR3ZtcEFUSGxHS0pkdkRGbFdQWXk1QXFEZWRGdj\n",
-        "VUSmEydzBpMjFlcTNNWXl3TFZKWm5GT3I3QzBrdzJBaVR6U0NqSXpkaXRROD0ifQ==\n"};
-
-    const std::string tokenManifest =
-        "JAAAAAFxIe1FtwmimvGtH2iCcMJqC9gVFKilGfw1/vCxHXXLplc2GnMhAkE1agqXxBwD"
-        "wDbID6OMSYuM0FDAlpAgNk8SKFn7MO2fdkcwRQIhAOngu9sAKqXYouJ+l2V0W+sAOkVB"
-        "+ZRS6PShlJAfUsXfAiBsVJGesaadOJc/aAZokS1vymGmVrlHPKWX3Yywu6in8HASQKPu"
-        "gBD67kMaRFGvmpATHlGKJdvDFlWPYy5AqDedFv5TJa2w0i21eq3MYywLVJZnFOr7C0kw"
-        "2AiTzSCjIzditQ8=";
-
-    // Manifest does not match private key
-    const std::vector<std::string> invalidTokenBlob = {
-        "eyJtYW5pZmVzdCI6IkpBQUFBQVZ4SWUyOVVBdzViZFJudHJ1elVkREk4aDNGV1JWZl\n",
-        "k3SXVIaUlKQUhJd3MxdzZzM01oQWtsa1VXQWR2RnFRVGRlSEpvS1pNY0hlS0RzOExo\n",
-        "b3d3bDlHOEdkVGNJbmFka1l3UkFJZ0h2Q01lQU1aSzlqQnV2aFhlaFRLRzVDQ3BBR1\n",
-        "k0bGtvZHRXYW84UGhzR3NDSUREVTA1d1c3bWNiMjlVNkMvTHBpZmgvakZPRGhFR21i\n",
-        "NWF6dTJMVHlqL1pjQkpBbitmNGhtQTQ0U0tYbGtTTUFqak1rSWRyR1Rxa21SNjBzVG\n",
-        "JaTjZOOUYwdk9UV3VYcUZ6eDFoSGIyL0RqWElVZXhDVGlITEcxTG9UdUp1eXdXbk55\n",
-        "RFE9PSIsInZhbGlkYXRpb25fc2VjcmV0X2tleSI6IjkyRDhCNDBGMzYwMTc5MTkwMU\n",
-        "MzQTUzMzI3NzBDMkUwMTA4MDI0NTZFOEM2QkI0NEQ0N0FFREQ0NzJGMDQ2RkYifQ==\n"};
-
-    const std::vector<std::string> tokenBlobEd25519 = {
-        "eyJtYW5pZmVzdCI6IkpBQUFBQUp4SWUzbWVnMnh1aTMxWWhyTDMvOHpCOHE3SWduaXJ2d2xQ\n",
-        "TTlqeDBoZy8zODV1bk1oN2ZYUUdaU0l6SjlLMGJFaFNlMjB4RjRrdTcvVFlFVXJTbkFlQnRp\n",
-        "ZitjVUhka0FXZFlJQU5xMTN4WUZ1blEvVElsSFhPeVRSZTgxc2gvbWVQV3pyU2djeFFMMlhI\n",
-        "S0VuKzFIdkJWcnRXZXZ1ay9hUUNqL0pMMnF0Z05KdFlwVkxIUndHY0JKQUM1aWpxMkJVREdi\n",
-        "dUxvRGZxZmRCUVpsMERQdTgzcElzNDlsUXNHKzI5eXZsZmxBcDhCVjd3UE9HK0hYMkF6d2gz\n",
-        "d3FzbXRodURlTENNM25WS1hOZkN3PT0iLCJ2YWxpZGF0aW9uX3NlY3JldF9rZXkiOiI5RUQ0\n",
-        "NUY4NjYyNDFDQzE4QTI3NDdCNTQzODdDMDYyNTkwNzk3MkY0RTcxOTAyMzFGQUE5Mzc0NTdG\n",
-        "QTlEQUY2In0=\n"};
-
-    const std::string tokenManifestEd25519 =
-        "JAAAAAJxIe3meg2xui31YhrL3/8zB8q7IgnirvwlPM9jx0hg/385unMh7fXQGZSIzJ9K"
-        "0bEhSe20xF4ku7/TYEUrSnAeBtif+cUHdkAWdYIANq13xYFunQ/TIlHXOyTRe81sh/me"
-        "PWzrSgcxQL2XHKEn+1HvBVrtWevuk/aQCj/JL2qtgNJtYpVLHRwGcBJAC5ijq2BUDGbu"
-        "LoDfqfdBQZl0DPu83pIs49lQsG+29yvlflAp8BV7wPOG+HX2Azwh3wqsmthuDeLCM3nV"
-        "KXNfCw==";
-
 public:
     void
     run() override
     {
         beast::Journal j;
 
+        auto testMakeValidatorKeys = [&](std::vector<
+            std::pair<std::string, std::vector<std::string>>> const& sections,
+            bool expectValid = true)
         {
-            // No config -> no key but valid
             Config c;
+            for (auto const& s : sections)
+                c.section(s.first).append(s.second);
             ValidatorKeys k{c, j};
-            BEAST_EXPECT(k.publicKey.size() == 0);
-            BEAST_EXPECT(k.manifest.empty());
-            BEAST_EXPECT(!k.configInvalid());
+            BEAST_EXPECT(expectValid ^ k.configInvalid());
+            if (! expectValid)
+            {
+                BEAST_EXPECT(k.publicKey.size() == 0);
+                BEAST_EXPECT(k.manifest.empty());
+            }
+            return k;
+        };
 
+        // No config -> no key but valid
+        testMakeValidatorKeys({});
+
+        {
+            // invalid validation seed
+            std::string const seed = "badseed";
+            testMakeValidatorKeys(
+                {{SECTION_VALIDATION_SEED, {seed}}},
+                false /* expectValid */);
         }
+
+        {
+            // invalid validator token
+            std::string const tokenBlob = "badtoken";
+            testMakeValidatorKeys(
+                {{SECTION_VALIDATOR_TOKEN, {tokenBlob}}},
+                false /* expectValid */);
+        }
+
+        {
+            // Invalid token manifest
+            const std::vector<std::string> invalidTokenBlob = {
+                "eyJtYW5pZmVzdCI6ImJhZG1hbmlmZXN0IiwidmFsaWRhdGlvbl9zZWNyZXR\n",
+                "fa2V5IjoiOTJEOEI0MEYzNjAxNzkxOTAxQzNBNTMzMjc3MEMyRTAxMDgwMj\n",
+                "Q1NkU4QzZCQjQ0RDQ3QUVERDQ3MkYwNDZGRiJ9\n"};
+
+            testMakeValidatorKeys(
+                {{SECTION_VALIDATOR_TOKEN, {invalidTokenBlob}}},
+                false /* expectValid */);
+        }
+
+        {
+            // Manifest does not match private key
+            const std::vector<std::string> invalidTokenBlob = {
+                "eyJtYW5pZmVzdCI6IkpBQUFBQVZ4SWUyOVVBdzViZFJudHJ1elVkREk4aDN\n",
+                "GV1JWZlk3SXVIaUlKQUhJd3MxdzZzM01oQWtsa1VXQWR2RnFRVGRlSEpvS1\n",
+                "pNY0hlS0RzOExob3d3bDlHOEdkVGNJbmFka1l3UkFJZ0h2Q01lQU1aSzlqQ\n",
+                "nV2aFhlaFRLRzVDQ3BBR1k0bGtvZHRXYW84UGhzR3NDSUREVTA1d1c3bWNi\n",
+                "MjlVNkMvTHBpZmgvakZPRGhFR21iNWF6dTJMVHlqL1pjQkpBbitmNGhtQTQ\n",
+                "0U0tYbGtTTUFqak1rSWRyR1Rxa21SNjBzVGJaTjZOOUYwdk9UV3VYcUZ6eD\n",
+                "FoSGIyL0RqWElVZXhDVGlITEcxTG9UdUp1eXdXbk55RFE9PSIsInZhbGlkY\n",
+                "XRpb25fc2VjcmV0X2tleSI6IjkyRDhCNDBGMzYwMTc5MTkwMUMzQTUzMzI3\n",
+                "NzBDMkUwMTA4MDI0NTZFOEM2QkI0NEQ0N0FFREQ0NzJGMDQ2RkYifQ=="};
+
+            // Token manifest and private key must match
+            testMakeValidatorKeys(
+                {{SECTION_VALIDATOR_TOKEN, {invalidTokenBlob}}},
+                false /* expectValid */);
+        }
+
+        const std::string seed = "shUwVw52ofnCUX5m7kPTKzJdr4HEH";
+
         {
             // validation seed section -> empty manifest and valid seeds
             SecretKey const seedSecretKey =
@@ -110,105 +113,99 @@ public:
                 derivePublicKey(KeyType::secp256k1, seedSecretKey);
             NodeID const seedNodeID = calcNodeID(seedPublicKey);
 
-            Config c;
-            c.section(SECTION_VALIDATION_SEED).append(seed);
+            auto const k = testMakeValidatorKeys(
+                {{SECTION_VALIDATION_SEED, {seed}}});
 
-            ValidatorKeys k{c, j};
             BEAST_EXPECT(k.publicKey == seedPublicKey);
             BEAST_EXPECT(k.secretKey == seedSecretKey);
             BEAST_EXPECT(k.nodeID == seedNodeID);
             BEAST_EXPECT(k.manifest.empty());
-            BEAST_EXPECT(!k.configInvalid());
         }
 
-        {
-            // validation seed bad seed -> invalid
-            Config c;
-            c.section(SECTION_VALIDATION_SEED).append("badseed");
-
-            ValidatorKeys k{c, j};
-            BEAST_EXPECT(k.configInvalid());
-            BEAST_EXPECT(k.publicKey.size() == 0);
-            BEAST_EXPECT(k.manifest.empty());
-        }
-
+        const std::string tokenSecretStr =
+            "paQmjZ37pKKPMrgadBLsuf9ab7Y7EUNzh27LQrZqoexpAs31nJi";
         auto const tokenSecretKey = *parseBase58<SecretKey>(
             TokenType::NodePrivate, tokenSecretStr);
 
+        auto testValidatorToken = [&](
+            KeyType const& keyType,
+            std::string const& tokenManifest,
+            std::vector<std::string> const& tokenBlob)
         {
-            // validator token
             auto const tokenPublicKey =
-                derivePublicKey(KeyType::secp256k1, tokenSecretKey);
+                derivePublicKey(keyType, tokenSecretKey);
 
             auto const m = Manifest::make_Manifest(
                 boost::beast::detail::base64_decode(tokenManifest));
             BEAST_EXPECT(m);
             NodeID const tokenNodeID = calcNodeID(m->masterKey);
 
-            Config c;
-            c.section(SECTION_VALIDATOR_TOKEN).append(tokenBlob);
-            ValidatorKeys k{c, j};
+            auto const k = testMakeValidatorKeys(
+                {{SECTION_VALIDATOR_TOKEN, tokenBlob}});
 
             BEAST_EXPECT(k.publicKey == tokenPublicKey);
             BEAST_EXPECT(k.secretKey == tokenSecretKey);
             BEAST_EXPECT(k.nodeID == tokenNodeID);
             BEAST_EXPECT(k.manifest == tokenManifest);
-            BEAST_EXPECT(!k.configInvalid());
+        };
+
+        {
+            // validator token with secp256k1 key
+            const std::vector<std::string> tokenBlob = {
+                "    "
+                "eyJ2YWxpZGF0aW9uX3NlY3JldF9rZXkiOiI5ZWQ0NWY4NjYyNDFjYzE4YTI\n",
+                " \t3NDdiNTQzODdjMDYyNTkwNzk3MmY0ZTcxOTAyMzFmYWE5Mzc0NTdmYTlk "
+                "    \n",
+                "\tYWY2IiwibWFuaWZlc3QiOiJKQUFBQUFGeEllMUZ0d21pbXZHdEgyaUNjTUp"
+                "\n",
+                "\t "
+                "xQzlnVkZLaWxHZncxL3ZDeEhYWExwbGMyR25NaEFrRTFhZ3FYeEJ3RHdE\t  "
+                "\t\n",
+                "YklENk9NU1l1TTBGREFscEFnTms4U0tGbjdNTzJmZGtjd1JRSWhBT25ndTl\n",
+                "zQUtxWFlvdUorbDJWMFcrc0FPa1ZCK1pSUzZQU2hsSkFmVXNYZkFpQnNWSk\n",
+                "dlc2FhZE9KYy9hQVpva1MxdnltR21WcmxIUEtXWDNZeXd1NmluOEhBU1FLU\n",
+                "HVnQkQ2N2tNYVJGR3ZtcEFUSGxHS0pkdkRGbFdQWXk1QXFEZWRGdjVUSmEy\n",
+                "dzBpMjFlcTNNWXl3TFZKWm5GT3I3QzBrdzJBaVR6U0NqSXpkaXRROD0ifQ==\n"
+            };
+
+            const std::string tokenManifest =
+                "JAAAAAFxIe1FtwmimvGtH2iCcMJqC9gVFKilGfw1/vCxHXXLplc2GnMhAkE1ag"
+                "qXxBwDwDbID6OMSYuM0FDAlpAgNk8SKFn7MO2fdkcwRQIhAOngu9sAKqXYouJ+"
+                "l2V0W+sAOkVB+ZRS6PShlJAfUsXfAiBsVJGesaadOJc/aAZokS1vymGmVrlHPK"
+                "WX3Yywu6in8HASQKPugBD67kMaRFGvmpATHlGKJdvDFlWPYy5AqDedFv5TJa2w"
+                "0i21eq3MYywLVJZnFOr7C0kw2AiTzSCjIzditQ8=";
+
+            testValidatorToken(KeyType::secp256k1, tokenManifest, tokenBlob);
+
+            // Cannot specify both seed and token
+            testMakeValidatorKeys({
+                {SECTION_VALIDATION_SEED, {seed}},
+                {SECTION_VALIDATOR_TOKEN, {tokenBlob}}},
+                false /* expectValid */);
         }
 
         {
             // validator token with ed25519 key
-            auto const tokenPublicKey =
-                derivePublicKey(KeyType::ed25519, tokenSecretKey);
-            auto const m = Manifest::make_Manifest(
-                boost::beast::detail::base64_decode(tokenManifestEd25519));
-            BEAST_EXPECT(m);
-            NodeID const tokenNodeID = calcNodeID(m->masterKey);
+            const std::vector<std::string> tokenBlob = {
+                "eyJtYW5pZmVzdCI6IkpBQUFBQUp4SWUzbWVnMnh1aTMxWWhyTDMvOHpCOHE\n",
+                "3SWduaXJ2d2xQTTlqeDBoZy8zODV1bk1oN2ZYUUdaU0l6SjlLMGJFaFNlMj\n",
+                "B4RjRrdTcvVFlFVXJTbkFlQnRpZitjVUhka0FXZFlJQU5xMTN4WUZ1blEvV\n",
+                "ElsSFhPeVRSZTgxc2gvbWVQV3pyU2djeFFMMlhIS0VuKzFIdkJWcnRXZXZ1\n",
+                "ay9hUUNqL0pMMnF0Z05KdFlwVkxIUndHY0JKQUM1aWpxMkJVREdidUxvRGZ\n",
+                "xZmRCUVpsMERQdTgzcElzNDlsUXNHKzI5eXZsZmxBcDhCVjd3UE9HK0hYMk\n",
+                "F6d2gzd3FzbXRodURlTENNM25WS1hOZkN3PT0iLCJ2YWxpZGF0aW9uX3NlY\n",
+                "3JldF9rZXkiOiI5RUQ0NUY4NjYyNDFDQzE4QTI3NDdCNTQzODdDMDYyNTkw\n",
+                "Nzk3MkY0RTcxOTAyMzFGQUE5Mzc0NTdGQTlEQUY2In0=\n"};
 
-            Config c;
-            c.section(SECTION_VALIDATOR_TOKEN).append(tokenBlobEd25519);
-            ValidatorKeys k{c, j};
+            const std::string tokenManifest =
+                "JAAAAAJxIe3meg2xui31YhrL3/8zB8q7IgnirvwlPM9jx0hg/385unMh7fX"
+                "QGZSIzJ9K0bEhSe20xF4ku7/TYEUrSnAeBtif+cUHdkAWdYIANq13xYFunQ"
+                "/TIlHXOyTRe81sh/mePWzrSgcxQL2XHKEn+1HvBVrtWevuk/aQCj/JL2qtg"
+                "NJtYpVLHRwGcBJAC5ijq2BUDGbuLoDfqfdBQZl0DPu83pIs49lQsG+29yvl"
+                "flAp8BV7wPOG+HX2Azwh3wqsmthuDeLCM3nVKXNfCw==";
 
-            BEAST_EXPECT(k.publicKey == tokenPublicKey);
-            BEAST_EXPECT(k.secretKey == tokenSecretKey);
-            BEAST_EXPECT(k.nodeID == tokenNodeID);
-            BEAST_EXPECT(k.manifest == tokenManifestEd25519);
-            BEAST_EXPECT(!k.configInvalid());
+            testValidatorToken(KeyType::ed25519, tokenManifest, tokenBlob);
         }
-
-        {
-            // invalid validator token
-            Config c;
-            c.section(SECTION_VALIDATOR_TOKEN).append("badtoken");
-            ValidatorKeys k{c, j};
-            BEAST_EXPECT(k.configInvalid());
-            BEAST_EXPECT(k.publicKey.size() == 0);
-            BEAST_EXPECT(k.manifest.empty());
-        }
-
-        {
-            // Cannot specify both
-            Config c;
-            c.section(SECTION_VALIDATION_SEED).append(seed);
-            c.section(SECTION_VALIDATOR_TOKEN).append(tokenBlob);
-            ValidatorKeys k{c, j};
-
-            BEAST_EXPECT(k.configInvalid());
-            BEAST_EXPECT(k.publicKey.size() == 0);
-            BEAST_EXPECT(k.manifest.empty());
-        }
-
-        {
-            // Token manifest and private key must match
-            Config c;
-            c.section(SECTION_VALIDATOR_TOKEN).append(invalidTokenBlob);
-            ValidatorKeys k{c, j};
-
-            BEAST_EXPECT(k.configInvalid());
-            BEAST_EXPECT(k.publicKey.size() == 0);
-            BEAST_EXPECT(k.manifest.empty());
-        }
-
     }
 };  // namespace test
 
