c906db9469b058003795ae329f60f82e50d863d3
Joseph Busch <jbusch@ripple.com>
2019-01-14 17:32:10 -0600

    [FOLD] Addressed feedback: Updated example config with new options

--- src/ripple/rpc/handlers/GetCounts.h
+++ src/ripple/rpc/handlers/GetCounts.h
@@ -1,7 +1,7 @@
 //------------------------------------------------------------------------------
 /*
     This file is part of rippled: https://github.com/ripple/rippled
-    Copyright (c) 2012, 2013 Ripple Labs Inc.
+    Copyright (c) 2012-2019 Ripple Labs Inc.
 
     Permission to use, copy, modify, and/or distribute this software for any
     purpose  with  or without fee is hereby granted, provided that the above
