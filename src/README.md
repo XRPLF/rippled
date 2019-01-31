2aed24a5525b6634a94c2c182c633f59a3c9d734
Mike Ellery <mellery451@gmail.com>
2019-01-31 10:19:44 -0800

    Build RocksDB by ExternalProject

--- src/README.md
+++ src/README.md
@@ -18,9 +18,7 @@ Source folders:
 |:----------------|:---------------------------------------------|:------------|
 | `beast`         | N/A                                          | legacy utility code that was formerly associated with boost::beast
 | `ed25519-donna` | https://github.com/floodyberry/ed25519-donna | [Ed25519](http://ed25519.cr.yp.to/) digital signatures |
-| `protobuf`      | https://github.com/google/protobuf           | Protocol buffer data interchange format. Ripple has changed some names in order to support the unity-style of build (a single .cpp added to the project, instead of linking to a separately built static library). |
 | `ripple`        | N/A                                          | **Core source code for `rippled`** |
-| `rocksdb2`      | https://github.com/facebook/rocksdb          | Fast key/value database. (Supports rotational disks better than NuDB.) |
 | `secp256k1`     | https://github.com/bitcoin-core/secp256k1    | ECDSA digital signatures using the **secp256k1** curve |
 | `test`          | N/A                                          | **Unit tests for `rippled`** |
 
@@ -35,4 +33,6 @@ details about how these sources are built :
 | `snappy`        | https://github.com/google/snappy             | "Snappy" lossless compression algorithm. |
 | `soci`          | https://github.com/SOCI/soci                 | Abstraction layer for database access. |
 | `sqlite`        | https://www.sqlite.org/src                   | An embedded database engine that writes to simple files. |
+| `rocksdb`       | https://github.com/facebook/rocksdb          | Fast key/value database. (Supports rotational disks better than NuDB.) |
+| `protobuf`      | https://github.com/google/protobuf           | Protocol buffer data interchange format. Only downloaded/built if a suitable version is not found by find_package, or if the local_protobuf option is explicitly set |
 
--- src/ripple/unity/rocksdb.h
+++ src/ripple/unity/rocksdb.h
@@ -22,29 +22,29 @@
 
 #if RIPPLE_ROCKSDB_AVAILABLE
 //#include <rocksdb2/port/port_posix.h>
-#include <rocksdb2/include/rocksdb/cache.h>
-#include <rocksdb2/include/rocksdb/compaction_filter.h>
-#include <rocksdb2/include/rocksdb/comparator.h>
-#include <rocksdb2/include/rocksdb/convenience.h>
-#include <rocksdb2/include/rocksdb/db.h>
-#include <rocksdb2/include/rocksdb/env.h>
-#include <rocksdb2/include/rocksdb/filter_policy.h>
-#include <rocksdb2/include/rocksdb/flush_block_policy.h>
-#include <rocksdb2/include/rocksdb/iterator.h>
-#include <rocksdb2/include/rocksdb/memtablerep.h>
-#include <rocksdb2/include/rocksdb/merge_operator.h>
-#include <rocksdb2/include/rocksdb/options.h>
-#include <rocksdb2/include/rocksdb/perf_context.h>
-#include <rocksdb2/include/rocksdb/slice.h>
-#include <rocksdb2/include/rocksdb/slice_transform.h>
-#include <rocksdb2/include/rocksdb/statistics.h>
-#include <rocksdb2/include/rocksdb/status.h>
-#include <rocksdb2/include/rocksdb/table.h>
-#include <rocksdb2/include/rocksdb/table_properties.h>
-#include <rocksdb2/include/rocksdb/transaction_log.h>
-#include <rocksdb2/include/rocksdb/types.h>
-#include <rocksdb2/include/rocksdb/universal_compaction.h>
-#include <rocksdb2/include/rocksdb/write_batch.h>
+#include <rocksdb/cache.h>
+#include <rocksdb/compaction_filter.h>
+#include <rocksdb/comparator.h>
+#include <rocksdb/convenience.h>
+#include <rocksdb/db.h>
+#include <rocksdb/env.h>
+#include <rocksdb/filter_policy.h>
+#include <rocksdb/flush_block_policy.h>
+#include <rocksdb/iterator.h>
+#include <rocksdb/memtablerep.h>
+#include <rocksdb/merge_operator.h>
+#include <rocksdb/options.h>
+#include <rocksdb/perf_context.h>
+#include <rocksdb/slice.h>
+#include <rocksdb/slice_transform.h>
+#include <rocksdb/statistics.h>
+#include <rocksdb/status.h>
+#include <rocksdb/table.h>
+#include <rocksdb/table_properties.h>
+#include <rocksdb/transaction_log.h>
+#include <rocksdb/types.h>
+#include <rocksdb/universal_compaction.h>
+#include <rocksdb/write_batch.h>
 
 #endif
 
