0de81700fb16830c6b4a0f96dc4012fd3d6ae6be
John Freeman <jfreeman08@gmail.com>
2019-03-05 13:19:31 -0600

    Add test for revoking regular key

--- src/test/app/SetRegularKey_test.cpp
+++ src/test/app/SetRegularKey_test.cpp
@@ -27,116 +27,106 @@ class SetRegularKey_test : public beast::unit_test::suite
 {
 public:
 
-    void testDisableMasterKey()
-    {
-        testcase("Disable master key");
-        using namespace test::jtx;
-        Env env(*this);
-        Account const alice("alice");
-        Account const bob("bob");
-        env.fund(XRP(10000), alice, bob);
-
-        // Master and Regular key
-        env(regkey(alice, bob));
-        auto const ar = env.le(alice);
-        BEAST_EXPECT(ar->isFieldPresent(sfRegularKey) && (ar->getAccountID(sfRegularKey) == bob.id()));
-
-        env(noop(alice), sig(bob));
-        env(noop(alice), sig(alice));
-
-        // Regular key only
-        env(fset(alice, asfDisableMaster), sig(alice));
-        env(noop(alice), sig(bob));
-        env(noop(alice), sig(alice), ter(tefMASTER_DISABLED));
-        env(fclear(alice, asfDisableMaster), sig(alice), ter(tefMASTER_DISABLED));
-        env(fclear(alice, asfDisableMaster), sig(bob));
-        env(noop(alice), sig(alice));
-    }
-
-    /**
-     * @see https://ripplelabs.atlassian.net/browse/RIPD-1721
-     */
-    void testSetRegularKeyToMasterKey()
-    {
-        testcase("Set regular key to master key (before fix1721)");
-        using namespace test::jtx;
-        Env env {*this, supported_amendments() - fix1721};
-        Account const alice("alice");
-        env.fund(XRP(10000), alice);
-
-        // Must be possible unless amendment `fix1721` enabled.
-        log << "set regular key to master key";
-        env(regkey(alice, alice), sig(alice));
-        log << "disable master key";
-        env(fset(alice, asfDisableMaster), sig(alice));
-        // No way to sign.
-        log << "implicitly sign transaction with master key";
-        env(noop(alice), ter(tefMASTER_DISABLED));
-        env(noop(alice), sig(alice), ter(tefMASTER_DISABLED));
-    }
-
-    /**
-     * @see https://ripplelabs.atlassian.net/browse/RIPD-1721
-     */
-    void testSetRegularKeyToMasterKeyAfterFix1721()
-    {
-        testcase("Set regular key to master key (after fix1721)");
-        using namespace test::jtx;
-        Env env {*this, supported_amendments() | fix1721};
-        Account const alice("alice");
-        env.fund(XRP(10000), alice);
-
-        // Must be possible unless amendment `fix1721` enabled.
-        env(regkey(alice, alice), ter(temCANT_USE_MASTER_KEY));
-    }
-
-    void testPasswordSpent()
-    {
-        testcase("Password spent");
-        using namespace test::jtx;
-        Env env(*this);
-        Account const alice("alice");
-        Account const bob("bob");
-        env.fund(XRP(10000), alice, bob);
-
-        auto ar = env.le(alice);
-        BEAST_EXPECT(ar->isFieldPresent(sfFlags) && ((ar->getFieldU32(sfFlags) & lsfPasswordSpent) == 0));
-
-        env(regkey(alice, bob), sig(alice), fee(0));
-
-        ar = env.le(alice);
-        BEAST_EXPECT(ar->isFieldPresent(sfFlags) && ((ar->getFieldU32(sfFlags) & lsfPasswordSpent) == lsfPasswordSpent));
-
-        // The second SetRegularKey transaction with Fee=0 should fail.
-        env(regkey(alice, bob), sig(alice), fee(0), ter(telINSUF_FEE_P));
-
-        env.trust(bob["USD"](1), alice);
-        env(pay(bob, alice, bob["USD"](1)));
-        ar = env.le(alice);
-        BEAST_EXPECT(ar->isFieldPresent(sfFlags) && ((ar->getFieldU32(sfFlags) & lsfPasswordSpent) == 0));
-    }
-
-    void testUniversalMaskError()
-    {
-        testcase("Universal mask");
-        using namespace test::jtx;
-        Env env(*this);
-        Account const alice("alice");
-        Account const bob("bob");
-        env.fund(XRP(10000), alice, bob);
-
-        auto jv = regkey(alice, bob);
-        jv[sfFlags.fieldName] = tfUniversalMask;
-        env(jv, ter(temINVALID_FLAG));
-    }
-
     void run() override
     {
-        testDisableMasterKey();
-        testSetRegularKeyToMasterKey();
-        testSetRegularKeyToMasterKeyAfterFix1721();
-        testPasswordSpent();
-        testUniversalMaskError();
+        using namespace test::jtx;
+
+        {
+            testcase("Set regular key");
+            Env env(*this);
+            Account const alice("alice");
+            Account const bob("bob");
+            env.fund(XRP(10000), alice, bob);
+
+            env(regkey(alice, bob));
+            auto const ar = env.le(alice);
+            BEAST_EXPECT(ar->isFieldPresent(sfRegularKey) && (ar->getAccountID(sfRegularKey) == bob.id()));
+
+            env(noop(alice), sig(bob));
+            env(noop(alice), sig(alice));
+
+            testcase("Disable master key");
+            env(fset(alice, asfDisableMaster), sig(alice));
+            env(noop(alice), sig(bob));
+            env(noop(alice), sig(alice), ter(tefMASTER_DISABLED));
+
+            testcase("Re-enable master key");
+            env(fclear(alice, asfDisableMaster), sig(alice), ter(tefMASTER_DISABLED));
+
+            env(fclear(alice, asfDisableMaster), sig(bob));
+            env(noop(alice), sig(bob));
+            env(noop(alice), sig(alice));
+
+            testcase("Revoke regular key");
+            env(regkey(alice, disabled));
+            env(noop(alice), sig(bob), ter(tefBAD_AUTH_MASTER));
+            env(noop(alice), sig(alice));
+        }
+
+        {
+            // See https://ripplelabs.atlassian.net/browse/RIPD-1721.
+            testcase("Set regular key to master key (before fix1721)");
+            Env env {*this, supported_amendments() - fix1721};
+            Account const alice("alice");
+            env.fund(XRP(10000), alice);
+
+            // Must be possible unless amendment `fix1721` enabled.
+            log << "set regular key to master key";
+            env(regkey(alice, alice), sig(alice));
+            log << "disable master key";
+            env(fset(alice, asfDisableMaster), sig(alice));
+            // No way to sign.
+            log << "implicitly sign transaction with master key";
+            env(noop(alice), ter(tefMASTER_DISABLED));
+            env(noop(alice), sig(alice), ter(tefMASTER_DISABLED));
+        }
+
+        {
+            testcase("Set regular key to master key (after fix1721)");
+            Env env {*this, supported_amendments() | fix1721};
+            Account const alice("alice");
+            env.fund(XRP(10000), alice);
+
+            // Must be possible unless amendment `fix1721` enabled.
+            env(regkey(alice, alice), ter(temCANT_USE_MASTER_KEY));
+        }
+
+        {
+            testcase("Password spent");
+            Env env(*this);
+            Account const alice("alice");
+            Account const bob("bob");
+            env.fund(XRP(10000), alice, bob);
+
+            auto ar = env.le(alice);
+            BEAST_EXPECT(ar->isFieldPresent(sfFlags) && ((ar->getFieldU32(sfFlags) & lsfPasswordSpent) == 0));
+
+            env(regkey(alice, bob), sig(alice), fee(0));
+
+            ar = env.le(alice);
+            BEAST_EXPECT(ar->isFieldPresent(sfFlags) && ((ar->getFieldU32(sfFlags) & lsfPasswordSpent) == lsfPasswordSpent));
+
+            // The second SetRegularKey transaction with Fee=0 should fail.
+            env(regkey(alice, bob), sig(alice), fee(0), ter(telINSUF_FEE_P));
+
+            env.trust(bob["USD"](1), alice);
+            env(pay(bob, alice, bob["USD"](1)));
+            ar = env.le(alice);
+            BEAST_EXPECT(ar->isFieldPresent(sfFlags) && ((ar->getFieldU32(sfFlags) & lsfPasswordSpent) == 0));
+        }
+
+        {
+            testcase("Universal mask");
+            Env env(*this);
+            Account const alice("alice");
+            Account const bob("bob");
+            env.fund(XRP(10000), alice, bob);
+
+            auto jv = regkey(alice, bob);
+            jv[sfFlags.fieldName] = tfUniversalMask;
+            env(jv, ter(temINVALID_FLAG));
+        }
+
     }
 
 };
