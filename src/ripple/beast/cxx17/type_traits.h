f94b660b37ba4ae04935304700060d66783f0165
seelabs <scott.determan@yahoo.com>
2019-03-22 09:19:35 -0400

    Enable c++-17

--- src/ripple/beast/cxx17/type_traits.h
+++ src/ripple/beast/cxx17/type_traits.h
@@ -24,82 +24,18 @@
 #include <type_traits>
 #include <utility>
 
+#if defined(_LIBCPP_VERSION) && _LIBCPP_VERSION < 6000
+
 namespace std {
 
-#ifndef _MSC_VER
+template <class _Ret, class _Fn, class... _Args>
+struct _LIBCPP_TEMPLATE_VIS is_invocable_r
+    : integral_constant<bool, __invokable_r<_Ret, _Fn, _Args...>::value>
+{
+};
 
-    #if ! __cpp_lib_void_t
-
-        template<class...>
-        using void_t = void;
-
-    #endif // ! __cpp_lib_void_t
-
-    #if ! __cpp_lib_bool_constant
-
-        template<bool B>
-        using bool_constant = std::integral_constant<bool, B>;
-
-    #endif // ! __cpp_lib_bool_constant
+}  // namespace std
 
 #endif
-
-// Ideas from Howard Hinnant
-//
-// Specializations of is_constructible for pair and tuple which
-// work around an apparent defect in the standard that causes well
-// formed expressions involving pairs or tuples of non default-constructible
-// types to generate compile errors.
-//
-template <class T, class U>
-struct is_constructible <pair <T, U>>
-    : integral_constant <bool,
-        is_default_constructible <T>::value &&
-        is_default_constructible <U>::value>
-{
-    explicit is_constructible() = default;
-};
-
-//------------------------------------------------------------------------------
-
-namespace detail {
-template<class R, class C, class ...A>
-auto
-is_invocable_test(C&& c, int, A&& ...a)
-        -> decltype(std::is_convertible<
-                    decltype(c(std::forward<A>(a)...)), R>::value ||
-                    std::is_same<R, void>::value,
-                    std::true_type());
-
-template<class R, class C, class ...A>
-std::false_type
-is_invocable_test(C&& c, long, A&& ...a);
-} // detail
-
-/** Metafunction returns `true` if F callable as R(A...)
-
-    Example:
-
-    @code
-    is_invocable<T, void(std::string)>
-    @endcode
-*/
-/** @{ */
-template<class C, class F>
-struct is_invocable : std::false_type
-{
-};
-
-template<class C, class R, class ...A>
-struct is_invocable<C, R(A...)>
-        : decltype(std::detail::is_invocable_test<R>(
-            std::declval<C>(), 1, std::declval<A>()...))
-{
-};
-/** @} */
-
-//------------------------------------------------------------------------------
-
-} // std
 
 #endif
