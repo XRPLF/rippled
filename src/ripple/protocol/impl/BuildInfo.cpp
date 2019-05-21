e83d367f49cceb4b5f28830fb1965a5978744450
Manoj doshi <manoj.s.doshi@gmail.com>
2019-05-21 09:13:56 -0700

    Set version to 1.3.0-b4

--- src/ripple/protocol/impl/BuildInfo.cpp
+++ src/ripple/protocol/impl/BuildInfo.cpp
@@ -30,7 +30,7 @@ namespace BuildInfo {
 //  The build version number. You must edit this for each release
 //  and follow the format described at http://semver.org/
 //------------------------------------------------------------------------------
-char const* const versionString = "1.3.0-b3"
+char const* const versionString = "1.3.0-b4"
 
 #if defined(DEBUG) || defined(SANITIZER)
        "+"
