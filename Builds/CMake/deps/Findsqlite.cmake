213edf8eda457387864ea413cb661e07aeb9daa1
Mo Morsi <mo@morsi.org>
2019-10-30 12:17:35 -0400

    Search for sqlite3 before vendoring

---
+++ Builds/CMake/deps/Findsqlite.cmake
@@ -0,0 +1,22 @@
+find_package (PkgConfig REQUIRED)
+pkg_search_module (sqlite_PC QUIET sqlite3>=3.26.0)
+
+if(static)
+  set(SQLITE_LIB libsqlite3.a)
+else()
+  set(SQLITE_LIB sqlite3.so)
+endif()
+
+find_library (sqlite3
+  NAMES ${SQLITE_LIB}
+  HINTS
+    ${sqlite_PC_LIBDIR}
+    ${sqlite_PC_LIBRARY_DIRS}
+  NO_DEFAULT_PATH)
+
+find_path (SQLITE_INCLUDE_DIR
+  NAMES sqlite3.h
+  HINTS
+    ${sqlite_PC_INCLUDEDIR}
+    ${sqlite_PC_INCLUDEDIRS}
+  NO_DEFAULT_PATH)
