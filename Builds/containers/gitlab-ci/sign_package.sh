767c2a5d38b097802908210855439deeafac011a
John Northrup <jnorthrup@ripple.com>
2019-11-01 23:42:28 -0500

    revised package signing, looking for package artifacts

--- Builds/containers/gitlab-ci/sign_package.sh
+++ Builds/containers/gitlab-ci/sign_package.sh
@@ -1,16 +1,37 @@
-#!/usr/bin/env sh
-set -ex
-apt -y update
-apt -y install software-properties-common curl git gnupg
-curl -sk -o rippled-pubkeys.txt "${GIT_SIGN_PUBKEYS_URL}"
-gpg --import rippled-pubkeys.txt
-if git verify-commit HEAD; then
-    echo "git commit signature check passed"
-else
-    echo "git commit signature check failed"
-    git log -n 5 --color \
-        --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an> [%G?]%Creset' \
-        --abbrev-commit
-    exit 1
-fi
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+sign_dpkg() {
+  if [ -n "${GPG_KEYID}" ]; then
+    dpkg-sig \
+      -g "--no-tty --digest-algo 'sha512' --passphrase '${GPG_PASSPHRASE}' --pinentry-mode=loopback" \
+			-k "${GPG_KEYID}" \
+			--sign builder \
+			"out/deb/${PACKAGE_NAME}_${PACKAGE_ARCH}.deb"
+	fi
+}
+
+sign_rpm() {
+  if [ -n "${GPG_KEYID}" ] ; then
+	echo "yes" | setsid rpm \
+			--define "_gpg_name ${GPG_KEYID}" \
+			--define "_signature gpg" \
+			--define "__gpg_check_password_cmd /bin/true" \
+			--define "__gpg_sign_cmd %{__gpg} gpg --batch --no-armor --digest-algo 'sha512' --passphrase '${GPG_PASSPHRASE}' --pinentry-mode=loopback --no-secmem-warning -u '%{_gpg_name}' --sign --detach-sign --output %{__signature_filename} %{__plaintext_filename}" \
+			--addsign "out/rpm/${PACKAGE_NAME}_${PACKAGE_ARCH}.rpm"
+	fi
+}
+
+case "${1}" in
+    dpkg)
+        sign_dpkg
+        ;;
+    rpm)
+        sign_rpm
+        ;;
+    *)
+        echo "Usage: ${0} (dpkg|rpm)"
+        ;;
+esac
 
