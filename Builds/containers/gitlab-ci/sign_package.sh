c9b773345fc4672cc4edff150ab6d85378553554
John Northrup <jnorthrup@ripple.com>
2019-11-03 02:30:42 -0600

    loop through rpm files

--- Builds/containers/gitlab-ci/sign_package.sh
+++ Builds/containers/gitlab-ci/sign_package.sh
@@ -14,12 +14,13 @@ sign_dpkg() {
 
 sign_rpm() {
   if [ -n "${GPG_KEYID}" ] ; then
+    find build/rpm/packages -name "*.rpm" -exec bash -c '
 	echo "yes" | setsid rpm \
 			--define "_gpg_name ${GPG_KEYID}" \
 			--define "_signature gpg" \
 			--define "__gpg_check_password_cmd /bin/true" \
 			--define "__gpg_sign_cmd %{__gpg} gpg --batch --no-armor --digest-algo 'sha512' --passphrase '${GPG_PASSPHRASE}' --pinentry-mode=loopback --no-secmem-warning -u '%{_gpg_name}' --sign --detach-sign --output %{__signature_filename} %{__plaintext_filename}" \
-			--addsign "build/rpm/packages/*.rpm"
+			--addsign '{} \;
 	fi
 }
 
