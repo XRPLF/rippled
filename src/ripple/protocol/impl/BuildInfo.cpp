232975bfdbde12a65499130d78f938f261c989ba
Manoj doshi <mdoshi@ripple.com>
2019-11-04 11:45:24 -0800

    Set version to 1.4.0-rc1

--- src/ripple/protocol/impl/BuildInfo.cpp
+++ src/ripple/protocol/impl/BuildInfo.cpp
@@ -30,7 +30,7 @@ namespace BuildInfo {
 //  The build version number. You must edit this for each release
 //  and follow the format described at http://semver.org/
 //------------------------------------------------------------------------------
-char const* const versionString = "1.4.0-b8"
+char const* const versionString = "1.4.0-rc1"
 
 #if defined(DEBUG) || defined(SANITIZER)
        "+"
