4a1148eb2849513dd1e7ae080288fd47ab57a376
Nik Bougalis <nikb@bougalis.net>
2019-11-13 20:17:05 -0800

    Set version to 1.5.0-b1

--- src/ripple/protocol/impl/BuildInfo.cpp
+++ src/ripple/protocol/impl/BuildInfo.cpp
@@ -30,7 +30,7 @@ namespace BuildInfo {
 //  The build version number. You must edit this for each release
 //  and follow the format described at http://semver.org/
 //------------------------------------------------------------------------------
-char const* const versionString = "1.4.0"
+char const* const versionString = "1.5.0-b1"
 
 #if defined(DEBUG) || defined(SANITIZER)
         "+"
