ae8df949743d3b0b675a18ce4b0ab474edafa1d1
Howard Hinnant <howard.hinnant@gmail.com>
2019-12-12 16:51:09 -0800

    [FOLD] Add comment

--- src/ripple/core/JobQueue.h
+++ src/ripple/core/JobQueue.h
@@ -27,8 +27,8 @@
 #include <ripple/core/Stoppable.h>
 #include <ripple/core/impl/Workers.h>
 #include <ripple/json/json_value.h>
-#include <boost/range/begin.hpp>
-#include <boost/range/end.hpp>
+#include <boost/range/begin.hpp>   // workaround for boost 1.72 bug
+#include <boost/range/end.hpp>     // workaround for boost 1.72 bug
 #include <boost/coroutine/all.hpp>
 
 namespace ripple {
