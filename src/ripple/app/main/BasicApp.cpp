3a779907816ca2a86f8d3577cde4eaabc64f8e15
Nik Bougalis <nikb@bougalis.net>
2019-12-15 20:37:10 -0800

    Improve automatic I/O thread tuning algorithm

--- src/ripple/app/main/BasicApp.cpp
+++ src/ripple/app/main/BasicApp.cpp
@@ -24,20 +24,22 @@ BasicApp::BasicApp(std::size_t numberOfThreads)
 {
     work_.emplace (io_service_);
     threads_.reserve(numberOfThreads);
+
     while(numberOfThreads--)
-        threads_.emplace_back(
-            [this, numberOfThreads]()
+    {
+        threads_.emplace_back([this, numberOfThreads]()
             {
-                beast::setCurrentThreadName(
-                    std::string("io_service #") +
-                        std::to_string(numberOfThreads));
-                this->io_service_.run();
+              beast::setCurrentThreadName("io svc #" +
+                  std::to_string(numberOfThreads));
+              this->io_service_.run();
             });
+    }
 }
 
 BasicApp::~BasicApp()
 {
     work_ = boost::none;
-    for (auto& _ : threads_)
-        _.join();
+
+    for (auto& t : threads_)
+        t.join();
 }
