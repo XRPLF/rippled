XRPLF/rippled

commit 66fad62e661bc8b8739d109e78c18c8d5002c6e9

Implement Shard SQLite support

src/ripple/app/main/Application.cpp
@@ -582,7 +582,7 @@ public:
     void signalStop() override;
     bool checkSigs() const override;
     void checkSigs(bool) override;
-    int fdlimit () const override;
+    int fdRequired() const override;
 
     //--------------------------------------------------------------------------
 
@@ -858,12 +858,12 @@ public:
             // transaction database
             mTxnDB = std::make_unique <DatabaseCon>(
                 setup,
-                TxnDBName,
-                TxnDBInit,
-                TxnDBCount);
+                TxDBName,
+                TxDBPragma,
+                TxDBInit);
             mTxnDB->getSession() <<
                 boost::str(boost::format("PRAGMA cache_size=-%d;") %
-                (config_->getSize(siTxnDBCache) * kilobytes(1)));
+                kilobytes(config_->getSize(siTxnDBCache)));
             mTxnDB->setupCheckpointing(m_jobQueue.get(), logs());
 
             if (!setup.standAlone ||
@@ -900,20 +900,20 @@ public:
             // ledger database
             mLedgerDB = std::make_unique <DatabaseCon>(
                 setup,
-                LedgerDBName,
-                LedgerDBInit,
-                LedgerDBCount);
+                LgrDBName,
+                LgrDBPragma,
+                LgrDBInit);
             mLedgerDB->getSession() <<
                 boost::str(boost::format("PRAGMA cache_size=-%d;") %
-                (config_->getSize(siLgrDBCache) * kilobytes(1)));
+                kilobytes(config_->getSize(siLgrDBCache)));
             mLedgerDB->setupCheckpointing(m_jobQueue.get(), logs());
 
             // wallet database
             mWalletDB = std::make_unique <DatabaseCon>(
                 setup,
                 WalletDBName,
-                WalletDBInit,
-                WalletDBCount);
+                std::array<char const*, 0>(),
+                WalletDBInit);
         }
         catch (std::exception const& e)
         {
@@ -963,11 +963,8 @@ public:
         family().treecache().setTargetAge(
             seconds{config_->getSize(siTreeCacheAge)});
 
-        if (shardStore_)
+        if (sFamily_)
         {
-            shardStore_->tune(
-                config_->getSize(siNodeCacheSize),
-                seconds{config_->getSize(siNodeCacheAge)});
             sFamily_->treecache().setTargetSize(
                 config_->getSize(siTreeCacheSize));
             sFamily_->treecache().setTargetAge(
@@ -1174,9 +1171,10 @@ public:
             }
 
             DatabaseCon::Setup dbSetup = setup_DatabaseCon(*config_);
-            boost::filesystem::path dbPath = dbSetup.dataDir / TxnDBName;
+            boost::filesystem::path dbPath = dbSetup.dataDir / TxDBName;
             boost::system::error_code ec;
-            boost::optional<std::uint64_t> dbSize = boost::filesystem::file_size(dbPath, ec);
+            boost::optional<std::uint64_t> dbSize =
+                boost::filesystem::file_size(dbPath, ec);
             if (ec)
             {
                 JLOG(m_journal.error())
@@ -1632,7 +1630,7 @@ void ApplicationImp::checkSigs(bool check)
     checkSigs_ = check;
 }
 
-int ApplicationImp::fdlimit() const
+int ApplicationImp::fdRequired() const
 {
     // Standard handles, config file, misc I/O etc:
     int needed = 128;
@@ -1642,10 +1640,10 @@ int ApplicationImp::fdlimit() const
 
     // the number of fds needed by the backend (internally
     // doubled if online delete is enabled).
-    needed += std::max(5, m_shaMapStore->fdlimit());
+    needed += std::max(5, m_shaMapStore->fdRequired());
 
     if (shardStore_)
-        needed += shardStore_->fdlimit();
+        needed += shardStore_->fdRequired();
 
     // One fd per incoming connection a port can accept, or
     // if no limit is set, assume it'll handle 256 clients.

src/ripple/app/misc/SHAMapStoreImp.cpp
@@ -254,7 +254,7 @@ SHAMapStoreImp::makeNodeStore(std::string const& name, std::int32_t readThreads)
             std::move(archiveBackend),
             app_.config().section(ConfigSection::nodeDatabase()),
             app_.logs().journal(nodeStoreName_));
-        fdlimit_ += dbr->fdlimit();
+        fdRequired_ += dbr->fdRequired();
         dbRotating_ = dbr.get();
         db.reset(dynamic_cast<NodeStore::Database*>(dbr.release()));
     }
@@ -267,7 +267,7 @@ SHAMapStoreImp::makeNodeStore(std::string const& name, std::int32_t readThreads)
             app_.getJobQueue(),
             app_.config().section(ConfigSection::nodeDatabase()),
             app_.logs().journal(nodeStoreName_));
-        fdlimit_ += db->fdlimit();
+        fdRequired_ += db->fdRequired();
     }
     return db;
 }
@@ -298,9 +298,9 @@ SHAMapStoreImp::rendezvous() const
 }
 
 int
-SHAMapStoreImp::fdlimit () const
+SHAMapStoreImp::fdRequired() const
 {
-    return fdlimit_;
+    return fdRequired_;
 }
 
 bool

src/ripple/core/Config.h
@@ -44,7 +44,7 @@ class Rules;
 
 enum SizedItemName
 {
-    siSweepInterval,
+    siSweepInterval = 0,
     siNodeCacheSize,
     siNodeCacheAge,
     siTreeCacheSize,
@@ -56,14 +56,32 @@ enum SizedItemName
     siLedgerFetch,
     siHashNodeDBCache,
     siTxnDBCache,
-    siLgrDBCache,
+    siLgrDBCache
 };
 
-struct SizedItem
-{
-    SizedItemName   item;
-    int             sizes[5];
-};
+static constexpr
+std::array<std::array<int, 5>, 13> sizedItems
+{{
+    //  tiny      small   medium  large   huge
+    {{  10,       30,     60,     90,     120     }}, // siSweepInterval
+    {{  2,        3,      5,      5,      8       }}, // siLedgerFetch
+
+    {{  16384,    32768,  131072, 262144, 524288  }}, // siNodeCacheSize
+    {{  60,       90,     120,    900,    1800    }}, // siNodeCacheAge
+
+    {{  128000,   256000, 512000, 768000, 2048000 }}, // siTreeCacheSize
+    {{  30,       60,     90,     120,    900     }}, // siTreeCacheAge
+
+    {{  4096,     8192,   16384,  65536,  131072  }}, // siSLECacheSize
+    {{  30,       60,     90,     120,    300     }}, // siSLECacheAge
+
+    {{  32,       128,    256,    384,    768     }}, // siLedgerSize
+    {{  30,       90,     180,    240,    900     }}, // siLedgerAge
+
+    {{  4,        12,     24,     64,     128     }}, // siHashNodeDBCache
+    {{  4,        12,     24,     64,     128     }}, // siTxnDBCache
+    {{  4,        8,      16,     32,     128     }}  // siLgrDBCache
+}};
 
 //  This entire derived class is deprecated.
 //  For new config information use the style implied
@@ -182,11 +200,23 @@ public:
     std::unordered_set<uint256, beast::uhash<>> features;
 
 public:
-    Config()
-    : j_ {beast::Journal::getNullSink()}
-    { }
+    Config() : j_ {beast::Journal::getNullSink()} {}
 
-    int getSize (SizedItemName) const;
+    static
+    int
+    getSize(SizedItemName item, std::uint32_t nodeSize)
+    {
+        assert(item < sizedItems.size() && nodeSize < sizedItems[item].size());
+        return sizedItems[item][nodeSize];
+    }
+
+    int
+    getSize(SizedItemName item) const
+    {
+        assert(item < sizedItems.size());
+        return getSize(item, NODE_SIZE);
+    }
+
     /* Be very careful to make sure these bool params
         are in the right order. */
     void setup (std::string const& strConf, bool bQuiet,

src/ripple/core/impl/Config.cpp
@@ -559,42 +559,6 @@ void Config::loadFromString (std::string const& fileContents)
     }
 }
 
-int Config::getSize (SizedItemName item) const
-{
-    SizedItem sizeTable[] =   //    tiny    small   medium  large       huge
-    {
-
-        { siSweepInterval,      {   10,     30,     60,     90,         120     } },
-
-        { siLedgerFetch,        {   2,      3,      5,      5,          8       } },
-
-        { siNodeCacheSize,      {   16384,  32768,  131072, 262144,     524288  } },
-        { siNodeCacheAge,       {   60,     90,     120,    900,        1800    } },
-
-        { siTreeCacheSize,      {   128000, 256000, 512000, 768000,     2048000 } },
-        { siTreeCacheAge,       {   30,     60,     90,     120,        900     } },
-
-        { siSLECacheSize,       {   4096,   8192,   16384,  65536,      131072  } },
-        { siSLECacheAge,        {   30,     60,     90,     120,        300     } },
-
-        { siLedgerSize,         {   32,     128,    256,    384,        768     } },
-        { siLedgerAge,          {   30,     90,     180,    240,        900     } },
-
-        { siHashNodeDBCache,    {   4,      12,     24,     64,         128     } },
-        { siTxnDBCache,         {   4,      12,     24,     64,         128     } },
-        { siLgrDBCache,         {   4,      8,      16,     32,         128     } },
-    };
-
-    for (int i = 0; i < (sizeof (sizeTable) / sizeof (SizedItem)); ++i)
-    {
-        if (sizeTable[i].item == item)
-            return sizeTable[i].sizes[NODE_SIZE];
-    }
-
-    assert (false);
-    return -1;
-}
-
 boost::filesystem::path Config::getDebugLogFile () const
 {
     auto log_file = DEBUG_LOGFILE;

CMakeLists.txt
@@ -2086,7 +2086,6 @@ else ()
     src/ripple/app/main/Application.cpp
     src/ripple/app/main/BasicApp.cpp
     src/ripple/app/main/CollectorManager.cpp
-    src/ripple/app/main/DBInit.cpp
     src/ripple/app/main/LoadManager.cpp
     src/ripple/app/main/Main.cpp
     src/ripple/app/main/NodeIdentity.cpp

src/ripple/app/main/Application.h
@@ -183,8 +183,8 @@ public:
 
     virtual beast::Journal journal (std::string const& name) = 0;
 
-    /* Returns the number of file descriptors the application wants */
-    virtual int fdlimit () const = 0;
+    /* Returns the number of file descriptors the application needs */
+    virtual int fdRequired() const = 0;
 
     /** Retrieve the "wallet database" */
     virtual DatabaseCon& getWalletDB () = 0;

src/ripple/app/main/DBInit.cpp
@@ -1,149 +1,0 @@
-//------------------------------------------------------------------------------
-/*
-    This file is part of rippled: https://github.com/ripple/rippled
-    Copyright (c) 2012, 2013 Ripple Labs Inc.
-
-    Permission to use, copy, modify, and/or distribute this software for any
-    purpose  with  or without fee is hereby granted, provided that the above
-    copyright notice and this permission notice appear in all copies.
-
-    THE  SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-    WITH  REGARD  TO  THIS  SOFTWARE  INCLUDING  ALL  IMPLIED  WARRANTIES  OF
-    MERCHANTABILITY  AND  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-    ANY  SPECIAL ,  DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-    WHATSOEVER  RESULTING  FROM  LOSS  OF USE, DATA OR PROFITS, WHETHER IN AN
-    ACTION  OF  CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-*/
-//==============================================================================
-
-#include <ripple/app/main/DBInit.h>
-#include <type_traits>
-
-namespace ripple {
-
-// Transaction database holds transactions and public keys
-const char* TxnDBName = "transaction.db";
-const char* TxnDBInit[] =
-{
-    "PRAGMA page_size=4096;",
-    "PRAGMA synchronous=NORMAL;",
-    "PRAGMA journal_mode=WAL;",
-    "PRAGMA journal_size_limit=1582080;",
-    "PRAGMA max_page_count=2147483646;",
-
-#if (ULONG_MAX > UINT_MAX) && !defined (NO_SQLITE_MMAP)
-    "PRAGMA mmap_size=17179869184;",
-#endif
-
-    "BEGIN TRANSACTION;",
-
-    "CREATE TABLE IF NOT EXISTS Transactions (                \
-        TransID     CHARACTER(64) PRIMARY KEY,  \
-        TransType   CHARACTER(24),              \
-        FromAcct    CHARACTER(35),              \
-        FromSeq     BIGINT UNSIGNED,            \
-        LedgerSeq   BIGINT UNSIGNED,            \
-        Status      CHARACTER(1),               \
-        RawTxn      BLOB,                       \
-        TxnMeta     BLOB                        \
-    );",
-    "CREATE INDEX IF NOT EXISTS TxLgrIndex ON                 \
-        Transactions(LedgerSeq);",
-
-    "CREATE TABLE IF NOT EXISTS AccountTransactions (         \
-        TransID     CHARACTER(64),              \
-        Account     CHARACTER(64),              \
-        LedgerSeq   BIGINT UNSIGNED,            \
-        TxnSeq      INTEGER                     \
-    );",
-    "CREATE INDEX IF NOT EXISTS AcctTxIDIndex ON              \
-        AccountTransactions(TransID);",
-    "CREATE INDEX IF NOT EXISTS AcctTxIndex ON                \
-        AccountTransactions(Account, LedgerSeq, TxnSeq, TransID);",
-    "CREATE INDEX IF NOT EXISTS AcctLgrIndex ON               \
-        AccountTransactions(LedgerSeq, Account, TransID);",
-
-    "END TRANSACTION;"
-};
-int TxnDBCount = std::extent<decltype(TxnDBInit)>::value;
-
-// Ledger database holds ledgers and ledger confirmations
-const char* LedgerDBName = "ledger.db";
-const char* LedgerDBInit[] =
-{
-    "PRAGMA synchronous=NORMAL;",
-    "PRAGMA journal_mode=WAL;",
-    "PRAGMA journal_size_limit=1582080;",
-
-    "BEGIN TRANSACTION;",
-
-    "CREATE TABLE IF NOT EXISTS Ledgers (           \
-        LedgerHash      CHARACTER(64) PRIMARY KEY,  \
-        LedgerSeq       BIGINT UNSIGNED,            \
-        PrevHash        CHARACTER(64),              \
-        TotalCoins      BIGINT UNSIGNED,            \
-        ClosingTime     BIGINT UNSIGNED,            \
-        PrevClosingTime BIGINT UNSIGNED,            \
-        CloseTimeRes    BIGINT UNSIGNED,            \
-        CloseFlags      BIGINT UNSIGNED,            \
-        AccountSetHash  CHARACTER(64),              \
-        TransSetHash    CHARACTER(64)               \
-    );",
-    "CREATE INDEX IF NOT EXISTS SeqLedger ON Ledgers(LedgerSeq);",
-
-    // Old table and indexes no longer needed
-    "DROP TABLE IF EXISTS Validations;",
-
-    "END TRANSACTION;"
-};
-int LedgerDBCount = std::extent<decltype(LedgerDBInit)>::value;
-
-const char* WalletDBName = "wallet.db";
-const char* WalletDBInit[] =
-{
-    "BEGIN TRANSACTION;",
-
-    // A node's identity must be persisted, including
-    // for clustering purposes. This table holds one
-    // entry: the server's unique identity, but the
-    // value can be overriden by specifying a node
-    // identity in the config file using a [node_seed]
-    // entry.
-    "CREATE TABLE IF NOT EXISTS NodeIdentity (      \
-        PublicKey       CHARACTER(53),              \
-        PrivateKey      CHARACTER(52)               \
-    );",
-
-    // Peer reservations.
-    "CREATE TABLE IF NOT EXISTS PeerReservations (     \
-        PublicKey       CHARACTER(53) UNIQUE NOT NULL, \
-        Description     CHARACTER(64) NOT NULL         \
-    );",
-
-    // Validator Manifests
-    "CREATE TABLE IF NOT EXISTS ValidatorManifests ( \
-        RawData          BLOB NOT NULL               \
-    );",
-
-    "CREATE TABLE IF NOT EXISTS PublisherManifests ( \
-        RawData          BLOB NOT NULL               \
-    );",
-
-    // Old tables that were present in wallet.db and we
-    // no longer need or use.
-    "DROP INDEX IF EXISTS SeedNodeNext;",
-    "DROP INDEX IF EXISTS SeedDomainNext;",
-    "DROP TABLE IF EXISTS Features;",
-    "DROP TABLE IF EXISTS TrustedNodes;",
-    "DROP TABLE IF EXISTS ValidatorReferrals;",
-    "DROP TABLE IF EXISTS IpReferrals;",
-    "DROP TABLE IF EXISTS SeedNodes;",
-    "DROP TABLE IF EXISTS SeedDomains;",
-    "DROP TABLE IF EXISTS Misc;",
-
-    "END TRANSACTION;"
-};
-int WalletDBCount = std::extent<decltype(WalletDBInit)>::value;
-
-} // ripple

commit 31459d5bd82976214ce62158fe87d258d1a24fb4

Fix node auto-configuration code:
The `node_size` configuration option is used to automatically
configure various parameters (cache sizes, timeouts, etc) for
the server.

A previous commit included changes that caused incorrect values
to be returned which can result in sub-optimal performance that
can manifest as difficulty syncing to the network, or increased
disk I/O and/or memory usage. The problem was introduced with
commit 66fad62e661bc8b8739d109e78c18c8d5002c6e9.

This commit, if merged, fixes the code to ensure that the correct
values are returned and introduces a compile-time check to prevent
this issue from reoccurring.

src/ripple/app/ledger/impl/LedgerMaster.cpp
@@ -152,7 +152,7 @@ LedgerMaster::LedgerMaster (Application& app, Stopwatch& stopwatch,
     , fetch_depth_ (app_.getSHAMapStore ().clampFetchDepth (
         app_.config().FETCH_DEPTH))
     , ledger_history_ (app_.config().LEDGER_HISTORY)
-    , ledger_fetch_size_ (app_.config().getSize (siLedgerFetch))
+    , ledger_fetch_size_ (app_.config().getValueFor(SizedItem::ledgerFetch))
     , fetch_packs_ ("FetchPack", 65536, std::chrono::seconds {45}, stopwatch,
         app_.journal("TaggedCache"))
 {

src/ripple/app/main/Application.cpp
@@ -866,7 +866,7 @@ public:
                 TxDBInit);
             mTxnDB->getSession() <<
                 boost::str(boost::format("PRAGMA cache_size=-%d;") %
-                kilobytes(config_->getSize(siTxnDBCache)));
+                kilobytes(config_->getValueFor(SizedItem::txnDBCache)));
             mTxnDB->setupCheckpointing(m_jobQueue.get(), logs());
 
             if (!setup.standAlone ||
@@ -908,7 +908,7 @@ public:
                 LgrDBInit);
             mLedgerDB->getSession() <<
                 boost::str(boost::format("PRAGMA cache_size=-%d;") %
-                kilobytes(config_->getSize(siLgrDBCache)));
+                kilobytes(config_->getValueFor(SizedItem::lgrDBCache)));
             mLedgerDB->setupCheckpointing(m_jobQueue.get(), logs());
 
             // wallet database
@@ -964,24 +964,24 @@ public:
         // tune caches
         using namespace std::chrono;
         m_nodeStore->tune(
-            config_->getSize(siNodeCacheSize),
-            seconds{config_->getSize(siNodeCacheAge)});
+            config_->getValueFor(SizedItem::nodeCacheSize),
+            seconds{config_->getValueFor(SizedItem::nodeCacheAge)});
 
         m_ledgerMaster->tune(
-            config_->getSize(siLedgerSize),
-            seconds{config_->getSize(siLedgerAge)});
+            config_->getValueFor(SizedItem::ledgerSize),
+            seconds{config_->getValueFor(SizedItem::ledgerAge)});
 
         family().treecache().setTargetSize(
-            config_->getSize (siTreeCacheSize));
+            config_->getValueFor(SizedItem::treeCacheSize));
         family().treecache().setTargetAge(
-            seconds{config_->getSize(siTreeCacheAge)});
+            seconds{config_->getValueFor(SizedItem::treeCacheAge)});
 
         if (sFamily_)
         {
             sFamily_->treecache().setTargetSize(
-                config_->getSize(siTreeCacheSize));
+                config_->getValueFor(SizedItem::treeCacheSize));
             sFamily_->treecache().setTargetAge(
-                seconds{config_->getSize(siTreeCacheAge)});
+                seconds{config_->getValueFor(SizedItem::treeCacheAge)});
         }
 
         return true;
@@ -1135,7 +1135,7 @@ public:
         {
             using namespace std::chrono;
             sweepTimer_.expires_from_now(
-                seconds{config_->getSize(siSweepInterval)});
+                seconds{config_->getValueFor(SizedItem::sweepInterval)});
             sweepTimer_.async_wait (std::move (*optionalCountedHandler));
         }
     }

src/ripple/app/misc/SHAMapStoreImp.cpp
@@ -190,7 +190,7 @@ SHAMapStoreImp::SHAMapStoreImp(
         if (!section.exists("cache_mb"))
         {
             section.set("cache_mb", std::to_string(
-                config.getSize(siHashNodeDBCache)));
+                config.getValueFor(SizedItem::hashNodeDBCache)));
         }
 
         if (!section.exists("filter_bits") && (config.NODE_SIZE >= 2))

src/ripple/core/Config.h
@@ -29,6 +29,7 @@
 #include <boost/filesystem.hpp> // VFALCO FIX: This include should not be here
 #include <boost/lexical_cast.hpp>
 #include <boost/optional.hpp>
+#include <algorithm>
 #include <cstdint>
 #include <map>
 #include <string>
@@ -42,46 +43,68 @@ class Rules;
 
 //------------------------------------------------------------------------------
 
-enum SizedItemName
+enum class SizedItem : std::size_t
 {
-    siSweepInterval = 0,
-    siNodeCacheSize,
-    siNodeCacheAge,
-    siTreeCacheSize,
-    siTreeCacheAge,
-    siSLECacheSize,
-    siSLECacheAge,
-    siLedgerSize,
-    siLedgerAge,
-    siLedgerFetch,
-    siHashNodeDBCache,
-    siTxnDBCache,
-    siLgrDBCache
+    sweepInterval = 0,
+    treeCacheSize,
+    treeCacheAge,
+    ledgerSize,
+    ledgerAge,
+    ledgerFetch,
+    nodeCacheSize,
+    nodeCacheAge,
+    hashNodeDBCache,
+    txnDBCache,
+    lgrDBCache
 };
 
-static constexpr
-std::array<std::array<int, 5>, 13> sizedItems
+inline constexpr
+std::array<std::pair<SizedItem, std::array<int, 5>>, 11>
+sizedItems
 {{
-    //  tiny      small   medium  large   huge
-    {{  10,       30,     60,     90,     120     }}, // siSweepInterval
-    {{  2,        3,      5,      5,      8       }}, // siLedgerFetch
-
-    {{  16384,    32768,  131072, 262144, 524288  }}, // siNodeCacheSize
-    {{  60,       90,     120,    900,    1800    }}, // siNodeCacheAge
-
-    {{  128000,   256000, 512000, 768000, 2048000 }}, // siTreeCacheSize
-    {{  30,       60,     90,     120,    900     }}, // siTreeCacheAge
-
-    {{  4096,     8192,   16384,  65536,  131072  }}, // siSLECacheSize
-    {{  30,       60,     90,     120,    300     }}, // siSLECacheAge
-
-    {{  32,       128,    256,    384,    768     }}, // siLedgerSize
-    {{  30,       90,     180,    240,    900     }}, // siLedgerAge
-
-    {{  4,        12,     24,     64,     128     }}, // siHashNodeDBCache
-    {{  4,        12,     24,     64,     128     }}, // siTxnDBCache
-    {{  4,        8,      16,     32,     128     }}  // siLgrDBCache
+    // FIXME: We should document each of these items, explaining exactly what
+    //        they control and whether there exists an explicit config option
+    //        that can be used to override the default.
+        { SizedItem::sweepInterval,
+            {{      10,      30,       60,       90,      120  }} },
+        { SizedItem::treeCacheSize,
+            {{  128000,  256000,   512000,   768000,  2048000  }} },
+        { SizedItem::treeCacheAge,
+            {{      30,      60,       90,      120,      900  }} },
+        { SizedItem::ledgerSize,
+            {{      32,     128,      256,      384,      768  }} },
+        { SizedItem::ledgerAge,
+            {{      30,      90,      180,      240,      900  }} },
+        { SizedItem::ledgerFetch,
+            {{       2,       3,        4,        5,        8  }} },
+        { SizedItem::nodeCacheSize,
+            {{   16384,   32768,   131072,   262144,   524288  }} },
+        { SizedItem::nodeCacheAge,
+            {{      60,      90,      120,      900,     1800  }} },
+        { SizedItem::hashNodeDBCache,
+            {{       4,      12,       24,       64,      128  }} },
+        { SizedItem::txnDBCache,
+            {{       4,      12,       24,       64,      128  }} },
+        { SizedItem::lgrDBCache,
+            {{       4,       8,       16,       32,      128  }} },
 }};
+
+// Ensure that the order of entries in the table corresponds to the
+// order of entries in the enum:
+static_assert([]() constexpr -> bool
+{
+    std::underlying_type_t<SizedItem> idx = 0;
+
+    for (auto const& i : sizedItems)
+    {
+        if (static_cast<std::underlying_type_t<SizedItem>>(i.first) != idx)
+            return false;
+
+        ++idx;
+    }
+
+    return true;
+}(), "Mismatch between sized item enum & array indices");
 
 //  This entire derived class is deprecated.
 //  For new config information use the style implied
@@ -185,7 +208,8 @@ public:
     // Node storage configuration
     std::uint32_t                      LEDGER_HISTORY = 256;
     std::uint32_t                      FETCH_DEPTH = 1000000000;
-    int                         NODE_SIZE = 0;
+
+    std::size_t                        NODE_SIZE = 0;
 
     bool                        SSL_VERIFY = true;
     std::string                 SSL_VERIFY_FILE;
@@ -202,19 +226,35 @@ public:
 public:
     Config() : j_ {beast::Journal::getNullSink()} {}
 
-    static
-    int
-    getSize(SizedItemName item, std::uint32_t nodeSize)
-    {
-        assert(item < sizedItems.size() && nodeSize < sizedItems[item].size());
-        return sizedItems[item][nodeSize];
-    }
+    /** Retrieve the default value for the item at the specified node size
 
+        @param item The item for which the default value is needed
+        @param node Optional value, used to adjust the result to match the
+                    size of a node (0: tiny, ..., 4: huge). If unseated,
+                    uses the configured size (NODE_SIZE).
+
+        @throw This method can throw std::out_of_range if you ask for values
+               that it does not recognize or request a non-default node-size.
+
+        @return The value for the requested item.
+
+        @note The defaults are selected so as to be reasonable, but the node
+              size is an imprecise metric that combines multiple aspects of
+              the underlying system; this means that we can't provide optimal
+              defaults in the code for every case.
+    */
     int
-    getSize(SizedItemName item) const
+    getValueFor(SizedItem item,
+        boost::optional<std::size_t> node = boost::none) const
     {
-        assert(item < sizedItems.size());
-        return getSize(item, NODE_SIZE);
+        auto const index = static_cast<std::underlying_type_t<SizedItem>>(item);
+        assert(index < sizedItems.size());
+
+        if (!node)
+            node = NODE_SIZE;
+
+        assert(*node <= 4);
+        return sizedItems.at(index).second.at(*node);
     }
 
     /* Be very careful to make sure these bool params

src/ripple/core/impl/Config.cpp
@@ -330,13 +330,13 @@ void Config::loadFromString (std::string const& fileContents)
             NODE_SIZE = 4;
         else
         {
-            NODE_SIZE = beast::lexicalCastThrow <int> (strTemp);
+            NODE_SIZE = beast::lexicalCastThrow <std::size_t>(strTemp);
 
-            if (NODE_SIZE < 0)
-                NODE_SIZE = 0;
-            else if (NODE_SIZE > 4)
+            if (NODE_SIZE > 4)
                 NODE_SIZE = 4;
         }
+
+        assert (NODE_SIZE <= 4);
     }
 
     if (getSingleSection (secConfig, SECTION_SIGNING_SUPPORT, strTemp, j_))

src/ripple/nodestore/impl/Shard.cpp
@@ -422,24 +422,25 @@ Shard::setCache(std::lock_guard<std::mutex> const&)
     if (!pCache_)
     {
         auto const name {"shard " + std::to_string(index_)};
-        auto const sz {complete_ ?
-            Config::getSize(siNodeCacheSize, 0) :
-            app_.config().getSize(siNodeCacheSize)};
-        auto const age {std::chrono::seconds{complete_ ?
-            Config::getSize(siNodeCacheAge, 0) :
-            app_.config().getSize(siNodeCacheAge)}};
+        auto const sz = app_.config().getValueFor(SizedItem::nodeCacheSize,
+            complete_ ? boost::optional<std::size_t>(0) : boost::none);
+        auto const age = std::chrono::seconds{
+            app_.config().getValueFor(SizedItem::nodeCacheAge,
+                complete_ ? boost::optional<std::size_t>(0) : boost::none)};
 
         pCache_ = std::make_shared<PCache>(name, sz, age, stopwatch(), j_);
         nCache_ = std::make_shared<NCache>(name, stopwatch(), sz, age);
     }
     else
     {
-        auto const sz {Config::getSize(siNodeCacheSize, 0)};
+        auto const sz = app_.config().getValueFor(
+            SizedItem::nodeCacheSize, 0);
         pCache_->setTargetSize(sz);
         nCache_->setTargetSize(sz);
 
-        auto const age {std::chrono::seconds{
-            Config::getSize(siNodeCacheAge, 0)}};
+        auto const age = std::chrono::seconds{
+            app_.config().getValueFor(
+                SizedItem::nodeCacheAge, 0)};
         pCache_->setTargetAge(age);
         nCache_->setTargetAge(age);
     }
@@ -496,7 +497,7 @@ Shard::initSQLite(std::lock_guard<std::mutex> const&)
                 LgrDBInit);
             lgrSQLiteDB_->getSession() <<
                 boost::str(boost::format("PRAGMA cache_size=-%d;") %
-                kilobytes(Config::getSize(siLgrDBCache, 0)));
+                kilobytes(config.getValueFor(SizedItem::lgrDBCache, boost::none)));
 
             txSQLiteDB_ = std::make_unique <DatabaseCon>(
                 setup,
@@ -505,7 +506,7 @@ Shard::initSQLite(std::lock_guard<std::mutex> const&)
                 TxDBInit);
             txSQLiteDB_->getSession() <<
                 boost::str(boost::format("PRAGMA cache_size=-%d;") %
-                kilobytes(Config::getSize(siTxnDBCache, 0)));
+                kilobytes(config.getValueFor(SizedItem::txnDBCache, boost::none)));
         }
         else
         {
@@ -517,7 +518,7 @@ Shard::initSQLite(std::lock_guard<std::mutex> const&)
                 LgrDBInit);
             lgrSQLiteDB_->getSession() <<
                 boost::str(boost::format("PRAGMA cache_size=-%d;") %
-                kilobytes(config.getSize(siLgrDBCache)));
+                kilobytes(config.getValueFor(SizedItem::lgrDBCache)));
             lgrSQLiteDB_->setupCheckpointing(&app_.getJobQueue(), app_.logs());
 
             txSQLiteDB_ = std::make_unique <DatabaseCon>(
@@ -527,7 +528,7 @@ Shard::initSQLite(std::lock_guard<std::mutex> const&)
                 TxDBInit);
             txSQLiteDB_->getSession() <<
                 boost::str(boost::format("PRAGMA cache_size=-%d;") %
-                kilobytes(config.getSize(siTxnDBCache)));
+                kilobytes(config.getValueFor(SizedItem::txnDBCache)));
             txSQLiteDB_->setupCheckpointing(&app_.getJobQueue(), app_.logs());
         }
     }