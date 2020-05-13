0b9e935806e76e66b6c22cbd434d0ea49b54cf07
John Freeman <jfreeman08@gmail.com>
2020-05-13 14:39:09 -0500

    Find date::date in CMake package configuration file

--- Builds/CMake/RippleConfig.cmake
+++ Builds/CMake/RippleConfig.cmake
@@ -46,6 +46,7 @@ endif ()
 set (OPENSSL_MSVC_STATIC_RT ON)
 find_dependency (OpenSSL 1.0.2 REQUIRED)
 find_dependency (ZLIB)
+find_dependency (date)
 if (TARGET ZLIB::ZLIB)
   set_target_properties(OpenSSL::Crypto PROPERTIES
     INTERFACE_LINK_LIBRARIES ZLIB::ZLIB)
