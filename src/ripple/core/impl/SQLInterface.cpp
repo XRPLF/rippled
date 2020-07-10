8cab521ac5721a02de9648e37eccca60fad3fb5e
p2peer <dcherukhin@ripple.com>
2020-07-10 12:48:05 -0400

    fix2

--- src/ripple/core/impl/SQLInterface.cpp
+++ src/ripple/core/impl/SQLInterface.cpp
@@ -193,7 +193,7 @@ SQLInterface::iterate_forward(
     std::lock_guard lock{maps_mutex_};
     if (!db2ind_.count(db))
         return false;
-    auto [index, type] = db2ind_[db];
+    [[maybe_unused]] auto [index, type] = db2ind_[db];
     std::map<LedgerIndex, SQLDatabase_*>::iterator it, eit;
     switch (type)
     {
@@ -232,7 +232,7 @@ SQLInterface::iterate_back(
     std::lock_guard lock{maps_mutex_};
     if (!db2ind_.count(db))
         return false;
-    auto [index, type] = db2ind_[db];
+    [[maybe_unused]] auto [index, type] = db2ind_[db];
     std::map<LedgerIndex, SQLDatabase_*>::reverse_iterator it, eit;
     switch (type)
     {
