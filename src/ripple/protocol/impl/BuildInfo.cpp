e1a2939f8925265ab8582432729e320edc825560
Carl Hua <carlhua@gmail.com>
2020-09-16 12:59:14 -0400

    Set version to 1.7.0-b2

--- src/ripple/protocol/impl/BuildInfo.cpp
+++ src/ripple/protocol/impl/BuildInfo.cpp
@@ -33,7 +33,7 @@ namespace BuildInfo {
 //  and follow the format described at http://semver.org/
 //------------------------------------------------------------------------------
 // clang-format off
-char const* const versionString = "1.7.0-b1"
+char const* const versionString = "1.7.0-b2"
 // clang-format on
 
 #if defined(DEBUG) || defined(SANITIZER)
