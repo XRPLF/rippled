0046b36044cceaa577b06d4f0d04f44db47cc138
Gregory Tsipenyuk <gtsipenyuk@ripple.com>
2020-12-12 09:31:58 -0500

    [FOLD] Add back required rfc functions removed in develop

--- src/ripple/beast/rfc2616.h
+++ src/ripple/beast/rfc2616.h
@@ -37,6 +37,19 @@ namespace rfc2616 {
 
 namespace detail {
 
+struct ci_equal_pred
+{
+    explicit ci_equal_pred() = default;
+
+    bool
+    operator()(char c1, char c2)
+    {
+        // VFALCO TODO Use a table lookup here
+        return std::tolower(static_cast<unsigned char>(c1)) ==
+            std::tolower(static_cast<unsigned char>(c2));
+    }
+};
+
 /** Returns `true` if `c` is linear white space.
 
     This excludes the CRLF sequence allowed for line continuations.
@@ -193,6 +206,179 @@ Result
 split_commas(boost::beast::string_view const& s)
 {
     return split_commas(s.begin(), s.end());
+}
+
+//------------------------------------------------------------------------------
+
+/** Iterates through a comma separated list.
+
+    Meets the requirements of ForwardIterator.
+
+    List defined in rfc2616 2.1.
+
+    @note Values returned may contain backslash escapes.
+*/
+class list_iterator
+{
+    using iter_type = boost::string_ref::const_iterator;
+
+    iter_type it_;
+    iter_type end_;
+    boost::string_ref value_;
+
+public:
+    using value_type = boost::string_ref;
+    using pointer = value_type const*;
+    using reference = value_type const&;
+    using difference_type = std::ptrdiff_t;
+    using iterator_category = std::forward_iterator_tag;
+
+    list_iterator(iter_type begin, iter_type end) : it_(begin), end_(end)
+    {
+        if (it_ != end_)
+            increment();
+    }
+
+    bool
+    operator==(list_iterator const& other) const
+    {
+        return other.it_ == it_ && other.end_ == end_ &&
+            other.value_.size() == value_.size();
+    }
+
+    bool
+    operator!=(list_iterator const& other) const
+    {
+        return !(*this == other);
+    }
+
+    reference
+    operator*() const
+    {
+        return value_;
+    }
+
+    pointer
+    operator->() const
+    {
+        return &*(*this);
+    }
+
+    list_iterator&
+    operator++()
+    {
+        increment();
+        return *this;
+    }
+
+    list_iterator
+    operator++(int)
+    {
+        auto temp = *this;
+        ++(*this);
+        return temp;
+    }
+
+private:
+    template <class = void>
+    void
+    increment();
+};
+
+template <class>
+void
+list_iterator::increment()
+{
+    using namespace detail;
+    value_.clear();
+    while (it_ != end_)
+    {
+        if (*it_ == '"')
+        {
+            // quoted-string
+            ++it_;
+            if (it_ == end_)
+                return;
+            if (*it_ != '"')
+            {
+                auto start = it_;
+                for (;;)
+                {
+                    ++it_;
+                    if (it_ == end_)
+                    {
+                        value_ = boost::string_ref(
+                            &*start, std::distance(start, it_));
+                        return;
+                    }
+                    if (*it_ == '"')
+                    {
+                        value_ = boost::string_ref(
+                            &*start, std::distance(start, it_));
+                        ++it_;
+                        return;
+                    }
+                }
+            }
+            ++it_;
+        }
+        else if (*it_ == ',')
+        {
+            it_++;
+            continue;
+        }
+        else if (is_lws(*it_))
+        {
+            ++it_;
+            continue;
+        }
+        else
+        {
+            auto start = it_;
+            for (;;)
+            {
+                ++it_;
+                if (it_ == end_ || *it_ == ',' || is_lws(*it_))
+                {
+                    value_ =
+                        boost::string_ref(&*start, std::distance(start, it_));
+                    return;
+                }
+            }
+        }
+    }
+}
+/** Returns true if two strings are equal.
+
+    A case-insensitive comparison is used.
+*/
+inline bool
+ci_equal(boost::string_ref s1, boost::string_ref s2)
+{
+    return boost::range::equal(s1, s2, detail::ci_equal_pred{});
+}
+
+/** Returns a range representing the list. */
+inline boost::iterator_range<list_iterator>
+make_list(boost::string_ref const& field)
+{
+    return boost::iterator_range<list_iterator>{
+        list_iterator{field.begin(), field.end()},
+        list_iterator{field.end(), field.end()}};
+}
+
+/** Returns true if the specified token exists in the list.
+
+    A case-insensitive comparison is used.
+*/
+template <class = void>
+bool
+token_in_list(boost::string_ref const& value, boost::string_ref const& token)
+{
+    for (auto const& item : make_list(value))
+        if (ci_equal(item, token))
+            return true;
+    return false;
 }
 
 template <bool isRequest, class Body, class Fields>
