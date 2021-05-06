a6f61bbd32f6c9b4582dcfa1ca3814f482ddd031
Howard Hinnant <howard.hinnant@gmail.com>
2021-05-06 14:58:47 -0400

    Add scope_exit utilities

---
+++ src/ripple/basics/scope.h
@@ -0,0 +1,196 @@
+//------------------------------------------------------------------------------
+/*
+    This file is part of rippled: https://github.com/ripple/rippled
+    Copyright (c) 2021 Ripple Inc.
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose  with  or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE  SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH  REGARD  TO  THIS  SOFTWARE  INCLUDING  ALL  IMPLIED  WARRANTIES  OF
+    MERCHANTABILITY  AND  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY  SPECIAL ,  DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER  RESULTING  FROM  LOSS  OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION  OF  CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+//==============================================================================
+
+#ifndef RIPPLE_BASICS_SCOPE_H_INCLUDED
+#define RIPPLE_BASICS_SCOPE_H_INCLUDED
+
+#include <exception>
+#include <type_traits>
+#include <utility>
+
+namespace ripple {
+
+// RAII scope helpers.  As specified in Library Fundamental, Version 3
+// Basic design of idea:  https://www.youtube.com/watch?v=WjTrfoiB0MQ
+// Specification:
+//   http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2020/n4873.html#scopeguard
+
+template <class EF>
+class scope_exit
+{
+    EF exit_function_;
+    bool execute_on_destruction_{true};
+
+public:
+    ~scope_exit()
+    {
+        if (execute_on_destruction_)
+            exit_function_();
+    }
+
+    scope_exit(scope_exit&& rhs) noexcept(
+        std::is_nothrow_move_constructible_v<EF> ||
+        std::is_nothrow_copy_constructible_v<EF>)
+        : exit_function_{std::forward<EF>(rhs.exit_function_)}
+        , execute_on_destruction_{rhs.execute_on_destruction_}
+    {
+        rhs.release();
+    }
+
+    scope_exit&
+    operator=(scope_exit&&) = delete;
+
+    template <class EFP>
+    explicit scope_exit(
+        EFP&& f,
+        std::enable_if_t<
+            !std::is_same_v<std::remove_cv_t<EFP>, scope_exit> &&
+            std::is_constructible_v<EF, EFP>>* =
+            0) noexcept(std::is_nothrow_constructible_v<EF, EFP> || std::is_nothrow_constructible_v<EF, EFP&>)
+    try : exit_function_
+    {
+        std::forward<EFP>(f)
+    }
+    {
+    }
+    catch (...)
+    {
+        f();
+        throw;
+    }
+
+    void
+    release() noexcept
+    {
+        execute_on_destruction_ = false;
+    }
+};
+
+template <class EF>
+scope_exit(EF) -> scope_exit<EF>;
+
+template <class EF>
+class scope_fail
+{
+    EF exit_function_;
+    bool execute_on_destruction_{true};
+    int uncaught_on_creation_{std::uncaught_exceptions()};
+
+public:
+    ~scope_fail()
+    {
+        if (execute_on_destruction_ &&
+            std::uncaught_exceptions() > uncaught_on_creation_)
+            exit_function_();
+    }
+
+    scope_fail(scope_fail&& rhs) noexcept(
+        std::is_nothrow_move_constructible_v<EF> ||
+        std::is_nothrow_copy_constructible_v<EF>)
+        : exit_function_{std::forward<EF>(rhs.exit_function_)}
+        , execute_on_destruction_{rhs.execute_on_destruction_}
+        , uncaught_on_creation_{rhs.uncaught_on_creation_}
+    {
+        rhs.release();
+    }
+
+    scope_fail&
+    operator=(scope_fail&&) = delete;
+
+    template <class EFP>
+    explicit scope_fail(
+        EFP&& f,
+        std::enable_if_t<
+            !std::is_same_v<std::remove_cv_t<EFP>, scope_fail> &&
+            std::is_constructible_v<EF, EFP>>* =
+            0) noexcept(std::is_nothrow_constructible_v<EF, EFP> || std::is_nothrow_constructible_v<EF, EFP&>)
+    try : exit_function_
+    {
+        std::forward<EFP>(f)
+    }
+    {
+    }
+    catch (...)
+    {
+        f();
+        throw;
+    }
+
+    void
+    release() noexcept
+    {
+        execute_on_destruction_ = false;
+    }
+};
+
+template <class EF>
+scope_fail(EF) -> scope_fail<EF>;
+
+template <class EF>
+class scope_success
+{
+    EF exit_function_;
+    bool execute_on_destruction_{true};
+    int uncaught_on_creation_{std::uncaught_exceptions()};
+
+public:
+    ~scope_success() noexcept(noexcept(exit_function_()))
+    {
+        if (execute_on_destruction_ &&
+            std::uncaught_exceptions() <= uncaught_on_creation_)
+            exit_function_();
+    }
+
+    scope_success(scope_success&& rhs) noexcept(
+        std::is_nothrow_move_constructible_v<EF> ||
+        std::is_nothrow_copy_constructible_v<EF>)
+        : exit_function_{std::forward<EF>(rhs.exit_function_)}
+        , execute_on_destruction_{rhs.execute_on_destruction_}
+        , uncaught_on_creation_{rhs.uncaught_on_creation_}
+    {
+        rhs.release();
+    }
+
+    scope_success&
+    operator=(scope_success&&) = delete;
+
+    template <class EFP>
+    explicit scope_success(
+        EFP&& f,
+        std::enable_if_t<
+            !std::is_same_v<std::remove_cv_t<EFP>, scope_success> &&
+            std::is_constructible_v<EF, EFP>>* =
+            0) noexcept(std::is_nothrow_constructible_v<EF, EFP> || std::is_nothrow_constructible_v<EF, EFP&>)
+        : exit_function_{std::forward<EFP>(f)}
+    {
+    }
+
+    void
+    release() noexcept
+    {
+        execute_on_destruction_ = false;
+    }
+};
+
+template <class EF>
+scope_success(EF) -> scope_success<EF>;
+
+}  // namespace ripple
+
+#endif
