5e6d4e28a441602aac3e1ae86ca38c95fff79574
Edward Hennis <ed@ripple.com>
2021-05-17 17:34:54 -0400

    Improve Windows Visual Studio CMake build compatibility

--- Builds/CMake/RippledCore.cmake
+++ Builds/CMake/RippledCore.cmake
@@ -1015,5 +1015,18 @@ if (tests)
     # these two seem to produce conflicts in beast teardown template methods
     src/test/rpc/ValidatorRPC_test.cpp
     src/test/rpc/ShardArchiveHandler_test.cpp
+    # These files make the Windows linker run out of memory
+    src/test/server/ServerStatus_test.cpp
+    src/test/basics/Buffer_test.cpp
     PROPERTIES SKIP_UNITY_BUILD_INCLUSION TRUE)
 endif () #tests
+if (NOT MSVC)
+  if (tests)
+    # MSVC has no problem with self-assignment, but does have a
+    # problem with this compile flag in some configs.
+    set_source_files_properties(
+        # This file intentionally tests self-assignments
+        src/test/basics/Buffer_test.cpp
+        PROPERTIES COMPILE_OPTIONS -Wno-self-assign-overloaded)
+  endif () #tests
+endif()
