5d449983684d1cdd59fbc27280651234bb2adc78
manojsdoshi <mdoshi@ripple.com>
2021-10-28 15:07:24 -0700

    Set version to 1.8.0-rc1

--- src/ripple/protocol/impl/BuildInfo.cpp
+++ src/ripple/protocol/impl/BuildInfo.cpp
@@ -33,7 +33,7 @@ namespace BuildInfo {
 //  and follow the format described at http://semver.org/
 //------------------------------------------------------------------------------
 // clang-format off
-char const* const versionString = "1.8.0-b7"
+char const* const versionString = "1.8.0-rc1"
 // clang-format on
 
 #if defined(DEBUG) || defined(SANITIZER)
