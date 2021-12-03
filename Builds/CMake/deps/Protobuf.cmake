dc75c61569dcc7126be901b7343f7236a4a29afe
Donovan Hide <donovanhide@gmail.com>
2021-12-03 17:44:58 +0000

    Rename ripple.proto to xrpl.proto

--- Builds/CMake/deps/Protobuf.cmake
+++ Builds/CMake/deps/Protobuf.cmake
@@ -123,7 +123,7 @@ set (CMAKE_CURRENT_BINARY_DIR ${CMAKE_BINARY_DIR}/proto_gen)
 protobuf_generate_cpp (
   PROTO_SRCS
   PROTO_HDRS
-  src/ripple/proto/ripple.proto)
+  src/ripple/proto/xrpl.proto)
 set (CMAKE_CURRENT_BINARY_DIR ${save_CBD})
 
 add_library (pbufs STATIC ${PROTO_SRCS} ${PROTO_HDRS})
