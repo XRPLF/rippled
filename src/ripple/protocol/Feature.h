905aeb0af2597266d89867488c50f9f290288307
Crypto Brad Garlinghouse <cryptobradgarlinghouse@protonmail.com>
2022-07-19 18:51:31 +0000

    Introduce amendment to handle trustlines to self:
    Trustlines must be between two different accounts but two trustlines exist
    where an account extends trust to itself. They were created in the early
    days, likely because of bugs that have been fixed. The new fixTrustLinesToSelf
    amendment will remove those trustlines when it activates.

--- src/ripple/protocol/Feature.h
+++ src/ripple/protocol/Feature.h
@@ -338,6 +338,7 @@ extern uint256 const featureExpandedSignerList;
 extern uint256 const fixNFTokenDirV1;
 extern uint256 const fixNFTokenNegOffer;
 extern uint256 const featureNonFungibleTokensV1_1;
+extern uint256 const fixTrustLinesToSelf;
 
 }  // namespace ripple
 
