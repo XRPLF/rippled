7c6dbd0de63002e7559cf081e3d2361ca11d3e0a
Scott Schurr <scott@ripple.com>
2022-08-31 10:19:13 -0700

    [FOLD] Fix build and CI issues

--- Builds/CMake/RippledCore.cmake
+++ Builds/CMake/RippledCore.cmake
@@ -910,7 +910,6 @@ if (tests)
     src/test/protocol/BuildInfo_test.cpp
     src/test/protocol/InnerObjectFormats_test.cpp
     src/test/protocol/Issue_test.cpp
-    src/test/protocol/KnownFormatToGRPC_test.cpp
     src/test/protocol/Hooks_test.cpp
     src/test/protocol/PublicKey_test.cpp
     src/test/protocol/Quality_test.cpp
