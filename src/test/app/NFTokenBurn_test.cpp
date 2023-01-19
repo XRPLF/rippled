a05e1f6cd220b5c8f68ee1d42f06075b2ca9c1e0
Shawn Xie <shawnxie920@gmail.com>
2023-01-19 17:57:45 -0500

    [FOLD] clang

--- src/test/app/NFTokenBurn_test.cpp
+++ src/test/app/NFTokenBurn_test.cpp
@@ -697,7 +697,7 @@ class NFTokenBurn_test : public beast::unit_test::suite
             env.close();
 
             uint32_t offerDeletedCount = 0;
-            // Count the number of buy offers that have been deleted 
+            // Count the number of buy offers that have been deleted
             for (uint256 const& offerIndex : offerIndexes)
             {
                 if (!env.le(keylet::nftoffer(offerIndex)))
