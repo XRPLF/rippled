32559463efba03b90b67f51b489f217881f31599
Howard Hinnant <howard.hinnant@gmail.com>
2023-02-10 00:01:48 -0500

    Change default vote on fixUniversalNumber from yes to no (#4414)

--- src/ripple/protocol/impl/Feature.cpp
+++ src/ripple/protocol/impl/Feature.cpp
@@ -452,7 +452,7 @@ REGISTER_FIX    (fixRemoveNFTokenAutoTrustLine, Supported::yes, DefaultVote::yes
 REGISTER_FEATURE(ImmediateOfferKilled,          Supported::yes, DefaultVote::no);
 REGISTER_FEATURE(DisallowIncoming,              Supported::yes, DefaultVote::no);
 REGISTER_FEATURE(XRPFees,                       Supported::yes, DefaultVote::no);
-REGISTER_FIX    (fixUniversalNumber,            Supported::yes, DefaultVote::yes);
+REGISTER_FIX    (fixUniversalNumber,            Supported::yes, DefaultVote::no);
 
 // The following amendments have been active for at least two years. Their
 // pre-amendment code has been removed and the identifiers are deprecated.
