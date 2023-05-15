60916974c8476585efa8c52fae9d455c544dc1a7
David Fuelling <sappenin@gmail.com>
2023-05-15 17:03:59 -0600

    Remove unused transaction formats

--- src/ripple/protocol/TxFormats.h
+++ src/ripple/protocol/TxFormats.h
@@ -58,38 +58,17 @@ enum TxType : std::uint16_t
     /** This transaction type executes a payment. */
     ttPAYMENT = 0,
 
-    /** This transaction type creates an escrow object. */
-    ttESCROW_CREATE = 1,
-
-    /** This transaction type completes an existing escrow. */
-    ttESCROW_FINISH = 2,
-
     /** This transaction type adjusts various account settings. */
     ttACCOUNT_SET = 3,
 
-    /** This transaction type cancels an existing escrow. */
-    ttESCROW_CANCEL = 4,
-
     /** This transaction type sets or clears an account's "regular key". */
     ttREGULAR_KEY_SET = 5,
-
-    /** This transaction type is deprecated; it is retained for historical purposes. */
-    ttNICKNAME_SET [[deprecated("This transaction type is not supported and should not be used.")]] = 6,
 
     /** This transaction type creates an offer to trade one asset for another. */
     ttOFFER_CREATE = 7,
 
     /** This transaction type cancels existing offers to trade one asset for another. */
     ttOFFER_CANCEL = 8,
-
-    /** This transaction type is deprecated; it is retained for historical purposes. */
-    ttCONTRACT [[deprecated("This transaction type is not supported and should not be used.")]] = 9,
-
-    /** This transaction type creates a new set of tickets. */
-    ttTICKET_CREATE = 10,
-
-    /** This identifier was never used, but the slot is reserved for historical purposes. */
-    ttSPINAL_TAP [[deprecated("This transaction type is not supported and should not be used.")]] = 11,
 
     /** This transaction type modifies the signer list associated with an account. */
     ttSIGNER_LIST_SET = 12,
@@ -99,9 +78,6 @@ enum TxType : std::uint16_t
 
     /** This transaction type deletes an existing account. */
     ttACCOUNT_DELETE = 21,
-
-    /** This transaction type installs a hook. */
-    ttHOOK_SET [[maybe_unused]] = 22,
 
     /** This system-generated transaction type is used to update the status of the various amendments.
 
