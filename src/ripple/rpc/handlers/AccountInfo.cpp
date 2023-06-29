d626a6b6d324f0102d3b3fa314e6812261cb89a0
Peter Chen <arborius2.0@gmail.com>
2023-06-29 11:51:55 -0400

    comment change

--- src/ripple/rpc/handlers/AccountInfo.cpp
+++ src/ripple/rpc/handlers/AccountInfo.cpp
@@ -126,8 +126,8 @@ doAccountInfo(RPC::JsonContext& context)
         result[jss::account_flags] = std::move(acctFlags);
 
         // The document states that signer_lists is a bool, however
-        // assigning any string value works. Do not allow this
-        // this for api v2 onwards
+        // assigning any string value works. Do not allow this.
+        // This check is for api Version 2 onwards only
         if (!params[jss::signer_lists].isBool() && context.apiVersion > 1)
         {
             RPC::inject_error(rpcINVALID_PARAMS, result);
