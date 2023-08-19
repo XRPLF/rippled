e8a7b2a1fc5b62b7ea92bd12fa63b2852125906a
Mark Travis <mtravis@ripple.com>
2023-08-18 22:11:24 -0400

    Several changes to improve Consensus stability: (#4505)
     * Verify accepted ledger becomes validated, and retry
       with a new consensus transaction set if not.
     * Always store proposals.
     * Track proposals by ledger sequence. This helps slow peers catch
       up with the rest of the network.
     * Acquire transaction sets for proposals with future ledger sequences.
       This also helps slow peers catch up.
     * Optimize timer delay for establish phase to wait based on how
       long validators have been sending proposals. This also helps slow
       peers to catch up.
     * Fix impasse achieving close time consensus.
     * Don't wait between open and establish phases.

--- src/ripple/overlay/impl/PeerImp.cpp
+++ src/ripple/overlay/impl/PeerImp.cpp
@@ -33,6 +33,7 @@
 #include <ripple/basics/base64.h>
 #include <ripple/basics/random.h>
 #include <ripple/basics/safe_cast.h>
+#include <ripple/beast/clock/abstract_clock.h>
 #include <ripple/beast/core/LexicalCast.h>
 #include <ripple/beast/core/SemanticVersion.h>
 #include <ripple/nodestore/DatabaseShard.h>
@@ -1994,6 +1995,10 @@ PeerImp::onMessage(std::shared_ptr<protocol::TMProposeSet> const& m)
     JLOG(p_journal_.trace())
         << "Proposal: " << (isTrusted ? "trusted" : "untrusted");
 
+    std::optional<LedgerIndex> ledgerSeq;
+    if (set.has_ledgerseq())
+        ledgerSeq = set.ledgerseq();
+
     auto proposal = RCLCxPeerPos(
         publicKey,
         sig,
@@ -2004,7 +2009,9 @@ PeerImp::onMessage(std::shared_ptr<protocol::TMProposeSet> const& m)
             proposeHash,
             closeTime,
             app_.timeKeeper().closeTime(),
-            calcNodeID(app_.validatorManifests().getMasterKey(publicKey))});
+            calcNodeID(app_.validatorManifests().getMasterKey(publicKey)),
+            ledgerSeq,
+            beast::get_abstract_clock<std::chrono::steady_clock>()});
 
     std::weak_ptr<PeerImp> weak = shared_from_this();
     app_.getJobQueue().addJob(
