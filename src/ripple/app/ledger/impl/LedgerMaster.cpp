7ca1c644d10b6de2a2a442a03ee32b4734508740
Elliot Lee <github.public@intelliot.com>
2023-08-29 16:52:31 -0700

    Revert "Several changes to improve Consensus stability: (#4505)"
    This reverts commit e8a7b2a1fc5b62b7ea92bd12fa63b2852125906a.

--- src/ripple/app/ledger/impl/LedgerMaster.cpp
+++ src/ripple/app/ledger/impl/LedgerMaster.cpp
@@ -367,8 +367,6 @@ LedgerMaster::setValidLedger(std::shared_ptr<Ledger const> const& l)
     }
 
     mValidLedger.set(l);
-    // In case we're waiting for a valid before proceeding with Consensus.
-    validCond_.notify_one();
     mValidLedgerSign = signTime.time_since_epoch().count();
     assert(
         mValidLedgerSeq || !app_.getMaxDisallowedLedger() ||
