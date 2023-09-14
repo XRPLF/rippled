4854508d39cfa1ac50db4a229f0461edfc2e1464
Elliot Lee <github.public@intelliot.com>
2023-09-14 13:11:28 -0700

    Set version to 2.0.0-b1

--- src/ripple/protocol/impl/BuildInfo.cpp
+++ src/ripple/protocol/impl/BuildInfo.cpp
@@ -33,7 +33,7 @@ namespace BuildInfo {
 //  and follow the format described at http://semver.org/
 //------------------------------------------------------------------------------
 // clang-format off
-char const* const versionString = "1.12.0"
+char const* const versionString = "2.0.0-b1"
 // clang-format on
 
 #if defined(DEBUG) || defined(SANITIZER)
