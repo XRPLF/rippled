ddcd062389c29afdb3254b71ebf033498fd2376d
Scott Determan <scott.determan@yahoo.com>
2023-09-15 11:08:17 -0400

    Change `XChainBridge` amendment to `Supported::yes` (#4709)

--- src/ripple/protocol/impl/Feature.cpp
+++ src/ripple/protocol/impl/Feature.cpp
@@ -455,7 +455,7 @@ REGISTER_FIX    (fixNFTokenRemint,              Supported::yes, VoteBehavior::De
 REGISTER_FIX    (fixReducedOffersV1,            Supported::yes, VoteBehavior::DefaultNo);
 REGISTER_FEATURE(Clawback,                      Supported::yes, VoteBehavior::DefaultNo);
 REGISTER_FEATURE(AMM,                           Supported::yes, VoteBehavior::DefaultNo);
-REGISTER_FEATURE(XChainBridge,                  Supported::no,  VoteBehavior::DefaultNo);
+REGISTER_FEATURE(XChainBridge,                  Supported::yes, VoteBehavior::DefaultNo);
 
 // The following amendments are obsolete, but must remain supported
 // because they could potentially get enabled.
