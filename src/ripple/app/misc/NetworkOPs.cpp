2bb8de030fc887787f311e428e2e91466e41a240
Scott Schurr <scott@ripple.com>
2023-09-28 11:47:48 -0700

    fix: stabilize voting threshold for amendment majority mechanism (#4410)
    Amendment "flapping" (an amendment repeatedly gaining and losing
    majority) usually occurs when an amendment is on the verge of gaining
    majority, and a validator not in favor of the amendment goes offline or
    loses sync. This fix makes two changes:
    
    1. The number of validators in the UNL determines the threshold required
       for an amendment to gain majority.
    2. The AmendmentTable keeps a record of the most recent Amendment vote
       received from each trusted validator (and, with `trustChanged`, stays
       up-to-date when the set of trusted validators changes). If no
       validation arrives from a given validator, then the AmendmentTable
       assumes that the previously-received vote has not changed.
    
    In other words, when missing an `STValidation` from a remote validator,
    each server now uses the last vote seen. There is a 24 hour timeout for
    recorded validator votes.
    
    These changes do not require an amendment because they do not impact
    transaction processing, but only the threshold at which each individual
    validator decides to propose an EnableAmendment pseudo-transaction.
    
    Fix #4350

--- src/ripple/app/misc/NetworkOPs.cpp
+++ src/ripple/app/misc/NetworkOPs.cpp
@@ -1850,7 +1850,12 @@ NetworkOPsImp::beginConsensus(uint256 const& networkClosed)
         app_.getHashRouter());
 
     if (!changes.added.empty() || !changes.removed.empty())
+    {
         app_.getValidations().trustChanged(changes.added, changes.removed);
+        // Update the AmendmentTable so it tracks the current validators.
+        app_.getAmendmentTable().trustChanged(
+            app_.validators().getQuorumKeys().second);
+    }
 
     mConsensus.startRound(
         app_.timeKeeper().closeTime(),
