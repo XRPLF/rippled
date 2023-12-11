0cec21a260aa326156ef035305e929c525fc160b
Bronek Kozicki <brok@incorrekt.com>
2023-12-11 17:00:25 +0000

    Disable fastcov and lcov

--- Builds/CMake/RippledCov.cmake
+++ Builds/CMake/RippledCov.cmake
@@ -5,36 +5,11 @@
 ProcessorCount(PROCESSOR_COUNT)
 
 if (coverage)
-  if (NOT GENHTML_PATH)
-    message(FATAL_ERROR "Cannot find genhtml, aborting ...")
-  endif()
-
-  math(EXPR TEST_PARALLELISM "(${PROCESSOR_COUNT} + 1)/ 2")
   set(COVERAGE_EXCLUDES "/usr/include" "src/ed25519-donna" "src/secp256k1" "src/test")
 
-  if (NOT FASTCOV_PATH)
-    if (NOT GCOVR_PATH)
-      if (NOT LCOV_PATH)
-        message(FATAL_ERROR "Cannot find either of fastco, gcovr or lcov, aborting ...")
-      else()
-        setup_target_for_coverage_lcov(
-          NAME coverage_report
-          EXECUTABLE rippled -u --unittest-jobs ${TEST_PARALLELISM} --quiet --unittest-log
-          DEPENDENCIES rippled
-          )
-      endif()
-    else()
-      setup_target_for_coverage_gcovr_html(
-          NAME coverage_report
-          EXECUTABLE rippled -u --unittest-jobs ${TEST_PARALLELISM} --quiet --unittest-log
-          DEPENDENCIES rippled
-      )
-    endif()
-  else()
-    setup_target_for_coverage_fastcov(
-          NAME coverage_report
-          EXECUTABLE rippled -u --unittest-jobs ${TEST_PARALLELISM} --quiet --unittest-log
-          DEPENDENCIES rippled
-    )
-  endif()
+  setup_target_for_coverage_gcovr_html(
+      NAME coverage_report
+      EXECUTABLE rippled -u --unittest-jobs ${PROCESSOR_COUNT} --quiet --unittest-log
+      DEPENDENCIES rippled
+  )
 endif()
