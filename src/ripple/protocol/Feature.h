be12136b8aa21926b45b3162249410f8779d2467
Gregory Tsipenyuk <gregtatcam@users.noreply.github.com>
2024-02-07 16:58:12 -0500

    `fixInnerObjTemplate`: set inner object template (#4906)
    Add `STObject` constructor to explicitly set the inner object template.
    This allows certain AMM transactions to apply in the same ledger:
    
    There is no issue if the trading fee is greater than or equal to 0.01%.
    If the trading fee is less than 0.01%, then:
    - After AMM create, AMM transactions must wait for one ledger to close
      (3-5 seconds).
    - After one ledger is validated, all AMM transactions succeed, as
      appropriate, except for AMMVote.
    - The first AMMVote which votes for a 0 trading fee in a ledger will
      succeed. Subsequent AMMVote transactions which vote for a 0 trading
      fee will wait for the next ledger (3-5 seconds). This behavior repeats
      for each ledger.
    
    This has no effect on the ultimate correctness of AMM. This amendment
    will allow the transactions described above to succeed as expected, even
    if the trading fee is 0 and the transactions are applied within one
    ledger (block).

--- src/ripple/protocol/Feature.h
+++ src/ripple/protocol/Feature.h
@@ -74,7 +74,7 @@ namespace detail {
 // Feature.cpp. Because it's only used to reserve storage, and determine how
 // large to make the FeatureBitset, it MAY be larger. It MUST NOT be less than
 // the actual number of amendments. A LogicError on startup will verify this.
-static constexpr std::size_t numFeatures = 66;
+static constexpr std::size_t numFeatures = 67;
 
 /** Amendments that this server supports and the default voting behavior.
    Whether they are enabled depends on the Rules defined in the validated
@@ -353,6 +353,7 @@ extern uint256 const fixDisallowIncomingV1;
 extern uint256 const featureDID;
 extern uint256 const fixFillOrKill;
 extern uint256 const fixNFTokenReserve;
+extern uint256 const fixInnerObjTemplate;
 
 }  // namespace ripple
 
