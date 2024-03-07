b40d3fcf8b623f41a675e71d28f0dd70c02dd096
Scott Schurr <scott@ripple.com>
2024-03-06 17:15:26 -0800

    Add the fixNFTokenTrustlineSurprise amendment:
    Fix bugs in interactions between NFTokenOffers and trust lines.
    
    Since the NFTokenAcceptOffer does not check the trust line that
    the issuer receives as a transfer fee in the NFTokenAcceptOffer,
    if the issuer deletes the trust line after NFTokenCreateOffer,
    the trust line is created for the issuer by the
    NFTokenAcceptOffer.  That's fixed.
    
    Also if Alice issues an IOU and also mints NFTokens with a
    transfer fee, Alice's IOU cannot be used to pay for transfers
    of those NFTokens.  That's fixed.
    
    Resolves #4925.
    Resolves #4941.

--- src/ripple/app/tx/impl/NFTokenAcceptOffer.cpp
+++ src/ripple/app/tx/impl/NFTokenAcceptOffer.cpp
@@ -270,6 +270,34 @@ NFTokenAcceptOffer::preclaim(PreclaimContext const& ctx)
         }
     }
 
+    // Fix a bug where the transfer of an NFToken with a transfer fee could
+    // give the NFToken issuer an undesired trust line.
+    if (ctx.view.rules().enabled(fixNFTokenTrustlineSurprise))
+    {
+        std::shared_ptr<SLE const> const& offer = bo ? bo : so;
+        if (!offer)
+            // Should be caught in preflight.
+            return tecINTERNAL;
+
+        // - If the NFToken has a transfer fee, and
+        // - If the NFToken doesn't have the flagCreateTrustLines flag set, and
+        // - If the Amount is not XRP, and
+        // - If the NFToken issuer is not the Amount issuer, and
+        // - If the NFToken issuer does not have a trust line for the Amount
+        // - Then reject the token accept.
+        uint256 const& tokenID = offer->at(sfNFTokenID);
+        STAmount const& amount = offer->at(sfAmount);
+        if (nft::getTransferFee(tokenID) != 0 &&
+            (nft::getFlags(tokenID) & nft::flagCreateTrustLines) == 0 &&
+            !amount.native())
+        {
+            auto const issuer = nft::getIssuer(tokenID);
+            // Issuer doesn't need a trust line to accept their own currency.
+            if (issuer != amount.getIssuer() &&
+                !ctx.view.read(keylet::line(issuer, amount.issue())))
+                return tecNO_LINE;
+        }
+    }
     return tesSUCCESS;
 }
 
