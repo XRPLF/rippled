c210ad4e5a9fa9c8a794f0870d58cd690dbda87d
Howard Hinnant <howard.hinnant@gmail.com>
2024-03-28 16:43:50 -0400

    Suppress warning: unused variable 'index'

--- src/test/protocol/MultiApiJson_test.cpp
+++ src/test/protocol/MultiApiJson_test.cpp
@@ -52,12 +52,6 @@ struct MultiApiJson_test : beast::unit_test::suite
         return obj1;
     }
 
-    constexpr static auto index =
-        [](unsigned int v) constexpr noexcept -> std::size_t
-    {
-        return v - 1;
-    };
-
     template <int size>
     constexpr static auto valid = [](unsigned int v) constexpr noexcept -> bool
     {
