a6495111b89e1a2c7de4f49c6637c42b9ed8d9ef
Michael Legleux <mlegleux@ripple.com>
2024-04-12 11:47:10 -0700

    2.2.5

--- src/ripple/protocol/impl/BuildInfo.cpp
+++ src/ripple/protocol/impl/BuildInfo.cpp
@@ -33,7 +33,7 @@ namespace BuildInfo {
 //  and follow the format described at http://semver.org/
 //------------------------------------------------------------------------------
 // clang-format off
-char const* const versionString = "2.2.4"
+char const* const versionString = "2.2.5"
 // clang-format on
 
 #if defined(DEBUG) || defined(SANITIZER)
