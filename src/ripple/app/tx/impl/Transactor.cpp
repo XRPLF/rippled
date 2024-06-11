f9adeb91fe5d0645d438ffff1bf4ba0a21c769c0
Bronek Kozicki <brok@incorrekt.com>
2024-06-11 19:34:02 +0100

    Add new command line option to make replaying transactions easier: (#5027)
    * Add trap_tx_hash command line option
    
    This new option can be used only if replay is also enabled. It takes a transaction hash from the ledger loaded for replay, and will cause a specific line to be hit in Transactor.cpp, right before the selected transaction is applied.

--- src/ripple/app/tx/impl/Transactor.cpp
+++ src/ripple/app/tx/impl/Transactor.cpp
@@ -854,6 +854,12 @@ Transactor::operator()()
     }
 #endif
 
+    if (auto const& trap = ctx_.app.trapTxID();
+        trap && *trap == ctx_.tx.getTransactionID())
+    {
+        JLOG(j_.debug()) << "Transaction trapped: " << *trap;
+    }
+
     auto result = ctx_.preclaimResult;
     if (result == tesSUCCESS)
         result = apply();
