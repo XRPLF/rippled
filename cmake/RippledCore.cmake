69f859069ca91c99d4ded6772f27a46f9fd9e989
Ed Hennis <ed@ripple.com>
2024-07-05 13:27:15 -0400

    Invariant: prevent a deleted account from leaving (most) artifacts on the ledger. (#4663)
    * Add feature / amendment "InvariantsV1_1"
    
    * Adds invariant AccountRootsDeletedClean:
    
    * Checks that a deleted account doesn't leave any directly
      accessible artifacts behind.
    * Always tests, but only changes the transaction result if
      featureInvariantsV1_1 is enabled.
    * Unit tests.
    
    * Resolves #4638
    
    * [FOLD] Review feedback from @gregtatcam:
    
    * Fix unused variable warning
    * Improve Invariant test const correctness
    
    * [FOLD] Review feedback from @mvadari:
    
    * Centralize the account keylet function list, and some optimization
    
    * [FOLD] Some structured binding doesn't work in clang
    
    * [FOLD] Review feedback 2 from @mvadari:
    
    * Clean up and clarify some comments.
    
    * [FOLD] Change InvariantsV1_1 to unsupported
    
    * Will allow multiple PRs to be merged over time using the same amendment.
    
    * fixup! [FOLD] Change InvariantsV1_1 to unsupported
    
    * [FOLD] Update and clarify some comments. No code changes.
    
    * Move CMake directory
    
    * Rearrange sources
    
    * Rewrite includes
    
    * Recompute loops
    
    * Fix merge issue and formatting
    
    ---------
    
    Co-authored-by: Pretty Printer <cpp@ripple.com>

--- cmake/RippledCore.cmake
+++ cmake/RippledCore.cmake
@@ -142,5 +142,6 @@ if(tests)
     # these two seem to produce conflicts in beast teardown template methods
     src/test/rpc/ValidatorRPC_test.cpp
     src/test/rpc/ShardArchiveHandler_test.cpp
+    src/test/ledger/Invariants_test.cpp
     PROPERTIES SKIP_UNITY_BUILD_INCLUSION TRUE)
 endif()
