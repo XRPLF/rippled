625bfd9f077eb9462e5c639b22fca17b310d7e30
Mayukha Vadari <mvadari@gmail.com>
2024-07-12 19:08:18 -0400

    fix account_lines

--- src/ripple/rpc/handlers/AccountLines.cpp
+++ src/ripple/rpc/handlers/AccountLines.cpp
@@ -80,6 +80,9 @@ doAccountLines(RPC::JsonContext& context)
     if (!params.isMember(jss::account))
         return RPC::missing_field_error(jss::account);
 
+    if (!params[jss::account].isString())
+        return rpcError(rpcINVALID_PARAMS);
+
     std::shared_ptr<ReadView const> ledger;
     auto result = RPC::lookupLedger(ledger, context);
     if (!ledger)
