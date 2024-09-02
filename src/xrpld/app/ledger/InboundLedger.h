094572a1068d33c6d89fc65e22b4b5e15c9751e1
Ed Hennis <ed@ripple.com>
2024-09-02 11:41:16 -0400

    Optimize when to acquire ledgers from the network.
    Particularly avoid acquiring ledgers likely to be
    produced locally very soon.
    
    Derived from #4764
    
    Co-authored-by: Mark Travis <mtravis@ripple.com>

--- src/xrpld/app/ledger/InboundLedger.h
+++ src/xrpld/app/ledger/InboundLedger.h
@@ -40,8 +40,10 @@ public:
     ~InboundLedger();
 
     // Called when another attempt is made to fetch this same ledger
-    void
-    update(std::uint32_t seq);
+    //
+    // Returns true if this triggers requests to be sent
+    bool
+    update(std::uint32_t seq, bool broadcast);
 
     /** Returns true if we got all the data. */
     bool
@@ -72,7 +74,7 @@ public:
     bool
     checkLocal();
     void
-    init(ScopedLockType& collectionLock);
+    init(ScopedLockType& collectionLock, bool broadcast);
 
     bool
     gotData(
