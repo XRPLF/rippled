ef2abc422dd17a93f9bf0826f9ea62ef219ef9a7
Ed Hennis <ed@ripple.com>
2024-09-06 15:40:33 -0400

    Include context on calls to InboundLedgers::acquire

--- src/xrpld/app/ledger/detail/LedgerMaster.cpp
+++ src/xrpld/app/ledger/detail/LedgerMaster.cpp
@@ -880,7 +880,8 @@ void
 LedgerMaster::failedSave(std::uint32_t seq, uint256 const& hash)
 {
     clearLedger(seq);
-    app_.getInboundLedgers().acquire(hash, seq, InboundLedger::Reason::GENERIC);
+    app_.getInboundLedgers().acquire(
+        hash, seq, InboundLedger::Reason::GENERIC, "failedSave");
 }
 
 // Check if the specified ledger can become the new last fully-validated
@@ -928,7 +929,7 @@ LedgerMaster::checkAccept(uint256 const& hash, std::uint32_t seq)
         // FIXME: We may not want to fetch a ledger with just one
         // trusted validation
         ledger = app_.getInboundLedgers().acquire(
-            hash, seq, InboundLedger::Reason::GENERIC);
+            hash, seq, InboundLedger::Reason::GENERIC, "checkAccept");
     }
 
     if (ledger)
@@ -1303,7 +1304,10 @@ LedgerMaster::findNewLedgersToPublish(
                 // Can we try to acquire the ledger we need?
                 if (!ledger && (++acqCount < ledger_fetch_size_))
                     ledger = app_.getInboundLedgers().acquire(
-                        *hash, seq, InboundLedger::Reason::GENERIC);
+                        *hash,
+                        seq,
+                        InboundLedger::Reason::GENERIC,
+                        "findNewLedgersToPublish");
             }
 
             // Did we acquire the next ledger we need to publish?
@@ -1494,7 +1498,8 @@ LedgerMaster::updatePaths()
                 app_.getInboundLedgers().acquire(
                     lastLedger->info().parentHash,
                     lastLedger->info().seq - 1,
-                    InboundLedger::Reason::GENERIC);
+                    InboundLedger::Reason::GENERIC,
+                    "updatePaths open");
             }
             else
             {
@@ -1502,7 +1507,8 @@ LedgerMaster::updatePaths()
                 app_.getInboundLedgers().acquire(
                     lastLedger->info().hash,
                     lastLedger->info().seq,
-                    InboundLedger::Reason::GENERIC);
+                    InboundLedger::Reason::GENERIC,
+                    "updatePaths closed");
             }
         }
     }
@@ -1703,7 +1709,7 @@ LedgerMaster::walkHashBySeq(
         if (!ledger)
         {
             if (auto const l = app_.getInboundLedgers().acquire(
-                    *refHash, refIndex, reason))
+                    *refHash, refIndex, reason, "walkHashBySeq"))
             {
                 ledgerHash = hashOfSeq(*l, index, m_journal);
                 XRPL_ASSERT(
@@ -1829,8 +1835,8 @@ LedgerMaster::fetchForHistory(
         {
             if (!app_.getInboundLedgers().isFailure(*hash))
             {
-                ledger =
-                    app_.getInboundLedgers().acquire(*hash, missing, reason);
+                ledger = app_.getInboundLedgers().acquire(
+                    *hash, missing, reason, "fetchForHistory");
                 if (!ledger && missing != fetch_seq_ &&
                     missing > app_.getNodeStore().earliestLedgerSeq())
                 {
@@ -1897,7 +1903,8 @@ LedgerMaster::fetchForHistory(
                             h->isNonZero(),
                             "ripple::LedgerMaster::fetchForHistory : "
                             "prefetched ledger");
-                        app_.getInboundLedgers().acquire(*h, seq, reason);
+                        app_.getInboundLedgers().acquire(
+                            *h, seq, reason, "fetchForHistory no ledger");
                     }
                 }
             }
