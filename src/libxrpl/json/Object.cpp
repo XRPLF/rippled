13bf38839d59274016a76f2686ae48584c819c36
Bronek Kozicki <brok@incorrekt.com>
2024-10-22 18:19:54 +0100

    Update src/libxrpl/json/Object.cpp
    Co-authored-by: Bart <bthomee@users.noreply.github.com>

--- src/libxrpl/json/Object.cpp
+++ src/libxrpl/json/Object.cpp
@@ -168,7 +168,7 @@ Array::append(Json::Value const& v)
             return;
         }
     }
-    UNREACHABLE("Json::Array::append : invalid type");  // Can't get here.
+    UNREACHABLE("Json::Array::append : invalid type"); 
 }
 
 void
