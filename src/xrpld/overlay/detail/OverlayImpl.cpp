49628e8eb4bb690d539baeb22a56d7eec67f1956
Alloy Networks <45832257+alloynetworks@users.noreply.github.com>
2024-10-29 09:25:44 +0200

    Add hubs.xrpkuwait to bootstrap - Clang formatting
    Modified for CLANG formatting error

--- src/xrpld/overlay/detail/OverlayImpl.cpp
+++ src/xrpld/overlay/detail/OverlayImpl.cpp
@@ -492,7 +492,7 @@ OverlayImpl::start()
         // Pool of servers operated by ISRDC - https://isrdc.in
         bootstrapIps.push_back("sahyadri.isrdc.in 51235");
 
-        //Pool of servers operated by @Xrpkuwait
+        // Pool of servers operated by @Xrpkuwait - https://xrpkuwait.com
         bootstrapIps.push_back("hubs.xrpkuwait.com 51235");
     }
 
