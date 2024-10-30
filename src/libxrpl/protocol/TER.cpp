ee0381172f46fabce7b85be3a065a755d129051c
Denis Angell <dangell@transia.co>
2024-10-30 22:09:31 +0100

    [fold] change error response text

--- src/libxrpl/protocol/TER.cpp
+++ src/libxrpl/protocol/TER.cpp
@@ -115,7 +115,7 @@ transResults()
         MAKE_ERROR(tecTOKEN_PAIR_NOT_FOUND,          "Token pair is not found in Oracle object."),
         MAKE_ERROR(tecARRAY_EMPTY,                   "Array is empty."),
         MAKE_ERROR(tecARRAY_TOO_LARGE,               "Array is too large."),
-        MAKE_ERROR(tecBATCH_FAILURE,                 "Tx Batch Failure."),
+        MAKE_ERROR(tecBATCH_FAILURE,                 "Batch transaction failure."),
 
         MAKE_ERROR(tefALREADY,                     "The exact transaction was already in this ledger."),
         MAKE_ERROR(tefBAD_ADD_AUTH,                "Not authorized to add account."),
@@ -206,7 +206,7 @@ transResults()
         MAKE_ERROR(temXCHAIN_BRIDGE_BAD_REWARD_AMOUNT, "Malformed: Bad reward amount."),
         MAKE_ERROR(temARRAY_EMPTY,               "Malformed: Array is empty."),
         MAKE_ERROR(temARRAY_TOO_LARGE,           "Malformed: Array is too large."),
-        MAKE_ERROR(temINVALID_BATCH,             "Invalid inner batch transaction type."),
+        MAKE_ERROR(temINVALID_BATCH,             "Malformed: Invalid inner batch transaction type."),
 
         MAKE_ERROR(terRETRY,                  "Retry transaction."),
         MAKE_ERROR(terFUNDS_SPENT,            "DEPRECATED."),
