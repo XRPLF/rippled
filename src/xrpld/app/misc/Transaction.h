022142f14d0e91168fc098dce7eaaba63d82d340
Ed Hennis <ed@ripple.com>
2024-11-13 19:05:16 -0500

    Document why a lock is not used for Transaction::mApplying

--- src/xrpld/app/misc/Transaction.h
+++ src/xrpld/app/misc/Transaction.h
@@ -396,8 +396,24 @@ private:
     LedgerIndex mLedgerIndex = 0;
     TransStatus mStatus = INVALID;
     TER mResult = temUNCERTAIN;
-    // Note that all access to mApplying are made by NetworkOPsImp, and must be
-    // done under that class's lock.
+    /* Note that all access to mApplying are made by NetworkOPsImp,
+        and must be done under that class's lock. This avoids the overhead of
+        taking a separate lock, and the consequences of a race condition are
+        nearly-zero.
+
+        1. If there is a race condition, and getApplying returns false when it
+            should be true, the transaction will be processed again. Not that
+            big a deal if it's a rare one-off. Most of the time, it'll get
+            tefALREADY or tefPAST_SEQ.
+        2. On the flip side, if it returns true, when it should be false, then
+            the transaction must have been attempted recently, so no big deal if
+            it doesn't immediately get tried right away.
+        3. If there's a race between setApplying and clearApplying, and the
+            flag ends up set, then a batch is about to try to process the
+            transaction and will call clearApplying later. If it ends up
+            cleared, then it might get attempted again later as is the case with
+            item 1.
+    */
     bool mApplying = false;
 
     /** different ways for transaction to be accepted */
