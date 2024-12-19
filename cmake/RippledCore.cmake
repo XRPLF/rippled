XRPLF/rippled

commit ea1fffeebfe6be7711e19914f272ad15813189cf

Enforce levelization in libxrpl with CMake (#5111)
Adds two CMake functions:

* add_module(library subdirectory): Declares an OBJECT "library" (a CMake abstraction for a collection of object files) with sources from the given subdirectory of the given library, representing a module. Isolates the module's headers by creating a subdirectory in the build directory, e.g. .build/tmp123, that contains just a symlink, e.g. .build/tmp123/basics, to the module's header directory, e.g. include/xrpl/basics, in the source directory, and putting .build/tmp123 (but not include/xrpl) on the include path of the module sources. This prevents the module sources from including headers not explicitly linked to the module in CMake with target_link_libraries.
* target_link_modules(library scope modules...): Links the library target to each of the module targets, and removes their sources from its source list (so they are not compiled and linked twice).

Uses these functions to separate and explicitly link modules in libxrpl:

    Level 01: beast
    Level 02: basics
    Level 03: json, crypto
    Level 04: protocol
    Level 05: resource, server

cmake/RippledCore.cmake
@@ -47,6 +47,57 @@ target_link_libraries(xrpl.libpb
     gRPC::grpc++
 )
 
+# TODO: Clean up the number of library targets later.
+add_library(xrpl.imports.main INTERFACE)
+target_link_libraries(xrpl.imports.main INTERFACE
+    LibArchive::LibArchive
+    OpenSSL::Crypto
+    Ripple::boost
+    Ripple::opts
+    Ripple::syslibs
+    absl::random_random
+    date::date
+    ed25519::ed25519
+    secp256k1::secp256k1
+    xxHash::xxhash
+)
+
+include(add_module)
+include(target_link_modules)
+
+# Level 01
+add_module(xrpl beast)
+target_link_libraries(xrpl.libxrpl.beast PUBLIC
+  xrpl.imports.main
+  xrpl.libpb
+)
+
+# Level 02
+add_module(xrpl basics)
+target_link_libraries(xrpl.libxrpl.basics PUBLIC xrpl.libxrpl.beast)
+
+# Level 03
+add_module(xrpl json)
+target_link_libraries(xrpl.libxrpl.json PUBLIC xrpl.libxrpl.basics)
+
+add_module(xrpl crypto)
+target_link_libraries(xrpl.libxrpl.crypto PUBLIC xrpl.libxrpl.basics)
+
+# Level 04
+add_module(xrpl protocol)
+target_link_libraries(xrpl.libxrpl.protocol PUBLIC
+  xrpl.libxrpl.crypto
+  xrpl.libxrpl.json
+)
+
+# Level 05
+add_module(xrpl resource)
+target_link_libraries(xrpl.libxrpl.resource PUBLIC xrpl.libxrpl.protocol)
+
+add_module(xrpl server)
+target_link_libraries(xrpl.libxrpl.server PUBLIC xrpl.libxrpl.protocol)
+
+
 add_library(xrpl.libxrpl)
 set_target_properties(xrpl.libxrpl PROPERTIES OUTPUT_NAME xrpl)
 if(unity)
@@ -60,10 +111,24 @@ file(GLOB_RECURSE sources CONFIGURE_DEPENDS
 )
 target_sources(xrpl.libxrpl PRIVATE ${sources})
 
-target_include_directories(xrpl.libxrpl
-  PUBLIC
-    $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
-    $<INSTALL_INTERFACE:include>)
+target_link_modules(xrpl PUBLIC
+  basics
+  beast
+  crypto
+  json
+  protocol
+  resource
+  server
+)
+
+# All headers in libxrpl are in modules.
+# Uncomment this stanza if you have not yet moved new headers into a module.
+# target_include_directories(xrpl.libxrpl
+#   PRIVATE
+#     $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/src>
+#   PUBLIC
+#     $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
+#     $<INSTALL_INTERFACE:include>)
 
 target_compile_definitions(xrpl.libxrpl
   PUBLIC

commit e103657a578e36a0e769bbec82f974ae6d8c174f

Use the same compiler options for xrpl.libxrpl and all its submodules
* Resolves an issue introduced by #5111, which inadvertently removed the
  -Wno-maybe-uninitialized compiler option from those modules. This
  resulted in new "may be used uninitialized" build warnings, first
  noticed in the "protocol" module. When compiling with derr=TRUE, those
  warnings became errors, which made the build fail.

cmake/RippledCore.cmake
@@ -67,6 +67,7 @@ include(target_link_modules)
 
 # Level 01
 add_module(xrpl beast)
+list(APPEND submodules beast)
 target_link_libraries(xrpl.libxrpl.beast PUBLIC
   xrpl.imports.main
   xrpl.libpb
@@ -74,17 +75,21 @@ target_link_libraries(xrpl.libxrpl.beast PUBLIC
 
 # Level 02
 add_module(xrpl basics)
+list(APPEND submodules basics)
 target_link_libraries(xrpl.libxrpl.basics PUBLIC xrpl.libxrpl.beast)
 
 # Level 03
 add_module(xrpl json)
+list(APPEND submodules json)
 target_link_libraries(xrpl.libxrpl.json PUBLIC xrpl.libxrpl.basics)
 
 add_module(xrpl crypto)
+list(APPEND submodules crypto)
 target_link_libraries(xrpl.libxrpl.crypto PUBLIC xrpl.libxrpl.basics)
 
 # Level 04
 add_module(xrpl protocol)
+list(APPEND submodules protocol)
 target_link_libraries(xrpl.libxrpl.protocol PUBLIC
   xrpl.libxrpl.crypto
   xrpl.libxrpl.json
@@ -92,9 +97,11 @@ target_link_libraries(xrpl.libxrpl.protocol PUBLIC
 
 # Level 05
 add_module(xrpl resource)
+list(APPEND submodules resource)
 target_link_libraries(xrpl.libxrpl.resource PUBLIC xrpl.libxrpl.protocol)
 
 add_module(xrpl server)
+list(APPEND submodules server)
 target_link_libraries(xrpl.libxrpl.server PUBLIC xrpl.libxrpl.protocol)
 
 
@@ -112,13 +119,7 @@ file(GLOB_RECURSE sources CONFIGURE_DEPENDS
 target_sources(xrpl.libxrpl PRIVATE ${sources})
 
 target_link_modules(xrpl PUBLIC
-  basics
-  beast
-  crypto
-  json
-  protocol
-  resource
-  server
+  ${submodules}
 )
 
 # All headers in libxrpl are in modules.
@@ -130,33 +131,48 @@ target_link_modules(xrpl PUBLIC
 #     $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
 #     $<INSTALL_INTERFACE:include>)
 
-target_compile_definitions(xrpl.libxrpl
-  PUBLIC
-    BOOST_ASIO_USE_TS_EXECUTOR_AS_DEFAULT
-    BOOST_CONTAINER_FWD_BAD_DEQUE
-    HAS_UNCAUGHT_EXCEPTIONS=1)
-
-target_compile_options(xrpl.libxrpl
-  PUBLIC
-    $<$<BOOL:${is_gcc}>:-Wno-maybe-uninitialized>
-    $<$<BOOL:${voidstar}>:-DENABLE_VOIDSTAR>
+# These settings must be applied to all the header modules, too.
+set(base "xrpl.libxrpl")
+foreach(submodule
+  ""
+  ${submodules}
 )
+  STRING(LENGTH "${submodule}" len)
+  if(len EQUAL 0)
+    set(module "${base}")
+  else()
+    set(module "${base}.${submodule}")
+  endif()
+  message(STATUS "Setting up module ${module}")
 
-target_link_libraries(xrpl.libxrpl
-  PUBLIC
-    LibArchive::LibArchive
-    OpenSSL::Crypto
-    Ripple::boost
-    Ripple::opts
-    Ripple::syslibs
-    absl::random_random
-    date::date
-    ed25519::ed25519
-    secp256k1::secp256k1
-    xrpl.libpb
-    xxHash::xxhash
-    $<$<BOOL:${voidstar}>:antithesis-sdk-cpp>
-)
+  target_compile_definitions("${module}"
+    PUBLIC
+      BOOST_ASIO_USE_TS_EXECUTOR_AS_DEFAULT
+      BOOST_CONTAINER_FWD_BAD_DEQUE
+      HAS_UNCAUGHT_EXCEPTIONS=1)
+
+  target_compile_options("${module}"
+    PUBLIC
+      $<$<BOOL:${is_gcc}>:-Wno-maybe-uninitialized>
+      $<$<BOOL:${voidstar}>:-DENABLE_VOIDSTAR>
+  )
+
+  target_link_libraries("${module}"
+    PUBLIC
+      LibArchive::LibArchive
+      OpenSSL::Crypto
+      Ripple::boost
+      Ripple::opts
+      Ripple::syslibs
+      absl::random_random
+      date::date
+      ed25519::ed25519
+      secp256k1::secp256k1
+      xrpl.libpb
+      xxHash::xxhash
+      $<$<BOOL:${voidstar}>:antithesis-sdk-cpp>
+  )
+endforeach()
 
 if(xrpld)
   add_executable(rippled)