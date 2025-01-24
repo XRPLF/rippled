e0c1a1e62602737b36d1eeb99f21ff444f0dba89
Peng Wang <pwang200@gmail.com>
2025-01-24 12:14:53 -0500

    add a failure test case to P2P3

--- src/test/app/Wasm_test.cpp
+++ src/test/app/Wasm_test.cpp
@@ -2303,7 +2303,10 @@ struct Wasm_test : public beast::unit_test::suite
            "hash" : "74465121372813CBA4C77E31F12E137163F5B2509B16AC1703ECF0DA194B2DD4"
         })";
 
-        std::string escrow_lo_json_str = R"({
+        std::vector<uint8_t> escrow_tx_json_data(
+            escrow_tx_json_str.begin(), escrow_tx_json_str.end());
+        {
+            std::string escrow_lo_json_str = R"({
            "Account" : "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh",
            "Amount" : "100000",
            "CancelAfter" : 790297421,
@@ -2319,14 +2322,38 @@ struct Wasm_test : public beast::unit_test::suite
            "index" : "9BC6631F3EC761CF9BD846D006560E2D57B0A5C91D4570AEB209645B189A702F"
         })";
 
-        std::vector<uint8_t> escrow_tx_json_data(
-            escrow_tx_json_str.begin(), escrow_tx_json_str.end());
-        std::vector<uint8_t> escrow_lo_json_data(
-            escrow_lo_json_str.begin(), escrow_lo_json_str.end());
-        auto re = runEscrowWasm(
-            wasm, funcName, escrow_tx_json_data, escrow_lo_json_data);
-        if (BEAST_EXPECT(re.has_value()))
-            BEAST_EXPECT(re.value());
+            std::vector<uint8_t> escrow_lo_json_data(
+                escrow_lo_json_str.begin(), escrow_lo_json_str.end());
+            auto re = runEscrowWasm(
+                wasm, funcName, escrow_tx_json_data, escrow_lo_json_data);
+            if (BEAST_EXPECT(re.has_value()))
+                BEAST_EXPECT(re.value());
+        }
+
+        {
+            std::string escrow_lo_json_str = R"({
+           "Account" : "rHb9CJAWyB4rj91VRWn96DkukG4bwdty00",
+           "Amount" : "100000",
+           "CancelAfter" : 790297421,
+           "Destination" : "rBYn44yhs8cf8G2t79XMUHYQpp2ayhqwcw",
+           "DestinationNode" : "0",
+           "FinishAfter" : 790297403,
+           "FinishFunction" : "0061736D0100000001180460027F7F0060017F017F60027F7F017F60047F7F7F7F00030C0B01010200000000000003000405017001030305030100110619037F01418080C0000B7F0041DD85C0000B7F0041E085C0000B074205066D656D6F7279020008616C6C6F6361746500000F636865636B5F6163636F756E74494400020A5F5F646174615F656E6403010B5F5F686561705F6261736503020908010041010B02060A0AF5360B610002",
+           "Flags" : 0,
+           "LedgerEntryType" : "Escrow",
+           "OwnerNode" : "0",
+           "PreviousTxnID" : "CF25D1C6B8E637C7DAC61B586F820A16896A3090D9F6FBF9FA00D8B13A265647",
+           "PreviousTxnLgrSeq" : 4,
+           "index" : "9BC6631F3EC761CF9BD846D006560E2D57B0A5C91D4570AEB209645B189A702F"
+        })";
+
+            std::vector<uint8_t> escrow_lo_json_data(
+                escrow_lo_json_str.begin(), escrow_lo_json_str.end());
+            auto re = runEscrowWasm(
+                wasm, funcName, escrow_tx_json_data, escrow_lo_json_data);
+            if (BEAST_EXPECT(re.has_value()))
+                BEAST_EXPECT(!re.value());
+        }
     }
 
     void
