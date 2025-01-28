f3d39ec680b9ddd57021373f9bffa44e044cfa6e
Olek <115580134+oleks-rip@users.noreply.github.com>
2025-01-28 10:45:59 -0500

    Fix CI unit tests (#5196)
    - Add retries for rpc client
    - Add dynamic port allocation for rpc servers

--- src/xrpld/overlay/detail/OverlayImpl.cpp
+++ src/xrpld/overlay/detail/OverlayImpl.cpp
@@ -478,7 +478,7 @@ OverlayImpl::start()
 {
     PeerFinder::Config config = PeerFinder::Config::makeConfig(
         app_.config(),
-        serverHandler_.setup().overlay.port,
+        serverHandler_.setup().overlay.port(),
         app_.getValidationPublicKey().has_value(),
         setup_.ipLimit);
 
