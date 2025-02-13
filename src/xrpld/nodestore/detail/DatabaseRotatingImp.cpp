3994ae321a38fe2bf0d0096c7bfebd4b74d3bd1d
Ed Hennis <ed@ripple.com>
2025-02-13 13:16:41 -0500

    Revert "Make the `rotate()` internals const"
    This reverts commit 4986662b6507d9916fb20710a75015134ad947a7.

--- src/xrpld/nodestore/detail/DatabaseRotatingImp.cpp
+++ src/xrpld/nodestore/detail/DatabaseRotatingImp.cpp
@@ -47,23 +47,20 @@ DatabaseRotatingImp::rotate(
         std::string const& writableName,
         std::string const& archiveName)> const& f)
 {
-    auto const
-        [newWritableBackendName, newArchiveBackendName, oldArchiveBackend] =
-            [&]() {
-                std::lock_guard lock(mutex_);
+    std::string newWritableBackendName;
+    std::string newArchiveBackendName;
+    std::shared_ptr<NodeStore::Backend> oldArchiveBackend;
+    {
+        std::lock_guard lock(mutex_);
 
-                archiveBackend_->setDeletePath();
-                auto const oldArchiveBackend = std::move(archiveBackend_);
+        archiveBackend_->setDeletePath();
+        oldArchiveBackend = std::move(archiveBackend_);
 
-                archiveBackend_ = std::move(writableBackend_);
-                auto const newArchiveBackendName = archiveBackend_->getName();
-                writableBackend_ = std::move(newBackend);
-                auto const newWritableBackendName = writableBackend_->getName();
-                return std::tuple{
-                    newWritableBackendName,
-                    newArchiveBackendName,
-                    oldArchiveBackend};
-            }();
+        archiveBackend_ = std::move(writableBackend_);
+        newArchiveBackendName = archiveBackend_->getName();
+        writableBackend_ = std::move(newBackend);
+        newWritableBackendName = writableBackend_->getName();
+    }
 
     f(newWritableBackendName, newArchiveBackendName);
 }
