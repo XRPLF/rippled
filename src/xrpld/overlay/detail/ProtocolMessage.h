dd5e6559dda14f8a1684cf81d90b86920628a4df
Ed Hennis <ed@ripple.com>
2025-02-14 18:51:51 -0500

    Reduce duplicate peer traffic for ledger data (#5126)
    - Drop duplicate outgoing TMGetLedger messages per peer
      - Allow a retry after 30s in case of peer or network congestion.
      - Addresses RIPD-1870
      - (Changes levelization. That is not desirable, and will need to be fixed.)
    - Drop duplicate incoming TMGetLedger messages per peer
      - Allow a retry after 15s in case of peer or network congestion.
      - The requestCookie is ignored when computing the hash, thus increasing
        the chances of detecting duplicate messages.
      - With duplicate messages, keep track of the different requestCookies
        (or lack of cookie). When work is finally done for a given request,
        send the response to all the peers that are waiting on the request,
        sending one message per peer, including all the cookies and
        a "directResponse" flag indicating the data is intended for the
        sender, too.
      - Addresses RIPD-1871
    - Drop duplicate incoming TMLedgerData messages
      - Addresses RIPD-1869
    - Improve logging related to ledger acquisition
    - Class "CanProcess" to keep track of processing of distinct items
    
    ---------
    
    Co-authored-by: Valentin Balaschenko <13349202+vlntb@users.noreply.github.com>

--- src/xrpld/overlay/detail/ProtocolMessage.h
+++ src/xrpld/overlay/detail/ProtocolMessage.h
@@ -44,6 +44,12 @@ protocolMessageType(protocol::TMGetLedger const&)
 }
 
 inline protocol::MessageType
+protocolMessageType(protocol::TMLedgerData const&)
+{
+    return protocol::mtLEDGER_DATA;
+}
+
+inline protocol::MessageType
 protocolMessageType(protocol::TMReplayDeltaRequest const&)
 {
     return protocol::mtREPLAY_DELTA_REQ;
@@ -485,5 +491,65 @@ invokeProtocolMessage(
 }
 
 }  // namespace ripple
+
+namespace protocol {
+
+template <class Hasher>
+void
+hash_append(Hasher& h, TMGetLedger const& msg)
+{
+    using beast::hash_append;
+    using namespace ripple;
+    hash_append(h, safe_cast<int>(protocolMessageType(msg)));
+    hash_append(h, safe_cast<int>(msg.itype()));
+    if (msg.has_ltype())
+        hash_append(h, safe_cast<int>(msg.ltype()));
+
+    if (msg.has_ledgerhash())
+        hash_append(h, msg.ledgerhash());
+
+    if (msg.has_ledgerseq())
+        hash_append(h, msg.ledgerseq());
+
+    for (auto const& nodeId : msg.nodeids())
+        hash_append(h, nodeId);
+    hash_append(h, msg.nodeids_size());
+
+    // Do NOT include the request cookie. It does not affect the content of the
+    // request, but only where to route the results.
+    // if (msg.has_requestcookie())
+    //    hash_append(h, msg.requestcookie());
+
+    if (msg.has_querytype())
+        hash_append(h, safe_cast<int>(msg.querytype()));
+
+    if (msg.has_querydepth())
+        hash_append(h, msg.querydepth());
+}
+
+template <class Hasher>
+void
+hash_append(Hasher& h, TMLedgerData const& msg)
+{
+    using beast::hash_append;
+    using namespace ripple;
+    hash_append(h, safe_cast<int>(protocolMessageType(msg)));
+    hash_append(h, msg.ledgerhash());
+    hash_append(h, msg.ledgerseq());
+    hash_append(h, safe_cast<int>(msg.type()));
+    for (auto const& node : msg.nodes())
+    {
+        hash_append(h, node.nodedata());
+        if (node.has_nodeid())
+            hash_append(h, node.nodeid());
+    }
+    hash_append(h, msg.nodes_size());
+    if (msg.has_requestcookie())
+        hash_append(h, msg.requestcookie());
+    if (msg.has_error())
+        hash_append(h, safe_cast<int>(msg.error()));
+}
+
+}  // namespace protocol
 
 #endif
