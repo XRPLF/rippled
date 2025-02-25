48e0d20f9260d842ea2a044d0f4b71822b24a84c
Bronek Kozicki <brok@incorrekt.com>
2025-02-25 14:14:10 +0000

    fix: Remove 'new parent hash' assert (#5313)
    This assert is known to occasionally trigger, without causing errors
    downstream. It is replaced with a log message.

--- src/xrpld/app/misc/TxQ.h
+++ src/xrpld/app/misc/TxQ.h
@@ -785,13 +785,10 @@ private:
     */
     std::optional<size_t> maxSize_;
 
-#if !NDEBUG
     /**
-        parentHash_ checks that no unexpected ledger transitions
-        happen, and is only checked via debug asserts.
+        parentHash_ used for logging only
     */
     LedgerHash parentHash_{beast::zero};
-#endif
 
     /** Most queue operations are done under the master lock,
         but use this mutex for the RPC "fee" command, which isn't.
