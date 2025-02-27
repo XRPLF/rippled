9a8a9acbc2f0c45edf7a862644d13389a291fefe
Bart Thomee <11445373+bthomee@users.noreply.github.com>
2025-02-26 20:05:56 -0500

    Fix platform incompatibilities

--- src/xrpld/app/misc/detail/ValidatorSite.cpp
+++ src/xrpld/app/misc/detail/ValidatorSite.cpp
@@ -22,7 +22,6 @@
 #include <xrpld/app/misc/detail/WorkFile.h>
 #include <xrpld/app/misc/detail/WorkPlain.h>
 #include <xrpld/app/misc/detail/WorkSSL.h>
-#include <xrpl/basics/safe_cast.h>
 #include <xrpl/json/json_reader.h>
 #include <xrpl/protocol/digest.h>
 #include <xrpl/protocol/jss.h>
@@ -697,7 +696,7 @@ ValidatorSite::getJson() const
                         site.lastRefreshStatus->message;
             }
             v[jss::refresh_interval_min] =
-                unsafe_cast<Int>(site.refreshInterval.count());
+                static_cast<Int>(site.refreshInterval.count());
         }
     }
     return jrr;
