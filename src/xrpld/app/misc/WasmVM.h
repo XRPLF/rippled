2e9047ca1c71e8abe4af8196a98a662dc59a7f02
Peng Wang <pwang200@gmail.com>
2025-03-11 09:55:20 -0400

    devnet 1 host functions

--- src/xrpld/app/misc/WasmVM.h
+++ src/xrpld/app/misc/WasmVM.h
@@ -23,6 +23,7 @@
 // #include <xrpl/beast/utility/Journal.h>
 #include <xrpl/protocol/TER.h>
 
+#include "xrpl/basics/base_uint.h"
 #include <wasmedge/wasmedge.h>
 
 namespace ripple {
@@ -83,6 +84,73 @@ runEscrowWasm(
     int32_t gasLimit,
     int32_t input);
 
+/////////////// Devnet 1 /////////////////
+typedef std::vector<uint8_t> Bytes;
+typedef ripple::uint256 Hash;
+
+template <typename T>
+struct WasmResult
+{
+    T result;
+    uint64_t cost;
+};
+typedef WasmResult<bool> EscrowResult;
+
+struct HostFunctions{
+    virtual int32_t
+    getLedgerSqn()
+    {
+        return 1;
+    }
+
+    virtual int32_t
+    getParentLedgerTime()
+    {
+        return 1;
+    }
+
+    virtual std::optional<Bytes>
+    getTxField(std::string const& fname)
+    {
+        return Bytes{};
+    }
+
+    virtual std::optional<Bytes>
+    getLedgerEntryField(
+        int32_t type,
+        Bytes const& kdata,
+        std::string const& fname)
+    {
+        return Bytes{};
+    }
+
+    virtual std::optional<Bytes>
+    getCurrentLedgerEntryField(std::string const& fname)
+    {
+        return Bytes{};
+    }
+
+    virtual bool
+    updateData(Bytes const& data)
+    {
+        return true;
+    }
+
+    virtual Hash
+    computeSha512HalfHash(Bytes const& data){
+        return Hash {};
+    }
+
+    virtual ~HostFunctions() = default;
+};
+
+Expected<EscrowResult, TER>
+runEscrowWasm(
+    std::vector<uint8_t> const& wasmCode,
+    std::string const& funcName,
+    HostFunctions* hfs,
+    uint64_t gasLimit);
+
 }  // namespace ripple
 #endif  // RIPPLE_APP_MISC_WASMVM_H_INLCUDED
 
@@ -95,3 +163,5 @@ runEscrowWasm(
 // private:
 //     beast::Journal j_;
 // };
+
+
