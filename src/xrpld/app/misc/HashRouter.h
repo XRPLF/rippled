5822cca6016efe5097e28cc2f462dff53d50a5cf
Ed Hennis <ed@ripple.com>
2025-04-25 11:36:04 -0400

    Reduce duplicate peer traffic for ledger data (#5126)
    - Drop duplicate outgoing TMGetLedger messages per peer
      - Allow a retry after 30s in case of peer or network congestion.
      - Addresses RIPD-1870
      - (Changes levelization. That is not desirable, and will need to be fixed.)
    - Drop duplicate incoming TMGetLedger messages per peer
      - Allow a retry after 15s in case of peer or network congestion.
      - The requestCookie is ignored when computing the hash, thus increasing
        the chances of detecting duplicate messages.
      - With duplicate messages, keep track of the different requestCookies
        (or lack of cookie). When work is finally done for a given request,
        send the response to all the peers that are waiting on the request,
        sending one message per peer, including all the cookies and
        a "directResponse" flag indicating the data is intended for the
        sender, too.
      - Addresses RIPD-1871
    - Drop duplicate incoming TMLedgerData messages
      - Addresses RIPD-1869

--- src/xrpld/app/misc/HashRouter.h
+++ src/xrpld/app/misc/HashRouter.h
@@ -120,6 +120,13 @@ private:
             return std::move(peers_);
         }
 
+        /** Return set of peers waiting for reply. Leaves list unchanged. */
+        std::set<PeerShortID> const&
+        peekPeerSet()
+        {
+            return peers_;
+        }
+
         /** Return seated relay time point if the message has been relayed */
         std::optional<Stopwatch::time_point>
         relayed() const
@@ -153,6 +160,21 @@ private:
             return true;
         }
 
+        bool
+        shouldProcessForPeer(
+            PeerShortID peer,
+            Stopwatch::time_point now,
+            std::chrono::seconds interval)
+        {
+            if (peerProcessed_.contains(peer) &&
+                ((peerProcessed_[peer] + interval) > now))
+                return false;
+            // Peer may already be in the list, but adding it again doesn't hurt
+            addPeer(peer);
+            peerProcessed_[peer] = now;
+            return true;
+        }
+
     private:
         int flags_ = 0;
         std::set<PeerShortID> peers_;
@@ -160,6 +182,7 @@ private:
         // than one flag needs to expire independently.
         std::optional<Stopwatch::time_point> relayed_;
         std::optional<Stopwatch::time_point> processed_;
+        std::map<PeerShortID, Stopwatch::time_point> peerProcessed_;
     };
 
 public:
@@ -200,6 +223,18 @@ public:
         int& flags,
         std::chrono::seconds tx_interval);
 
+    /** Determines whether the hashed item should be processed for the given
+       peer. Could be an incoming or outgoing message.
+
+       Items filtered with this function should only be processed for the given
+       peer once. Unlike shouldProcess, it can be processed for other peers.
+     */
+    bool
+    shouldProcessForPeer(
+        uint256 const& key,
+        PeerShortID peer,
+        std::chrono::seconds interval);
+
     /** Set the flags on a hash.
 
         @return `true` if the flags were changed. `false` if unchanged.
@@ -224,6 +259,11 @@ public:
     */
     std::optional<std::set<PeerShortID>>
     shouldRelay(uint256 const& key);
+
+    /** Returns a copy of the set of peers in the Entry for the key
+     */
+    std::set<PeerShortID>
+    getPeers(uint256 const& key);
 
 private:
     // pair.second indicates whether the entry was created
