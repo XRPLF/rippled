14e0228c7c20975c8a7e676b10d87a7873f10408
Bronek Kozicki <brok@incorrekt.com>
2025-05-20 13:15:20 +0100

    Review feedback
    Co-authored-by: Valentin Balaschenko <13349202+vlntb@users.noreply.github.com>

--- src/xrpld/app/tx/detail/SetTrust.cpp
+++ src/xrpld/app/tx/detail/SetTrust.cpp
@@ -298,14 +298,10 @@ SetTrust::preclaim(PreclaimContext const& ctx)
             else
                 return tecINTERNAL;  // LCOV_EXCL_LINE
         }
-        else if (sleDst->isFieldPresent(sfVaultID))
+        else if (sleDst->isFieldPresent(sfVaultID) &&
+         !ctx.view.exists(keylet::line(id, uDstAccountID, currency)))
         {
-            if (ctx.view.exists(keylet::line(id, uDstAccountID, currency)))
-            {
-                // pass
-            }
-            else
-                return tecNO_PERMISSION;
+            return tecNO_PERMISSION;
         }
         else
             return tecPSEUDO_ACCOUNT;
