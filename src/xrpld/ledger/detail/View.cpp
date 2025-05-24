5e76f42f3c041c11c74c6c52295266b5d67e8fa0
Denis Angell <dangell@transia.co>
2025-05-24 17:07:40 +0200

    clang-format

--- src/xrpld/ledger/detail/View.cpp
+++ src/xrpld/ledger/detail/View.cpp
@@ -1294,10 +1294,8 @@ trustCreate(
         bSetHigh ? sfHighLimit : sfLowLimit, saLimit);
     sleRippleState->setFieldAmount(
         bSetHigh ? sfLowLimit : sfHighLimit,
-        STAmount(
-            Issue{
-                saBalance.getCurrency(),
-                bSetDst ? uSrcAccountID : uDstAccountID}));
+        STAmount(Issue{
+            saBalance.getCurrency(), bSetDst ? uSrcAccountID : uDstAccountID}));
 
     if (uQualityIn)
         sleRippleState->setFieldU32(
