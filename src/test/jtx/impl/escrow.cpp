d9e24b723defe982a488de0a49ea30e0166d2ca8
Denis Angell <dangell@transia.co>
2025-05-24 18:10:49 +0200

    fix tfUniversal

--- src/test/jtx/impl/escrow.cpp
+++ src/test/jtx/impl/escrow.cpp
@@ -34,7 +34,7 @@ create(AccountID const& account, AccountID const& to, STAmount const& amount)
 {
     Json::Value jv;
     jv[jss::TransactionType] = jss::EscrowCreate;
-    jv[jss::Flags] = tfUniversal;
+    jv[jss::Flags] = tfFullyCanonicalSig;
     jv[jss::Account] = to_string(account);
     jv[jss::Destination] = to_string(to);
     jv[jss::Amount] = amount.getJson(JsonOptions::none);
@@ -46,7 +46,7 @@ finish(AccountID const& account, AccountID const& from, std::uint32_t seq)
 {
     Json::Value jv;
     jv[jss::TransactionType] = jss::EscrowFinish;
-    jv[jss::Flags] = tfUniversal;
+    jv[jss::Flags] = tfFullyCanonicalSig;
     jv[jss::Account] = to_string(account);
     jv[sfOwner.jsonName] = to_string(from);
     jv[sfOfferSequence.jsonName] = seq;
@@ -58,7 +58,7 @@ cancel(AccountID const& account, Account const& from, std::uint32_t seq)
 {
     Json::Value jv;
     jv[jss::TransactionType] = jss::EscrowCancel;
-    jv[jss::Flags] = tfUniversal;
+    jv[jss::Flags] = tfFullyCanonicalSig;
     jv[jss::Account] = to_string(account);
     jv[sfOwner.jsonName] = from.human();
     jv[sfOfferSequence.jsonName] = seq;
