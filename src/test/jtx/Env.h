8654e4f48f7483af00e723307eaff3e12191a14d
Mayukha Vadari <mvadari@gmail.com>
2025-05-28 12:16:48 -0400

    add comment

--- src/test/jtx/Env.h
+++ src/test/jtx/Env.h
@@ -589,13 +589,15 @@ public:
     }
 
     /** Return metadata for the last JTx.
-
-        Effects:
-
-            The open ledger is closed as if by a call
-            to close(). The metadata for the last
-            transaction ID, if any, is returned.
-    */
+     *
+     * NOTE: this has a side effect of closing the open ledger.
+     *
+     *  Effects:
+     *
+     *      The open ledger is closed as if by a call
+     *      to close(). The metadata for the last
+     *      transaction ID, if any, is returned.
+     */
     std::shared_ptr<STObject const>
     meta();
 
