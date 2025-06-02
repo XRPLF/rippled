6c80a59364ec21b3d6a3cb93635c800e92b076ad
Valentin Balaschenko <13349202+vlntb@users.noreply.github.com>
2025-06-02 19:20:45 +0100

    all hw threads

--- include/xrpl/basics/partitioned_unordered_map.h
+++ include/xrpl/basics/partitioned_unordered_map.h
@@ -255,7 +255,7 @@ public:
     {
         // Set partitions to use half of the number of hardware threads if the
         // parameter is either empty or set to 0.
-        partitions_ = std::size_t{std::thread::hardware_concurrency() / 2};
+        partitions_ = std::size_t{std::thread::hardware_concurrency()};
         // partitions && *partitions
         // ? *partitions
         // : std::size_t{std::thread::hardware_concurrency() / 2};
