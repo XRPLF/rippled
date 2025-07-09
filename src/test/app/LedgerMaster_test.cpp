708350e37f2bc17e3c06497a832e20cefd17e39b
Ed Hennis <ed@ripple.com>
2025-07-08 21:03:23 -0400

    fix: Update lastRotated to the current validated seq + 2
    - First approximation. Good enough and consistent enough for unit tests.
      Not sure about real world, but it'll be an improvement either way.

--- src/test/app/LedgerMaster_test.cpp
+++ src/test/app/LedgerMaster_test.cpp
@@ -175,7 +175,8 @@ class LedgerMaster_test : public beast::unit_test::suite
             if (maxSeq == lastRotated + deleteInterval)
             {
                 minSeq = lastRotated;
-                lastRotated = maxSeq;
+                lastRotated = store.getLastRotated();
+                BEAST_EXPECT(lastRotated = maxSeq + 2);
             }
             BEAST_EXPECTS(
                 env.closed()->info().seq == maxSeq,
