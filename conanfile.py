900131d09f38575b3d68796ab329df4cedeed852
Bart Thomee <11445373+bthomee@users.noreply.github.com>
2025-07-21 16:36:24 -0400

    Temporarily disable ripple remote

--- conanfile.py
+++ conanfile.py
@@ -32,10 +32,6 @@ class Xrpl(ConanFile):
         'zlib/1.3.1',
     ]
 
-    replace_requires = {
-        'abseil/*': 'abseil/20250127.0',
-    }
-
     test_requires = [
         'doctest/2.4.11',
     ]
