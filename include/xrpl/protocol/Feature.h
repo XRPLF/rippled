c4a06dd1b7614f108abfd5513aade8638cd6bd9f
Jingchen <a1q123456@users.noreply.github.com>
2025-07-22 12:29:51 +0100

    Update include/xrpl/protocol/Feature.h
    Co-authored-by: Ed Hennis <ed@ripple.com>

--- include/xrpl/protocol/Feature.h
+++ include/xrpl/protocol/Feature.h
@@ -54,8 +54,18 @@
  *    then change the macro parameter in features.macro to
  *    `VoteBehavior::DefaultYes`. The communication process is beyond
  *    the scope of these instructions.
- * 5) A feature marked as Obsolete means it is in the ledger (marked as
- *      Supported::yes) and it is on its way to become Retired.
+
+ * 5) If a supported feature (`Supported::yes`) was _ever_ in a released
+ *     version, it can never be changed back to `Supported::no`, because
+ *     it _may_ still become enabled at any time. This would cause newer
+ *     versions of `rippled` to become amendment blocked.
+ *     Instead, to prevent newer versions from voting on the feature, use
+ *     `VoteBehavior::Obsolete`. Obsolete features can not be voted for
+ *     by any versions of `rippled` built with that setting, but will still
+ *     work correctly if they get enabled. If a feature remains obsolete
+ *     for long enough that _all_ clients that could vote for it are
+ *     amendment blocked, the feature can be removed from the code
+ *     as if it was unsupported.
  *
  *
  * When a feature has been enabled for several years, the conditional code
