bd4a6b197f3effac180020755d63467b9bca27fb
Alex Kremer <akremer@ripple.com>
2025-07-24 03:14:43 +0100

    Attempt to workaround stacktrace_from_exception duplicate symbol

--- conanfile.py
+++ conanfile.py
@@ -100,6 +100,8 @@ class Xrpl(ConanFile):
     def configure(self):
         if self.settings.compiler == 'apple-clang':
             self.options['boost'].visibility = 'global'
+        if self.settings.compiler in ['clang', 'gcc']:
+            self.options['boost'].without_stacktrace_from_exception = True
 
     def requirements(self):
         # Conan 2 requires transitive headers to be specified
