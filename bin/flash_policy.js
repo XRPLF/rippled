c7f8276ea0f981c396485daacaa4efd2975c1f62
Mayukha Vadari <mvadari@gmail.com>
2025-08-07 13:26:56 -0400

    run VSCode autolint on all files

--- bin/flash_policy.js
+++ bin/flash_policy.js
@@ -10,22 +10,23 @@
 // [websocket_public_port].
 //
 
-var net	    = require("net"),
-    port    = "*",
-    domains = ["*:"+port]; // Domain:Port
+var net = require("net"), port = "*",
+    domains = [ "*:" + port ]; // Domain:Port
 
-net.createServer(
-  function(socket) {
-    socket.write("<?xml version='1.0' ?>\n");
-    socket.write("<!DOCTYPE cross-domain-policy SYSTEM 'http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd'>\n");
-    socket.write("<cross-domain-policy>\n");
-    domains.forEach(
-      function(domain) {
-        var parts = domain.split(':');
-        socket.write("\t<allow-access-from domain='" + parts[0] + "' to-ports='" + parts[1] + "' />\n");
-      }
-    );
-    socket.write("</cross-domain-policy>\n");
-    socket.end();
-  }
-).listen(843);
+net.createServer(function(socket) {
+     socket.write("<?xml version='1.0' ?>\n");
+     socket.write(
+         "<!DOCTYPE cross-domain-policy SYSTEM 'http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd'>\n",
+     );
+     socket.write("<cross-domain-policy>\n");
+     domains.forEach(function(domain) {
+       var parts = domain.split(":");
+       socket.write(
+           "\t<allow-access-from domain='" + parts[0] + "' to-ports='" +
+               parts[1] + "' />\n",
+       );
+     });
+     socket.write("</cross-domain-policy>\n");
+     socket.end();
+   })
+    .listen(843);
--- bin/jsonrpc_request.js
+++ bin/jsonrpc_request.js
@@ -7,36 +7,33 @@
 // Requires: npm simple-jsonrpc
 //
 
-var jsonrpc   = require('simple-jsonrpc');
+var jsonrpc = require("simple-jsonrpc");
 
-var program   = process.argv[1];
+var program = process.argv[1];
 
 if (5 !== process.argv.length) {
   console.log("Usage: %s <URL> <method> <json>", program);
-}
-else {
-  var url       = process.argv[2];
-  var method    = process.argv[3];
-  var json_raw  = process.argv[4];
+} else {
+  var url = process.argv[2];
+  var method = process.argv[3];
+  var json_raw = process.argv[4];
   var json;
 
   try {
-    json      = JSON.parse(json_raw);
-  }
-  catch (e) {
-      console.log("JSON parse error: %s", e.message);
-      throw e;
+    json = JSON.parse(json_raw);
+  } catch (e) {
+    console.log("JSON parse error: %s", e.message);
+    throw e;
   }
 
-  var client  = jsonrpc.client(url);
+  var client = jsonrpc.client(url);
 
-  client.call(method, json,
-    function (result) {
-      console.log(JSON.stringify(result, undefined, 2));
-    },
-    function (error) {
-      console.log(JSON.stringify(error, undefined, 2));
-    });
+  client.call(
+      method,
+      json,
+      function(result) { console.log(JSON.stringify(result, undefined, 2)); },
+      function(error) { console.log(JSON.stringify(error, undefined, 2)); },
+  );
 }
 
 // vim:sw=2:sts=2:ts=8:et
