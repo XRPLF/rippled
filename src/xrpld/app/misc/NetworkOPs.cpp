a7f6989e30b505afdca7fb7f70c225e8d1ff6da3
JCW <a1q123456@users.noreply.github.com>
2025-08-15 15:55:44 +0100

    Improve logging
    Signed-off-by: JCW <a1q123456@users.noreply.github.com>

--- src/xrpld/app/misc/NetworkOPs.cpp
+++ src/xrpld/app/misc/NetworkOPs.cpp
@@ -1450,7 +1450,7 @@ NetworkOPsImp::processTransactionSet(CanonicalTXSet const& set)
     }
     if (mTransactions.empty())
     {
-        JLOG(m_journal.info()) << "No transactions to process!";
+        JLOG(m_journal.info()) << "No transaction to process!";
         return;
     }
 
