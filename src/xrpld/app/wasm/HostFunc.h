92e513a618c956c4b1c18aaa4bb92c1159393de9
Mayukha Vadari <mvadari@gmail.com>
2025-08-22 16:00:51 -0400

    exclude the bulk of HostFunc.h from codecov

--- src/xrpld/app/wasm/HostFunc.h
+++ src/xrpld/app/wasm/HostFunc.h
@@ -99,6 +99,13 @@ floatLogImpl(Slice const& x, int32_t mode);
 
 struct HostFunctions
 {
+    virtual beast::Journal
+    getJournal()
+    {
+        return beast::Journal{beast::Journal::getNullSink()};
+    }
+
+    // LCOV_EXCL_START
     virtual void
     setRT(void const*)
     {
@@ -108,12 +115,6 @@ struct HostFunctions
     getRT() const
     {
         return nullptr;
-    }
-
-    virtual beast::Journal
-    getJournal()
-    {
-        return beast::Journal{beast::Journal::getNullSink()};
     }
 
     virtual Expected<std::uint32_t, HostFunctionError>
@@ -519,6 +520,7 @@ struct HostFunctions
     }
 
     virtual ~HostFunctions() = default;
+    // LCOV_EXCL_STOP
 };
 
 }  // namespace ripple
