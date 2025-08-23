1320eaee4472e1c5162654d6375680d7e9df9ecd
ximinez <ximinez@users.noreply.github.com>
2025-08-22 17:35:57 -0700

    deploy: 7775c725f375c6cba7a6b2380e20184c461e9d66

--- search/enums_2.js
+++ search/enums_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['category_0',['category',['../classripple_1_1TrafficCount.html#adbaee77c7a063c9aac783e54594aeda7',1,'ripple::TrafficCount::category'],['../classripple_1_1TxConsequences.html#af40159818c89963a904c9c4c0848cae9',1,'ripple::TxConsequences::Category']]],
+  ['category_0',['category',['../classripple_1_1TxConsequences.html#af40159818c89963a904c9c4c0848cae9',1,'ripple::TxConsequences::Category'],['../classripple_1_1TrafficCount.html#adbaee77c7a063c9aac783e54594aeda7',1,'ripple::TrafficCount::category']]],
   ['chaintype_1',['ChainType',['../classripple_1_1STXChainBridge.html#a3b751ce2ed6b3f2cbde126e6e2d2f051',1,'ripple::STXChainBridge']]],
   ['collectiontype_2',['CollectionType',['../classJson_1_1Writer.html#a53ecc840ddaba276dc429fd953d41ae0',1,'Json::Writer']]],
   ['compressed_3',['Compressed',['../namespaceripple_1_1compression.html#a63d59b12f7e712c378860771ef3fdf5f',1,'ripple::compression']]],
