c227bdc200984011137f90238ab3f7df77b8c608
Mayukha Vadari <mvadari@gmail.com>
2025-08-29 17:42:16 -0400

    fix tests

--- src/test/app/FeeVote_test.cpp
+++ src/test/app/FeeVote_test.cpp
@@ -866,7 +866,9 @@ class FeeVote_test : public beast::unit_test::suite
             Env env(*this, testable_amendments() | featureXRPFees);
 
             // establish what the current fees are
-            BEAST_EXPECT(env.current()->fees().base == XRPAmount{10});
+            BEAST_EXPECT(
+                env.current()->fees().base ==
+                XRPAmount{UNIT_TEST_REFERENCE_FEE});
             BEAST_EXPECT(
                 env.current()->fees().reserve == XRPAmount{200'000'000});
             BEAST_EXPECT(
@@ -976,7 +978,9 @@ class FeeVote_test : public beast::unit_test::suite
                 testable_amendments() | featureXRPFees | featureSmartEscrow);
 
             // establish what the current fees are
-            BEAST_EXPECT(env.current()->fees().base == XRPAmount{10});
+            BEAST_EXPECT(
+                env.current()->fees().base ==
+                XRPAmount{UNIT_TEST_REFERENCE_FEE});
             BEAST_EXPECT(
                 env.current()->fees().reserve == XRPAmount{200'000'000});
             BEAST_EXPECT(
