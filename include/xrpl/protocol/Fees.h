d42feec6d87f888784d63b12e6f42f9601a991aa
Oleksandr <115580134+oleks-rip@users.noreply.github.com>
2025-09-24 13:26:14 -0400

    Wasm limits(fee) voting

--- include/xrpl/protocol/Fees.h
+++ include/xrpl/protocol/Fees.h
@@ -37,9 +37,9 @@ struct Fees
     XRPAmount reserve{0};    // Reserve base (drops)
     XRPAmount increment{0};  // Reserve increment (drops)
     std::uint32_t extensionComputeLimit{
-        1'000'000};  // Extension compute limit (instructions)
-    std::uint32_t extensionSizeLimit{100'000};  // Extension size limit (bytes)
-    std::uint32_t gasPrice{1'000'000};            // price of WASM gas (micro-drops)
+        0};  // Extension compute limit (instructions)
+    std::uint32_t extensionSizeLimit{0};  // Extension size limit (bytes)
+    std::uint32_t gasPrice{0};            // price of WASM gas (micro-drops)
 
     explicit Fees() = default;
     Fees(Fees const&) = default;
