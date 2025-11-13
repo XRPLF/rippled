98151163344f8800c61ef699839746b1f1a01092
Oleksandr <115580134+oleks-rip@users.noreply.github.com>
2025-11-12 19:30:56 -0500

    Fix nullptr resolving, no db config

--- src/xrpld/app/rdb/backend/detail/Node.cpp
+++ src/xrpld/app/rdb/backend/detail/Node.cpp
@@ -171,7 +171,7 @@ getRowsMinMax(soci::session& session, TableType type)
 bool
 saveValidatedLedger(
     DatabaseCon& ldgDB,
-    DatabaseCon& txnDB,
+    std::unique_ptr<DatabaseCon> const& txnDB,
     Application& app,
     std::shared_ptr<Ledger const> const& ledger,
     bool current)
@@ -254,7 +254,7 @@ saveValidatedLedger(
 
         if (app.config().useTxTables())
         {
-            auto db = txnDB.checkoutDb();
+            auto db = txnDB->checkoutDb();
 
             soci::transaction tr(*db);
 
