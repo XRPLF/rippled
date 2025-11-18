ea8b1c74dc91f9cde7f2c69bbc8449bbb28c0403
Bart <bthomee@users.noreply.github.com>
2025-11-18 15:38:41 -0500

    Update src/test/app/PermissionedDEX_test.cpp

--- src/test/app/PermissionedDEX_test.cpp
+++ src/test/app/PermissionedDEX_test.cpp
@@ -967,7 +967,7 @@ class PermissionedDEX_test : public beast::unit_test::suite
         testcase("Remove unfunded offer");
 
         // checking that an unfunded offer will be implicitly removed by a
-        // successfully payment tx
+        // successful payment tx
         Env env(*this, features);
         auto const& [gw, domainOwner, alice, bob, carol, USD, domainID, credType] =
             PermissionedDEX(env);
