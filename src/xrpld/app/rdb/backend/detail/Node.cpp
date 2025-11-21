ab3df74e7030cc84d37dc05c5cd5ec42b865cad9
Olek <115580134+oleks-rip@users.noreply.github.com>
2025-11-21 17:20:45 -0500

    Fix: nullptr resolving without db config (#6029)
    If the config disables SQL db usage, such as a validator:
    
    ```
    [ledger_tx_tables]
    use_tx_tables = 0
    ```
    
    then the pointer to DB engine is null, but it was still resolved during startup. Although it didn't crash in Release mode, possibly due to the compiler optimizing it away, it did crash in Debug mode. This change explicitly checks for the validity of the pointer and generates a runtime error if not set.

--- src/xrpld/app/rdb/backend/detail/Node.cpp
+++ src/xrpld/app/rdb/backend/detail/Node.cpp
@@ -190,7 +190,7 @@ getRowsMinMax(soci::session& session, TableType type)
 bool
 saveValidatedLedger(
     DatabaseCon& ldgDB,
-    DatabaseCon& txnDB,
+    std::unique_ptr<DatabaseCon> const& txnDB,
     Application& app,
     std::shared_ptr<Ledger const> const& ledger,
     bool current)
@@ -273,7 +273,15 @@ saveValidatedLedger(
 
         if (app.config().useTxTables())
         {
-            auto db = txnDB.checkoutDb();
+            if (!txnDB)
+            {
+                // LCOV_EXCL_START
+                JLOG(j.fatal()) << "TxTables db isn't available";
+                Throw<std::runtime_error>("TxTables db isn't available");
+                // LCOV_EXCL_STOP
+            }
+
+            auto db = txnDB->checkoutDb();
 
             soci::transaction tr(*db);
 
