f77013758eba52c2b86ca1402079cd52a841e475
JCW <a1q123456@users.noreply.github.com>
2025-11-24 12:39:05 +0000

    Fix errors
    Signed-off-by: JCW <a1q123456@users.noreply.github.com>

--- src/test/rpc/Feature_test.cpp
+++ src/test/rpc/Feature_test.cpp
@@ -420,9 +420,9 @@ class Feature_test : public beast::unit_test::suite
                 break;
         }
 
-        // There should be at least 5 amendments.  Don't do exact comparison
+        // There should be at least 4 amendments.  Don't do exact comparison
         // to avoid maintenance as more amendments are added in the future.
-        BEAST_EXPECT(majorities.size() >= 5);
+        BEAST_EXPECT(majorities.size() >= 4);
         std::map<std::string, VoteBehavior> const& votes =
             ripple::detail::supportedAmendments();
 
