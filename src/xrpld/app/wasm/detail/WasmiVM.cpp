f44fc39c7de30749d1e12a019f5c010dfebdd251
Peng Wang <pwang200@gmail.com>
2025-11-24 20:40:31 -0500

    address PR comments

--- src/xrpld/app/wasm/detail/WasmiVM.cpp
+++ src/xrpld/app/wasm/detail/WasmiVM.cpp
@@ -498,80 +498,15 @@ WasmiEngine::init()
         return std::unique_ptr<wasm_engine_t, decltype(&wasm_engine_delete)>{
             nullptr, &wasm_engine_delete};
     wasmi_config_consume_fuel_set(config, true);
-
-    /**
-     * \brief Whether or not to ignore Wasm custom sections.
-     *
-     * When enabled it will ignoe Wasm custom sections when creating a Wasm
-     * module.
-     *
-     * Default value: `false`
-     */
     wasmi_config_ignore_custom_sections_set(config, true);
-
-    /**
-     * \brief Whether or not to Wasm mutable-globals proposal is enabled.
-     *
-     * Default value: `true`
-     */
     wasmi_config_wasm_mutable_globals_set(config, false);
-
-    /**
-     * \brief Whether or not to Wasm multi-value proposal is enabled.
-     *
-     * Default value: `true`
-     */
     wasmi_config_wasm_multi_value_set(config, false);
-
-    /**
-     * \brief Whether or not to Wasm sign-extension proposal is enabled.
-     *
-     * Default value: `true`
-     */
     wasmi_config_wasm_sign_extension_set(config, false);
-
-    /**
-     * \brief Whether or not to Wasm non-trapping-float-to-int-conversions
-     * proposal is enabled.
-     *
-     * Default value: `true`
-     */
     wasmi_config_wasm_saturating_float_to_int_set(config, false);
-
-    /**
-     * \brief Whether or not to Wasm bulk-memory-ops proposal is enabled.
-     *
-     * Default value: `true`
-     */
     wasmi_config_wasm_bulk_memory_set(config, false);
-
-    /**
-     * \brief Whether or not to Wasm reference-types proposal is enabled.
-     *
-     * Default value: `true`
-     */
     wasmi_config_wasm_reference_types_set(config, false);
-
-    /**
-     * \brief Whether or not to Wasm tail-call proposal is enabled.
-     *
-     * Default value: `true`
-     */
     wasmi_config_wasm_tail_call_set(config, false);
-
-    /**
-     * \brief Whether or not to Wasm extended-const proposal is enabled.
-     *
-     * Default value: `true`
-     */
     wasmi_config_wasm_extended_const_set(config, false);
-
-    /**
-     * \brief Whether or not to floating Wasm point types and operations are
-     * enabled.
-     *
-     * Default value: `true`
-     */
     wasmi_config_floats_set(config, false);
 
     return std::unique_ptr<wasm_engine_t, decltype(&wasm_engine_delete)>(
