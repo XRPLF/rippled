7dea70d34d504bb2ff8f4d260ffa1dc6f34c4d1c
Mayukha Vadari <mvadari@gmail.com>
2025-12-12 11:07:52 -0800

    add finishFee - 1

--- src/test/app/EscrowSmart_test.cpp
+++ src/test/app/EscrowSmart_test.cpp
@@ -784,6 +784,11 @@ struct EscrowSmart_test : public beast::unit_test::suite
                     ter(telINSUF_FEE_P));
 
                 env(escrow::finish(alice, alice, seq),
+                    fee(finishFeeOverflow - 1),
+                    escrow::comp_allowance(bigAllowance),
+                    ter(telINSUF_FEE_P));
+
+                env(escrow::finish(alice, alice, seq),
                     fee(finishFee),
                     escrow::comp_allowance(bigAllowance),
                     ter(tesSUCCESS));
