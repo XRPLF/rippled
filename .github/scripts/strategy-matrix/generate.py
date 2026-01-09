97cbf8830a3f9cca0b39403489bc0129bb7e5fc2
Jingchen <a1q123456@users.noreply.github.com>
2026-01-09 15:50:30 +0000

    Update generate.py

--- .github/scripts/strategy-matrix/generate.py
+++ .github/scripts/strategy-matrix/generate.py
@@ -180,7 +180,8 @@ def generate_strategy_matrix(all: bool, config: Config) -> list:
             # Windows:
             # - Release and Unity on windows/amd64.
             if os["distro_name"] == "windows" and not (
-                "-Dunity=ON" in cmake_args
+                build_type == "Release"
+                and "-Dunity=ON" in cmake_args
                 and architecture["platform"] == "windows/amd64"
             ):
                 continue
@@ -293,7 +294,7 @@ if __name__ == "__main__":
             args.all, read_config(THIS_DIR / "macos.json")
         )
         matrix += generate_strategy_matrix(
-            args.all, read_config(THIS_DIR / "windows.json")
+            args.all or True, read_config(THIS_DIR / "windows.json")
         )
     else:
         matrix += generate_strategy_matrix(args.all, read_config(args.config))
