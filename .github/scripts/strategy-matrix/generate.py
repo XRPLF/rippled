cfa6a0a2e068fcf2619ad42b9ebb88caea13245d
Jingchen <a1q123456@users.noreply.github.com>
2026-01-09 15:45:25 +0000

    Update generate.py

--- .github/scripts/strategy-matrix/generate.py
+++ .github/scripts/strategy-matrix/generate.py
@@ -180,8 +180,7 @@ def generate_strategy_matrix(all: bool, config: Config) -> list:
             # Windows:
             # - Release and Unity on windows/amd64.
             if os["distro_name"] == "windows" and not (
-                build_type == "Release"
-                and "-Dunity=ON" in cmake_args
+                "-Dunity=ON" in cmake_args
                 and architecture["platform"] == "windows/amd64"
             ):
                 continue
