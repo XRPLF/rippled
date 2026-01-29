87718bdb3bb3e4bf91ed60f0a16511a43a5c6875
tequ <git@tequ.dev>
2026-01-29 10:58:06 +0900

    remove unused HashPrefix

--- include/xrpl/protocol/HashPrefix.h
+++ include/xrpl/protocol/HashPrefix.h
@@ -69,9 +69,6 @@ enum class HashPrefix : std::uint32_t {
 
     /** Batch */
     batch = detail::make_hash_prefix('B', 'C', 'H'),
-
-    /** Sponsor */
-    sponsor = detail::make_hash_prefix('S', 'P', 'N'),
 };
 
 template <class Hasher>
