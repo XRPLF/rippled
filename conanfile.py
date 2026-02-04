2319139eb55d7fa010b700a961017ee96ad503d3
Ayaz Salikhov <mathbunnyru@users.noreply.github.com>
2026-02-04 18:27:10 +0000

    chore: Update secp256k1 and openssl (#6327)

--- conanfile.py
+++ conanfile.py
@@ -31,7 +31,7 @@ class Xrpl(ConanFile):
         "grpc/1.72.0",
         "libarchive/3.8.1",
         "nudb/2.0.9",
-        "openssl/3.5.4",
+        "openssl/3.5.5",
         "secp256k1/0.7.0",
         "soci/4.0.3",
         "zlib/1.3.1",
