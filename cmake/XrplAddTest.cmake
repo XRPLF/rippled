08a7347d78c87a93390f6fe650345e817bc1da6d
Bart <11445373+bthomee@users.noreply.github.com>
2026-02-06 08:26:01 -0500

    Remove disabling unity for unit tests

--- cmake/XrplAddTest.cmake
+++ cmake/XrplAddTest.cmake
@@ -9,9 +9,5 @@ function (xrpl_add_test name)
 
     isolate_headers(${target} "${CMAKE_SOURCE_DIR}" "${CMAKE_SOURCE_DIR}/tests/${name}" PRIVATE)
 
-    # Disable unity build for the test target to avoid potential conflicts
-    # between test source files.
-    set_target_properties(${target} PROPERTIES UNITY_BUILD_MODE GROUP UNITY_BUILD_BATCH_SIZE 0)
-
     add_test(NAME ${target} COMMAND ${target})
 endfunction ()
