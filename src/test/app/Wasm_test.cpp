628eecadbfaf593be8eb9a351aa564be5fb2d2b0
Mayukha Vadari <mvadari@gmail.com>
2026-02-12 15:57:31 -0500

    fix ubsan

--- src/test/app/Wasm_test.cpp
+++ src/test/app/Wasm_test.cpp
@@ -326,9 +326,9 @@ struct Wasm_test : public beast::unit_test::suite
         }
 
         {  // fail because trying to access nonexistent field
-            struct BadTestHostFunctions : public TestHostFunctions
+            struct FieldNotFoundHostFunctions : public TestHostFunctions
             {
-                explicit BadTestHostFunctions(Env& env) : TestHostFunctions(env)
+                explicit FieldNotFoundHostFunctions(Env& env) : TestHostFunctions(env)
                 {
                 }
                 Expected<Bytes, HostFunctionError>
@@ -338,15 +338,15 @@ struct Wasm_test : public beast::unit_test::suite
                 }
             };
 
-            std::shared_ptr<HostFunctions> hfs(new BadTestHostFunctions(env));
+            std::shared_ptr<HostFunctions> hfs(new FieldNotFoundHostFunctions(env));
             auto re = runEscrowWasm(allHFWasm, hfs, ESCROW_FUNCTION_NAME, {}, 100'000);
             checkResult(re, -201, 28'965);
         }
 
         {  // fail because trying to allocate more than MAX_PAGES memory
-            struct BadTestHostFunctions : public TestHostFunctions
+            struct OversizedFieldHostFunctions : public TestHostFunctions
             {
-                explicit BadTestHostFunctions(Env& env) : TestHostFunctions(env)
+                explicit OversizedFieldHostFunctions(Env& env) : TestHostFunctions(env)
                 {
                 }
                 Expected<Bytes, HostFunctionError>
@@ -356,7 +356,7 @@ struct Wasm_test : public beast::unit_test::suite
                 }
             };
 
-            std::shared_ptr<HostFunctions> hfs(new BadTestHostFunctions(env));
+            std::shared_ptr<HostFunctions> hfs(new OversizedFieldHostFunctions(env));
             auto re = runEscrowWasm(allHFWasm, hfs, ESCROW_FUNCTION_NAME, {}, 100'000);
             checkResult(re, -201, 28'965);
         }
