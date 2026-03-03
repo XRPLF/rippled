546d80ac7d7499225bf963b8f1061d7c34a5e524
Peter Chen <ychen@ripple.com>
2026-03-03 09:25:01 -0500

    fix test

--- src/xrpld/rpc/handlers/GatewayBalances.cpp
+++ src/xrpld/rpc/handlers/GatewayBalances.cpp
@@ -131,6 +131,10 @@ doGatewayBalances(RPC::JsonContext& context)
             if (sle->getType() == ltESCROW)
             {
                 auto const& escrow = sle->getFieldAmount(sfAmount);
+                // Gateway Balance should not include MPTs
+                if (escrow.holds<MPTIssue>())
+                    return;
+
                 auto& bal = locked[escrow.getCurrency()];
                 if (bal == beast::zero)
                 {
@@ -139,11 +143,6 @@ doGatewayBalances(RPC::JsonContext& context)
                 }
                 else
                 {
-                    // Gateway Balance should not include MPTs
-                    auto const& escrow = sle->getFieldAmount(sfAmount);
-                    if (escrow.holds<MPTIssue>())
-                        return;
-
                     try
                     {
                         bal += escrow;
