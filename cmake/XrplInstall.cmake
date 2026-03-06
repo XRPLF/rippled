a0346944797da2a8d37542c4e7da10182f268b4f
Michael Legleux <mlegleux@ripple.com>
2026-03-06 12:24:57 -0800

    reformat

--- cmake/XrplInstall.cmake
+++ cmake/XrplInstall.cmake
@@ -2,61 +2,38 @@
    install stuff
 #]===================================================================]
 
-include(CMakePackageConfigHelpers)
 include(GNUInstallDirs)
 
-set(xrpl_cmakedir "${CMAKE_INSTALL_LIBDIR}/cmake/xrpl")
-
 if(is_root_project AND TARGET xrpld)
-  install(TARGETS xrpld
-    RUNTIME DESTINATION "${CMAKE_INSTALL_BINDIR}"
-    COMPONENT runtime
-  )
+    install(
+        TARGETS xrpld
+        RUNTIME DESTINATION "${CMAKE_INSTALL_BINDIR}" COMPONENT runtime
+    )
 
-  install(FILES "${CMAKE_CURRENT_SOURCE_DIR}/cfg/xrpld-example.cfg"
+    install(
+        FILES "${CMAKE_CURRENT_SOURCE_DIR}/cfg/xrpld-example.cfg"
         DESTINATION "${CMAKE_INSTALL_SYSCONFDIR}/xrpld"
         RENAME xrpld.cfg
         COMPONENT runtime
-  )
-  install(FILES "${CMAKE_CURRENT_SOURCE_DIR}/cfg/validators-example.txt"
+    )
+
+    install(
+        FILES "${CMAKE_CURRENT_SOURCE_DIR}/cfg/validators-example.txt"
         DESTINATION "${CMAKE_INSTALL_SYSCONFDIR}/xrpld"
         RENAME validators.txt
         COMPONENT runtime
-  )
+    )
 endif()
 
-install(TARGETS common
-                opts
-                xrpl_boost
-                xrpl_libs
-                xrpl_syslibs
-                xrpl.imports.main
-                xrpl.libpb
-                xrpl.libxrpl
-                xrpl.libxrpl.basics
-                xrpl.libxrpl.beast
-                xrpl.libxrpl.conditions
-                xrpl.libxrpl.core
-                xrpl.libxrpl.crypto
-                xrpl.libxrpl.git
-                xrpl.libxrpl.json
-                xrpl.libxrpl.rdb
-                xrpl.libxrpl.ledger
-                xrpl.libxrpl.net
-                xrpl.libxrpl.nodestore
-                xrpl.libxrpl.protocol
-                xrpl.libxrpl.resource
-                xrpl.libxrpl.server
-                xrpl.libxrpl.shamap
-                xrpl.libxrpl.tx
-  EXPORT xrpl_targets
-  LIBRARY DESTINATION "${CMAKE_INSTALL_LIBDIR}" COMPONENT development
-  ARCHIVE DESTINATION "${CMAKE_INSTALL_LIBDIR}" COMPONENT development
-  RUNTIME DESTINATION "${CMAKE_INSTALL_BINDIR}" COMPONENT development
-  INCLUDES DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
+install(
+    TARGETS xrpl.libpb xrpl.libxrpl
+    LIBRARY DESTINATION "${CMAKE_INSTALL_LIBDIR}" COMPONENT development
+    ARCHIVE DESTINATION "${CMAKE_INSTALL_LIBDIR}" COMPONENT development
+    RUNTIME DESTINATION "${CMAKE_INSTALL_BINDIR}" COMPONENT development
 )
 
-install(DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/include/xrpl"
-        DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
-        COMPONENT development
+install(
+    DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/include/xrpl"
+    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
+    COMPONENT development
 )
