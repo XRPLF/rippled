363c50bc4ca9dabb704fa76f02afb2cbf0084090
Pratik Mankawde <3397372+pratikmankawde@users.noreply.github.com>
2026-03-09 16:41:30 +0000

    added #pragma once
    Signed-off-by: Pratik Mankawde <3397372+pratikmankawde@users.noreply.github.com>

--- include/xrpl/basics/sanitizers.h
+++ include/xrpl/basics/sanitizers.h
@@ -1,3 +1,5 @@
+#pragma once
+
 // Helper to disable ASan/HwASan for specific functions
 #if defined(__GNUC__) || defined(__clang__)
 #define XRPL_NO_SANITIZE_ADDRESS __attribute__((no_sanitize("address", "hwaddress")))
