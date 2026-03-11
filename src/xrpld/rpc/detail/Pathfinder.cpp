69e28bd5ef4cb3f84044f0dc6afc9899f6c99fba
shortthefomo <jucallme@gmail.com>
2026-03-11 18:39:44 -0300

    remove duplicate and kept the shallower weigth as it was to far down to produce any usable results

--- src/xrpld/rpc/detail/Pathfinder.cpp
+++ src/xrpld/rpc/detail/Pathfinder.cpp
@@ -1254,7 +1254,6 @@ Pathfinder::initPathTable()
          {5, "sabxd"},  // source -> gateway -> book -> book(XRP) -> dest
          {6, "saaxd"},
          {7, "sbxd"},
-         {8, "sabxd"},
          {8, "sabbxd"},  // source -> gateway -> book -> book -> book(XRP) -> dest
          {9, "sabaxd"}});
 
