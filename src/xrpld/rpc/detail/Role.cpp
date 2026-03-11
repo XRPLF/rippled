c32d2b88c6f94f5cd217c5ccf3a4d5cac65664cc
Mayukha Vadari <mvadari@gmail.com>
2026-03-10 23:19:01 -0400

    fix read-past-end in Role

--- src/xrpld/rpc/detail/Role.cpp
+++ src/xrpld/rpc/detail/Role.cpp
@@ -252,12 +252,12 @@ forwardedFor(http_request_type const& request)
 
         // We found a "for=".  Scan for the end of the IP address.
         std::size_t const pos = [&found, &it]() {
-            std::size_t pos =
-                std::string_view(found, it->value().end() - found).find_first_of(",;");
+            auto const remaining = static_cast<std::size_t>(it->value().end() - found);
+            std::size_t pos = std::string_view(found, remaining).find_first_of(",;");
             if (pos != std::string_view::npos)
                 return pos;
 
-            return it->value().size() - forStr.size();
+            return remaining;
         }();
 
         return extractIpAddrFromField({found, pos});
