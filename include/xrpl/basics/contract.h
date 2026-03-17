4072ee521b9b6f00a61f5b7b80089e2fa1e02995
Pratik Mankawde <3397372+pratikmankawde@users.noreply.github.com>
2026-03-17 10:50:14 +0000

    added comments
    Signed-off-by: Pratik Mankawde <3397372+pratikmankawde@users.noreply.github.com>

--- include/xrpl/basics/contract.h
+++ include/xrpl/basics/contract.h
@@ -24,6 +24,10 @@ LogThrow(std::string const& title);
     When called from within a catch block, it will pass
     control to the next matching exception handler, if any.
     Otherwise, std::terminate will be called.
+
+    ASAN can't handle sudden jumps in control flow very well. This
+    function is marked as XRPL_NO_SANITIZE_ADDRESS to prevent it from
+    triggering false positives, since it throws.
 */
 [[noreturn]] XRPL_NO_SANITIZE_ADDRESS inline void
 Rethrow()
@@ -31,6 +35,14 @@ Rethrow()
     LogThrow("Re-throwing exception");
     throw;
 }
+
+/*
+    Logs and throws an exception of type E.
+
+    ASAN can't handle sudden jumps in control flow very well. This
+    function is marked as XRPL_NO_SANITIZE_ADDRESS to prevent it from
+    triggering false positives, since it throws.
+*/
 
 template <class E, class... Args>
 [[noreturn]] XRPL_NO_SANITIZE_ADDRESS inline void
