253918c168174823eee236d65c7dff2db915e857
Mayukha Vadari <mvadari@ripple.com>
2026-03-19 14:27:27 -0400

    explicitly initialize sfBalance in Payment.cpp

--- src/libxrpl/tx/transactors/payment/Payment.cpp
+++ src/libxrpl/tx/transactors/payment/Payment.cpp
@@ -383,6 +383,7 @@ Payment::doApply()
         sleDst = std::make_shared<SLE>(k);
         sleDst->setAccountID(sfAccount, dstAccountID);
         sleDst->setFieldU32(sfSequence, view().seq());
+        sleDst->setFieldAmount(sfBalance, XRPAmount(beast::zero);
 
         view().insert(sleDst);
     }
