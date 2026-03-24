380d4f9ab91b065d7cb0b9826f3f9e6b1c336200
Mayukha Vadari <mvadari@ripple.com>
2026-03-24 11:53:49 -0700

    fix: Assorted Vault fixes (#6607)

--- src/xrpld/app/tx/detail/VaultDelete.cpp
+++ src/xrpld/app/tx/detail/VaultDelete.cpp
@@ -73,8 +73,7 @@ VaultDelete::preclaim(PreclaimContext const& ctx)
     if (!sleMPT)
     {
         // LCOV_EXCL_START
-        JLOG(ctx.j.error())
-            << "VaultDeposit: missing issuance of vault shares.";
+        JLOG(ctx.j.error()) << "VaultDelete: missing issuance of vault shares.";
         return tecOBJECT_NOT_FOUND;
         // LCOV_EXCL_STOP
     }
@@ -82,7 +81,7 @@ VaultDelete::preclaim(PreclaimContext const& ctx)
     if (sleMPT->at(sfIssuer) != vault->getAccountID(sfAccount))
     {
         // LCOV_EXCL_START
-        JLOG(ctx.j.error()) << "VaultDeposit: invalid owner of vault shares.";
+        JLOG(ctx.j.error()) << "VaultDelete: invalid owner of vault shares.";
         return tecNO_PERMISSION;
         // LCOV_EXCL_STOP
     }
@@ -225,8 +224,6 @@ VaultDelete::doApply()
 
     // Destroy the vault.
     view().erase(vault);
-
-    associateAsset(*vault, asset);
 
     return tesSUCCESS;
 }
