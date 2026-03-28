c60cf1590ee22e6b7fa6bc4728bff7a826761ec8
Alex Kremer <akremer@ripple.com>
2026-03-28 01:08:29 +0000

    Test

--- include/xrpl/protocol_autogen/ledger_entries/AMM.h
+++ include/xrpl/protocol_autogen/ledger_entries/AMM.h
@@ -1,18 +1,20 @@
 // This file is auto-generated. Do not edit.
 #pragma once
 
+#include <xrpl/json/json_value.h>
 #include <xrpl/protocol/STLedgerEntry.h>
 #include <xrpl/protocol/STParsedJSON.h>
 #include <xrpl/protocol/jss.h>
 #include <xrpl/protocol_autogen/LedgerEntryBase.h>
 #include <xrpl/protocol_autogen/LedgerEntryBuilderBase.h>
-#include <xrpl/json/json_value.h>
 
-#include <stdexcept>
 #include <optional>
+#include <stdexcept>
 
 namespace xrpl::ledger_entries {
-
+namespace {
+static constexpr int test{1};
+}
 class AMMBuilder;
 
 /**
@@ -33,8 +35,7 @@ public:
      * @brief Construct a AMM ledger entry wrapper from an existing SLE object.
      * @throws std::runtime_error if the ledger entry type doesn't match.
      */
-    explicit AMM(std::shared_ptr<SLE const> sle)
-        : LedgerEntryBase(std::move(sle))
+    explicit AMM(std::shared_ptr<SLE const> sle) : LedgerEntryBase(std::move(sle))
     {
         // Verify ledger entry type
         if (sle_->getType() != entryType)
@@ -241,7 +242,12 @@ public:
      * @param asset2 The sfAsset2 field value.
      * @param ownerNode The sfOwnerNode field value.
      */
-    AMMBuilder(std::decay_t<typename SF_ACCOUNT::type::value_type> const& account,std::decay_t<typename SF_AMOUNT::type::value_type> const& lPTokenBalance,std::decay_t<typename SF_ISSUE::type::value_type> const& asset,std::decay_t<typename SF_ISSUE::type::value_type> const& asset2,std::decay_t<typename SF_UINT64::type::value_type> const& ownerNode)
+    AMMBuilder(
+        std::decay_t<typename SF_ACCOUNT::type::value_type> const& account,
+        std::decay_t<typename SF_AMOUNT::type::value_type> const& lPTokenBalance,
+        std::decay_t<typename SF_ISSUE::type::value_type> const& asset,
+        std::decay_t<typename SF_ISSUE::type::value_type> const& asset2,
+        std::decay_t<typename SF_UINT64::type::value_type> const& ownerNode)
         : LedgerEntryBuilderBase<AMMBuilder>(ltAMM)
     {
         setAccount(account);
