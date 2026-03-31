c0f53da4f5a7d01c988f4b9a37fbd34f263229ea
Vito Tumas <5780819+Tapanito@users.noreply.github.com>
2026-03-31 18:38:20 +0200

    Apply suggestions from code review
    Co-authored-by: Shawn Xie <35279399+shawnxie999@users.noreply.github.com>

--- src/libxrpl/tx/invariants/LoanBrokerInvariant.cpp
+++ src/libxrpl/tx/invariants/LoanBrokerInvariant.cpp
@@ -196,7 +196,7 @@ ValidLoanBroker::finalize(
             if (tx.getTxnType() != ttLOAN_BROKER_DELETE &&
                 after->at(sfCoverAvailable) > pseudoBalance)
             {
-                JLOG(j.fatal()) <<  //
+                JLOG(j.fatal()) <<  
                     "Invariant failed: Loan Broker cover available is greater "
                     "than pseudo-account asset balance";
                 return false;
