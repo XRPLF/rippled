f5995cc2aad65bee6e0cb86c0acd0f5fe7fe6a43
Zhiyuan Wang <1830604455@qq.com>
2026-03-31 15:10:43 -0400

    fix: Bidirection Object Storage for delegation

--- include/xrpl/tx/transactors/delegate/DelegateSet.h
+++ include/xrpl/tx/transactors/delegate/DelegateSet.h
@@ -24,11 +24,7 @@ public:
 
     // Interface used by AccountDelete
     static TER
-    deleteDelegate(
-        ApplyView& view,
-        std::shared_ptr<SLE> const& sle,
-        AccountID const& account,
-        beast::Journal j);
+    deleteDelegate(ApplyView& view, std::shared_ptr<SLE> const& sle, beast::Journal j);
 };
 
 }  // namespace xrpl
