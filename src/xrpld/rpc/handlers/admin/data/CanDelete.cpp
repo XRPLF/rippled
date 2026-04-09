586a983dd5100863bc92b8ac8fbe6d7d0681b6ea
shortthefomo <508629+shortthefomo@users.noreply.github.com>
2026-04-09 16:08:47 -0400

    fix: can_delete RPC returns rpcINVALID_PARAMS for non-uint, non-string types (prevents Json::LogicError)

--- src/xrpld/rpc/handlers/admin/data/CanDelete.cpp
+++ src/xrpld/rpc/handlers/admin/data/CanDelete.cpp
@@ -29,7 +29,7 @@ doCanDelete(RPC::JsonContext& context)
         {
             canDeleteSeq = canDelete.asUInt();
         }
-        else
+        else if (canDelete.isString())
         {
             std::string canDeleteStr = canDelete.asString();
             boost::to_lower(canDeleteStr);
@@ -46,28 +46,16 @@ doCanDelete(RPC::JsonContext& context)
             {
                 canDeleteSeq = std::numeric_limits<std::uint32_t>::max();
             }
-            else if (canDeleteStr == "now")
-            {
-                canDeleteSeq = context.app.getSHAMapStore().getLastRotated();
-                if (canDeleteSeq == 0u)
-                    return RPC::make_error(rpcNOT_READY);
-            }
-            else if (uint256 lh; lh.parseHex(canDeleteStr))
-            {
-                auto ledger = context.ledgerMaster.getLedgerByHash(lh);
-
-                if (!ledger)
-                    return RPC::make_error(rpcLGR_NOT_FOUND, "ledgerNotFound");
-
-                canDeleteSeq = ledger->header().seq;
-            }
             else
             {
                 return RPC::make_error(rpcINVALID_PARAMS);
             }
         }
-
-        ret[jss::can_delete] = context.app.getSHAMapStore().setCanDelete(canDeleteSeq);
+        else
+        {
+            // Not a uint or string: reject with INVALID_PARAMS
+            return RPC::make_error(rpcINVALID_PARAMS);
+        }
     }
     else
     {
