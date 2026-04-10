28328e392903468845b640029249dca4e4515be5
Shawn Xie <shawnxie920@gmail.com>
2026-04-10 14:04:44 -0400

    fix delegate test failure

--- src/test/app/Delegate_test.cpp
+++ src/test/app/Delegate_test.cpp
@@ -2250,7 +2250,7 @@ class Delegate_test : public beast::unit_test::suite
         // DO NOT modify expectedDelegableCount unless all scenarios, including
         // edge cases, have been fully tested and verified.
         // ====================================================================
-        std::size_t const expectedDelegableCount = 80;
+        std::size_t const expectedDelegableCount = 82;
 
         BEAST_EXPECTS(
             delegableCount == expectedDelegableCount,
