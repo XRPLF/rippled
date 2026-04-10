f5ed1f363ea94b68a0fb97341dcabe72e41f185d
Shawn Xie <shawnxie920@gmail.com>
2026-04-10 11:20:39 -0400

    add comment

--- include/xrpl/protocol/detail/secp256k1.h
+++ include/xrpl/protocol/detail/secp256k1.h
@@ -11,6 +11,9 @@ secp256k1Context()
     struct holder
     {
         secp256k1_context* impl;
+        // SECP256K1_CONTEXT_SIGN and SECP256K1_CONTEXT_VERIFY were
+        // deprecated. All contexts support both signing and verification, so
+        // SECP256K1_CONTEXT_NONE is the correct flag to use.
         holder() : impl(secp256k1_context_create(SECP256K1_CONTEXT_NONE))
         {
         }
