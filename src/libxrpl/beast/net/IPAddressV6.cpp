f04c12bc840b3793a6b2ac30c1dcafb7e5ed0786
JCW <a1q123456@users.noreply.github.com>
2026-04-16 14:01:26 +0100

    Fix formatting

--- src/libxrpl/beast/net/IPAddressV6.cpp
+++ src/libxrpl/beast/net/IPAddressV6.cpp
@@ -23,8 +23,7 @@ is_public(AddressV6 const& addr)
     if (addr.is_loopback())
         return false;
     if (addr.is_v4_mapped())
-        return is_public(
-            boost::asio::ip::make_address_v4(boost::asio::ip::v4_mapped, addr));
+        return is_public(boost::asio::ip::make_address_v4(boost::asio::ip::v4_mapped, addr));
     if (is_private(addr))
         return false;
     if (addr.is_multicast())
@@ -36,8 +35,8 @@ is_public(AddressV6 const& addr)
     if (b[0] == 0xfe && (b[1] & 0xc0) == 0x80)
         return false;
     // 100::/64 - Discard prefix (RFC 6666)
-    if (b[0] == 0x01 && b[1] == 0x00 && b[2] == 0 && b[3] == 0 && b[4] == 0 &&
-        b[5] == 0 && b[6] == 0 && b[7] == 0)
+    if (b[0] == 0x01 && b[1] == 0x00 && b[2] == 0 && b[3] == 0 && b[4] == 0 && b[5] == 0 &&
+        b[6] == 0 && b[7] == 0)
         return false;
     // 2001:db8::/32 - Documentation (RFC 3849)
     if (b[0] == 0x20 && b[1] == 0x01 && b[2] == 0x0d && b[3] == 0xb8)
