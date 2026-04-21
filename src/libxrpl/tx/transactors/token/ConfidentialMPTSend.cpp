456db6649c2d1764720fcf94b34b617fa12bab67
Shawn Xie <shawnxie920@gmail.com>
2026-04-21 13:52:07 -0400

    add tag

--- src/libxrpl/tx/transactors/token/ConfidentialMPTSend.cpp
+++ src/libxrpl/tx/transactors/token/ConfidentialMPTSend.cpp
@@ -118,8 +118,15 @@ ConfidentialMPTSend::preclaim(PreclaimContext const& ctx)
 
     // Check if destination account exists
     auto const destination = ctx.tx[sfDestination];
-    if (!ctx.view.exists(keylet::account(destination)))
+    auto const sleDst = ctx.view.read(keylet::account(destination));
+    if (!sleDst)
         return tecNO_TARGET;
+
+    // Check destination tag
+    if ((sleDst->getFlags() & lsfRequireDestTag) && !ctx.tx.isFieldPresent(sfDestinationTag))
+    {
+        return tecDST_TAG_NEEDED;
+    }
 
     // Check if MPT issuance exists
     auto const mptIssuanceID = ctx.tx[sfMPTokenIssuanceID];
