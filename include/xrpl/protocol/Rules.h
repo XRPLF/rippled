31d4d082480f4cc57ddc8371eaf0843df4325d7a
Ed Hennis <ed@ripple.com>
2026-04-22 08:50:59 -0400

    fix: Prevents overwritting a bool value in an invariant (#6609) (#6961)
    Co-authored-by: Vito <5780819+Tapanito@users.noreply.github.com>

--- include/xrpl/protocol/Rules.h
+++ include/xrpl/protocol/Rules.h
@@ -28,6 +28,22 @@
 
 namespace ripple {
 
+/** Check whether a feature is enabled in the current ledger rules
+ *
+ * @param feature The feature to be tested.
+ * @param resultIfNoRules What to return if called from outside a Transactor
+ * context.
+ */
+bool
+isFeatureEnabled(uint256 const& feature, bool resultIfNoRules);
+
+/** Check whether a feature is enabled in the current ledger rules
+ *
+ * @param feature The feature to be tested.
+ *
+ * Returns false if no global Rules object is available. i.e. Outside of
+ * a Transactor context
+ */
 bool
 isFeatureEnabled(uint256 const& feature);
 
