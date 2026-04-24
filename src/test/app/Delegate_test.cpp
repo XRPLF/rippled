9b4ab67864a74da499a4373030f8b54805217be3
Oleksandr <115580134+oleks-rip@users.noreply.github.com>
2026-04-24 12:15:42 -0400

    Fix delegable test

--- src/test/app/Delegate_test.cpp
+++ src/test/app/Delegate_test.cpp
@@ -1892,7 +1892,7 @@ class Delegate_test : public beast::unit_test::suite
         // DO NOT modify expectedDelegableCount unless all scenarios, including
         // edge cases, have been fully tested and verified.
         // ====================================================================
-        std::size_t const expectedDelegableCount = 75;
+        std::size_t const expectedDelegableCount = 77;
 
         BEAST_EXPECTS(
             delegableCount == expectedDelegableCount,
