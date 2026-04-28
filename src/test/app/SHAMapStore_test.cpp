778e2b3ce8ca08916a41bf471bbae192e0d4dd50
Ed Hennis <ed@ripple.com>
2026-04-28 15:44:14 -0500

    Fix clang-tidy: missing include

--- src/test/app/SHAMapStore_test.cpp
+++ src/test/app/SHAMapStore_test.cpp
@@ -12,6 +12,7 @@
 #include <xrpld/core/ConfigSections.h>
 
 #include <xrpl/basics/ByteUtilities.h>
+#include <xrpl/basics/Number.h>
 #include <xrpl/basics/base_uint.h>
 #include <xrpl/beast/unit_test/suite.h>
 #include <xrpl/json/json_value.h>
