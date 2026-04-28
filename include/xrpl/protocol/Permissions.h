1bbd668e685a6027866459a5bfd48b0819a505a5
yinyiqian1 <yqian@ripple.com>
2026-04-28 17:09:33 -0400

    add nodiscard to the getters

--- include/xrpl/protocol/Permissions.h
+++ include/xrpl/protocol/Permissions.h
@@ -58,35 +58,35 @@ public:
     Permission&
     operator=(Permission const&) = delete;
 
-    std::optional<std::string>
+    [[nodiscard]] std::optional<std::string>
     getPermissionName(std::uint32_t value) const;
 
     [[nodiscard]] std::optional<std::uint32_t>
     getGranularValue(std::string const& name) const;
 
-    std::optional<std::string>
+    [[nodiscard]] std::optional<std::string>
     getGranularName(GranularPermissionType value) const;
 
-    std::optional<TxType>
+    [[nodiscard]] std::optional<TxType>
     getGranularTxType(GranularPermissionType gpType) const;
 
     // Returns a reference to avoid copying uint256 - 32 bytes. std::optional
     // cannot hold references directly, so std::reference_wrapper is used.
-    std::optional<std::reference_wrapper<uint256 const>>
+    [[nodiscard]] std::optional<std::reference_wrapper<uint256 const>>
     getTxFeature(TxType txType) const;
 
-    bool
+    [[nodiscard]] bool
     isDelegable(std::uint32_t permissionValue, Rules const& rules) const;
 
-    bool
+    [[nodiscard]] bool
     hasGranularPermissions(TxType txType) const;
 
     // for tx level permission, permission value is equal to tx type plus one
-    static uint32_t
+    [[nodiscard]] static uint32_t
     txToPermissionType(TxType type);
 
     // tx type value is permission value minus one
-    static TxType
+    [[nodiscard]] static TxType
     permissionToTxType(std::uint32_t value);
 
     /**
