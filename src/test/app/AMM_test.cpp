e57bbcd618f8a195e35b2e2734448f65a523ab83
Pratik Mankawde <3397372+pratikmankawde@users.noreply.github.com>
2026-04-30 12:22:53 +0100

    added using-namespace back
    Signed-off-by: Pratik Mankawde <3397372+pratikmankawde@users.noreply.github.com>

--- src/test/app/AMM_test.cpp
+++ src/test/app/AMM_test.cpp
@@ -4340,7 +4340,7 @@ private:
     {
         testcase("Amendment");
         FeatureBitset const noAMM{testable_amendments() - featureAMM};
-
+        using namespace jtx;
         Env env{*this, noAMM};
         fund(env, gw, {alice}, {USD(1'000)}, Fund::All);
         AMM amm(env, alice, XRP(1'000), USD(1'000), ter(temDISABLED));
