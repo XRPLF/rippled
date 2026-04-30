10f5cfc8e49e594932bcc55996608c56285d619f
yinyiqian1 <yqian@ripple.com>
2026-04-30 11:12:08 -0400

    fix multisign and signfor

--- src/xrpld/rpc/detail/TransactionSign.cpp
+++ src/xrpld/rpc/detail/TransactionSign.cpp
@@ -1226,7 +1226,9 @@ transactionSignFor(
         signers.emplaceBack(std::move(signer));
 
         // The array must be sorted and validated.
-        auto err = sortAndValidateSigners(signers, (*sttx)[sfAccount]);
+        // For delegated transactions, the delegate account is
+        // the one forbidden from appearing in its own Signers array.
+        auto err = sortAndValidateSigners(signers, sttx->getFeePayer());
         if (RPC::containsError(err))
             return err;
     }
@@ -1393,7 +1395,9 @@ transactionSubmitMultiSigned(
     }
 
     // The array must be sorted and validated.
-    auto err = sortAndValidateSigners(signers, srcAddressID);
+    // For delegated transactions, getFeePayer() returns sfDelegate,
+    // that account is the one forbidden from appearing in its own Signers array.
+    auto err = sortAndValidateSigners(signers, stTx->getFeePayer());
     if (RPC::containsError(err))
         return err;
 
