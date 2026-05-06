46b241ace8b30d9c9775d60ffba7d24b21903896
Ed Hennis <ed@ripple.com>
2026-05-06 15:37:35 -0400

    Set version to 3.1.3

--- src/libxrpl/protocol/BuildInfo.cpp
+++ src/libxrpl/protocol/BuildInfo.cpp
@@ -36,7 +36,7 @@ namespace BuildInfo {
 //  and follow the format described at http://semver.org/
 //------------------------------------------------------------------------------
 // clang-format off
-char const* const versionString = "3.1.3-rc4"
+char const* const versionString = "3.1.3"
 // clang-format on
 
 #if defined(DEBUG) || defined(SANITIZER)
