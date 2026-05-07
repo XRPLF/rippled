dd2a2c8ccd41aab6cbb5f35790909fcc698dece3
Shawn Xie <shawnxie@live.ca>
2026-05-07 11:51:38 -0400

    add back comment

--- src/libxrpl/tx/transactors/token/MPTokenIssuanceSet.cpp
+++ src/libxrpl/tx/transactors/token/MPTokenIssuanceSet.cpp
@@ -247,6 +247,7 @@ MPTokenIssuanceSet::preclaim(PreclaimContext const& ctx)
                 }))
             return tecNO_PERMISSION;
 
+        // Clearing lsfMPTRequireAuth is invalid when the issuance already has
         // a DomainID set, because a DomainID requires RequireAuth to be active.
         if ((*mutableFlags & tmfMPTClearRequireAuth) != 0u &&
             sleMptIssuance->isFieldPresent(sfDomainID))
