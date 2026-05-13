9715573b2b22e61ed9af614a4dce059a690ae7b9
Bart <11445373+bthomee@users.noreply.github.com>
2026-05-13 17:57:16 -0400

    release: Bump version to 3.2.0-b5

--- src/libxrpl/protocol/BuildInfo.cpp
+++ src/libxrpl/protocol/BuildInfo.cpp
@@ -23,7 +23,7 @@ namespace {
 //------------------------------------------------------------------------------
 // clang-format off
 // NOLINTNEXTLINE(readability-identifier-naming)
-char const* const versionString = "3.2.0-b4"
+char const* const versionString = "3.2.0-b5"
     // clang-format on
     ;
 
