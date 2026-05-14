4e32f8dbbe50feab184735c91a73cf00001bfbb6
JCW <a1q123456@users.noreply.github.com>
2026-05-14 17:03:47 +0100

    Sync changes

--- src/test/beast/IPEndpoint_test.cpp
+++ src/test/beast/IPEndpoint_test.cpp
@@ -243,6 +243,7 @@ public:
         ep = Endpoint(AddressV4{d}, 80);
         BEAST_EXPECT(!isUnspecified(ep));
         BEAST_EXPECT(!isPublic(ep));
+        BEAST_EXPECT(isPrivate(ep));
         BEAST_EXPECT(!isMulticast(ep));
         BEAST_EXPECT(isLoopback(ep));
         BEAST_EXPECT(to_string(ep) == "127.0.0.1:80");
@@ -251,6 +252,7 @@ public:
             boost::asio::ip::make_address_v6(boost::asio::ip::v4_mapped, AddressV4{d}), 80);
         BEAST_EXPECT(!isUnspecified(ep));
         BEAST_EXPECT(!isPublic(ep));
+        BEAST_EXPECT(isPrivate(ep));
         BEAST_EXPECT(!isMulticast(ep));
         BEAST_EXPECT(!isLoopback(ep));  // mapped loopback is not a loopback
         BEAST_EXPECTS(to_string(ep) == "[::ffff:127.0.0.1]:80", to_string(ep));
