6ee1ebd0d6b06cc5637b2c8d3aefa0c0c7b52dae
Vito <5780819+Tapanito@users.noreply.github.com>
2026-05-14 15:42:29 +0200

    chore: please clang-tidy

--- src/libxrpl/tx/transactors/vault/VaultDeposit.cpp
+++ src/libxrpl/tx/transactors/vault/VaultDeposit.cpp
@@ -1,17 +1,20 @@
 #include <xrpl/tx/transactors/vault/VaultDeposit.h>
 
 #include <xrpl/basics/Log.h>
+#include <xrpl/basics/Number.h>
 #include <xrpl/beast/utility/Zero.h>
 #include <xrpl/beast/utility/instrumentation.h>
 #include <xrpl/ledger/helpers/CredentialHelpers.h>
 #include <xrpl/ledger/helpers/MPTokenHelpers.h>
 #include <xrpl/ledger/helpers/TokenHelpers.h>
 #include <xrpl/ledger/helpers/VaultHelpers.h>
+#include <xrpl/protocol/Feature.h>
 #include <xrpl/protocol/Indexes.h>
 #include <xrpl/protocol/Issue.h>
 #include <xrpl/protocol/LedgerFormats.h>
 #include <xrpl/protocol/MPTIssue.h>
 #include <xrpl/protocol/SField.h>
+#include <xrpl/protocol/STAmount.h>
 #include <xrpl/protocol/STLedgerEntry.h>
 #include <xrpl/protocol/STNumber.h>  // IWYU pragma: keep
 #include <xrpl/protocol/STTakesAsset.h>
