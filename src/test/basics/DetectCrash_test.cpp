bb286b799bf3714ea9deef7975332eca5be7c2a2
Marek Foss <marek.foss@neti-soft.com>
2026-05-15 19:20:42 +0200

    fix: Rollback DetectCrash migration

---
+++ src/test/basics/DetectCrash_test.cpp
@@ -0,0 +1,26 @@
+#include <xrpl/beast/unit_test/suite.h>
+
+#include <exception>
+
+namespace xrpl::test {
+
+struct DetectCrash_test : public beast::unit_test::Suite
+{
+    void
+    testDetectCrash()
+    {
+        testcase("Detect Crash");
+        // Kill the process. This is used to test that the multi-process
+        // unit test will correctly report the crash.
+        std::terminate();
+    }
+    void
+    run() override
+    {
+        testDetectCrash();
+    }
+};
+
+BEAST_DEFINE_TESTSUITE_MANUAL(DetectCrash, basics, beast);
+
+}  // namespace xrpl::test
