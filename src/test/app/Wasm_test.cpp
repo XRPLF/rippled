c531dc0c815c5408bb0305812aab43e509310725
Mayukha Vadari <mvadari@gmail.com>
2026-05-20 11:20:52 -0400

    last fixes

--- src/test/app/Wasm_test.cpp
+++ src/test/app/Wasm_test.cpp
@@ -279,7 +279,7 @@ struct Wasm_test : public beast::unit_test::Suite
 
         Env env{*this};
         TestLedgerDataProvider hfs(env);
-        ImportVec const imports;
+        ImportVec imports;
         WASM_IMPORT_FUNC2(imports, getLedgerSqn, "get_ledger_sqn", &hfs, 33);
         auto& engine = WasmEngine::instance();
 
@@ -569,7 +569,7 @@ struct Wasm_test : public beast::unit_test::Suite
             // expected import not provided
             auto const lgrSqnWasm = hexToBytes(kLedgerSqnWasmHex);
             TestLedgerDataProvider hfs(env);
-            ImportVec const imports;
+            ImportVec imports;
             WASM_IMPORT_FUNC2(imports, getLedgerSqn, "get_ledger_sqn2", &hfs);
 
             auto& engine = WasmEngine::instance();
@@ -600,7 +600,7 @@ struct Wasm_test : public beast::unit_test::Suite
             // bad function name
             auto const lgrSqnWasm = hexToBytes(kLedgerSqnWasmHex);
             TestLedgerDataProvider hfs(env);
-            ImportVec const imports;
+            ImportVec imports;
             WASM_IMPORT_FUNC2(imports, getLedgerSqn, "get_ledger_sqn", &hfs);
 
             auto& engine = WasmEngine::instance();
