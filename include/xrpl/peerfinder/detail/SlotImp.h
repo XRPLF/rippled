3a3091bc75d4a8540a3cf1e6231a17777e98608f
Marek Foss <marek.foss@neti-soft.com>
2026-05-21 12:38:35 +0200

    fix: Align peerfinder field names with clang tidy

--- include/xrpl/peerfinder/detail/SlotImp.h
+++ include/xrpl/peerfinder/detail/SlotImp.h
@@ -56,19 +56,19 @@ public:
     beast::IP::Endpoint const&
     remoteEndpoint() const override
     {
-        return remote_endpoint_;
+        return remoteEndpoint_;
     }
 
     std::optional<beast::IP::Endpoint> const&
     localEndpoint() const override
     {
-        return local_endpoint_;
+        return localEndpoint_;
     }
 
     std::optional<PublicKey> const&
     publicKey() const override
     {
-        return public_key_;
+        return publicKey_;
     }
 
     std::string
@@ -80,7 +80,7 @@ public:
     std::optional<std::uint16_t>
     listeningPort() const override
     {
-        std::uint32_t const value = listening_port_;
+        std::uint32_t const value = listeningPort_;
         if (value == kUnknownPort)
             return std::nullopt;
         return value;
@@ -89,25 +89,25 @@ public:
     void
     setListeningPort(std::uint16_t port)
     {
-        listening_port_ = port;
+        listeningPort_ = port;
     }
 
     void
     localEndpoint(beast::IP::Endpoint const& endpoint)
     {
-        local_endpoint_ = endpoint;
+        localEndpoint_ = endpoint;
     }
 
     void
     remoteEndpoint(beast::IP::Endpoint const& endpoint)
     {
-        remote_endpoint_ = endpoint;
+        remoteEndpoint_ = endpoint;
     }
 
     void
     publicKey(PublicKey const& key)
     {
-        public_key_ = key;
+        publicKey_ = key;
     }
 
     void
@@ -164,12 +164,12 @@ private:
     bool const fixed_;
     bool reserved_;
     State state_;
-    beast::IP::Endpoint remote_endpoint_;
-    std::optional<beast::IP::Endpoint> local_endpoint_;
-    std::optional<PublicKey> public_key_;
+    beast::IP::Endpoint remoteEndpoint_;
+    std::optional<beast::IP::Endpoint> localEndpoint_;
+    std::optional<PublicKey> publicKey_;
 
     static std::int32_t constexpr kUnknownPort = -1;
-    std::atomic<std::int32_t> listening_port_;
+    std::atomic<std::int32_t> listeningPort_;
 
 public:
     // DEPRECATED public data members
