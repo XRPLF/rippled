5bdd606cf188dc5c962f2c48d6a9dab1b4bde29d
Gregory Tsipenyuk <gtsipenyuk@ripple.com>
2026-05-22 07:09:25 -0400

    Fix clang-tidy caused errors

--- src/libxrpl/ledger/helpers/MPTokenHelpers.cpp
+++ src/libxrpl/ledger/helpers/MPTokenHelpers.cpp
@@ -22,13 +22,13 @@
 #include <xrpl/protocol/Rate.h>
 #include <xrpl/protocol/SField.h>
 #include <xrpl/protocol/STAmount.h>
+#include <xrpl/protocol/STArray.h>
+#include <xrpl/protocol/STBase.h>
 #include <xrpl/protocol/STLedgerEntry.h>
 #include <xrpl/protocol/TER.h>
 #include <xrpl/protocol/TxFlags.h>
 #include <xrpl/protocol/UintTypes.h>
 #include <xrpl/protocol/XRPAmount.h>
-#include "xrpl/protocol/STArray.h"
-#include "xrpl/protocol/STBase.h"
 
 #include <algorithm>
 #include <cstdint>
