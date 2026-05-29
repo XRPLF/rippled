1740fb22aeff556c9f3c7d8e4cd94e2a5bef0b76
yinyiqian1 <yqian@ripple.com>
2026-05-29 14:29:40 -0400

    fix clang-tidy

--- src/libxrpl/ledger/helpers/PermissionedDEXHelpers.cpp
+++ src/libxrpl/ledger/helpers/PermissionedDEXHelpers.cpp
@@ -3,6 +3,7 @@
 #include <xrpl/basics/Log.h>
 #include <xrpl/basics/base_uint.h>
 #include <xrpl/beast/utility/Journal.h>
+#include <xrpl/beast/utility/Zero.h>
 #include <xrpl/ledger/ReadView.h>
 #include <xrpl/ledger/helpers/CredentialHelpers.h>
 #include <xrpl/protocol/AccountID.h>
