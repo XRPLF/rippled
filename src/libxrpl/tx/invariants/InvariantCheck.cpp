88b81ee66ebfef2a15fbe836f2cdafb9b4057ef5
Ed Hennis <ed@ripple.com>
2026-05-28 23:32:28 -0400

    fix: Improve Invariant documentation to emphasize that "after" is never null
    - Expand the description in InvariantChecker_PROTOTYPE::visitEntry.
    - Add an explicit assertion in "XRPLNotCreated::visitEntry".

--- src/libxrpl/tx/invariants/InvariantCheck.cpp
+++ src/libxrpl/tx/invariants/InvariantCheck.cpp
@@ -141,24 +141,28 @@ XRPNotCreated::visitEntry(
         }
     }
 
-    if (after)
+    if (!after)
     {
-        switch (after->getType())
-        {
-            case ltACCOUNT_ROOT:
-                drops_ += (*after)[sfBalance].xrp().drops();
-                break;
-            case ltPAYCHAN:
-                if (!isDelete)
-                    drops_ += ((*after)[sfAmount] - (*after)[sfBalance]).xrp().drops();
-                break;
-            case ltESCROW:
-                if (!isDelete && isXRP((*after)[sfAmount]))
-                    drops_ += (*after)[sfAmount].xrp().drops();
-                break;
-            default:
-                break;
-        }
+        // LCOV_EXCL_START
+        UNREACHABLE("xrpl::XRPNotCreated::visitEntry : after can't be null");
+        return;
+        // LCOV_EXCL_STOP
+    }
+    switch (after->getType())
+    {
+        case ltACCOUNT_ROOT:
+            drops_ += (*after)[sfBalance].xrp().drops();
+            break;
+        case ltPAYCHAN:
+            if (!isDelete)
+                drops_ += ((*after)[sfAmount] - (*after)[sfBalance]).xrp().drops();
+            break;
+        case ltESCROW:
+            if (!isDelete && isXRP((*after)[sfAmount]))
+                drops_ += (*after)[sfAmount].xrp().drops();
+            break;
+        default:
+            break;
     }
 }
 
