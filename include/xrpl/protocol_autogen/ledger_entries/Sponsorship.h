942a950e9392f1de1a31cd4862578be95f141d16
Oleksandr <115580134+oleks-rip@users.noreply.github.com>
2026-06-03 19:48:09 -0400

    Codegen update

--- include/xrpl/protocol_autogen/ledger_entries/Sponsorship.h
+++ include/xrpl/protocol_autogen/ledger_entries/Sponsorship.h
@@ -33,7 +33,7 @@ public:
      * @brief Construct a Sponsorship ledger entry wrapper from an existing SLE object.
      * @throws std::runtime_error if the ledger entry type doesn't match.
      */
-    explicit Sponsorship(std::shared_ptr<SLE const> sle)
+    explicit Sponsorship(SLE::const_pointer sle)
         : LedgerEntryBase(std::move(sle))
     {
         // Verify ledger entry type
@@ -219,7 +219,7 @@ public:
      * @param sle The existing ledger entry to copy from.
      * @throws std::runtime_error if the ledger entry type doesn't match.
      */
-    SponsorshipBuilder(std::shared_ptr<SLE const> sle)
+    SponsorshipBuilder(SLE::const_pointer sle)
     {
         if (sle->at(sfLedgerEntryType) != ltSPONSORSHIP)
         {
