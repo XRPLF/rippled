4438a02fbe2dfb2c37582a77bd95f97360b78fc5
yinyiqian1 <yqian@ripple.com>
2026-06-24 18:04:26 -0400

    Add back API as temporary solution

--- include/xrpl/ledger/helpers/AccountRootHelpers.h
+++ include/xrpl/ledger/helpers/AccountRootHelpers.h
@@ -91,6 +91,16 @@ adjustOwnerCount(
 inline void
 adjustOwnerCount(
     ApplyView& view,
+    SLE::ref accountSle,
+    std::int32_t amount,
+    beast::Journal j = beast::Journal{beast::Journal::getNullSink()})
+{
+    adjustOwnerCount(view, accountSle, SLE::pointer(), amount, j);
+}
+
+inline void
+adjustOwnerCount(
+    ApplyView& view,
     AccountID const& account,
     std::optional<AccountID> const& sponsor,
     std::int32_t amount,
--- include/xrpl/protocol/Fees.h
+++ include/xrpl/protocol/Fees.h
@@ -33,6 +33,17 @@ struct Fees
         : base(base), reserve(reserve), increment(increment)
     {
     }
+
+    /** Returns the account reserve given the owner count, in drops.
+
+        The reserve is calculated as the reserve base plus
+        the reserve increment times the number of increments.
+    */
+    [[nodiscard]] XRPAmount
+    accountReserve(std::size_t ownerCount) const
+    {
+        return reserve + ownerCount * increment;
+    }
 };
 
 }  // namespace xrpl
