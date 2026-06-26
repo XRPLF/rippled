b9ee38cd7dd5179ac26eca6a01d103dc9333d3ca
yinyiqian1 <yqian@ripple.com>
2026-06-26 00:19:46 -0400

    update mpt-crypto version

--- conanfile.py
+++ conanfile.py
@@ -30,7 +30,7 @@ class Xrpl(ConanFile):
         "ed25519/2015.03",
         "grpc/1.81.0",
         "libarchive/3.8.7",
-        "mpt-crypto/0.3.0-rc5",
+        "mpt-crypto/0.4.0-rc2",
         "nudb/2.0.9",
         "secp256k1/0.7.1",
         "soci/4.0.3",
