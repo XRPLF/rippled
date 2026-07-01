da1fe00da9a0df610d06078349fcf99c2b3133e6
Mayukha Vadari <mvadari@gmail.com>
2026-07-01 17:27:09 -0400

    more cleanup

--- src/libxrpl/tx/transactors/payment/DepositPreauth.cpp
+++ src/libxrpl/tx/transactors/payment/DepositPreauth.cpp
@@ -166,7 +166,7 @@ DepositPreauth::doApply()
         if (!sponsorSle)
             return sponsorSle.error();  // LCOV_EXCL_LINE
         if (auto const ret = checkInsufficientReserve(
-                ctx_.getApplyViewContext(), sleOwner, preFeeBalance_, *sponsorSle, 1, 0, j_);
+                applyViewContext, sleOwner, preFeeBalance_, *sponsorSle, 1, 0, j_);
             !isTesSuccess(ret))
             return ret;
 
@@ -214,7 +214,7 @@ DepositPreauth::doApply()
         if (!sponsorSle)
             return sponsorSle.error();  // LCOV_EXCL_LINE
         if (auto const ret = checkInsufficientReserve(
-                ctx_.getApplyViewContext(), sleOwner, preFeeBalance_, *sponsorSle, 1, 0, j_);
+                applyViewContext, sleOwner, preFeeBalance_, *sponsorSle, 1, 0, j_);
             !isTesSuccess(ret))
             return ret;
 
