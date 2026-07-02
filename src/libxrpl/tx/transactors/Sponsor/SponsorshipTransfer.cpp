010de1265848a0c00534924d984c074df47a01dc
yinyiqian1 <yqian@ripple.com>
2026-07-02 15:27:03 -0400

    address comment

--- src/libxrpl/tx/transactors/Sponsor/SponsorshipTransfer.cpp
+++ src/libxrpl/tx/transactors/Sponsor/SponsorshipTransfer.cpp
@@ -43,7 +43,8 @@ SponsorshipTransfer::getFlagsMask(PreflightContext const& ctx)
 NotTEC
 SponsorshipTransfer::preflight(PreflightContext const& ctx)
 {
-    auto const transferFlags = tfSponsorshipCreate | tfSponsorshipReassign | tfSponsorshipEnd;
+    static constexpr auto transferFlags =
+        tfSponsorshipCreate | tfSponsorshipReassign | tfSponsorshipEnd;
     if (std::popcount(ctx.tx.getFlags() & transferFlags) != 1)
     {
         JLOG(ctx.j.debug()) << "preflight: Only one SponsorshipTransfer flag can be set per tx.";
