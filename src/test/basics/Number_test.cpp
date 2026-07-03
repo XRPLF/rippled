82ca28f32935c3499765b3f59539073cb13e5025
Ed Hennis <ed@ripple.com>
2026-07-02 21:07:59 -0400

    Fix some UB in test

--- src/test/basics/Number_test.cpp
+++ src/test/basics/Number_test.cpp
@@ -2057,7 +2057,7 @@ public:
             // number to avoid overflow and UB
             Number const n{
                 true,
-                -static_cast<std::uint64_t>(std::numeric_limits<std::int64_t>::min()) + 1,
+                static_cast<std::uint64_t>(std::numeric_limits<std::int64_t>::max()) + 1,
                 0,
                 Number::Normalized{}};
 
