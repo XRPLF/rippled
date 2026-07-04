b305cf9146d61ee5a4d144ada1433737d1e4804c
Ayaz Salikhov <asalikhov@ripple.com>
2026-07-04 01:24:09 +0100

    Delete comparators.h completely

--- src/test/csf/ledgers.h
+++ src/test/csf/ledgers.h
@@ -3,7 +3,6 @@
 #include <test/csf/Tx.h>
 
 #include <xrpl/basics/chrono.h>
-#include <xrpl/basics/comparators.h>
 #include <xrpl/basics/tagged_integer.h>
 #include <xrpl/json/json_value.h>
 #include <xrpl/ledger/LedgerTiming.h>
@@ -13,6 +12,7 @@
 #include <cassert>
 #include <cstddef>
 #include <cstdint>
+#include <functional>
 #include <optional>
 #include <set>
 #include <string>
@@ -232,8 +232,8 @@ private:
 class LedgerOracle
 {
     using InstanceMap = boost::bimaps::bimap<
-        boost::bimaps::set_of<Ledger::Instance, xrpl::less<>>,
-        boost::bimaps::set_of<Ledger::ID, xrpl::less<>>>;
+        boost::bimaps::set_of<Ledger::Instance, std::less<>>,
+        boost::bimaps::set_of<Ledger::ID, std::less<>>>;
     using InstanceEntry = InstanceMap::value_type;
 
     // Set of all known ledgers; note this is never pruned
