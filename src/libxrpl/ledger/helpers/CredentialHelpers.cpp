45b12651f3561af21110f727e76b6c930344ff42
Mayukha Vadari <mvadari@gmail.com>
2026-07-07 10:41:53 -0400

    fix clang-tidy issues

--- src/libxrpl/ledger/helpers/CredentialHelpers.cpp
+++ src/libxrpl/ledger/helpers/CredentialHelpers.cpp
@@ -8,7 +8,6 @@
 #include <xrpl/ledger/ApplyView.h>
 #include <xrpl/ledger/ReadView.h>
 #include <xrpl/ledger/helpers/AccountRootHelpers.h>
-#include <xrpl/ledger/helpers/SponsorHelpers.h>
 #include <xrpl/protocol/AccountID.h>
 #include <xrpl/protocol/Feature.h>
 #include <xrpl/protocol/Indexes.h>
