7f402118c6f5d3689dde4e481e4a70404139c3a1
Zhiyuan Wang <1830604455@qq.com>
2026-07-08 17:23:38 -0400

    test

--- src/libxrpl/tx/transactors/payment/Payment.cpp
+++ src/libxrpl/tx/transactors/payment/Payment.cpp
@@ -512,7 +512,7 @@ Payment::doApply()
                 // LCOV_EXCL_START
                 JLOG(j_.fatal()) << "Sponsoring account count overflow for account "
                                  << to_string(accountID_);
-                return tecINTERNAL;  // LCOV_EXCL_LINE
+                return tecINTERNAL;
                 // LCOV_EXCL_STOP
             }
             sponsor->setFieldU32(sfSponsoringAccountCount, currentSponsoringAccountCount + 1);
