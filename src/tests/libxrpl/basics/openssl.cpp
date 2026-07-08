d560622c03b1bbf877aededb6618bd60a05bff7b
Valentin Balaschenko <13349202+vlntb@users.noreply.github.com>
2026-07-08 14:56:02 +0100

    namespace fix

--- src/tests/libxrpl/basics/openssl.cpp
+++ src/tests/libxrpl/basics/openssl.cpp
@@ -1,34 +1,14 @@
-//------------------------------------------------------------------------------
-/*
-    This file is part of rippled: https://github.com/ripple/rippled
-    Copyright (c) 2025 Ripple Labs Inc.
-
-    Permission to use, copy, modify, and/or distribute this software for any
-    purpose  with  or without fee is hereby granted, provided that the above
-    copyright notice and this permission notice appear in all copies.
-
-    THE  SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-    WITH  REGARD  TO  THIS  SOFTWARE  INCLUDING  ALL  IMPLIED  WARRANTIES  OF
-    MERCHANTABILITY  AND  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-    ANY  SPECIAL ,  DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-    WHATSOEVER  RESULTING  FROM  LOSS  OF USE, DATA OR PROFITS, WHETHER IN AN
-    ACTION  OF  CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-*/
-//==============================================================================
-
 #include <xrpl/basics/Slice.h>
 #include <xrpl/protocol/digest.h>
 
-#include <doctest/doctest.h>
+#include <gtest/gtest.h>
 
 #include <cmath>
-#include <cstdint>
-#include <iostream>
+#include <cstddef>
 #include <random>
 #include <vector>
 
-using namespace ripple;
+namespace xrpl::test {
 
 static std::vector<char> const data = []() {
     std::vector<char> strV(pow(10, 5));
@@ -42,21 +22,19 @@ static std::vector<char> const data = []() {
     return strV;
 }();
 
-TEST_SUITE_BEGIN("OpenSSL");
-
-TEST_CASE("SingleHashFullSlice")
+TEST(OpenSSL, SingleHashFullSlice)
 {
     Slice const s{data.data(), data.size()};
-    auto hash = sha512Half(s);
+    [[maybe_unused]] auto hash = sha512Half(s);
 }
 
-TEST_CASE("MultihashAllSlices")
+TEST(OpenSSL, MultihashAllSlices)
 {
     for (std::size_t i = 0; i < data.size(); ++i)
     {
         Slice s(&data[i], data.size() - i);
-        auto hash = sha512Half(s);
+        [[maybe_unused]] auto hash = sha512Half(s);
     }
 }
 
-TEST_SUITE_END();
+}  // namespace xrpl::test
