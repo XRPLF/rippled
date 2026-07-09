4de336568aeae3d79432ef6c3c1715d4de29d2c9
Dejan Cabrilo <dcabrilo@gmail.com>
2026-07-09 21:53:38 +0200

    Fix style
    doc-strings should start with a single space or newline

--- formal_verification/XRPL/Properties/Protocol/Number/Compare/Compare.lean
+++ formal_verification/XRPL/Properties/Protocol/Number/Compare/Compare.lean
@@ -3,9 +3,9 @@ import XRPL.Properties.Protocol.Number.Compare.Common.Proofs
 
 namespace XRPL.Model.Protocol
 
-/--  `x.operator_lt y` is `true` if and only if the rational value of `x` is below the rational value of `y` 
+/-- `x.operator_lt y` is `true` if and only if the rational value of `x` is below the rational value of `y`.
 
-We hide the actual proof in a helper file (Common/Proofs.lean), 
+We hide the actual proof in a helper file (Common/Proofs.lean),
 so readers of this file can focus on theorems and not tactics and proofs.
 -/
 theorem operator_lt_iff (x y : Number)
