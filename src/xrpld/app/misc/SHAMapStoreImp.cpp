6b23ee7cb9abbfab72aa0a01331e3b2df6bac78d
Sophia Xie <sxie@ripple.com>
2026-07-14 11:53:58 -0700

    Apply pre-commit hook fixes

--- src/xrpld/app/misc/SHAMapStoreImp.cpp
+++ src/xrpld/app/misc/SHAMapStoreImp.cpp
@@ -15,8 +15,8 @@
 #include <xrpl/config/Constants.h>
 #include <xrpl/ledger/Ledger.h>
 #include <xrpl/nodestore/Database.h>
-#include <xrpl/nodestore/NodeObject.h>
 #include <xrpl/nodestore/Manager.h>
+#include <xrpl/nodestore/NodeObject.h>
 #include <xrpl/nodestore/Scheduler.h>
 #include <xrpl/nodestore/detail/DatabaseRotatingImp.h>
 #include <xrpl/protocol/Protocol.h>
