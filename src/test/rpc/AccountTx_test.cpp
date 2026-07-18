3caeb81bdcc688b52c29dde404568bc3d2f1ea0a
shortthefomo <508629+shortthefomo@users.noreply.github.com>
2026-07-17 23:43:51 -0400

    relove dropped include

--- src/test/rpc/AccountTx_test.cpp
+++ src/test/rpc/AccountTx_test.cpp
@@ -27,6 +27,7 @@
 #include <xrpl/basics/chrono.h>
 #include <xrpl/basics/strHex.h>
 #include <xrpl/beast/unit_test/suite.h>
+#include <xrpl/config/Constants.h>
 #include <xrpl/json/json_value.h>
 #include <xrpl/json/to_string.h>
 #include <xrpl/protocol/AccountID.h>
