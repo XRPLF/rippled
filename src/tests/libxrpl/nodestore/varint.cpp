e13feec4b415ddccfd04aad71896b0ed2326825d
Alex Kremer <akremer@ripple.com>
2026-07-24 15:30:06 +0100

    Low hanging fruit for gtest

---
+++ src/tests/libxrpl/nodestore/varint.cpp
@@ -0,0 +1,45 @@
+#include <xrpl/nodestore/detail/varint.h>
+
+#include <gtest/gtest.h>
+
+#include <array>
+#include <cstddef>
+#include <cstdint>
+
+using namespace xrpl::NodeStore;
+
+TEST(varint, encode_decode)
+{
+    static constexpr auto kValues = std::to_array<std::size_t>({
+        0,
+        1,
+        2,
+        126,
+        127,
+        128,
+        253,
+        254,
+        255,
+        16127,
+        16128,
+        16129,
+        0xff,
+        0xffff,
+        0xffffffff,
+        0xffffffffffffUL,
+        0xffffffffffffffffUL,
+    });
+
+    for (auto const value : kValues)
+    {
+        std::array<std::uint8_t, varint_traits<std::size_t>::kMax> buffer{};
+        auto const bytesWritten = writeVarint(buffer.data(), value);
+        EXPECT_GT(bytesWritten, 0u);
+        EXPECT_EQ(bytesWritten, sizeVarint(value));
+
+        std::size_t decoded = 0;
+        auto const bytesRead = readVarint(buffer.data(), bytesWritten, decoded);
+        EXPECT_EQ(bytesRead, bytesWritten);
+        EXPECT_EQ(value, decoded);
+    }
+}
