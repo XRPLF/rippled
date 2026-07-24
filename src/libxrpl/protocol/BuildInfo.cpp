dbfd906fc285d6c602527a1ffcdcc8259ac17061
Bart <bthomee@users.noreply.github.com>
2026-07-24 18:40:02 -0400

    chore: Bump version to 3.3.0-rc4 (#328)

--- src/libxrpl/protocol/BuildInfo.cpp
+++ src/libxrpl/protocol/BuildInfo.cpp
@@ -23,7 +23,7 @@ namespace {
 //------------------------------------------------------------------------------
 // clang-format off
 // NOLINTNEXTLINE(readability-identifier-naming)
-char const* const versionString = "3.3.0-rc3"
+char const* const versionString = "3.3.0-rc4"
     // clang-format on
     ;
 
