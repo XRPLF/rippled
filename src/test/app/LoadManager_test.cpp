4568024daabbbbf6f407c8641e89bfed881a9c33
rippleitinnz <chris@chrismckay.nz>
2026-08-17 20:48:39 +0000

    fix: clang-tidy and pre-commit hook issues
    - Add missing #include <cstdint> to LoadManager_test.cpp
    - Add braces around if/else bodies in NetworkOPs.cpp reportFeeChange()
    - Fix copyright URL in LoadManager_test.cpp
    - Remove name from inline comment in NetworkOPs.cpp

--- src/test/app/LoadManager_test.cpp
+++ src/test/app/LoadManager_test.cpp
@@ -1,6 +1,6 @@
 //------------------------------------------------------------------------------
 /*
-    This file is part of rippled: https://github.com/ripple/rippled
+    This file is part of xrpld: https://github.com/XRPLF/rippled
     Copyright (c) 2025 Ripple Labs Inc.
 
     Permission to use, copy, modify, and/or distribute this software for any
@@ -19,6 +19,7 @@
 
 #include <xrpl/beast/unit_test/suite.h>
 #include <xrpl/server/LoadFeeTrack.h>
+#include <cstdint>
 
 namespace xrpl {
 
