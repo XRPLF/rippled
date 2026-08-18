a8521df7b336b6a7fdb04e3bc28919f06b6f41c2
TimothyBanks <tbanks@ripple.com>
2026-08-18 18:07:33 -0400

    chore: Address code review comments

--- include/xrpl/protocol/AMMCore.h
+++ include/xrpl/protocol/AMMCore.h
@@ -74,7 +74,8 @@ invalidAMMAssetPair(
 std::optional<std::uint8_t>
 ammAuctionTimeSlot(std::uint64_t current, STObject const& auctionSlot);
 
-/** Convert to the fee from the basis points
+/** 
+ * Convert to the fee from the basis points
  * @param tfee  trading fee in {0, 1000}
  * 1 = 1/10bps or 0.001%, 1000 = 1%
  */
