4e1de439398a59b50bc67b9ed0262629ae69b9ee
Ed Hennis <ed@ripple.com>
2026-08-19 11:42:59 -0400

    clang-tidy: Make changes uglier by adding a bunch of redundant {}s

--- src/xrpld/app/misc/SHAMapStoreImp.cpp
+++ src/xrpld/app/misc/SHAMapStoreImp.cpp
@@ -165,14 +165,20 @@ SHAMapStoreImp::SHAMapStoreImp(
         getIfExists(section, Keys::kAdvisoryDelete, advisoryDelete_);
 
         if (getIfExists(section, Keys::kMaxWaitingLedgers, temp))
+        {
             maxWaitingLedgers_ = temp;
+        }
         else
+        {
             maxWaitingLedgers_ = deleteInterval_;
+        }
 
         auto const minWaiting = minInterval / 4;
         if (maxWaitingLedgers_ < minWaiting)
+        {
             Throw<std::runtime_error>(
                 "max_waiting_ledgers must be at least " + std::to_string(minWaiting));
+        }
 
         stateDb_.init(config, dbName_);
         dbPaths();
@@ -376,9 +382,13 @@ SHAMapStoreImp::run()
 
             clearPrior(lastRotated);
             if (auto const health = healthWait(); health == HealthResult::Stopping)
+            {
                 return;
+            }
             else if (health != HealthResult::KeepGoing)
+            {
                 continue;
+            }
 
             JLOG(journal_.debug()) << "copying ledger " << validatedSeq;
             std::uint64_t nodeCount = 0;
@@ -398,9 +408,13 @@ SHAMapStoreImp::run()
             }
 
             if (auto const health = healthWait(); health == HealthResult::Stopping)
+            {
                 return;
+            }
             else if (health != HealthResult::KeepGoing)
+            {
                 continue;
+            }
             // Only log if we completed without a "health" abort
             JLOG(journal_.debug())
                 << "copied ledger " << validatedSeq << " nodecount " << nodeCount;
@@ -425,9 +439,13 @@ SHAMapStoreImp::run()
             JLOG(journal_.debug()) << "freshening caches";
             freshenCaches();
             if (auto const health = healthWait(); health == HealthResult::Stopping)
+            {
                 return;
+            }
             else if (health != HealthResult::KeepGoing)
+            {
                 continue;
+            }
             // Only log if we completed without a "health" abort
             JLOG(journal_.debug()) << validatedSeq << " freshened caches";
 
@@ -437,9 +455,13 @@ SHAMapStoreImp::run()
 
             clearCaches(validatedSeq);
             if (auto const health = healthWait(); health == HealthResult::Stopping)
+            {
                 return;
+            }
             else if (health != HealthResult::KeepGoing)
+            {
                 continue;
+            }
 
             lastRotated = validatedSeq;
 
