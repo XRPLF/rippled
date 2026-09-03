7dba4636954af815c680429db5df7ccfa6e053f0
TimothyBanks <tbanks@ripple.com>
2026-09-03 14:41:23 -0400

    chore: merge develop into branch

--- src/libxrpl/protocol/AMMCore.cpp
+++ src/libxrpl/protocol/AMMCore.cpp
@@ -123,10 +123,4 @@ ammAuctionTimeSlot(std::uint64_t current, STObject const& auctionSlot)
     return std::nullopt;
 }
 
-bool
-ammEnabled(Rules const& /*rules*/)
-{
-    return true;
-}
-
 }  // namespace xrpl
